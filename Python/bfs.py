1be4671f5aef575ba6d1f5175a056e9d06bcba66
unknown <ashu.atiwa@gmail.com>
2020-06-26 22:59:59 +0530

    Breadth First Search

--- Python/bfs.py
+++ Python/bfs.py
@@ -2,17 +2,13 @@
 
 #a key-value pair represent a vertex and all it's outward edges.
 adjacent = {}
-
 #stores the list of vertex.
 Vertex = []
-
 # a key-value pair represents a vertex and its level.
 # level of the root node is zero.
 level = {}
-
 # a key-value pair represents a vertex and its parent.
 parent = {}
-
 
 def initalizeVertex():
     n = int(input('Enter the no. of vertices.\n'))
@@ -50,38 +46,29 @@ def showEdges():
 def bfs():
     frontier = []
     neighbour = []
-
-    start = input('Enter the first node.\n')
-    
+    start = input('Enter the first node.\n')    
     level[start] = 0
     parent[start] = None
     level_no = 1
-
     frontier.append(start)
 
     while len(frontier):
         neighbour = []
-
         for u in frontier:
             for v in adjacent[u]:
                 if v not in level.keys():
                     level[v] = level_no
                     parent[v] = u
                     neighbour.append(v)
-
         frontier = neighbour.copy()
         level_no +=1    
-
 
 # program starts here.
 
 initalizeVertex()
 initalizeUndirectedEdges()  
-
 #showVertex()
 #showEdges()
-
 print('Implementing BFS.')
 bfs()
-
 print(level)

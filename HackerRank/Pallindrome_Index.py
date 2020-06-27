8d016c729c55a456e58c42c3bf40e0e7cc42153f
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-06-27 17:52:54 +0530

    Palindrome Index Solution!

--- HackerRank/Pallindrome_Index.py
+++ HackerRank/Pallindrome_Index.py
@@ -1,36 +1,23 @@
-# Function to check pallindrome or not
-def ispallin(s, start, end):
-    
-    if s[start : end + 1] == s[end : start - 1 : -1]:
-        return True
+# This is the main code for the problem! Driver code should be same as present on HackerRank!
+
+def check_pallindrome(string, start, end):
+    if start == 0:
+        if string[start: end + 1] == string[end:: -1]:
+            return True
+
+    else:
+        if string[start: end + 1] == string[end: start - 1: -1]:
+            return True
 
     return False
 
-# Function for checking pallindrome index
-def palindromeIndex(s):
-    
-    l = [i for i in s]
-    #r = list(reversed(l))
-    
-    for i in range(len(l)):
-        if l[i] != l[len(l) - i - 1]:
-            ind = i if ispallin(l, i + 1, len(l) - 1 - i) else len(l) - 1 - i
-            return ind
-    
-    
-    return -1
-    
-    
-    
-if __name__ == '__main__':
-    
-    q = 3
 
-    for q_itr in range(q):
-        s = input()
-
-        result = palindromeIndex(s)
-
-        print(str(result) + '\n')
-
-
+def pallindrome_index(string):
+    if check_pallindrome(string=string, start=0, end=len(string) - 1):
+        return -1
+    else:
+        for i in range(len(string)):
+            if check_pallindrome(string=string, start=i + 1, end=len(string) - 1 - i):
+                return i
+            if check_pallindrome(string=string, start=i, end=len(string) - 2 - i):
+                return len(string) - 1 - i

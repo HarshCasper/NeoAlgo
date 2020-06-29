bc4864298f8ad43b1c48fe7cb7a6d1a7c9b96b23
Ricardo Prins <ricardo@iamprins.com>
2020-06-29 17:05:16 -0600

    added alternate implementation in stack for c++

--- C++/Stack_Implementation.cpp
+++ C++/Stack_Implementation.cpp
@@ -30,7 +30,67 @@ public:
     }
 };
 
+//This program implements Stack Data stucture using Linked List representation
+//by @GudlaArunKumar
 
+#include <iostream>
+using namespace std;
+class Node{       //Class Node to store Data and next pointer to next Node
+public:
+    int data;
+    Node *Next;
+};
+
+class StackLinkedList{
+private:
+    Node *top;   // Top pointer is declared here
+public:
+    StackLinkedList(){
+        top=NULL;
+    }
+    void push(int x)
+    {
+        Node *t=new Node;  //If heap is full, then Stack Overflow occurs
+        if(t==NULL)
+        {
+            cout <<"Stack overflow" << endl;
+        }
+        else{
+            t->data=x;     // Top pointer points to Last Inserted Node
+            t->Next=top;
+            top=t;
+        }
+    }
+
+    int pop()    // Returns the elements at the Top 
+    {
+        int x=-1;
+        if(top==NULL)
+        {
+            cout << "Stack is empty" << endl;
+        }
+        else
+        {
+            Node *p=top;
+            x=p->data;
+            top=top->Next;
+            delete p;
+        }
+    return x;
+    }
+
+    void display()
+    {
+        cout << "Elements in the stack: "<< endl;
+        Node *q=top;
+        while(q!=NULL)
+        {
+            cout << q->data << " ";
+            q=q->Next;
+        }
+        cout << endl;
+    }
+};
 
 int main() {
 

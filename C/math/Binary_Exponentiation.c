419a5f9c2804170fe686c702344bc85691967f7a
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-07-06 11:02:31 +0530

    added iterative function for Binary Exponentiation in C

--- C/math/Binary_Exponentiation.c
+++ C/math/Binary_Exponentiation.c
@@ -1,16 +1,33 @@
-// C Program to find Binary Exponent Recursively.
+// C Program to find Binary Exponent Iteratively and Recursively.
 
 #include <stdio.h>
 
+// Iterative function to calculate exponent.
+
+int binExpo_iterate(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b % 2)
+        {
+            res = res * a;
+        }
+        a = a * a;
+        b /= 2;
+    }
+    return res;
+}
+
 // Recursive function to calculate exponent.
 
-int binExpo(int a, int b)
+int binExpo_recurse(int a, int b)
 {
     if (b == 0)
     {
         return 1;
     }
-    int res = binExpo(a, b / 2);
+    int res = binExpo_recurse(a, b / 2);
     if (b % 2)
     {
         return res * res * a;
@@ -39,8 +56,10 @@ int main()
     }
     else
     {
-        int resRecurse = binExpo(a, b);
+        int resIterate = binExpo_iterate(a, b);
+        int resRecurse = binExpo_recurse(a, b);
 
+        printf("%d", resIterate);
         printf("%d", resRecurse);
     }
 }

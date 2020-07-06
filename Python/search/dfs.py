e45e2ce7645581b01e9800869a298a47f8e1df0d
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-07-06 11:05:34 +0530

    Improved DFS

--- Python/search/dfs.py
+++ Python/search/dfs.py
@@ -1,70 +1,58 @@
-#depth first search.
+# Python3 Program to print BFS traversal 
+# From a given source vertex.  
+from collections import defaultdict 
+# This class represents a directed graph 
+# using adjacency list representation 
+class Graph: 
+    # Constructor 
+    def __init__(self): 
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
+    def addEdge(self,u,v): 
+        '''function to add an edge to graph '''
+        self.graph[u].append(v) 
+    def BFS(self, s): 
+        '''Function to print a BFS of graph '''
+        # Mark all the vertices as not visited 
+        visited = [False] * (len(self.graph)) 
+        # Create a queue for BFS 
+        queue = []  
+        # Mark the source node as visited and enqueue it .
+        queue.append(s) 
+        visited[s] = True
+        while queue: 
+            # Dequeue a vertex from queue and print it 
+            s = queue.pop(0) 
+            print (s, end = " ") 
+            # Get all adjacent vertices of the dequeued vertex s. 
+            # In case an adjacent has not been visited, then mark it visited and enqueue it 
+            for i in self.graph[s]: 
+                if visited[i] == False: 
+                    queue.append(i) 
+                    visited[i] = True
+  
+# Driver code 
+if __name__ == "__main__":
+    g = Graph() 
+                        # (OR)
+    '''
+    edges=int(input("Please enter the number of edges you need (min 2) :\t "))
+    j=1
+    while edges >0:
+        print("Edge"+str(j)+":")
+        x=int(input("Enter x-axis value : \t"))
+        y=int(input("Enter y-axis value : \t"))
+        g.addEdge(x,y)
+        j+=1
+        edges-=1
+        '''
+    g.addEdge(0, 1) 
+    g.addEdge(0, 2) 
+    g.addEdge(1, 2) 
+    g.addEdge(2, 0) 
+    g.addEdge(2, 3) 
+    g.addEdge(3, 3)              
 
-#a key-value pair represent a vertex and all its outward edges.
-adjacent = {}
-#stores the list of vertex.
-Vertex = []
-#stores the dfs order.
-stack = []
-# a key-value pair represents a vertex and its parent.
-parent = {}
-
-def initalizeVertex():
-    n = int(input('Enter the no. of vertices.\n'))
-    print("Enter the vertexes.")
-    for i in range(n):
-        vertex = input()
-        Vertex.append(vertex)
-        adjacent[vertex] = []
-
-def initalizeUndirectedEdges():
-    n = int(input("Enter the no. of edges.\n"))
-    print("Enter the space seperated edges.")
-    for i in range(n):
-        a,b = input().split()
-        adjacent[a].append(b)
-        adjacent[b].append(a)
-
-def initalizeDirectedEdges():
-    n = int(input("Enter the no. of edges.\n"))
-    print("Enter the space seperated edges.")
-    for i in range(n):
-        a,b = input().split()
-        adjacent[a].append(b)    
-
-def showVertex():
-    print('The vertexes are: ')
-    print(vertex)
-    print('\n')
-
-def showEdges():
-    print('The dictionary of edges are: ')
-    print(adjacent)
-    print('\n')
-
-def dfs(s):
-    for neighbour in adjacent[s]:
-        if neighbour not in parent.keys():
-            parent[neighbour] = s
-            stack.append(neighbour)
-            dfs(neighbour)
-            
-
-# program starts here
-
-initalizeVertex()
-#we can call initalizeDirectedEdges if it is directed graph.
-initalizeUndirectedEdges()  
-#showVertex()
-#showEdges()
-print('Implementing dFS.')
-#for a well connected grapgh, we don't need the below for loop.
-#dfs(starting_vertex) will give correct result.
-#the below for loop is used for unconnected graphs.
-for v in Vertex:
-    if v not in parent.keys():
-        parent[v] = None
-        stack.append(v)
-        dfs(v)
-print('DFS order is: ')
-print(stack)
+    print ("Following is Breadth First Traversal"
+                    " (starting from vertex 2)") 
+    g.BFS(2) 

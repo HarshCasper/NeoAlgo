b006ff74177c99b80c1b09d2a73f6eafbc6f9506
Adityanagraj <42292430+Adityanagraj@users.noreply.github.com>
2020-07-17 21:22:34 +0530

    string revearsal

---
+++ Reverse string using stack.py
@@ -0,0 +1,37 @@
+#class of stack 
+class stack:   
+    def __init__(self):
+        self.items=[]
+    def push(self,item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def is_empty(self):
+        return self.items==[]
+            
+       
+#function to reverse a function
+def reverse(string):
+    
+   """ 
+    >>Input:ziuQ
+    >>Output:Quiz
+    
+  """
+    x=len(string)
+    l=0
+    while l<x:
+        s.push(string[l])
+        l+=1
+    rev=''
+    while not s.is_empty():
+        rev+=s.pop()
+    return rev
+s=stack()
+        
+#  Driver Code Starts
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        str1=input()
+        print(reverse(str1))

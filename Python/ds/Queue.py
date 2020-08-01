ac5475582d1f573cc1c639d00a0871e688044f58
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-08-01 14:29:27 +0530

    Uploaded Queue.py

---
+++ Python/ds/Queue.py
@@ -0,0 +1,32 @@
+# Stack implementation using List in Python
+
+class Queue :
+  def __init__(self):
+    self.items =[]
+    
+  def print_queue(self):
+    print "\nQueue: ", self.items
+    print "Length of Queue: ", len(self.items), "\n"
+      
+  def enqueue(self, item):
+    self.items.insert(0,item)
+    print "Enqueued value: ", item
+  
+  def dequeue(self):
+    print "Dequeued value: ",self.items.pop()
+  
+
+if __name__ == '__main__' :
+  q = Queue()
+  q.print_queue()
+  q.enqueue(10)
+  q.enqueue(20)
+  q.enqueue(30)
+  q.enqueue(40)
+  q.enqueue(50)
+  q.print_queue()
+  q.dequeue()
+  q.dequeue()
+  q.dequeue()
+  q.print_queue()
+

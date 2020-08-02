23361994852a324cb44eb248515cc4f7390b5922
YashThakare <38381061+YashThakare@users.noreply.github.com>
2020-08-02 19:32:55 +0530

    Create Ackermann_fucntion.cpp
    Implemented Ackermann Function in c++

---
+++ C++/math/Ackermann_function.cpp
@@ -0,0 +1,28 @@
+// Ackermann Function implemented in C++
+
+#include <iostream> 
+using namespace std; 
+  
+int ackermann_function(int m, int n) 
+{ 
+    if (m == 0){ 
+        return n + 1; 
+    } 
+    else if((m > 0) && (n == 0)){ 
+        return ackermann_function(m - 1, 1); 
+    } 
+    else if((m > 0) && (n > 0)){ 
+        return ackermann_function(m - 1, ackermann_function(m, n - 1)); 
+    } 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    int m,n;
+    cout<<"Enter two non negative integers:"<<endl;
+    cin>>m>>n;
+    int result=ackermann_function(m,n);
+    cout << result << endl; 
+    return 0; 
+} 

9e77f5710285baf200f3da5895ffbba436d61ac9
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-08-05 13:12:08 +0530

    Update QueueLinkedList.cpp

--- C++/ds/QueueLinkedList.cpp
+++ C++/ds/QueueLinkedList.cpp
@@ -3,91 +3,89 @@
 #include<iostream>
 using namespace std;
 
-class Node{
-    int data;
-    Node *next;
-    public:
-    friend class queue;
+class Node {
+  int data;
+  Node *next;
+public:
+  friend class queue;
 };
 
 
-class queue{
-    Node *front , *rear;
-    public:
-    queue(){
-        front=rear=NULL;
+class queue {
+  Node *front , *rear;
+public:
+  queue() {
+    front = rear = NULL;
 
+  }
+  void enQueue(int x) {
+
+    if (rear == NULL) {
+      Node *temp = new Node();
+      temp->data = x;
+      temp->next = NULL;
+      rear = temp;
+      front = rear;
     }
-    void enQueue(int x){
-        
-        if(rear==NULL){
-            Node *temp = new Node();
-            temp->data = x;
-            temp->next=NULL;
-            rear=temp;
-            front=rear;
-        }
-        else{
-        Node *temp = new Node();
-        temp->data = x;
-        temp->next = NULL;
-        rear->next = temp;
-        rear = temp;
-        }
-        
+    else {
+      Node *temp = new Node();
+      temp->data = x;
+      temp->next = NULL;
+      rear->next = temp;
+      rear = temp;
     }
-    void deQueue(){
-        // Node *temp = front;
-        if(front==NULL)
-        {
-            return;
-        }
-        Node * temp = front;
-        front = front->next;
-        
-        if(front==NULL){
-            rear = NULL;
-        }
-        cout<<"Deleted Elemeted is :"<<temp->data<<endl;
-        free(temp);
-   
+
+  }
+  void deQueue() {
+    // Node *temp = front;
+    if (front == NULL)
+    {
+      return;
     }
-    void displayQueue(){
-        Node *temp = front;
-        if((front==NULL) || (rear==NULL)){
-            cout<<"Queue is empty!!"<<endl;
-            return;
-        }
-        cout<<"Queue Elements are :"<<endl;
-        while(temp!=NULL){
-            cout<<temp->data<<" ";
-            temp= temp->next;
-        }
-        cout<<endl;
+    Node * temp = front;
+    front = front->next;
+
+    if (front == NULL) {
+      rear = NULL;
     }
+    cout << "Deleted Elemeted is :" << temp->data << endl;
+    free(temp);
+
+  }
+  void displayQueue() {
+    Node *temp = front;
+    if ((front == NULL) || (rear == NULL)) {
+      cout << "Queue is empty!!" << endl;
+      return;
+    }
+    cout << "Queue Elements are :" << endl;
+    while (temp != NULL) {
+      cout << temp->data << " ";
+      temp = temp->next;
+    }
+    cout << endl;
+  }
 
 };
 
-int main(){
-    queue q;
-    int ch;
-    do{
-        cout<<"Enter the option below :\n1.Enqueue an Element \n2.Dequeue an Element \n3.Display all elements of a queue\n4.Exit \n";
-        cin>>ch;
-        switch(ch){
-            case 1:
-            int k;
-            cin>>k;
-            q.enQueue(k);
-            break;
-            case 2:
-            q.deQueue();
-            break;
-            case 3:
-            q.displayQueue();
-            break;
-        }
-    }while(ch!=4);
+int main() {
+  queue q;
+  int ch;
+  do {
+    cout << "Enter the option below :\n1.Enqueue an Element \n2.Dequeue an Element \n3.Display all elements of a queue\n4.Exit \n";
+    cin >> ch;
+    switch (ch) {
+    case 1:
+      int k;
+      cin >> k;
+      q.enQueue(k);
+      break;
+    case 2:
+      q.deQueue();
+      break;
+    case 3:
+      q.displayQueue();
+      break;
+    }
+  } while (ch != 4);
 }
-
-

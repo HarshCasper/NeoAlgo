ab8579ddbb665e37d6b651bcc00fc0c4d2f5c20c
HARSH AGARWAL <harshu07022002@gmail.com>
2020-08-10 22:19:04 +0530

    Create kadanes_Java.java

---
+++ Java/Algorithms/kadanes_Java.java
@@ -0,0 +1,29 @@
+package DsAlgo;
+import java.util.*;
+public class kadanes_Java {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		int maxSoFar=Integer.MIN_VALUE;
+		int curSum=0;
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<arr.length;i++) {
+			if(curSum+arr[i]<0) {
+				curSum=0;
+			}else {
+			curSum=curSum+arr[i];
+			}
+			if(curSum>maxSoFar) {
+				maxSoFar=curSum;
+			}
+		}
+		System.out.println(maxSoFar);
+		
+		sc.close();
+	}
+
+}

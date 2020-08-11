5a50ccdd7f335f24dece4006a7df467382b534bf
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-08-11 21:49:20 +0530

    Update kadanes_Java.java

--- Java/Algorithms/kadanes_Java.java
+++ Java/Algorithms/kadanes_Java.java
@@ -1,29 +1,26 @@
-package DsAlgo;
+// Java Program for Kadane's Algorithm.
+
 import java.util.*;
-public class kadane {
+public class Kadane {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] arr=new int[n];
-		int maxSoFar=Integer.MIN_VALUE;
-		int curSum=0;
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=sc.nextInt();
-		}
-		for(int i=0;i<arr.length;i++) {
-			if(curSum+arr[i]<0)
-				curSum=0;
-			else 
-			curSum=curSum+arr[i];
-			
-			if(curSum>maxSoFar) 
-				maxSoFar=curSum;
-			
-		}
-		System.out.println(maxSoFar);
-		
-		sc.close();
-	}
-
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      int maxSoFar = Integer.MIN_VALUE;
+      int curSum = 0;
+      for (int i = 0; i < arr.length; i++) {
+         arr[i] = sc.nextInt();
+      }
+      for (int i = 0; i < arr.length; i++) {
+         if (curSum + arr[i] < 0)
+            curSum = 0;
+         else
+            curSum = curSum + arr[i];
+         if (curSum > maxSoFar)
+            maxSoFar = curSum;
+      }
+      System.out.println(maxSoFar);
+      sc.close();
+   }
 }

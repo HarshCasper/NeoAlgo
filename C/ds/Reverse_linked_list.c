f4025699f43246cac5ee7b9838e04d2833016920
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-08-13 19:58:01 +0530

    Update Reverse_linked_list.c

--- C/ds/Reverse_linked_list.c
+++ C/ds/Reverse_linked_list.c
@@ -1,4 +1,5 @@
 //Implementation of Reversing a Linked List in C using Iterative and Recursive Method
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +9,7 @@ struct Node
 	int data;
 	struct Node* next;
 };
+
 //function to print given linked list
 void printList(struct Node* head)
 {
@@ -40,58 +42,63 @@ void reverseIterative(struct Node** head)
 	while (current != NULL)
 	{
 		struct Node* next = current->next;
-    current->next = prev;  
-    // advance the two pointers
+		current->next = prev;
+		// advance the two pointers
 		prev = current;
 		current = next;
 	}
 	*head = prev;
 }
+
 // Recursive function to reverse the given linked list Recursively
 void reverseRecursive(struct Node* head, struct Node** headRef)
 {
 	struct Node* first;
 	struct Node* rest;
-  
+
 	if (head == NULL)
-	   return;
-  first = head;		  
-	rest = first->next;	
+		return;
+	first = head;
+	rest = first->next;
 	if (rest == NULL)
 	{
 		*headRef = first;
-		 return;
+		return;
 	}
 	reverseRecursive(rest, headRef);
 	rest->next = first;
 	first->next = NULL;
 }
+
 int main()
 {
 	int n;
-  cout<<"Enter the number of elements:"<<endl;
-  cin>>n;
-  int keys[n];
-  for(int i=0;i<n;i++)
-  {
-    cin>>keys[i];
-  }
-  struct Node *head = NULL;
-	for (int i = n - 1; i >=0; i--)
-  {
+	cout << "Enter the number of elements:" << endl;
+	cin >> n;
+	int keys[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> keys[i];
+	}
+	struct Node *head = NULL;
+	for (int i = n - 1; i >= 0; i--)
+	{
 		push(&head, keys[i]);
-  }
-  reverseIterative(&head);
+	}
+	reverseIterative(&head);
 	reverseRecursive(&head);
-  printList(head);
-  return 0;
+	printList(head);
+	return 0;
 }
-/*Example:-
+
+/*
+Example:-
 Input-
 Enter the number of elements:
 5
 1 2 3 4 5
 
 Output-
-5->4->3->2->-1->NULL*/
+5->4->3->2->-1->NULL
+*/
 

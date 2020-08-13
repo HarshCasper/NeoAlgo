9d70e4250abe5b566f1e531687fd7eb5f752a12a
salonilakhotia <salonilakhotia242@gmail.com>
2020-08-13 17:44:06 +0530

    ShellSort in java

--- Java/sort/ShellSort.java
+++ Java/sort/ShellSort.java
@@ -28,36 +28,3 @@ public class ShellSort{
         }
     }
 }
-
-// int sort(int arr[]) 
-//     { 
-//         int n = arr.length; 
-  
-//         // Start with a big gap, then reduce the gap 
-//         for (int gap = n / 2; gap > 0; gap /= 2) { 
-//             // Do a gapped insertion sort for this gap size. 
-//             // The first gap elements a[0..gap-1] are already 
-//             // in gapped order keep adding one more element 
-//             // until the entire array is gap sorted 
-//             for (int i = gap; i < n; i += 1) { 
-//                 // add a[i] to the elements that have been gap 
-//                 // sorted save a[i] in temp and make a hole at 
-//                 // position i 
-//                 int temp = arr[i]; 
-  
-//                 // shift earlier gap-sorted elements up until 
-//                 // the correct location for a[i] is found 
-//                 int j; 
-//                 for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
-//                     arr[j] = arr[j - gap]; 
-  
-//                 // put temp (the original a[i]) in its correct 
-//                 // location 
-//                 arr[j] = temp; 
-//             } 
-//         } 
-//         return 0; 
-//     } 
-  
-//     // Driver method 
-//     public stati

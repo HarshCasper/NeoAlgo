f2d2b6bcefe12772b80f51ea31084643beac5470
Satyaban Sahoo <38886899+hemraj4545@users.noreply.github.com>
2020-08-14 21:32:39 +0530

    Create staircase_climbs.py

---
+++ Python/other/staircase_climbs.py
@@ -0,0 +1,34 @@
+# You are given a positive integer N which represents the number of steps in a staircase. 
+# You can either climb 1 or 2 steps at a time. Write a function that returns the number of unique ways to climb the stairs.
+'''
+Input: n = 4
+Output: 5
+(1, 1, 1, 1), (1, 1, 2), (2, 1, 1), (1, 2, 1), (2, 2)
+
+Input: n = 5
+Output: 8
+(1, 1, 1, 1, 1), (2, 1, 1, 1), (1, 2, 1, 1), (1, 1, 2, 1), (1, 1, 1, 2), (2, 2, 1), (2, 1, 2), (1, 2, 2)
+'''
+# Time Complexity - O(n^2)
+# Space Complexion - O(1)
+def staircase(n: int) -> int:
+  if n <= 3:
+    return n
+  else:
+    return staircase(n-1)+staircase(n-2)
+
+
+# Time Complexity -  O(n) 
+# Space Complexion -  O(n)
+def staircase(n: int) -> int:
+  res = [0]*(n+1)
+  res[0] = 1
+  res[1] = 1
+  for i in range(2,n+1):
+    res[i] = res[i-1]+res[i-2]
+  return res[n]
+
+print(staircase(4))
+# output - 5
+print(staircase(5))
+# output - 8

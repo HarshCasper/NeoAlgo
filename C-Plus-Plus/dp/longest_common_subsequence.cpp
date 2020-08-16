346f261b17f60a4451b920943bcdcb1e969c1d6b
Joshitha18 <joshitha1830@gmail.com>
2020-08-16 15:51:05 +0530

    Added longest_common_subsequence.cpp in DP

---
+++ C-Plus-Plus/dp/longest_common_subsequence.cpp
@@ -0,0 +1,52 @@
+//Given two sequences, find the length of longest subsequence present in both of them.
+//A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(char *x,char *y)
+{
+    int m=strlen(x);
+    int n=strlen(y);
+
+    int dp[100][100];
+
+    //base case
+    //making first row and column 0
+    for(int i=0;i<=m;i++)
+        dp[i][0]=0;
+    for(int j=0;j<=n;j++)
+        dp[0][j]=0;
+
+
+    for(int i=1;i<=m;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            int q=0;
+
+            if(x[i-1]==y[j-1]) //if matches
+                q=1+dp[i-1][j-1];
+            else     //if doesn't match
+                q=max(dp[i-1][j],dp[i][j-1]);
+
+        dp[i][j]=q;
+        }
+    }
+
+    return dp[m][n];   //return last element
+}
+
+int main()
+{
+    char str1[100],str2[100];
+    cin>>str1>>str2;
+
+    cout<<lcs(str1,str2);
+}
+
+// input=
+// ABCDEFGH
+// BDHCEG
+// output=
+// 4

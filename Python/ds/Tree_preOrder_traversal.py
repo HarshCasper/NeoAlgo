039cd5dea9a1d141eea00a8d0f687fc15f888010
pradeep98 <33161791+pradeep98@users.noreply.github.com>
2020-08-17 14:16:30 +0530

    Update Tree_preOrder_traversal.py
    user input added, comments modified

--- Python/ds/Tree_preOrder_traversal.py
+++ Python/ds/Tree_preOrder_traversal.py
@@ -5,8 +5,32 @@ class node:
         self.right = None
 
 class Tree:
-    def __init__(self, root):
-        self.root = node(root)
+    def __init__(self):
+        self.root = None
+    
+    def createTree(self, treeNodesArray):
+        queue = []
+        self.root = node(int(treeNodesArray.pop(0)))
+        queue.append(self.root)
+        size = len(queue)
+        while treeNodesArray:
+            while size:
+                root = queue.pop(0)
+                if root:
+                    val = treeNodesArray.pop(0)
+                    if val == 'null':
+                        root.left = None
+                    else:
+                        root.left = node(int(val))
+                        queue.append(root.left)
+                    val = treeNodesArray.pop(0)
+                    if val == 'null':
+                        root.right = None
+                    else:
+                        root.right = node(int(val))
+                        queue.append(root.right)
+                size -= 1
+            size = len(queue)
     
     def recursive_preorder_traversal(self, root, path):
         if root:
@@ -28,27 +52,27 @@ class Tree:
                                              
         return self.path
  	
-preOrdertree = Tree(2)
-
-preOrdertree.root.left  = node(4)
-preOrdertree.root.right = node(8)
-
-preOrdertree.root.left.left   = node(16)
-preOrdertree.root.left.right  = node(32)
-preOrdertree.root.right.left  = node(64)
-preOrdertree.root.right.right = node(128)
+treeNodesArray = list(input().split(','))
+preOrdertree = Tree()
+preOrdertree.createTree(treeNodesArray)
 
 #### Pre-Order Traversal :- root.val -> root.left -> root.right
 
-print(preOrdertree.recursive_preorder_traversal(preOrdertree.root, []))  # Expected result: [2,4,16,32,8,64,128]
-print(preOrdertree.iterative_preorder_traversal(preOrdertree.root))  # Expected result: [2,4,16,32,8,64,128]
+print(preOrdertree.recursive_preorder_traversal(preOrdertree.root, []))  
+print(preOrdertree.iterative_preorder_traversal(preOrdertree.root))  
 
 '''
-      2
-   /     \
-  4       8
- / \     / \
-16  32  64  128
+    Input should be of this type->    1,2,3,null,null,4,5,null,null,6,7      
+    Children of leaf node are null
+        
+        1
+       / \
+      2   3
+         / \
+        4   5
+           / \
+          6   7
+     Expected Output: 1,2,3,4,5,6,7
      
 Stack is used to implement the iterative approach. Push nodes into the stack. We leave the inner loop when left leaf is reached. 
 Latest node is popped out of the stack and we move to its right child and repeat the above steps until we reach the right-most child. 

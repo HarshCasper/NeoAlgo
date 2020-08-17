6557247f99ccd1f95afecb0acaa5f0463822fb98
Surajv <vsuraj2405@gmail.com>
2020-08-17 15:53:43 +0530

    refractored code

--- C/other/Number_of_duplicates_using_Binary_search.c
+++ C/other/Number_of_duplicates_using_Binary_search.c
@@ -3,82 +3,85 @@
 #include <stdio.h>
 int Binary_search_algo_for__finding_occurrence(int array[], int sizeOf_array, int number, int found)
 {
-int start = 0, end = sizeOf_array - 1;
+    int start = 0, end = sizeOf_array - 1;
 
 // initializing value
-int value = -1;
+    int value = -1;
 
-while (start <= end)
-{
-int middle = (start + end)/2;
+    while (start <= end)
+    {
+        int middle = (start + end) / 2;
 
-/*** CASE : number equals middle element ***/
-if (number == array[middle]){  // if both are equal then we update the 'value'
+        /*** CASE : number equals middle element ***/
+        if (number == array[middle]) // if both are equal then we update the 'value'
+        {
+            value = middle;
+            if (found) { // searching in left side
+                end = middle - 1;
+            }
+            else { // searching in right side
+                start = middle + 1;
+            }
+        }
 
-value = middle;
-if (found){ // searching in left side
-end = middle - 1;
-}
-else{ // searching in right side
-start = middle + 1;
+        /*** CASE : number less than middle element ***/
+        else if (number < array[middle])
+        {
+            end = middle - 1;
+        }
+
+        /*** CASE : number greater than middle element (last case) ***/
+        else
+        {
+            start = middle + 1;
+        }
+    }
+    return value;
 }
 
-}
-
-/*** CASE : number less than middle element ***/
-else if (number < array[middle]){
-end = middle - 1;
-}
-
-/*** CASE : number greater than middle element (last case) ***/
-else{
-start = middle + 1;
-}
-}
-return value;
-}
-int quickSort(int array[] , int lower_bound ,int  upper_bound)
+int quickSort(int array[] , int lower_bound , int  upper_bound)
 {
     int temp ;
-     if(lower_bound<upper_bound)
-     {
-         temp = partition(array , lower_bound , upper_bound );// as partition function would return end index which would be used further
-        quickSort(array , lower_bound , temp-1);
-         quickSort(array ,temp+1, upper_bound);
 
-     }
+    if (lower_bound < upper_bound)
+    {
+        temp = partition(array , lower_bound , upper_bound );// as partition function would return end index which would be used further
+        quickSort(array , lower_bound , temp - 1);
+        quickSort(array , temp + 1, upper_bound);
+    }
 }
 
-int partition(int array[],int lower_bound,int upper_bound)
+int partition(int array[], int lower_bound, int upper_bound)
 {
-    int pivot , start , end ,t;
+    int pivot , start , end , t;
     pivot = array[lower_bound];
     start = lower_bound ;
     end = upper_bound ;
-    while(start<end)
+
+    while (start < end)
     {
-        while(array[start]<=pivot)
+        while (array[start] <= pivot)
         {
             start++;
         }
-    while(array[end]>pivot)
-    {
-        end--;
-    }
-
-    if(start<end)
-    {//swap
-        t = array[start];
-        array[start]= array[end];
-        array[end]=t;
-    }
+        while (array[end] > pivot)
+        {
+            end--;
+        }
+        if (start < end)
+        {   //swap
+            t = array[start];
+            array[start] = array[end];
+            array[end] = t;
+        }
     }
     //if the condition is not followed then we replace the 'end' with 'pivot' element and return end
     //swap
-   t =  array[lower_bound];
-   array[lower_bound] = array[end];
-   array[end] = t ;
-   return end ;
+    t =  array[lower_bound];
+    array[lower_bound] = array[end];
+    array[end] = t ;
+
+    return end ;
 }
 
 int main()
@@ -86,24 +89,24 @@ int main()
 //declaring the size of array
     int sizeOf_array;
     printf("The size of array is:\n");
-    scanf("%d",&sizeOf_array);
+    scanf("%d", &sizeOf_array);
 
 // defining the array
     int array[sizeOf_array];
     printf("Please input the elements in array:\n");
 
 // storing elements in array
-    for(int i = 0 ; i<sizeOf_array;i++){
-    scanf("%d",&array[i]);
+    for (int i = 0 ; i < sizeOf_array; i++) {
+        scanf("%d", &array[i]);
     }
     printf("\n");
 
     int number; // we would input the number whose occurrence must be returned.
     printf("The number whose occurrence must be found : ");
-    scanf("%d",&number);
+    scanf("%d", &number);
 
 // we would sort the array using quickSort
-    int lower_bound=0 , upper_bound =sizeOf_array-1;
+    int lower_bound = 0 , upper_bound = sizeOf_array - 1;
 
     quickSort(array , lower_bound , upper_bound);
 
@@ -118,12 +121,12 @@ int main()
 // now we would subtract the first & last occurrence to get the total occurrences of the number
     int total_occurrences_of_number = last_occurrence_of_number - first_occurrence_of_number + 1;
 
-	if (first_occurrence_of_number != -1){
-		printf("The number: %d occurs %d times in the given array.", number, total_occurrences_of_number);
-	}
-	else{
-		printf("Error! The number is not present in the array.");
-	}
+    if (first_occurrence_of_number != -1) {
+        printf("The number: %d occurs %d times in the given array.", number, total_occurrences_of_number);
+    }
+    else {
+        printf("Error! The number is not present in the array.");
+    }
 
-	return 0;
+    return 0;
 }

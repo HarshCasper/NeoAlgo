b38f8a33d2cd888e01675ce754f70c24c79a5b60
salonilakhotia <salonilakhotia242@gmail.com>
2020-08-17 12:14:45 +0530

    comments in Shell Sort

--- Java/sort/ShellSort.java
+++ Java/sort/ShellSort.java
@@ -2,21 +2,36 @@ import java.util.*;
 
 public class ShellSort{
     public static void main(String[] args){
-        int array[] = {30, 25, 77, 16 , 80, 32, 67} ;
-        shellSort(array);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array : ");
+        int n = scan.nextInt();// size of array
+        int[] array = new int[n];
+        System.out.println("Enter the elements in the array : ");
+        int n = scan.nextInt();// size of array
+        for(int i=0;i<n;i++){
+            array[i] = scan.nextInt(); //array elements
+        }
+        scan.close();
+
+        shellSort(array, n); // function call for sorting array
+
+       
     }
 
-    public static void shellSort(int array[]){
-        int size = array.length;
+    public static void shellSort(int array[], int size){
+        //In shell sort , we use gap to sort the array. Starting the gap from larger value and reduce it till it is 1.
         int gap = size/2;
         while(gap >= 1){
+            // All the elements from array[0....gap-1] is already sorted. We add the elements in sorted form till the array is fully sorted.
             for(int i=gap;i<size;i++){
+                // Store the value of array[i] to temp;
                 int temp = array[i];
                 int j;
-                for(j = i;j>=gap && array[j-gap] > temp; j=j-gap){
+                // Finding the coorrect place to store the value of temp
+                for(j = i; j>=gap && array[j-gap] > temp; j=j-gap){
                     array[j] =array[j-gap];
                 }
-                array[j] = temp;
+                array[j] = temp;// coorect place of placing temp
             }
             gap = gap/2;
         }
@@ -24,7 +39,7 @@ public class ShellSort{
         //printing sorted array
         System.out.print(" ShellSort : ");
         for(int i=0;i<size;i++){
-            System.out.print(array[i] + " ");
+            System.out.print(array[i] + " "); // Printing the sorted array
         }
     }
 }

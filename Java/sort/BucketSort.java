abe5b4112eabc2d35d25a08dc1ec9b69f964666f
ritvij14 <ritvij2001@gmail.com>
2020-08-17 22:09:15 +0530

    Code improvements

--- Java/sort/BucketSort.java
+++ Java/sort/BucketSort.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class BucketSort {
 
-    private static void bucketSort(int[] arr, int k) {
+    private static void bucketSort(float[] arr, int k) {
         if (arr.length < 2)
             return;
         
         // getting upper limit for dividing numbers in buckets
         int length = arr.length;
-        int max_val = arr[0];
+        float max_val = arr[0];
         for (int i = 1; i<length; i++) {
             max_val = Math.max(max_val, arr[i]);
         }
         max_val += 1;
 
         //making bucket list and adding buckets in it
-        ArrayList<ArrayList<Integer>> bucketList = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Float>> bucketList = new ArrayList<ArrayList<Float>>();
         for (int i=0; i<k; i++) {
-            bucketList.add(new ArrayList<Integer>());
+            bucketList.add(new ArrayList<Float>());
         }
 
         //dividing numbers in different buckets
         for (int i=0; i<length; i++) {
-            int bucketIndex = (arr[i] * k)/max_val;
+            int bucketIndex = (int) ((arr[i] * k) / max_val);
             bucketList.get(bucketIndex).add(arr[i]);
         }
 
@@ -42,19 +42,33 @@ class BucketSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {49, 72, 26, 11, 99, 61, 34, 93, 89, 81};
+        Scanner sc = new Scanner(System.in);
 
+        //taking input array
+        System.out.println("Enter size of array:");
+        int size = sc.nextInt();
+        float arr[] = new float[size];
+        System.out.println("Enter array elements:");
+        for (int i=0; i<size; i++) {
+            arr[i] = sc.nextFloat();
+        }
+        System.out.println("Enter number of buckets:");
+        int bucketNum = sc.nextInt();
+
+        // before sorting
         System.out.println("Array before bucket sort:");
         for (int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
 
-        bucketSort(arr, 5);
+        bucketSort(arr, bucketNum);
 
+        // after sorting
         System.out.println("Array after Bucket sort:");
         for (int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        sc.close();
     }
 }

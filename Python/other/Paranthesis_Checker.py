79fa477c2f407e1b51ba482e116a2b3b6f2ed7c3
Shishir Choudhary <shishirchoudhary198@gmail.com>
2020-08-18 18:43:07 +0530

    Modified Paranthesis Checker function as per change Request

---
+++ Python/other/Paranthesis_Checker.py
@@ -0,0 +1,33 @@
+def checkParanthesis(input_string):
+    ''' 
+        argument : string
+
+        Function checks whether the Brackets Combinations Provided As Input 
+        is Balanced or Not.
+        
+        usage : checkParanthesis(string)
+        
+    '''
+
+
+
+    # hashmap maintaing the open and closed brackets
+    occurence = {"(":0 , ")":0, "{":0 , "}":0 , "[":0 , "]" : 0 , "<":0 , ">":0}
+    # occurence supports common,curly,square and angle bracket
+
+    for bracket in input_string:
+        occurence[bracket]+=1
+
+    if occurence["("] == occurence[")"] and occurence["{"] == occurence["}"]:
+        if occurence["["] == occurence["]"] and occurence["<"] == occurence[">"]:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    inp = input("Enter the Brackets Combinations\n")
+    checkParanthesis(inp)
+    

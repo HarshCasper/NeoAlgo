9f324eef8c3818300ac12b73a065f087b81df07f
Sayantan Paul <belikesayantan12@gmail.com>
2020-08-18 21:39:36 +0530

    Selection Sort - Go

---
+++ Go/sort/selectionSort.go
@@ -0,0 +1,63 @@
+// Author: @belikesayantan
+
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var strArray []string
+	var intArray []int
+
+	fmt.Println("Enter an array with spaces")
+	reader := bufio.NewReader(os.Stdin)
+	arr, err := reader.ReadString('\n')
+	if err != nil {
+		inputErr := errors.New("something is wrong with your array input")
+		log.Fatal(inputErr)
+	}
+	arr = strings.TrimSpace(arr)
+	strArray = strings.Split(arr, " ")
+
+	for _, val := range strArray {
+		ele, err := strconv.Atoi(val)
+		if err != nil {
+			panic(err)
+		}
+		intArray = append(intArray, ele)
+	}
+
+	fmt.Println("Sorted Array is: ", SelectionSort(intArray))
+
+}
+
+// SelectionSort Algorithm Time Complexity : O(n^2)
+func SelectionSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		if min != i {
+			swap(arr, i, min)
+		}
+
+	}
+
+	return arr
+}
+
+func swap(arr []int, i, j int) {
+	temp := arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+}

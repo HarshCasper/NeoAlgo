612bea4b55f5abd542ce131e61ee2edaf8ee1bdf
kushagraiitbbs <66637263+kushagraiitbbs@users.noreply.github.com>
2020-08-20 01:27:44 +0530

    Restoring Euler's_Totient_Function.cpp
    I am restoring my code with proper indentation and clear definition of Euler's Totient Function at the beginning of code plus sample input and sample output.

--- C-Plus-Plus/math/Euler's_Totient_function.cpp
+++ C-Plus-Plus/math/Euler's_Totient_function.cpp
@@ -1,49 +1,37 @@
-// Euler's Totient Function represented by M(n)
-// For integer n it calculates the no. of coprimes
-// of n from 1 to n. Do refer line 18 for 
-// better grasp of question
+// Euler's Totient Function represented by F(n)
+// denotes the no. of coprime of n from 1 to n
 #include <bits/stdc++.h>
 using namespace std;
 int F(int n){
-    if(n<=0) return 0;             // we are calculating coprime from 1 to n
-    int arr[n+1],i,j; 
-    for(i=0;i<=n;i++){
-        arr[i]=i;
-    }
+    int arr[n+1],i,j;
+    arr[0]=0,arr[1]=1;     // as 1 is coprime with itself
     for(i=2;i<=n;i++){
-        if(arr[i]==i){             // checking if arr[i] has been untouched or not
+     arr[i]=i;    // initialising arr[i] with i
+    }  
+    for(i=2;i<=n;i++){
+        if(arr[i]==i){
             for(j=i;j<=n;j+=i){
-                arr[j]-=arr[j]/i;   // here we are removing no. of coprime of j integer
-            }
+                arr[j]-=(arr[j]/i); // just like sieve of eratosthenes we are decreasing values of multiple of primes
+            }  
         }
     }
-    // till now arr[i] shows no. of coprimes of n from 0 to n
-    // Let us represent Euler's Totient Function by M(n) where n is an integer
-    // M(n) denotes the number of coprimes of n from 1 to n
-    // so till now M(n)=arr[n] i.e. we have created Euler's Totient Function Array
-    // Now we have to calculate the no. of coprime pairs between 0 and n
-    for(i=2;i<=n;i++){
-        arr[i]+=arr[i-1]; // applying dp to calculate no. of coprime 
-    }                     // between 1 and n (both 1 and n inclusive)
-    return arr[n];
+    return arr[n];  //  here each arr[i] is giving number of coprimes of i from 1 to i
 }
 int main(){
     int n;
     cin>>n;
-    cout<<F(n)<<endl;
-    return 0;        
+    cout<<F(n); // printing Euler's Totient Function for int n
+return 0;
 }
-/*                      
-             Sample Input  3
-             Sample Output 4
-             Explanation: Coprime pairs between 1 and 4
-                          {(1,1),(1,2),(1,3),(2,3)}
-                          
-             Sample Input  4 
-             Sample Output 6
-             Explanation: Coprime pairs between 0 and 4
-                          {(1,1),(1,2),(1,3),(2,3),(1,4),(3,4)}
-             As you can see from second sample case that for integer 4 we have to 
-             add only coprime pairs which include 4 as one integer to the result of 
-             first sample case. This is the concept of Dynamic Programming
+// time complexity O(nloglogn) same as sieve of eratosthenes
+/*
+Sample Input: 4
+Sample Output: 2
+Explanation: As 4 is coprime with 1 and 2 only so its Euler's Totient function value is 2;
+
+Sample Input: 5
+Sample Output: 4
+Explanation: As 5 is a prime no. so it is coprime with all 1,2,3 & 4.
+             So, its Euler's Totient Function value is 4;
+
 */

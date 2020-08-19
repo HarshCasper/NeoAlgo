cfad36db567a912756cb9a0a8175e5a9e2758b59
kushagraiitbbs <66637263+kushagraiitbbs@users.noreply.github.com>
2020-08-19 13:34:36 +0530

    Modifying Euler's Totient Function
    @atarax665 I have updated the function to start from 1 plus I have removed (0,1) pair and added (1,1)

--- C-Plus-Plus/math/Euler's_Totient_function.cpp
+++ C-Plus-Plus/math/Euler's_Totient_function.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int F(int n){
-    if(n<=1) return 0;             // we are calculating coprime from 1 to n
+    if(n<=0) return 0;             // we are calculating coprime from 1 to n
     int arr[n+1],i,j; 
     for(i=0;i<=n;i++){
         arr[i]=i;
@@ -22,7 +22,7 @@ int F(int n){
     // M(n) denotes the number of coprimes of n from 1 to n
     // so till now M(n)=arr[n] i.e. we have created Euler's Totient Function Array
     // Now we have to calculate the no. of coprime pairs between 0 and n
-    for(i=3;i<=n;i++){
+    for(i=2;i<=n;i++){
         arr[i]+=arr[i-1]; // applying dp to calculate no. of coprime 
     }                     // between 1 and n (both 1 and n inclusive)
     return arr[n];
@@ -35,14 +35,14 @@ int main(){
 }
 /*                      
              Sample Input  3
-             Sample Output 3
+             Sample Output 4
              Explanation: Coprime pairs between 1 and 4
-                          {(1,2),(1,3),(2,3)}
+                          {(1,1),(1,2),(1,3),(2,3)}
                           
              Sample Input  4 
-             Sample Output 5
+             Sample Output 6
              Explanation: Coprime pairs between 0 and 4
-                          {(1,2),(1,3),(2,3),(1,4),(3,4)}
+                          {(1,1),(1,2),(1,3),(2,3),(1,4),(3,4)}
              As you can see from second sample case that for integer 4 we have to 
              add only coprime pairs which include 4 as one integer to the result of 
              first sample case. This is the concept of Dynamic Programming

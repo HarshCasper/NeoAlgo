60a541f4f0549d5d9393ccf0bbdecbc1f7bc908e
kushagraiitbbs <66637263+kushagraiitbbs@users.noreply.github.com>
2020-08-19 12:34:58 +0530

    Updating Euler's Totient function definition modifying code
    @atarax665 Sir, I have updated function and Euler's Totient Function definition. Now, I have removed cases coprime pairs including 0.

--- C-Plus-Plus/math/Euler's_Totient_function.cpp
+++ C-Plus-Plus/math/Euler's_Totient_function.cpp
@@ -1,10 +1,11 @@
 // Euler's Totient Function represented by M(n)
 // For integer n it calculates the no. of coprimes
-// of n from 0 to n. Do refer line 18 for 
+// of n from 1 to n. Do refer line 18 for 
 // better grasp of question
 #include <bits/stdc++.h>
 using namespace std;
 int F(int n){
+    if(n<=1) return 0;             // we are calculating coprime from 1 to n
     int arr[n+1],i,j; 
     for(i=0;i<=n;i++){
         arr[i]=i;
@@ -18,10 +19,10 @@ int F(int n){
     }
     // till now arr[i] shows no. of coprimes of n from 0 to n
     // Let us represent Euler's Totient Function by M(n) where n is an integer
-    // M(n) denotes the number of coprimes of n from 0 to n
+    // M(n) denotes the number of coprimes of n from 1 to n
     // so till now M(n)=arr[n] i.e. we have created Euler's Totient Function Array
     // Now we have to calculate the no. of coprime pairs between 0 and n
-    for(i=2;i<=n;i++){
+    for(i=3;i<=n;i++){
         arr[i]+=arr[i-1]; // applying dp to calculate no. of coprime 
     }                     // between 1 and n (both 1 and n inclusive)
     return arr[n];
@@ -29,19 +30,19 @@ int F(int n){
 int main(){
     int n;
     cin>>n;
-    cout<<F(n)<<endl; // always remember 0 is only coprime with 1
+    cout<<F(n)<<endl;
     return 0;        
 }
 /*                      
              Sample Input  3
-             Sample Output 4
-             Explanation: Coprime pairs between 0 and 4
-                          {(0,1),(1,2),(1,3),(2,3)}
+             Sample Output 3
+             Explanation: Coprime pairs between 1 and 4
+                          {(1,2),(1,3),(2,3)}
                           
              Sample Input  4 
-             Sample Output 6
+             Sample Output 5
              Explanation: Coprime pairs between 0 and 4
-                          {(0,1),(1,2),(1,3),(2,3),(1,4),(3,4)}
+                          {(1,2),(1,3),(2,3),(1,4),(3,4)}
              As you can see from second sample case that for integer 4 we have to 
              add only coprime pairs which include 4 as one integer to the result of 
              first sample case. This is the concept of Dynamic Programming

05c721fb0a1d2eb96eaea225ab828f87f7aa304b
yashaswibiyahut <yashaswibiyahut@mail.com>
2020-08-20 10:07:53 +0530

    Implementation of Interpolation Search

---
+++ C/search/Interpolation.c
@@ -0,0 +1,79 @@
+// C Program for Interpolation search algorithm
+ 
+#include <stdio.h>
+//Array size must be less tha MAX
+#define MAX 100
+
+
+/* Interpolation search function begin ::
+	Search for key element in array.
+	If key present in a[0..n-1],
+	then it return the index of that key element
+	else return -1
+*/
+int interpolation_search(int a[], int low, int high, int key)
+{
+    int mid;
+    while (low <= high && key>=a[low] && key<=a[high])  {
+    	
+    	 if (low == high){ 
+            if (a[low] == key) return low; 
+            return -1; 
+    }
+        //Finding the value of position     
+        mid = low + ((key-a[low])/(a[high]-a[low]))*(high-low);  
+        
+		// Condition of target found 
+		if (key == a[mid])
+            return mid+1;
+        
+		// If key is greater then 
+		// key must be in upper part 
+		if (key < a[mid])
+            high = mid - 1;
+        
+		// If key is smaller then
+		// key must be in lower part 
+		else
+            low = mid + 1;
+    }
+    return -1;
+}
+/* End of interpolation_search() */
+
+
+/* The main() begins */
+int main()
+{
+    int a[MAX];
+    int i, num;
+    int key, pos;
+    printf("\nEnter the size of array (num < %d) :", MAX);
+    scanf("%d", &num);
+    
+    //Interpolation search work best for uniform array
+    printf("Enter %d Elements of array  :\n",num);
+    for (i = 0; i < num; i++)
+        scanf("%d", &a[i]);
+    printf("\nEnter the value to be Search : ");
+    scanf("%d", &key);
+    
+    //Display the user inputs of element and searched key
+    printf("\nELEMENTS ARE : \n ");
+	for (i = 0; i < num; i++)
+		printf("%d\t", a[i]);
+		
+	printf("\nElement to be search is : %d",key);
+	
+    pos = interpolation_search(&a[0], 0, num - 1, key);
+    
+    //Check if the elemnet found in an array or not!
+    printf("\n\nOutput -->\n");
+    if (pos == -1)
+        printf("\nElement %d not found\n", key);
+    else
+        printf("\nElement %d found at position %d\n", key, pos);
+    return 0;
+}
+
+/* The main() end */

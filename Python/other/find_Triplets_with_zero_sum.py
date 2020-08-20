65f8d10a5f139c54c14fa3832c80f028a42a3001
Mohit Kumar <47894634+Mohitkumar6122@users.noreply.github.com>
2020-08-21 00:55:34 +0530

    Find triplets whose sum is zero from a given array in O(n^2) #578

---
+++ Python/other/find_Triplets_with_zero_sum.py
@@ -0,0 +1,60 @@
+'''
+  Author : Mohit Kumar
+  
+  python program to find triplets in a given  array whose sum is zero 
+
+'''
+
+# function to print triplets with 0 sum 
+
+def findTriplets(arr, n): 
+
+	found = False
+
+	# sort array elements 
+	arr.sort() 
+
+	for i in range(0, n-1): 
+	
+		# initialize left and right 
+		l = i + 1
+		r = n - 1
+		x = arr[i] 
+		while (l < r): 
+		
+			if (x + arr[l] + arr[r] == 0): 
+				# print elements if it's sum is zero 
+				print(x, arr[l], arr[r]) 
+				l+=1
+				r-=1
+				found = True
+			
+
+			# If sum of three elements is less 
+			# than zero then increment in left 
+			elif (x + arr[l] + arr[r] < 0): 
+				l+=1
+
+			# if sum is greater than zero than 
+			# decrement in right side 
+			else: 
+				r-=1
+		
+	if (found == False): 
+		print(" No Triplet Found") 
+
+
+# Driven source 
+arr = [0, -1, 2, -3, 1] 
+n = len(arr) 
+findTriplets(arr, n) 
+
+'''
+Complexity Analysis:
+
+Time Complexity : O(n^2).
+    Only two nested loops is required, so the time complexity is O(n2).
+
+Auxiliary Space : O(1), no extra space is required, so the time complexity is constant.
+
+'''

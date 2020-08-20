6e190e5a4c166c0290bf8b3e42297991aee11c07
Atul Goyal <68183845+ag278@users.noreply.github.com>
2020-08-20 22:28:00 +0530

    Update euler_totient_function.py

--- Python/math/euler_totient_function.py
+++ Python/math/euler_totient_function.py
@@ -8,21 +8,19 @@ def euler_totient(q):
       if arr[i]==i:                    #if this condition is satisfied then we are removing the coprimes of j
             for j in range(i,n+1,i):
                 arr[j]-=(arr[j]//i)  
-                
-    for i in range(2,n+1):
-        arr[i]+=arr[i-1]               #for calculating no of coprime between (1,n) we are using the concept of dyanmic programming 
-        
+                    
     return arr[n]  
     
     
-print("Enter number of elements : ")
+print("Enter a number : ")
 n = int(input())                       # taking input from the user
 if(n<0):                   
     print("inavlid input")
 else:
     print(euler_totient(n))
 #sample input
-#euler_totient(4) will be 6 as no of coprime between 1 and 4 are (1,1),(1,2),(1,3),(1,4),(2,3),(3,4)
-#euler_totient(5) will be 10 as no of coprime between 1 and 4 are (1,1),(1,2),(1,3),(1,4),(1,5),(2,3),(2,5),(3,4),(3,5),(4,5)
+#euler_totient(4) will be 2 as coprime will be (1,4),(3,4)
+#euler_totient(6) will be 2 as coprime will be (1,6),(5,6)
+#euler_totient(9) will be 6 as coprime will be (1,9),(2,9),(4,9),(5,9),(7,9),(8,9)
 #here we are considering (2,3) and (3,2) as same pair
  

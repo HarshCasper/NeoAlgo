1ac045da0fdfd9915156b96a2395e32f444cb254
Mohit Kumar <47894634+Mohitkumar6122@users.noreply.github.com>
2020-08-21 12:36:25 +0530

    Update find_Triplets_with_zero_sum.py

--- Python/other/find_Triplets_with_zero_sum.py
+++ Python/other/find_Triplets_with_zero_sum.py
@@ -7,47 +7,69 @@
 
 # function to print triplets with 0 sum 
 
-def findTriplets(arr, n): 
+def find_Triplets_with_zero_sum(arr, n): 
+   
+    ''' find triplets in a given  array whose sum is zero 
+        
+        Parameteres : 
+            arr : input array 
+            n = size of input array
 
-	found = False
+        Output :
+            if triplets found print their values 
+            else print No Triplet Found
 
-	# sort array elements 
-	arr.sort() 
+    '''
+   
+    found = False
 
-	for i in range(0, n-1): 
-	
-		# initialize left and right 
-		l = i + 1
-		r = n - 1
-		x = arr[i] 
-		while (l < r): 
-		
-			if (x + arr[l] + arr[r] == 0): 
-				# print elements if it's sum is zero 
-				print(x, arr[l], arr[r]) 
-				l+=1
-				r-=1
-				found = True
-			
+    # sort array elements 
+    arr.sort() 
 
-			# If sum of three elements is less 
-			# than zero then increment in left 
-			elif (x + arr[l] + arr[r] < 0): 
-				l+=1
+    for i in range(0, n - 1) : 
+    
+        # initialize left and right 
+        l = i + 1
+        r = n - 1
+        x = arr[i] 
+        
+        while (l < r): 
+            
+            temp = x + arr[l] + arr[r] 
+            
+            if (temp == 0) : 
+                # print elements if it's sum is zero 
+                print(x, arr[l], arr[r]) 
+                l+=1
+                r-=1
+                found = True
+            
 
-			# if sum is greater than zero than 
-			# decrement in right side 
-			else: 
-				r-=1
-		
-	if (found == False): 
-		print(" No Triplet Found") 
+            # If sum of three elements is less 
+            # than zero then increment in left 
+            elif (temp < 0) : 
+                l+=1
 
+            # if sum is greater than zero than 
+            # decrement in right side 
+            else: 
+                r-=1
+        
+    if (found == False): 
+        print(" No Triplet Found") 
 
-# Driven source 
-arr = [0, -1, 2, -3, 1] 
-n = len(arr) 
-findTriplets(arr, n) 
+# DRIVER CODE STARTS
+
+if __name__ == "__main__":
+    
+    n = int(input('Enter size of array\n'))
+    print('Enter elements of array\n')
+    
+    arr = list(map(int,input().split()))
+    
+    print('Triplets with 0 sum are as : ')
+    
+    find_Triplets_with_zero_sum(arr, n) 
 
 '''
 SAMPLE INPUT 1 :

e4d976cf6fc51032d9b173c506e0d2c8a0633660
akanksha-saini <akankshasaini0109@gmail.com>
2020-08-21 23:06:22 +0530

    Updated file sum of triplet in C

--- C/cp/sum of triplet.c
+++ C/cp/sum of triplet.c
@@ -17,8 +17,7 @@ void sort(int a[1000],int n)
 	//using bubble sort
 	int i,j,temp;
 	for(i=0; i<n; i++)
-	{   
-	     
+	{   	     
 		for(j=0; j<=(n-2)-i; j++)
 		{
 			if(a[j] > a[j+1] ) // if a[j] > a[j+1] then swap them
@@ -33,22 +32,22 @@ void sort(int a[1000],int n)
 void tripletSum(int a[1000],int n,int target)
 {
 	int i=0, j,k;
-  for(i=0; i<n; i++ ) 
+  for(i=0; i<n; i++ ) // to traverse the whole array we need a foor loop from i to n-1
   {
   	j = i+1; k = n-1; // using 2 iterator approach
-  	while(j<k)
+  	while(j<k) // loop will stop when j==k
 	{
 		if(a[i] + a[j] + a[k] == target) //if triplet sum is equal to target then print it
 		{
 			printf("%d, %d and %d\n",a[i],a[j],a[k]);
-		    j++; k--;
+		    j++; k--; //update both iterator as we have find a triplet sum equal to target
 		    
 		}
-		else if(a[i] + a[j] + a[k] > target)
+		else if(a[i] + a[j] + a[k] > target) // if triplet sum is less than target then decrement the 3 iterator k
 		{
 			k--;
 		}
-		else
+		else // increment the 2nd iterator j as triplet sum is greater than target
 		{
 			j++;
 		}
@@ -70,15 +69,20 @@ int main()
 	}
 	
 	scanf("%d",&target); //input the target number
+	
+	/* We need to sort the array as the output is in sorted form, also it would be easier to check the triplets in forward direction */
 	sort(a,n); // sort the array 
     
-		
-	tripletSum(a,n,target); //cal tripletSum() to find and print triplet 
+	//the solution is based on 2 pointer approach to find pair sum equal to target	
+	tripletSum(a,n,target); //call tripletSum() to find and print triplet 
 	
 	return 0;
 }
 /* Time Complexity of sort function : O(n^2)
    Time Complexity of tripletSum function : O(n^2)
+   
+   Space Complexity of  sort function : O(1) because only 1 extra space is required for temp
+   Space Complexity of  tripletSum function : O(1) because no extra space is required
    
 */
 

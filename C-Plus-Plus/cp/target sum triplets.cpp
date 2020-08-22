420258c2285f45e6fa140343735003d9d7af40c5
neehit goyal <44130067+NEEHITGOYAL@users.noreply.github.com>
2020-08-22 21:41:01 +0530

    Update target sum triplets.cpp
    added messages kindly review it

--- C-Plus-Plus/cp/target sum triplets.cpp
+++ C-Plus-Plus/cp/target sum triplets.cpp
@@ -8,7 +8,7 @@ void printtriplets(int arr[],int n,int sum)
 {
 	//sorting array initially
 	sort(arr,arr+n);
-	  
+	cout<<"The triplet sets are:"<<endl; 
 	for(int i =0;i<n-1;i++)
 	{
 		int left=i+1;
@@ -16,35 +16,39 @@ void printtriplets(int arr[],int n,int sum)
 		int current=arr[i];
 		while (left < right) 
 		{ 
-            if (current + arr[left] + arr[right] == sum) 
-			{ 
+                    if (current + arr[left] + arr[right] == sum) 
+	            { 
 			    // Print elements if it's sum is given sum.
 				//printing elements in ascending order 
-                cout<<current<<", "<<arr[left]<<" and "<<arr[right]<<endl;
-                left++; 
-                right--; 
-            } 
+                            cout<<current<<", "<<arr[left]<<" and "<<arr[right]<<endl;
+                            left++; 
+                            right--; 
+                    } 
   
-            // If current sum is less than 'sum' then increment the left index
-            else if (current + arr[left] + arr[right] < sum) 
-                left++; 
+                    // If current sum is less than 'sum' then increment the left index
+                    else if (current + arr[left] + arr[right] < sum) 
+                    left++; 
   
-            // if current sum is greater than  'sum', then decrement the right index 
-            else
-                right--; 
-        } 
-	}
+                    // if current sum is greater than  'sum', then decrement the right index 
+                    else
+                    right--; 
+                } 
+	 }
 }
 
 //Main function
 int main() 
 {
 	int end,input[1000],target;
+	cout<<"Enter the no. of elements in array you want to enter"<<endl; 
 	cin >> end;
+	
+	cout<<"Enter elements"<<endl;
 	
 	for(int ind=0;ind<end;ind++)
 		cin >> input[ind];
-		
+	
+	cout<<"Enter target"<<endl;	
 	cin>>target;
 	printtriplets(input,end,target);
 	return 0;

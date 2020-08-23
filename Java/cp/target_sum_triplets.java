df2438d49b18013b8f554e41756f577395fcde4f
Animesh Shukla <oneanimesh@gmail.com>
2020-08-23 19:49:42 +0530

    TARGET SUM TRIPLETS #585

---
+++ Java/cp/target_sum_triplets.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class target_sum_triplets
+  
+  {
+      
+    public static void main(String args [])
+    
+    {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt(); //  array size
+        
+        int arr[]=new int[n];
+        
+        for(int i=0;i<n;i++) 
+          arr[i]=sc.nextInt(); // array input
+          
+          int target =sc.nextInt(); // target sum
+          
+          Arrays.sort(arr);
+          
+          for(int j=0; j<n-1; j++)
+          {
+              int start=j+1;
+              int end=n-1;
+              
+              while(start<end){
+                  
+                  if(arr[j] + arr[start] + arr[end] == target)
+                  {
+                   System.out.println(arr[j] + ", " + arr[start] + " and " + arr[end]);
+                   start++;
+                   end--;
+                  } 
+                   else if(arr[j]+arr[start]+arr[end] < target)
+                   start++;
+                   
+                   else 
+                   end--;
+              }
+              
+          }
+          
+    }
+  }
+  
+  // Complexity Analysis
+  // Time Complexity - O(n^2)
+  // Space Complexity - O(1)
+  
+          

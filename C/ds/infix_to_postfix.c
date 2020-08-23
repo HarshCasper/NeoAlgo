09ff94deeeb6775cc305ed2fe94b813e451464e8
Prasad V Patil <69467300+prasadvpatil@users.noreply.github.com>
2020-08-23 17:02:26 +0530

    Update infix_to_postfix.c

--- C/ds/infix_to_postfix.c
+++ C/ds/infix_to_postfix.c
@@ -193,17 +193,6 @@ int main()
 
 
 /*
-The following is the process how the conversion of infix expression to postfix expression takes place by the above code
+The process how the conversion of infix expression to postfix expression takes place by the above code is specified in the infix_to_postfix.md file.
 */
 
-
-/*
-1. Print operands as they arrive.
-2. If the stack is empty or contains a left parenthesis on top, push the incoming operator onto the stack.
-3. If the incoming symbol is a left parenthesis, push it on the stack.
-4. If the incoming symbol is a right parenthesis, pop the stack and print the operators until you see a left parenthesis. Discard the pair of parentheses.
-5. If the incoming symbol has higher precedence than the top of the stack, push it on the stack.
-6. If the incoming symbol has equal precedence with the top of the stack, use association. If the association is left to right, pop and print the top of the stack and then push the incoming operator. If the association is right to left, push the incoming operator.
-7. If the incoming symbol has lower precedence than the symbol on the top of the stack, pop the stack and print the top operator. Then test the incoming operator against the new top of stack.
-8. At the end of the expression, pop and print all operators on the stack. (No parentheses should remain.)
-*/

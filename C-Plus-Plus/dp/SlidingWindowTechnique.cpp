34cefccbc265bb25dd60cddf998bfd1c306ebb13
Piyush Goel <piyushgoyaall@gmail.com>
2020-08-23 18:33:13 +0530

    Sliding Window Technique

--- C-Plus-Plus/dp/SlidingWindowTechnique.cpp
+++ C-Plus-Plus/dp/SlidingWindowTechnique.cpp
@@ -4,22 +4,29 @@
 
 using namespace std;
 
-int maxSum(vector<int> arr, int k)
+long long maxSum(const vector<int> &arr, size_t k)
 {
-  int n = arr.size();
+  size_t n = arr.size();
   // If size of sub-arry k is greater than original array size n
   if (k >= n)
     return -1;
   // computing sum for window size of k from begining
-  int curr_sum = 0;
-  for (int i = 0; i < k; i++)
+  long long curr_sum = 0;
+  for (size_t i = 0; i < k; i++)
     curr_sum += arr[i];
   // Computing sum of other windows of size k present in the array
-  int max_sum = curr_sum;
-  for (int i = k; i < n; i++)
+  long long max_sum = curr_sum;
+  for (size_t i = k; i < n; i++)
   {
     curr_sum += (arr[i] - arr[i - k]);
     max_sum = max(max_sum, curr_sum);
   }
   return max_sum;
+}
+
+int main()
+{
+  vector<int> arr = {100, 200, 300, 400};
+  size_t k = 2;
+  cout << maxSum(arr, k);
 }

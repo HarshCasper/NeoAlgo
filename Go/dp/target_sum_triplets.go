49ec4df2ec07f3a2cc1acd6a86693c318c3fcd95
Miten Gajjar <miten3377@gmail.com>
2020-08-23 13:48:36 +0530

    added input/output sample

--- Go/dp/target_sum_triplets.go
+++ Go/dp/target_sum_triplets.go
@@ -30,25 +30,51 @@ func sumTriplets(arr []int, targetSum int) {
 
 	arrayLength := len(arr)
 
-	for i := 0; i < arrayLength-2; i++ {
+	// it will count the number of triplets
+	count := 0
 
-		left = 0
-		right = arrayLength - 1
+	if arrayLength == 0 {
+		// corner case
+		fmt.Println("Array is empty")
 
-		for left < right {
+	} else {
+		for i := 0; i < arrayLength-2; i++ {
 
-			sum := arr[i] + arr[left] + arr[right]
+			// Initial condition
+			// index for first element is i
+			// index for second element is 0
+			// index for third element is index of last element
 
-			if sum == targetSum {
-				fmt.Printf("Triplet is %d %d %d\n", arr[i], arr[left], arr[right])
-				break
-			} else if sum < targetSum {
-				left++
-			} else {
-				right--
+			left = 0
+			right = arrayLength - 1
+
+			for left < right {
+
+				sum := arr[i] + arr[left] + arr[right]
+
+				// if the sum of all three elementsis equal to targetSum
+				// we should print it and then move forward
+				// to next element by breaking the inner loop
+				if sum == targetSum {
+					fmt.Printf("Triplet is %d %d %d\n", arr[i], arr[left], arr[right])
+					count++
+					break
+				} else if sum < targetSum {
+					// if the sum is less than targetSum we should
+					// increase the left index for the next element
+					left++
+				} else {
+					// if the sum is greater than targetSum we should
+					// decrease the right index for the next element
+					right--
+				}
 			}
-		}
 
+		}
+	}
+	// if there is no triplet exist
+	if count == 0 {
+		fmt.Println("Sorry couldn't found any triplets with target sum")
 	}
 
 }
@@ -58,13 +84,15 @@ func main() {
 	fmt.Println("please enter the number of element")
 	fmt.Scanf("%d", &length)
 
-	array := make([]int, length) //making the array of size length
+	//making the slice of size length
+	array := make([]int, length)
 	fmt.Println("enter the space separated elements")
 	for i := 0; i < length; i++ {
 		fmt.Scanf("%d", &array[i])
 	}
 
-	sort.Ints(array) // sorting the array of integers
+	// sort the array in ascending order
+	sort.Ints(array)
 
 	var targetSum int
 	fmt.Println("enter the target sum")
@@ -73,3 +101,23 @@ func main() {
 	sumTriplets(array, targetSum)
 
 }
+
+/*
+
+	input/output sample
+
+	please enter the number of element
+	9
+	enter the space separated elements
+	5 7 9 1 2 4 6 8 3
+	enter the target sum
+	10
+	Triplet is 1 1 8
+	Triplet is 2 1 7
+	Triplet is 3 1 6
+	Triplet is 4 1 5
+	Triplet is 5 1 4
+	Triplet is 6 1 3
+	Triplet is 7 1 2
+
+*/

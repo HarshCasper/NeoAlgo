ade886c9b081d4d458a17d9065b7cd8abf1e8488
Nagasuruthika N <66667949+Nagasuruthika@users.noreply.github.com>
2020-08-23 21:32:18 +0530

    Update Prime_factorisation_using_sieve.py

--- Python/cp/Prime_factorisation_using_sieve.py
+++ Python/cp/Prime_factorisation_using_sieve.py
@@ -1,58 +1,69 @@
 '''
-Python Program to calculate prime factorization  
-of a number n in O(Log n) time complexity using sieve. 
+Python Program to calculate prime factorization of a number n in O(Log n) time complexity using sieve. 
 '''
 import math
   
 Max_Num = 10001
   
-spf = [0 for i in range(Max_Num)] #smallest prime factor for every number 
+small_prime_factor = [0 for num in range(Max_Num)] # smallest prime factor for every number 
 
 def sieve(): 
-    ''' Function that calculates SPF (Smallest Prime Factor) for every number till Max_Num. '''
-    spf[1] = 1
-    for i in range(2, Max_Num):#smallest prime factor for every number is the number itself.
-        spf[i] = i 
+
+    ''' Function that calculates Smallest Prime Factor for every number till Max_Num. '''
+
+    
+    for num in range(1, Max_Num):# smallest prime factor for every number is the number itself
+        small_prime_factor[num] = num
   
   
-    for i in range(4, Max_Num, 2): #smallest prime factor of every even number is 2
-        spf[i] = 2 
+        if(num % 2 == 0):# smallest prime factor of every even number is 2
+           
+          small_prime_factor[num] = 2 
   
-    for i in range(3, math.ceil(math.sqrt(Max_Num))): 
+    for num in range(3, math.ceil(math.sqrt(Max_Num))): 
                 
-        if (spf[i] == i): # checks if i is a prime number
+        if (small_prime_factor[num] == num): # checks if num is a prime number
                          
-            for j in range(i * i, Max_Num, i): #small prime factors for all numbers divisible by i.                  
+            for num1 in range(num^2, Max_Num, num): # small prime factors for all numbers divisible by num.                  
                
-                if (spf[j] == j): 
-                    spf[j] = i 
+                if (small_prime_factor[num1] == num1): 
+                    small_prime_factor[num1] = num # marking small_prime_factor[num1]
   
 
-def getFactorization(x): 
+def get_factorization(element): # function definition
+
     '''
     Function that calculates prime factors by dividing by smallest  
     prime factor at every step.
-    Params: x (input)
-    returns: ret
+    Params: element (input)
+    returns: factors
+    
     '''
-    factors1 = set() #adding factors to set: factors1 to avoid duplicates
-    while (x != 1): 
-        factors1.add(spf[x]) 
-        x = x // spf[x] 
-    factors= list(factors1) #converting set to list to access the elements of the set
+    factors1 = set() # adding factors to set: factors1 to avoid duplicates
+
+    while (element != 1): 
+
+        factors1.add(small_prime_factor[element]) 
+
+        element = element // small_prime_factor[element] # floor divison
+
+    factors= list(factors1) # converting set to list to access the elements of the set
     return factors
   
-# precalculating Smallest Prime Factor 
-sieve() 
-x = int(input("Enter the number to find prime factors for:"))
-print("Prime factors of",x, ":", 
-                                end = "") 
-  
+if __name__ == "__main__": 
 
-y = getFactorization(x) #function calling
+  ''' Main function that runs functions: sieve and get_factorization '''
   
-for i in range(len(y)): 
-    print(y[i], end = " ") 
+  sieve() # Calculates Smallest Prime Factor 
+
+  element = int(input("Enter the number to find prime factors for:"))
+
+  print("Prime factors of",element, ":",end = "") 
+
+  y = get_factorization(element) # function calling
+  
+  for num in range(len(y)): 
+    print(y[num], end = " ") 
 
 ''' 
 Sample Output 

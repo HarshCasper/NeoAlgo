74a8b9ac664933a91f84cde42345b3f057480c23
Rajiv Singh <rajivperfect007@gmail.com>
2020-08-25 02:04:12 +0530

    formatted the code

--- C-Plus-Plus/graphs/Kosaraju_Algorithm.cpp
+++ C-Plus-Plus/graphs/Kosaraju_Algorithm.cpp
@@ -1,96 +1,97 @@
+//Implementation of Kosaraju's Algorithm to find out the strongly connected components (SCCs) in a graph.
+
 #include<bits/stdc++.h>
 using namespace std;
 
-//Implementation of Kosaraju's Algorithm to find out the strongly connected components (SCCs) in a graph.
-class Graph{
-    list<int> *l;
-    list<int> *rl;
-    int V;
-    vector<int> order;
-    int vis[1000],comp[1000];
+class Graph {
+  list<int> *l;
+  list<int> *rl;
+  int V;
+  vector<int> order;
+  int vis[1000], comp[1000];
 
-    public:
+public:
 
-        Graph(int V){
-            this->V=V;
-            l=new list<int>[V];
-            rl=new list<int>[V];
+  Graph(int V) {
+    this->V = V;
+    l = new list<int>[V];
+    rl = new list<int>[V];
 
-        }
+  }
 
-        void addEdge(int x,int y){
-            l[x].push_back(y);
-            rl[y].push_back(x);
-        }
+  void addEdge(int x, int y) {
+    l[x].push_back(y);
+    rl[y].push_back(x);
+  }
 
-        void dfs(int cur){
-            vis[cur]=1;
-            for(auto x:l[cur]){
-                if(!vis[x]){
-                    dfs(x);
-                }
-            }
-            order.push_back(cur);
-        }
+  void dfs(int cur) {
+    vis[cur] = 1;
+    for (auto x : l[cur]) {
+      if (!vis[x]) {
+        dfs(x);
+      }
+    }
+    order.push_back(cur);
+  }
 
-         void dfs_reverse(int cur,int col){
-            vis[cur]=1;
-            comp[cur]=col;
-            for(auto x:rl[cur]){
-                if(!vis[x]){
-                    dfs_reverse(x,col);
-                }
-            }
-
-        }
-
-        //for finding the strongly connected components
-        void solve(){
-
-            memset(vis,0,V);
-
-            for(int i=0;i<V;i++){
-                if(!vis[i]){
-                    dfs(i);
-                }
-            }
-            for(int i=0;i<V;i++){
-                vis[i]=0;
-            }
-            int col=1;
-            for(int i=V-1;i>=0;i--){
-                if(!vis[order[i]]){
-                    dfs_reverse(order[i],col);
-                    col++;
-                }
-
-            }
-
-            for(int i=0;i<V;i++){
-                cout<<i+1<<": "<<comp[i]<<endl;
-            }
-
-        }
-};
-
-int main(){
-    int n,e;
-    cout<<"Enter number of vertices and edges:";
-    cin>>n>>e;
-    Graph g(n);
-    cout<<"Enter edges:";
-    for(int i=0;i<e;i++){
-        int x,y;
-        cin>>x>>y;
-        x--;y--;
-        g.addEdge(x,y);
+  void dfs_reverse(int cur, int col) {
+    vis[cur] = 1;
+    comp[cur] = col;
+    for (auto x : rl[cur]) {
+      if (!vis[x]) {
+        dfs_reverse(x, col);
+      }
     }
 
-    g.solve();
-    return 0;
+  }
+
+  //for finding the strongly connected components
+  void solve() {
+    memset(vis, 0, V);
+    for (int i = 0; i < V; i++) {
+      if (!vis[i]) {
+        dfs(i);
+      }
+    }
+    for (int i = 0; i < V; i++) {
+      vis[i] = 0;
+    }
+    int col = 1;
+    for (int i = V - 1; i >= 0; i--) {
+      if (!vis[order[i]]) {
+        dfs_reverse(order[i], col);
+        col++;
+      }
+
+    }
+
+    for (int i = 0; i < V; i++) {
+      cout << i + 1 << ": " << comp[i] << endl;
+    }
+
+  }
+};
+
+int main() {
+  int n, e;
+  cout << "Enter number of vertices and edges:";
+  cin >> n >> e;
+  Graph g(n);
+  cout << "Enter edges:";
+  for (int i = 0; i < e; i++) {
+    int x, y;
+    cin >> x >> y;
+    x--; y--;
+    g.addEdge(x, y);
+  }
+  g.solve();
+  return 0;
 }
-/*Sample input-output:
-Input: 
+
+/*
+Sample input-output:
+
+Input:
 6 7
 1 2
 2 3
@@ -99,13 +100,13 @@ Input:
 4 5
 5 6
 6 4
-Output: 
-1:1
-2:1
-3:1
-4:2
-5:2
-6:2
+Output:
+1: 1
+2: 1
+3: 1
+4: 2
+5: 2
+6: 2
 
 Time Complexity: O(n+e)
 n: Number of vertices

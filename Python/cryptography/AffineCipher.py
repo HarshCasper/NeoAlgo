4bbc310b856a44815ea0848b043094e8d84efedb
RIYA JOSHI <53979947+riya-joshi-401@users.noreply.github.com>
2020-08-24 19:13:22 +0530

    Update AffineCipher.py

--- Python/cryptography/AffineCipher.py
+++ Python/cryptography/AffineCipher.py
@@ -13,38 +13,38 @@ def encryption():
 
   key1 ,key2=input("Enter the keys : ").split()
 
-  li=[] # a list for storing the encrypted characters which we later merged to a string i.e cipher_text
+  list_encrypted_characters=[] # a list for storing the encrypted characters which we later merged to a string i.e cipher_text
 
 
 
-  def gcd(a, b): # for checking coprime
-      while b != 0:
-          a, b = b, a % b
-      return a
+  def gcd(number1, number2): # for checking whether two numbers are coprime or not
+      while number2 != 0:
+          number1, number2 = number2, number1 % number2
+      return number1
 
 
   if gcd(int(key1),26)==1 and 0<int(key1)<26 and int(key2) <26  :  # checks whether key1 and 26 are coprime , key1 and key2 < 26
     
-    for i in plain_text:
-      if i.isalpha()==False: # if the character is not an alphabet then simply append for example if theres a white space or special characters like # , @ , ! etc.
-        li.append(i)
+    for character in plain_text:
+      if character.isalpha()==False: # if the character is not an alphabet then simply append for example if theres a white space or special characters like # , @ , ! etc.
+        list_encrypted_characters.append(character)
     
       else:
 
-        if i.isupper(): # if the alphabet is upper case
+        if character.isupper(): # if the alphabet is upper case
 
-          p=ord(i)-65 # converts alphabets to numbers ranging from 0-26 for example A has a ascii value of 65 so 65-65 =0 therefore A is represented as 0 
+          p=ord(character)-65 # converts alphabets to numbers ranging from 0-26 for example A has a ascii value of 65 so 65-65 =0 therefore A is represented as 0 
           e_temp=(int(p)*int(key1)+int(key2))%26   # encryption formula : (p*k1 +k2)mod26
-          li.append(chr(e_temp+65)) # storing the encrypted character in list
+          list_encrypted_characters.append(chr(e_temp+65)) # storing the encrypted character in list
 
         else:  # if the alphabet is lower case
 
-          p=ord(i)-97 # converts alphabets to numbers ranging from 0-26 for example c has a ascii value of 99 so 99-97 =2 therefore c is represented as 2
+          p=ord(character)-97 # converts alphabets to numbers ranging from 0-26 for example c has a ascii value of 99 so 99-97 =2 therefore c is represented as 2
           e_temp=(int(p)*int(key1)+int(key2))%26  # encryption formula : (p*k1 +k2)mod26
-          li.append(chr(e_temp+97)) # storing the encrypted character in list
+          list_encrypted_characters.append(chr(e_temp+97)) # storing the encrypted character in list
 
     
-    cipher_text=''.join(li)  
+    cipher_text=''.join(list_encrypted_characters)  
     print("The encrypted text is : ",cipher_text) 
   
   else:
@@ -54,40 +54,40 @@ def encryption():
 
 def decryption():
 
-  if cipher_text=='':  # if the keys conditions were not satisfied and the encryption didnt took place then we exit the decryption function
+  if cipher_text=='':  
     print('')
     exit(0)
 
   else:
 
 
-    def inverse(x,y): # function calculating inverse
-        for i in range(y):
-            if (x*i)%y==1:
-                return i
+    def inverse(number1,number2): # function calculating inverse
+        for character in range(number2):
+            if (number1*character)%number2==1:
+                return character
 
     key1_inv=inverse(int(key1),26) # calculating inverse of key1
 
-    final=[] # a list that stores decrypted characters that later is merged to a string i.e decrypted
+    list_decrypted_characyers=[] # a list that stores decrypted characters that later is merged to a string i.e decrypted
 
-    for m in cipher_text:
+    for cipher_character in cipher_text:
 
-      if m.isalpha()==False: # if the character is not an alphabet then simply append for example if theres a white space or special characters like # , @ , ! etc.
-        final.append(m)
+      if cipher_character.isalpha()==False: # if the character is not an alphabet then simply append for example if theres a white space or special characters like # , @ , ! etc.
+        list_decrypted_characyers.append(cipher_character)
 
       else:
 
-        if m.isupper():  # if the character is in upper case
-          d=(((ord(m)-65)-int(key2))*key1_inv)%26   # decryption formula : ((p-k2)*k1_inverse)mod26
-          final.append(chr(d+65)) #converting back to alphabets
+        if cipher_character.isupper():  # if the character is in upper case
+          d=(((ord(cipher_character)-65)-int(key2))*key1_inv)%26   # decryption formula : ((p-k2)*k1_inverse)mod26
+          list_decrypted_characyers.append(chr(d+65)) #converting back to alphabets
 
         else:  # if the character is in lower case
-          d=(((ord(m)-97)-int(key2))*key1_inv)%26  # decryption formula : ((p-k2)*k1_inverse)mod26
-          final.append(chr(d+97)) #converting back to alphabets
+          d=(((ord(cipher_character)-97)-int(key2))*key1_inv)%26  # decryption formula : ((p-k2)*k1_inverse)mod26
+          list_decrypted_characyers.append(chr(d+97)) #converting back to alphabets
 
 
 
-    decrypted=''.join(final)
+    decrypted=''.join(list_decrypted_characyers)
     print("The decrypted text is : ",decrypted) 
     
 encryption()

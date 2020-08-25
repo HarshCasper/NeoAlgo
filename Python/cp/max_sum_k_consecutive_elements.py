7f15b0c1a88b21259ad07654eac4a4b1799eb871
Sukriti Dawar <55523401+sukritidawar@users.noreply.github.com>
2020-08-25 22:23:01 +0530

    Update max_sum_k_consecutive_elements.py

--- Python/cp/max_sum_k_consecutive_elements.py
+++ Python/cp/max_sum_k_consecutive_elements.py
@@ -22,3 +22,12 @@ print("Enter k")
 k = int(input())                      #inputting k
 
 print("Maximum sum of k consecutive elements in array is: " + str(maxSumofKelements(arr, n, k)))
+
+#Sample test case: 
+# arr = {50, 70, 30, 10}, k = 2, n = 4
+# 1. ans = 0, now once the for loop is executed, ans becomes 120 (50 + 70)
+# 2. Now we have to check whether any other sum of k consecutive elements is greater than our initial sum.
+# 3. Now we run a loop from k to n, and everytime we calculate curr_sum and check whether it is greater than our ans or not, if it is greater, we update our ans.
+# 4. Iterations: curr_sum = 120 + 30 - 50 = 100(i.e 70 + 30) (So here we calculated arr[1] + arr[2]) (i = 2)(ans > curr_sum, therefore, ans = 120)
+#                curr_sum = 100 + 10 - 70 = 40(i.e. 30 + 40) (arr[2] + arr[3]) (i = 3)(ans > curr_sum, therefore, ans = 120)
+# 5. So the final ans is 120

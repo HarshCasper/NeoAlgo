dd3e7304925d7fcfbaba4f09ab4b6f08633355ba
odavhad <odavhad@gmail.com>
2020-08-25 17:48:17 +0530

    Update CircularLinkedList.go

--- Go/ds/CircularLinkedList.go
+++ Go/ds/CircularLinkedList.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 // A linked list node
 type node struct {
@@ -56,30 +60,32 @@ func (list *circularLinkedList) display() {
 }
 
 // Remove the node, if present, at the given index
-func (list *circularLinkedList) remove(index int) {
+func (list *circularLinkedList) remove(index int) (interface{}, error) {
 	if index >= list.size {
-		fmt.Println("Index not present in the Circular Linked List")
-		return
+		return nil, errors.New("Index not present")
 	}
 
+	list.size--
 	if index == 0 {
 		var temp = list.head
 		list.head = temp.next
 		list.tail.next = list.head
-	} else {
-		var iter = 0
-		var curr = list.head
 
-		for iter+1 < index {
-			iter++
-			fmt.Println(curr.data)
-			curr = curr.next
-		}
-		var temp = curr
-		curr.next = temp.next.next
+		return temp.data, nil
 	}
 
-	list.size--
+	var iter = 0
+	var curr = list.head
+
+	for iter+1 < index {
+		iter++
+		fmt.Println(curr.data)
+		curr = curr.next
+	}
+	var temp = curr
+	curr.next = temp.next.next
+
+	return temp.next.data, nil
 }
 
 // Returns the size of the given circular linked list
@@ -87,17 +93,57 @@ func (list *circularLinkedList) length() int {
 	return list.size
 }
 
+// Returns a random element to insert in the Circular Linked List
+func getElement() interface{} {
+	var temp interface{} = rand.Intn(100)
+
+	return temp
+}
+
+// Returns a random index to remove from the Circular Linked List
+func getIndex(size int) int {
+	var temp int = rand.Intn(100) % size
+
+	return temp
+}
+
 func main() {
 	var list circularLinkedList
+	var choice int
+	var flag bool
 
-	list.insert(0)
-	list.insert(1)
-	list.insert(2)
-	list.insert(3)
-	list.display()
-	list.remove(0)
-	list.display()
-	list.remove(1)
-	list.display()
-	list.remove(2)
+	flag = true
+
+	for flag {
+		fmt.Print("Menu\n1. Insert element\n2. Remove element\n3. Display elements\n4. Count of elements\n5. Exit\nEnter your choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			element := getElement()
+			list.insert(element)
+			fmt.Println("Inserted: ", element)
+		case 2:
+			if list.size == 0 {
+				fmt.Println("No element present in the list.")
+			} else {
+				index := getIndex(list.size)
+				data, err := list.remove(index)
+				if err == nil {
+					fmt.Println("Removed: ", data)
+				} else {
+					fmt.Println("Error: ", err)
+				}
+			}
+		case 3:
+			list.display()
+		case 4:
+			count := list.length()
+			fmt.Println("Length: ", count)
+		case 5:
+			flag = false
+		default:
+			fmt.Println("Please enter a valid choice.")
+		}
+	}
 }

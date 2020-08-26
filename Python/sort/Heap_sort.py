a50513adcda5fb3e31191aa96eef7ae34bddff97
Avinash Kr. Ranjan <ranjan.avinash@hotmail.com>
2020-08-26 19:57:30 +0530

    Updated Heap Sort

--- Python/sort/Heap_sort.py
+++ Python/sort/Heap_sort.py
@@ -1,55 +1,59 @@
-# Heap Sort in python
+# Python program for implementation of heap Sort
 
-def heapify(arr, n, i):
-    # Find largest among root and children
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
+# To heapify subtree rooted at index.
+# length is size of heap
 
-    if l < n and arr[i] < arr[l]:
-        largest = l
+def heapify(arr, length, index):
+    # base case
+    # we will call this function until the largest number is the index...
+    largest_num_index = index
+    left_index = (index * 2) + 1
+    right_index = (index * 2) + 2
 
-    if r < n and arr[largest] < arr[r]:
-        largest = r
+    if(left_index < length and arr[index] < arr[left_index]):
+        largest_num_index = left_index
 
-    # If root is not largest, swap with largest and continue heapifying
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
+    if(right_index < length and arr[largest_num_index] < arr[right_index]):
+        largest_num_index = right_index
 
-def heapSort(arr):
-    n = len(arr)
+    # if index is not the largest, make it the largest!
+    # and run it again!
+    if(largest_num_index != index):
+        arr[index], arr[largest_num_index] = arr[largest_num_index], arr[index]
+        heapify(arr, length, largest_num_index)
 
-    # Build max heap
-    for i in range(n//2, -1, -1):
-        heapify(arr, n, i)
 
-    for i in range(n-1, 0, -1):
-        # Swap
-        arr[i], arr[0] = arr[0], arr[i]
+def heap_sort(arr):
+    # need array length to create indices
+    length = len(arr)
 
-    # Heapify root element
-        heapify(arr, i, 0)
+    for index in range(length, -1, -1):
+        # ask about functions modifying arrays without return value
+        # build max heap
+        heapify(arr, length, index)
 
-arr = [int(input()) for i in range(int(input("Enter elements to be sorted : ")))] 
-heapSort(arr)
-n = len(arr)
-print("Sorted array:")
-for i in range(n):
-    print("%d " % arr[i], end='')
-    
+    # for each sorted heap, swap the root and the last number
+    for index in range(length - 1, 0, -1):
+        arr[index], arr[0] = arr[0], arr[index]
+        # then call heapify again with the new array
+        heapify(arr, index, 0)
+
+
+# Taking Elements to be Sorted
+data = [int(input())
+        for i in range(int(input("Enter elements to be sorted : ")))]
+# Sending element to get sorted
+heap_sort(data)
+# Priting Elements after Getting Sorted
+print(data)
+
 '''
 Sample Output:
-
-Enter elements to be sorted : 8
-5
-4
-9
-82
-65
-79
-97
-25
-Sorted array is
-4 5 9 25 65 79 82 97 
+Enter elements to be sorted : 5
+30
+50
+10
+20
+40
+[10, 20, 30, 40, 50]
 '''

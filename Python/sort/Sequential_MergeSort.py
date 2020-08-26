b82ab6e9f076fec3d9686d9d7906b194a483dba7
Pinky <50888480+PinkyWalia@users.noreply.github.com>
2020-08-27 00:48:09 +0530

    Update Sequential_MergeSort.py

--- Python/sort/Sequential_MergeSort.py
+++ Python/sort/Sequential_MergeSort.py
@@ -4,16 +4,16 @@
 import random
 
 """ sequential implementation of merge sort """
-def seq_mergesort(array, *args):
+def sequential_mergesort(array, *args):
     if not args: # first call
-        seq_mergesort(array, 0, len(array)-1)
+        sequential_mergesort(array, 0, len(array)-1)
         return array # return the sorted array
     else: # recursive call
         left, right = args
         if (left < right):
-            mid = (left + right) // 2 # find the middle point
-            seq_mergesort(array, left, mid) # sort the left half
-            seq_mergesort(array, mid+1, right) # sort the right half
+            mid = left + ((right - left) // 2) # find the middle point
+            sequential_mergesort(array, left, mid) # sort the left half
+            sequential_mergesort(array, mid+1, right) # sort the right half
             merge(array, left, mid, right) # merge the two sorted halves
 
 """ helper method to merge two sorted subarrays
@@ -22,12 +22,10 @@ def merge(array, left, mid, right):
     # copy data to temp subarrays to be merged
     left_temp_arr = array[left:mid+1].copy()
     right_temp_arr = array[mid+1:right+1].copy()
-
     # initial indexes for left, right and merged subarrays
     left_temp_index = 0
     right_temp_index = 0
     merge_index = left
-
     # merge temp arrays into original
     while (left_temp_index < (mid - left + 1) or right_temp_index < (right - mid)):
         if (left_temp_index < (mid - left + 1) and right_temp_index < (right - mid)):
@@ -45,26 +43,26 @@ def merge(array, left, mid, right):
             right_temp_index += 1
         merge_index += 1
 
-
 if __name__ == '__main__':
     NUM_EVAL_RUNS = 1
     userRange = int(input("Enter your range for generating random array: ")) 
-    
     print('Generating Random Array... ')
     array = [random.randint(0,1000) for i in range(userRange)]
-
     strRandomArray = ','.join([str(i) for i in array])
-
     print('Random Array to Sort is : ' + strRandomArray)
-
     print('Evaluating Sequential Implementation...')
-    sequential_result = seq_mergesort(array.copy())
+    sequential_result = sequential_mergesort(array.copy())
     for i in range(NUM_EVAL_RUNS):
-        seq_mergesort(array.copy())
-
+        sequential_mergesort(array.copy())
     strfinalSortResult =','.join([str(i) for i in sequential_result])
-
     print('final sorted array is : '+ strfinalSortResult)
 
-    
+    # input/output sample
+    # Enter your range for generating random array: 10
+    # Generating Random Array...
+    # Random Array to Sort is : 1807,7508,7166,7673,2000,1356,7665,4670,9395,6002
+    # Evaluating Parallel Implementation...
+    # final sorted array is : 1356,1807,2000,4670,6002,7166,7508,7665,7673,9395
+    # Time Complexity: O(nLog(n)) in worst case
+	#Space Complexity: O(n) in worst case
     

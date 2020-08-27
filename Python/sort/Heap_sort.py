498d772011f524d011b851f082f5af76db7b8645
Avinash Kr. Ranjan <ranjan.avinash@hotmail.com>
2020-08-27 15:24:47 +0530

    Added time and space complexity

--- Python/sort/Heap_sort.py
+++ Python/sort/Heap_sort.py
@@ -1,19 +1,66 @@
-def heapit(nums, heap_size, root_index):
-    largest = root_index
-    left_child = (2 * root_index) + 1
-    right_child = (2 * root_index) + 2
-    if left_child < heap_size and nums[left_child] > nums[largest]:
-        largest = left_child
-    if right_child < heap_size and nums[right_child] > nums[largest]:
-        largest = right_child
-    if largest != root_index:
-        nums[root_index], nums[largest] = nums[largest], nums[root_index]
-        heapit(nums, heap_size, largest)
-def heap_sort(nums):
-    n = len(nums)
-    for i in range(n, -1, -1):
-        heapit(nums, n, i)
+# Python program for implementation of heap Sort
 
-    for i in range(n - 1, 0, -1):
-        nums[i], nums[0] = nums[0], nums[i]
-        heapit(nums, i, 0)
+# To heapify subtree rooted at index.
+# length is size of heap
+
+def heapify(arr, length, index):
+    # base case
+    # we will call this function until the largest number is the index...
+    largest_num_index = index
+    left_index = (index * 2) + 1
+    right_index = (index * 2) + 2
+
+    if(left_index < length and arr[index] < arr[left_index]):
+        largest_num_index = left_index
+
+    if(right_index < length and arr[largest_num_index] < arr[right_index]):
+        largest_num_index = right_index
+
+    # if index is not the largest, make it the largest!
+    # and run it again!
+    if(largest_num_index != index):
+        arr[index], arr[largest_num_index] = arr[largest_num_index], arr[index]
+        heapify(arr, length, largest_num_index)
+
+
+def heap_sort(arr):
+    # need array length to create indices
+    length = len(arr)
+
+    for index in range(length, -1, -1):
+        # ask about functions modifying arrays without return value
+        # build max heap
+        heapify(arr, length, index)
+
+    # for each sorted heap, swap the root and the last number
+    for index in range(length - 1, 0, -1):
+        arr[index], arr[0] = arr[0], arr[index]
+        # then call heapify again with the new array
+        heapify(arr, index, 0)
+
+
+# Taking Elements to be Sorted
+data = [int(input())
+        for i in range(int(input("Enter elements to be sorted : ")))]
+# Sending element to get sorted
+heap_sort(data)
+# Priting Elements after Getting Sorted
+print(data)
+
+'''
+Sample Output:
+Enter elements to be sorted : 5
+30
+50
+10
+20
+40
+[10, 20, 30, 40, 50]
+
+Building the max heap requires O(n) calls to the buildHeap function which
+takes O(log n) time to build the max heap
+Therefore, total time complexity = O(nlog n)
+In heap sort since we are only keeping data in the array and only
+rearranging it so no data needs to be stored anywhere else (other than the swap step)
+So, space complexity = O(1)
+'''

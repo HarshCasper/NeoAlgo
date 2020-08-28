581c025347045c1f414127720c2bf392562b1618
Rajiv Singh <rajivperfect007@gmail.com>
2020-08-28 20:27:21 +0530

    formatted code

--- C-Plus-Plus/sort/Bubble_Sort.cpp
+++ C-Plus-Plus/sort/Bubble_Sort.cpp
@@ -1,27 +1,47 @@
-#include<iostream>			//Header file
-using namespace std;		//For cin and cout
-int main(){
-	int n, arr[n], num, temp;			//Declarartion of required variables
-	cout<<"Enter the number of elements of the array: ";
-	cin>>n;			//Getting the number of elements of the array from the user
-	cout<<endl<<"Enter the elements of the array to be sorted: ";
-	for (int i = 0; i < n; i++){			//For loop for taking elements of the array from the user, can be space-seperated as well
-		cin>>num;
-		arr[i] = num;
-	}
-	for (int i = 0; i < n-1; i++){			//Outer for loop
-		for(int j = 0; j < n-1; j++){		//Inner for loop
-			if(arr[j] > arr[j+1]){			//Checking if the element if larger than the next element
-				temp = arr[j+1];			//Swapping the elements, temp is used to store the temporary variable
-				arr[j+1] = arr[j];
-				arr[j] = temp; 
-				}
-			}
-	}
-	cout<<"Array after sorting is:"<<endl;
-	for(int i = 0; i < n; i++){			//For loop for printing out the array after sorting
-		cout<<arr[i]<<" ";
-		}
+#include <iostream>  //Header file
+using namespace std; //For cin and cout
 
-return 0;
+int main()
+{
+  int n; //Declarartion of required variables
+  cout << "Enter the number of elements of the array: ";
+  cin >> n; //Getting the number of elements of the array from the user
+  int arr[n], num, temp;
+  cout << endl;
+  cout << "Enter the elements of the array to be sorted: ";
+  for (int i = 0; i < n; i++)
+  {
+    //For loop for taking elements of the array from the user, can be space-seperated as well
+    cin >> num;
+    arr[i] = num;
+  }
+  for (int i = 0; i < n - 1; i++)
+  {
+    //Outer for loop
+    int sorted = 0; // declaring a sorted variable which will check if array is already sorted or not
+    for (int j = 0; j < n - 1; j++)
+    {
+      //Inner for loop
+      if (arr[j] > arr[j + 1])
+      {
+        //Checking if the element if larger than the next element
+        temp = arr[j + 1]; //Swapping the elements, temp is used to store the temporary variable
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        sorted = 1; // sorted value will be updated to 1 if there is swapping of elements occured
+      }
+    }
+    if (sorted == 0)
+    {
+      // If the elements aren't swapped and int sorted = 0 only then array is already sorted
+      break;
+    }
+  }
+  cout << "Array after sorting is:" << endl;
+  for (int i = 0; i < n; i++)
+  {
+    //For loop for printing out the array after sorting
+    cout << arr[i] << " ";
+  }
+  return 0;
 }

92e98c0eeec3ec8c54f6a2abd596f711238de0d2
Anubhav Sharma <anubhvshrma18@gmail.com>
2020-08-28 23:45:44 +0530

    Updated the requested changes

--- C-Plus-Plus/sort/Bubble_Sort.cpp
+++ C-Plus-Plus/sort/Bubble_Sort.cpp
@@ -4,22 +4,22 @@ int main(){
 	int n, arr[n], num, temp;			//Declarartion of required variables
 	cout<<"Enter the number of elements of the array: ";
 	cin>>n;			//Getting the number of elements of the array from the user
-	cout<<endl<<"Enter the elements of the array to be sorted: ";
+	cout<<endl<<"Enter the elements of the array to be sorted: " <<"\n";
 	for (int i = 0; i < n; i++){			//For loop for taking elements of the array from the user, can be space-seperated as well
 		cin>>num;
 		arr[i] = num;
 	}
 	for (int i = 0; i < n-1; i++){			//Outer for loop
-		int sorted =0; // declaring a sorted variable which will check if array is already sorted or not
-		for(int j = 0; j < n-1; j++){		//Inner for loop
+		bool sorted = true; // declaring a sorted variable which will check if array is already sorted or not
+		for(int j = 0; j < n-1-i; j++){		//Inner for loop
 			if(arr[j] > arr[j+1]){			//Checking if the element if larger than the next element
 				temp = arr[j+1];			//Swapping the elements, temp is used to store the temporary variable
 				arr[j+1] = arr[j];
 				arr[j] = temp;
-				sorted = 1; // sorted value will be updated to 1 if there is swapping of elements occured 
+				sorted = false; // sorted value will be updated to false if there is swapping of elements occured 
 				}
 			}
-		if(sorted ==0){ // If the elements aren't swapped and int sorted =0 only then array is already sorted
+		if(sorted){ // If the elements aren't swapped and sorted is true only then array is already sorted
 			break;
 		}
 	}
@@ -30,3 +30,25 @@ int main(){
 
 return 0;
 }
+/*
+// Input
+Enter the number of elements of the array: 5
+Enter the elements of the array to be sorted:
+1
+2
+3
+6
+5
+
+// Ouput
+Array after sorting is:
+1 2 3 5 6
+
+Time complexities:
+	Worst Case:O(n2) -When we have to sort the array in opposite order as 
+					 compared to given like ascending to descending. 
+    Best Case:O(n)   - When the array is already sorted.
+    Average Case:O(n2) - When elements are in jumbled order.
+
+Space Complexity: Auxilary Space:O(2) because of variable temp and sorted.  
+*/

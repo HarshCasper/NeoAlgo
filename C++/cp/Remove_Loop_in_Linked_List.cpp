2c37f916dfafce9751e7dd78dcb1fe06fa016265
Abhinav Anand <manasgmishra@gmail.com>
2020-08-29 22:18:45 +0530

    formatted the code.

--- C++/cp/Remove_Loop_in_Linked_List.cpp
+++ C++/cp/Remove_Loop_in_Linked_List.cpp
@@ -1,6 +1,5 @@
-/*You are given a linked list of N nodes. The task is to remove the loop from the linked list, if present.*/
+/*You are given a linked list of N nodes. The task is to remove the loop from the linked list, if present.
 
-/*
 Example:
 Input:
 2
@@ -15,7 +14,8 @@ Output:
 1
 
 Explanation:
-Testcase 1: In the first test case N = 3.The linked list with nodes N = 3 is given. Here, x = 2 which means last node is connected with xth node of linked list. Therefore, there exists a loop.
+Testcase 1: In the first test case N = 3.The linked list with nodes N = 3 is given.
+Here, x = 2 which means last node is connected with xth node of linked list. Therefore, there exists a loop.
 Testcase 2: N = 4 and x = 0, which means lastNode->next = NULL, thus the Linked list does not contains any loop.
 */
 

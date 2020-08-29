21b033fb5cbef798889c591087396498e32655cc
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-08-29 17:04:31 +0530

    Update Tower_Of_Hanoi.c

--- C/other/Tower_Of_Hanoi.c
+++ C/other/Tower_Of_Hanoi.c
@@ -12,8 +12,31 @@ void TowerOfHanoi(int n,int a,int b,int c){
 
 int main(){
     int n;
+    printf("Enter the value of n: ");
     scanf("%d",&n);
-    printf("The value of n is: %d\n", n);
+    printf("\nThe value of n is: %d\n", n);
     TowerOfHanoi(n,1,2,3);
     return 0;
 }
+
+/*
+OUTPUT
+Enter the value of n: 4
+The value of n is: 4
+1 -> 3
+1 -> 2
+3 -> 2
+1 -> 3
+2 -> 1
+2 -> 3
+1 -> 3
+1 -> 2
+3 -> 2
+3 -> 1
+2 -> 1
+3 -> 2
+1 -> 3
+1 -> 2
+3 -> 2
+
+*/

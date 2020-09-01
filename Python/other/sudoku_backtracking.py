18ee01513ea74f39c485d2efc70762b0ef2d0056
Rupesh Gelal <rupeshgelal9@gmail.com>
2020-09-01 14:44:10 +0545

    deleted settings.json and added dynamic input

--- Python/other/sudoku_backtracking.py
+++ Python/other/sudoku_backtracking.py
@@ -1,13 +1,3 @@
-board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0], 
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
-
 
 def solve(board, i=0, j=0):
         i,j = nextCell(board, i, j)
@@ -57,7 +47,10 @@ def isValid(board,x,y,n):
             if board[new_x + i][new_y + j] == n:
                 return False
 
-    return True        
+    return True 
 
-solve(board)  
-print_board(board)  
+if __name__ == "__main__":
+    print("Enter the numbers row by row, and put 0 for empty space:")
+    board = [[int(input()) for x in range (9)] for y in range(9)] 
+    solve(board)  
+    print_board(board)

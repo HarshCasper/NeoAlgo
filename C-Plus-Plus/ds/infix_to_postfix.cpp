2d7c1a2c2135c8056deb5430cb620e64fa4f52db
Prasad V Patil <69467300+prasadvpatil@users.noreply.github.com>
2020-09-03 14:38:48 +0530

    Update infix_to_postfix.cpp

--- C-Plus-Plus/ds/infix_to_postfix.cpp
+++ C-Plus-Plus/ds/infix_to_postfix.cpp
@@ -1,4 +1,4 @@
-# Auhor: Prasad V Patil
+# Author: Prasad V Patil
 
 
 #include<iostream>
@@ -7,16 +7,23 @@
 
 using namespace std;
 
+// Function to convert Infix expression to postfix
 string InfixToPostfix(string expression);
 
+// Function to verify whether an operator has higher precedence over other
 int HasHigherPrecedence(char operator1, char operator2);
 
+// Function to verify whether a character is operator symbol or not.
 bool IsOperator(char C);
 
+// Function to verify whether a character is alphanumeric chanaracter
+//(letter or numeric digit) or not.
 bool IsOperand(char C);
 
+// Function to check whether a char is an opening parenthesis i.e '(' or '{' or '['
 bool IsOpeningParentheses(char C);
 
+// Function to check whether a char is an closing parenthesis i.e ')' or '}' or ']'
 bool IsClosingParentheses(char C);
 
 int main()
@@ -28,19 +35,27 @@ int main()
 	cout<<"Output = "<<postfix<<"\n";
 }
 
+// Function to evaluate Postfix expression and return output
 string InfixToPostfix(string expression)
 {
+	// Declaring a Stack from Standard template library in C++.
 	stack<char> S;
-	string postfix = "";
+	string postfix = ""; // Initialize postfix as empty string.
 	for(int i = 0;i< expression.length();i++) {
 
+		// Scanning each character from left.
+		// If character is a delimiter, move on.
 		if(expression[i] == ' ' || expression[i] == ',') continue;
 
+		// Else if character is an operand then just append it in res string
 		else if(IsOperand(expression[i]))
 		{
 			postfix +=expression[i];
 		}
 
+		//If character is operator, check for Higher precedence operator in Stack top
+		//till first opening bracket and pop all such operators
+		//Finally push the current operator on the Stack
 		else if(IsOperator(expression[i]))
 		{
 			while(!S.empty() && !IsOpeningParentheses(S.top())
@@ -52,11 +67,14 @@ string InfixToPostfix(string expression)
 			S.push(expression[i]);
 		}
 
+		//If opening Parentheses simply push it on the Stack
 		else if(IsOpeningParentheses(expression[i]))
 		{
 			S.push(expression[i]);
 		}
 
+        //If Closing Parentheses then pop all the operators and append to res string
+        // until Stack top is opening Parentheses and finally one extra pop for Opening Parentheses
 		else if(IsClosingParentheses(expression[i]))
 		{
 			while(!S.empty() && !IsOpeningParentheses(S.top())) {
@@ -67,6 +85,7 @@ string InfixToPostfix(string expression)
 		}
 	}
 
+    //Finally pop and append all operators till Stack is not empty
 	while(!S.empty()) {
 		postfix += S.top();
 		S.pop();
@@ -75,6 +94,8 @@ string InfixToPostfix(string expression)
 	return postfix;
 }
 
+// Function to verify whether a character is english letter or numeric digit.
+// We are assuming in this solution that operand will be a single character
 bool IsOperand(char C)
 {
 	if(C >= '0' && C <= '9') return true;
@@ -83,6 +104,7 @@ bool IsOperand(char C)
 	return false;
 }
 
+// Function to verify whether a character is operator symbol or not.
 bool IsOperator(char C)
 {
 	if(C == '+' || C == '-' || C == '*' || C == '/' || C== '^')
@@ -104,12 +126,14 @@ bool IsClosingParentheses(char C)
         return true ;
     return false;
 }
+// Function to verify whether an operator is right associative or not.
 int IsRightAssociative(char op)
 {
 	if(op == '^') return true;
 	return false;
 }
 
+// Function to get weight of an operator. An operator with higher weight will have higher precedence.
 int GetOperatorWeight(char op)
 {
 	int weight = -1;
@@ -130,11 +154,15 @@ int GetOperatorWeight(char op)
 	return weight;
 }
 
+// Function to perform an operation and return output.
 int HasHigherPrecedence(char op1, char op2)
 {
 	int op1Weight = GetOperatorWeight(op1);
 	int op2Weight = GetOperatorWeight(op2);
 
+	// If operators have equal precedence, return true if they are left associative.
+	// return false, if right associative.
+	// if operator is left-associative, left one should be given priority.
 	if(op1Weight == op2Weight)
 	{
 		if(IsRightAssociative(op1)) return false;

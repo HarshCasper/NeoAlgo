02640cc5d46c69eed749e8cb7dbace8626397540
Pooja Rathore <poojarathore.nks@gmail.com>
2020-09-06 11:08:04 +0530

    Update maxsum_subarray_python.py

--- Python/cp/maxsum_subarray_python.py
+++ Python/cp/maxsum_subarray_python.py
@@ -1,56 +1,41 @@
-
 # Returns sum of maxium sum subarray in arr[l..h]
-def maxSumSubArray(arr, l, h) :
-
+def maxSumSubArray(arr, low, high) :
     # Base Case
-    if (l == h) :
-        return arr[l]
+    if (low == high) :
+        return arr[low]
     # Find middle element
-    m = (l + h) // 2
-
+    mid = (low + high) // 2
     # Return maximum of left ,right and mid crossing
-    return max(maxSumSubArray(arr, l, m),
-               maxSumSubArray(arr, m+1, h),
-               maxSumCrossing(arr, l, m, h))
+    return max(maxSumSubArray(arr, low, mid),
+               maxSumSubArray(arr, mid+1, high),
+               maxSumCrossing(arr, low, mid, high))
 
 # Find the maximum possible sum in arr[] such that arr[m] is part of it
-def maxSumCrossing(arr, a, m, h) :
-
+def maxSumCrossing(arr, low, mid, high) :
     # Include elements on left of mid.
-    sm = 0; left_sum = -10000
-
-    for i in range(m, a-1, -1) :
-        sm = sm + arr[i]
-
-        if (sm > left_sum) :
-            left_sum = sm
-
-
+    summ = 0; left_sum = -10000
+    for i in range(mid, low-1, -1) :
+        summ = summ + arr[i]
+        if (summ > left_sum) :
+            left_sum = summ
     # Include elements on right of mid
-    sm = 0; right_sum = -1000
-    for i in range(m + 1, h + 1) :
-        sm = sm + arr[i]
-
-        if (sm > right_sum) :
-            right_sum = sm
-
-
-    # Return sum of elements on left and right of mid returning only left_sum + right_sum will fail for [-2, 1]
+    summ = 0; right_sum = -1000
+    for i in range(mid + 1, high + 1) :
+        summ = summ + arr[i]
+        if (summ > right_sum) :
+            right_sum = summ
     return max(left_sum + right_sum, left_sum, right_sum)
 
 # Driver Code
 # creating an empty list
-lst = []
-
+listt = []
 # number of elemetns as input
 n = int(input("Enter number of elements in arr : "))
-
 # iterating till the range
 for i in range(0, n):
-    ele = int(input())
-    lst.append(ele) # adding the element
-
-print("input array is : "lst)
-n = len(lst)
-max_sum = maxSumSubArray(lst, 0, n-1)
+    element = int(input())
+    listt.append(element) # adding the element
+print("input array is : ",listt)
+n = len(listt)
+max_sum = maxSumSubArray(listt, 0, n-1)
 print("Maximum contiguous sum is ", max_sum)

5da7d06fb0ce64b786ea9fbe7a4a2893511aa8db
narayan2111 <narayandwivedi076@gmail.com>
2020-09-06 12:39:33 +0530

    Formated the code with PEP-8 rules

--- Python/sort/Shell_Sort.py
+++ Python/sort/Shell_Sort.py
@@ -1,48 +1,47 @@
-# Python program for implementation of Shell Sort 
-
-def shellSort(arr): 
-
-	# Start with a big gap, then reduce the gap 
-	size = len(arr) 
-	gap = size//2
-
-	# Do a gapped insertion sort for this gap size.  
-	while gap > 0: 
-
-		for i in range(gap,size): 
-
-			# add a[i] to the elements that have been gap sorted 
-			# save a[i] in temp and make a hole at position i 
-			temp = arr[i] 
-
-			# shift earlier gap-sorted elements up until the correct 
-			# location for a[i] is found 
-			j = i 
-			while j >= gap and arr[j-gap] >temp: 
-				arr[j] = arr[j-gap] 
-				j -= gap 
-
-			# put temp (the original a[i]) in its correct location 
-			arr[j] = temp 
-		gap //= 2
+# Python program for implementation of Shell Sort
 
 
-# Driver code to test above 
+def shellSort(arr):
+
+    # Start with a big gap, then reduce the gap
+
+    size = len(arr)
+    gap = size // 2
+
+    # Do a gapped insertion sort for this gap size.
+
+    while gap > 0:
+
+        for i in range(gap, size):
+
+            # add a[i] to the elements that have been gap sorted
+            # save a[i] in temp and make a hole at position i
+
+            temp = arr[i]
+
+            # shift earlier gap-sorted elements up until the correct
+            # location for a[i] is found
+
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+
+            # put temp (the original a[i]) in its correct location
+
+            arr[j] = temp
+        gap //= 2
+
 arr = []
 
-size = int(input("Enter size: "))
-print("Enter elements:")
-for i in range(0,size):
-	item = int(input())
-	arr.append(item)
+size = int(input('Enter size: '))
+print ('Enter elements:')
+for i in range(0, size):
+    item = int(input())
+    arr.append(item)
 
-print ("Array before sorting:") 
-for i in range(size): 
-	print(arr[i],end = ' ')
+shellSort(arr)
 
-shellSort(arr) 
-
-print ("\nArray after sorting:") 
-for i in range(size): 
-	print(arr[i],end = ' ') 
-
+print ('\nSorted Array:')
+for i in range(size):
+    print (arr[i])

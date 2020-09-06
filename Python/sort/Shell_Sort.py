234097ea00495baf2dbf6de856ecb8c10f47864f
narayan2111 <narayandwivedi076@gmail.com>
2020-09-06 12:20:57 +0530

    Formated the code with PEP-8 rules

--- Python/sort/Shell_Sort.py
+++ Python/sort/Shell_Sort.py
@@ -1,32 +1,33 @@
-# Python program for implementation of Shell Sort
+# Python program for implementation of Shell Sort 
 
-def shellsort(arr):
+def shellSort(arr): 
 
-	# Start with a big gap, then reduce the gap
-	size = len(arr)
+	# Start with a big gap, then reduce the gap 
+	size = len(arr) 
 	gap = size//2
 
-	# Do a gapped insertion sort for this gap size. 
-	while gap > 0:
+	# Do a gapped insertion sort for this gap size.  
+	while gap > 0: 
 
-		for iteration in range(gap,size):
+		for i in range(gap,size): 
 
-			# save a[i] in temp
-			temp = arr[iteration]
+			# add a[i] to the elements that have been gap sorted 
+			# save a[i] in temp and make a hole at position i 
+			temp = arr[i] 
 
-			#shift earlier gap-sorted elements up until the correct(new)
-			# location for a[i] is found
-			new_loc = iteration
-
-			while new_loc>= gap and arr[new_loc-gap] > temp:
-				arr[new_loc] = arr[new_loc-gap]
-				new_loc -= gap
+			# shift earlier gap-sorted elements up until the correct 
+			# location for a[i] is found 
+			j = i 
+			while j >= gap and arr[j-gap] >temp: 
+				arr[j] = arr[j-gap] 
+				j -= gap 
 
 			# put temp (the original a[i]) in its correct location 
-			arr[new_loc] = temp
+			arr[j] = temp 
 		gap //= 2
 
-#input array
+
+# Driver code to test above 
 arr = []
 
 size = int(input("Enter size: "))
@@ -35,25 +36,13 @@ for i in range(0,size):
 	item = int(input())
 	arr.append(item)
 
-shellsort(arr)
-
-#array after sorting
-print("\nArray after sorting:")
-for i in range(size):
+print ("Array before sorting:") 
+for i in range(size): 
 	print(arr[i],end = ' ')
 
+shellSort(arr) 
 
-'''
-Sample I/O
-	Input:
-	
-	Enter Size : 5
-	Enter Elements: 5 4 3 2 1
+print ("\nArray after sorting:") 
+for i in range(size): 
+	print(arr[i],end = ' ') 
 
-	Output:
-	Array After Sorting: 1 2 3 4 5
-
-Time Complexity: O(n*2)
-Space Complexity: O(1)
-
-'''

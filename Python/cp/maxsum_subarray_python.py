35ba5bd9e56b07ff362ac873e9ca8512a32ea186
Pooja Rathore <poojarathore.nks@gmail.com>
2020-09-07 10:40:29 +0530

    Added Sample test cases Issue #555

--- Python/cp/maxsum_subarray_python.py
+++ Python/cp/maxsum_subarray_python.py
@@ -6,7 +6,7 @@ def maxSumSubArray(arr, low, high) :
     if (low == high) :
         return arr[low]
     # Find middle element
-    mid = low + ((high - low) / 2)
+    mid = low + ((high - low) // 2)
     # Return maximum of left ,right and mid crossing
     return max(maxSumSubArray(arr, low, mid),
                maxSumSubArray(arr, mid+1, high),
@@ -41,3 +41,7 @@ print("input array is : ",listt)
 n = len(listt)
 max_sum = maxSumSubArray(listt, 0, n-1)
 print("Maximum contiguous sum is ", max_sum)
+
+#sample test case 
+#input array : [1, 3, 2, 4, 7]     [1,-1,2,3,-2]    [0,1,-1,0,0]     [2]     [-1,-1,-1,-1,-1]
+#output      :  17                  5                1                2       -1

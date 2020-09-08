b9c08d8a97c2beba17a3f48e4e7660160c8c8f69
Rutuja Dhanawade <53823042+rutujadhanawade@users.noreply.github.com>
2020-09-08 18:28:10 +0530

    Add files via upload

---
+++ C-Plus-Plus/other/merge_sort_without_extra_space.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int m, n;
+  cout << "Enter the total number of elements in array1 and array2 respectively\n";
+  cin >> m >> n;
+
+  int arr1[m], arr2[n];
+  cout << "enter 1st array elements\n";
+  for (int i = 0; i < m; i++) {
+    cin >> arr1[i];
+  }
+  cout << "enter 2nd array elements\n";
+  for (int i = 0; i < n; i++) {
+
+    cin >> arr2[i];
+  }
+
+  for (int i = m - 1, j = 0; i >= 0 && j < n; i--, j++) {
+    int temp;
+    if (arr1[i] > arr2[j]) {
+      temp = arr1[i];
+      arr1[i] = arr2[j];
+      arr2[j] = temp;
+    }
+  }
+
+  sort(arr1, arr1 + m);
+  sort(arr2, arr2 + n);
+  cout << "answer: ";
+  for (int i = 0; i < m; i++) {
+    cout << arr1[i] << " ";
+  }
+  for (int i = 0; i < n; i++) {
+    cout << arr2[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+
+/*
+
+Time complexity : NlogN (where N will be max of n and m)
+
+Sample output:
+Enter the total number of elements in array1 and array2 respectively
+3 4
+enter 1st array elements
+2 4 6
+enter 2nd array elements
+1 3 5 7
+answer: 1 2 3 4 5 6 7
+
+*/

c351402fd6a9f022bba7700998c9ef3d5af5d2e1
Naman-1234 <namankalrabhiwani54@gmail.com>
2020-09-08 18:05:19 +0530

    Add some changes as told by the maintainer

--- Python/ds/doubly_linked_list.py
+++ Python/ds/doubly_linked_list.py
@@ -22,7 +22,7 @@ class doublylist:
 
 
 
-    def length(self):
+    def count_nodes(self):
         temp=self.head
         count=0
         while(temp is not None):
@@ -37,7 +37,7 @@ class doublylist:
         # while(iteration is not None):
         #     count+=1
         #     iteration=iteration.next
-        count=self.length()
+        count=self.count_nodes()
         if pos==0:
             temp=Node(ele)
             temp.next=self.head
@@ -111,10 +111,37 @@ class doublylist:
 
 if __name__=="__main__":
     obj=doublylist()
-    obj.insertlast(1)
-    obj.insertlast(2)
-    obj.insertlast(3)
-    obj.insertlast(4)
-    obj.head=obj.reverse(obj.head)
-    obj.delete_at_pos(3)
-    obj.display()
+    while(True):
+        choice =int(input("Enter your choice,To  know more about the choices press 8"))
+        if choice ==1:
+            pos,value=map(int,input("Enter position and value u want to insert").split())
+            obj.insert_at_pos(pos,value)
+        elif choice==2:
+            value=int(input("Enter  value u want to insert"))
+            obj.insertlast(value)
+        elif choice ==3:
+            pos=int(input("Enter position at which u want to delete"))
+            obj.delete_at_pos(pos)
+        elif choice ==4:
+            obj.delete_last()
+        elif choice ==5:
+            obj.head=obj.reverse(obj.head)
+        elif choice ==6:
+            obj.count_nodes()
+        elif choice ==7:
+            obj.display()
+        elif choice ==8:
+            print('''So what do u want to perform
+            case 1: Insertion in the linked list
+                    For insertion at a particular position press 1
+                    For always inserting at end press 2
+            case 2: \nDeletion in the linked list
+                    For deletion at a particular position press 1
+                    For always deleting at end press 2
+            case 3: \nFor reversing the linked list press 5
+            case 4:\n  For getting the length of linked list press 6
+            case 5:\n For printing the linked list press 7
+            case 6:press 9 for exit()
+            ''')
+        elif choice ==9:
+            quit()

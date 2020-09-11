6ba89df81bdde5d2f7a158cf8c970347d30b102c
Aniket Singh <thor98571@gmail.com>
2020-09-11 08:33:55 +0530

    Added required statement in Shellsort.cpp #387

--- C-Plus-Plus/sort/Shellsort.cpp
+++ C-Plus-Plus/sort/Shellsort.cpp
@@ -30,6 +30,7 @@ int main(){
     cout<<"Enter the size of the array: ";
     cin>>size;
     int sort[size];
+    cout<<"Enter the Elements to be sorted:";
     for(int i=0;i<size;i++){
         cin>>sort[i];
     }

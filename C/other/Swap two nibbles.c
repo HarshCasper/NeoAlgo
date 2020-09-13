f5ce0f20c0808bc6ea0a8b7e98400aee0ebb544b
atul goyal <goyalatul278@gmail.com>
2020-09-13 18:30:07 +0530

    swap two nibbles

---
+++ C/other/Swap two nibbles.c
@@ -0,0 +1,25 @@
+//algorithm for swap nibbles in O(1) time complexity and O(1) space complexity
+
+#include <stdio.h>
+
+int swapnibbles(int num)
+{
+    return (((num & 0x0F) << 4) | ((num & 0xF0) >> 4));
+}
+
+int main()
+{   
+    //taking input
+    int num;
+    printf(" Enter a number : \n");
+    scanf("%d",&num);
+    int result = swapnibbles(num);
+    printf(" %d\n",result);
+}
+
+/*
+Input:
+Enter a number: 50
+Output: 
+35
+*/ 

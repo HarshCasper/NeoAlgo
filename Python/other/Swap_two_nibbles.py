bbb8b1b41658b469eab99fddf300aa6b59fb0f74
narayan2111 <narayandwivedi076@gmail.com>
2020-09-13 19:54:12 +0530

    Added Swap two nibbles

---
+++ Python/other/Swap_two_nibbles.py
@@ -0,0 +1,23 @@
+# python program Swap two nibbles in a byte 
+
+#function to swap nibbles
+def swapNibbles(number): 
+	return ( (number & 0x0F)<<4 | (number & 0xF0)>>4 ) 
+
+number = int(input("Enter Number: "))
+print("After swapping the nibbles:")
+print(swapNibbles(number)) 
+
+'''
+Sample I/O:
+	Input
+	Enter Number: 100
+
+	Output:
+	After swapping the nibbles:                                                                                                    
+	70   
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+
+'''

1f28f35faac0b60a0c79e01a09df7f09c853b4ec
Divyang Bagla <bagladivyang03@gmail.com>
2020-09-15 11:52:34 +0530

    comments issue solved

--- C/cp/doublefactorial.c
+++ C/cp/doublefactorial.c
@@ -1,5 +1,6 @@
-#Double factorial of a non-negative integer n is defined as the product of all the integers from 1 to n that have the same parity (odd or even) as n.
-#It is also called as semifactorial of a number and is denoted by n!!
+/*Double factorial of a non-negative integer n is defined as the product of all the integers from 1 to n that have the same parity (odd or even) as n.
+It is also called as semifactorial of a number and is denoted by n!!
+*/
 
 #include <stdio.h>
 int doublefactorial(int n){

ca5fb4e0aab3021e29db065ca5b6d289f4e4ae72
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 01:18:58 +0530

    Update Fibonacci.py

--- Python/math/Fibonacci.py
+++ Python/math/Fibonacci.py
@@ -3,14 +3,14 @@
  n takes the input of first n numbers to be printed
 """
 def fibonaccinumber(n):
-	"""
+    """
     >>fibonaccinumber(6)
     1 1 2 3 5 8 
 
     >>fibonaccinumber(10)
     1 1 2 3 5 8 13 21 34 55
 
-	"""
+    """
     a,b=0,1
     if n<1:
         return

d0287f5a0cb0944a2cf2ca94cf6ac07d477fd7da
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 01:16:54 +0530

    Update Depth_Of_Tree.py

--- Python/graphs/Depth_Of_Tree.py
+++ Python/graphs/Depth_Of_Tree.py
@@ -2,38 +2,38 @@
 #### Note that the levels start from 0(Zero).
 
 class TreeNode:
-	def __init__(self, val):
-		self.val = val
-		self.left = None
-		self.right = None
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
 
 class Tree:
-	def __init__(self, root):
-		self.root = TreeNode(root)
+    def __init__(self, root):
+        self.root = TreeNode(root)
 
 
-	def get_node_depth(self, node, val, depth):
-		if node == None:
-			return -1
+    def get_node_depth(self, node, val, depth):
+        if node == None:
+            return -1
 
-		if node.val ==  val:
-			return depth
+        if node.val ==  val:
+            return depth
 
-		l_depth = self.get_node_depth(node.left, val, depth+1)
-		r_depth = self.get_node_depth(node.right, val, depth+1)
+        l_depth = self.get_node_depth(node.left, val, depth+1)
+        r_depth = self.get_node_depth(node.right, val, depth+1)
 
 
-		return max(l_depth,r_depth)
+        return max(l_depth,r_depth)
 
-	def get_maximum_depth(self,node,depth):
-		if node == None:
-			return -1
+    def get_maximum_depth(self,node,depth):
+        if node == None:
+            return -1
 
-		l_depth = 1 + self.get_maximum_depth(node.left, depth+1)
-		r_depth = 1 + self.get_maximum_depth(node.right, depth+1)
+        l_depth = 1 + self.get_maximum_depth(node.left, depth+1)
+        r_depth = 1 + self.get_maximum_depth(node.right, depth+1)
 
-		return max(l_depth,r_depth)
+        return max(l_depth,r_depth)
 
 
 #### Creating a tree of the following structure:-
@@ -57,7 +57,7 @@ tree.root.right.right = TreeNode(7)
 
 ### Printing the depth of each node
 for i in range(1,8):
-	print(tree.get_node_depth(tree.root, i, 0)) # Should print : 0 1 1 2 2 2 2
+    print(tree.get_node_depth(tree.root, i, 0)) # Should print : 0 1 1 2 2 2 2
 
 ### Printing the maximum depth of the tree
 print("Maximum Depth: ", tree.get_maximum_depth(tree.root, 0)) # Should print: 2

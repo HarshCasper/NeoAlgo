f97f4d3eab2718e91ecb975504f86c1aeece1dd6
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 01:14:15 +0530

    Update Sparse_matrix_Operation.py

--- Python/ds/Sparse_matrix_Operation.py
+++ Python/ds/Sparse_matrix_Operation.py
@@ -2,27 +2,27 @@
 
 Input:
 
-	Enter the number of rows: 3
+    Enter the number of rows: 3
 
-	Enter the number of columns: 3
+    Enter the number of columns: 3
 
-	Enter the entries row wise:
-	1
-	1
-	0
-	0
-	0
-	0
-	0
-	2
-	0
+    Enter the entries row wise:
+    1
+    1
+    0
+    0
+    0
+    0
+    0
+    2
+    0
 
 Output:
 
-	Given matrix is a sparse matrix
-	[0, 0, 2]
-	[0, 1, 1]
-	[1, 1, 2]
+    Given matrix is a sparse matrix
+    [0, 0, 2]
+    [0, 1, 1]
+    [1, 1, 2]
 
 
 Time Complexity:O(noOfRows*noOfcolumns)
@@ -82,34 +82,34 @@ for i in range(0, matrix_rows):
 #Sparse Matrix definition  
 
 def sparse_matrix():
-	size = 0;
-	for i in range(ROW): 
-		for j in range(COLUMN): 
-			if (matrix[i][j] != 0): 
-				size += 1;
+    size = 0;
+    for i in range(ROW): 
+        for j in range(COLUMN): 
+            if (matrix[i][j] != 0): 
+                size += 1;
 
-	matrix_rows, matrix_cols = (3, size) 
-	Print_Matrix = [[0 for i in range(matrix_cols)] for j in range(matrix_rows)] 
+    matrix_rows, matrix_cols = (3, size) 
+    Print_Matrix = [[0 for i in range(matrix_cols)] for j in range(matrix_rows)] 
 
-	k = 0
-	for i in range(ROW): 
-		for j in range(COLUMN): 
-			if (matrix[i][j] != 0): 
-				Print_Matrix[0][k] = i 
-				Print_Matrix[1][k] = j 
-				Print_Matrix[2][k] = matrix[i][j] 
-				k += 1
+    k = 0
+    for i in range(ROW): 
+        for j in range(COLUMN): 
+            if (matrix[i][j] != 0): 
+                Print_Matrix[0][k] = i 
+                Print_Matrix[1][k] = j 
+                Print_Matrix[2][k] = matrix[i][j] 
+                k += 1
 
-	for i in Print_Matrix: 
-		print(i)
+    for i in Print_Matrix: 
+        print(i)
 
 
 if(count == size):
-	print("Null Mtrix");
+    print("Null Mtrix");
 
 elif(count > (size/2)):  
-	print("Given matrix is a sparse matrix");
-	sparse_matrix()   
+    print("Given matrix is a sparse matrix");
+    sparse_matrix()   
 
 else:  
     print("Given matrix is not a sparse matrix"); 

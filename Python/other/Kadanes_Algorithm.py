3ac87491c65380df030af1504fc9a68b0e716622
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 01:21:02 +0530

    Update Kadanes_Algorithm.py

--- Python/other/Kadanes_Algorithm.py
+++ Python/other/Kadanes_Algorithm.py
@@ -16,5 +16,5 @@ def kadane(nums):
 
 
 if __name__ == '__main__':
-	print(kadane([-2,-3,-1,-5]))             # Output: -1
-	print(kadane([-2,1,-3,4,-1,2,1,-5,4]))   # Output: 6
+    print(kadane([-2,-3,-1,-5]))             # Output: -1
+    print(kadane([-2,1,-3,4,-1,2,1,-5,4]))   # Output: 6

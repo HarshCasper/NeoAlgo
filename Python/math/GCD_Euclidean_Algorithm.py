e1b878d9d398d114cd1ec5ac2adb9d4d33a76caa
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 01:19:08 +0530

    Update GCD_Euclidean_Algorithm.py

--- Python/math/GCD_Euclidean_Algorithm.py
+++ Python/math/GCD_Euclidean_Algorithm.py
@@ -1,16 +1,16 @@
 def calculate_gcd(a, b):
 
-	while b:
+    while b:
 
-		a, b = b, a%b
+        a, b = b, a%b
 
-	return a
+    return a
 
 
 
 if __name__ == '__main__':
 
-	print(calculate_gcd(36, 20)) # Should print 4
-	print(calculate_gcd(16, 24)) # Should print 8
+    print(calculate_gcd(36, 20)) # Should print 4
+    print(calculate_gcd(16, 24)) # Should print 8
 
 

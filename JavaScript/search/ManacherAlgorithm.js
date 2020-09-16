b3eea2c4f2e8d687b0921c1041145e40efae6f7a
ritvij14 <ritvij2001@gmail.com>
2020-09-16 15:49:57 +0530

    Code styling updation

--- JavaScript/search/ManacherAlgorithm.js
+++ JavaScript/search/ManacherAlgorithm.js
@@ -9,49 +9,49 @@ const findPalindrome = (str) => {
   // i is our current center
   for (let i = 0; i < sequence.length;) {
 
-    // we check on both sides of i till where we can find same letters on 2
-    // positions
-    while (start > 0 && end < sequence.length - 1 && sequence[start - 1] === sequence[end + 1]) {
-      start--;
-      end++;
-    }
-
-    // getting the length of palindrome around current center
-    positions.splice(i, 0, end - start + 1);
-
-    // this is case 2. Current palindrome is proper suffix of input. Meaning input
-    // is the longest palindrome
-    if (end === (sequence.length - 1)) {
-      break;
-    }
-
-    // Mark newCenter to be either end or end + 1 depending on if we dealing with
-    // even or old number input.
-    let newCenter = end + (i % 2 == 0 ? 1 : 0);
-
-    for (let j = i + 1; j <= end; j++) {
-
-      // Its possible left mirror might go beyond current center palindrome. So take
-      // minimum of either left side palindrome or distance of j to end.
-      // T[j] = Math.min(T[i - (j - i)], 2 * (end - j) + 1);
-      num = positions[Math.abs(i - (j - i))];
-      positions.splice(j, 0, (Math.min(num, (2 * (end - j) + i))));
-
-      // This check is to make sure we do not pick j as new center. As soon as we find
-      // a center lets break out of this inner loop.
-      if (j + parseInt(positions[i - (j - i)] / 2) === end) {
-        newCenter = j;
-        break;
+      // we check on both sides of i till where we can find same letters on 2
+      // positions
+      while (start > 0 && end < sequence.length - 1 && sequence[start - 1] === sequence[end + 1]) {
+          start--;
+          end++;
       }
-    }
 
-    // make i as newCenter. Set right and left to atleast the value we already know
-    // should be matching based of left side palindrome.
-    i = newCenter;
-    if (i < positions.length) {
-      end = i + parseInt(positions[i] / 2);
-      start = i - parseInt(positions[i] / 2);
-    }
+      // getting the length of palindrome around current center
+      positions.splice(i, 0, end - start + 1);
+
+      // this is case 2. Current palindrome is proper suffix of input. Meaning input
+      // is the longest palindrome
+      if (end === (sequence.length - 1)) {
+          break;
+      }
+
+      // Mark newCenter to be either end or end + 1 depending on if we dealing with
+      // even or old number input.
+      let newCenter = end + (i % 2 == 0 ? 1 : 0);
+
+      for (let j = i + 1; j <= end; j++) {
+
+          // Its possible left mirror might go beyond current center palindrome. So take
+          // minimum of either left side palindrome or distance of j to end.
+          // T[j] = Math.min(T[i - (j - i)], 2 * (end - j) + 1);
+          num = positions[Math.abs(i - (j - i))];
+          positions.splice(j, 0, (Math.min(num, (2 * (end - j) + i))));
+
+          // This check is to make sure we do not pick j as new center. As soon as we find
+          // a center lets break out of this inner loop.
+          if (j + parseInt(positions[i - (j - i)] / 2) === end) {
+              newCenter = j;
+              break;
+          }
+      }
+
+      // make i as newCenter. Set right and left to atleast the value we already know
+      // should be matching based of left side palindrome.
+      i = newCenter;
+      if (i < positions.length) {
+          end = i + parseInt(positions[i] / 2);
+          start = i - parseInt(positions[i] / 2);
+      }
   }
 
   let max = Math.max.apply(null, positions);
@@ -60,9 +60,9 @@ const findPalindrome = (str) => {
   let pos1 = pos - parseInt(max / 2);
   let pos2 = pos + parseInt(max / 2);
   if (positions.length % 2 == 0) {
-    sequence = sequence.slice(pos1, pos2 + 1);
+      sequence = sequence.slice(pos1, pos2 + 1);
   } else {
-    sequence = sequence.slice(pos1, pos2);
+      sequence = sequence.slice(pos1, pos2);
   }
 
   // removing $ signs
@@ -76,14 +76,14 @@ const originalString = (arr) => {
 }
 
 const processString = (str) => {
-    let strArr = ['$'];
+  let strArr = ['$'];
 
-    for (let i = 0; i < str.length; i++) {
-        strArr.push(str[i]);
-        strArr.push('$');
-    }
+  for (let i = 0; i < str.length; i++) {
+      strArr.push(str[i]);
+      strArr.push('$');
+  }
 
-    return strArr;
+  return strArr;
 }
 
 const str1 = findPalindrome("abba");
@@ -95,11 +95,11 @@ console.log("Longest palindromic substring for input 2:" + str2);
 console.log("Size:" + str2.length);
 
 /**
- * Outputs for the sample inputs:
- * Longest palindromic substring for input 1:abba
- * Size:4
- * Longest palindromic substring for input 2:abcbaabcba
- * Size:10
- * 
- * Time complexity: O(n) Space complexity: O(n)
- */
+* Outputs for the sample inputs:
+* Longest palindromic substring for input 1:abba
+* Size:4
+* Longest palindromic substring for input 2:abcbaabcba
+* Size:10
+*
+* Time complexity: O(n) Space complexity: O(n)
+*/

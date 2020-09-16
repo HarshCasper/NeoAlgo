8e24755e9bf85ca6541baa35c3346248702cb666
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 18:32:10 +0530

    Update Tree_preOrder_traversal.py

--- Python/ds/Tree_preOrder_traversal.py
+++ Python/ds/Tree_preOrder_traversal.py
@@ -4,10 +4,11 @@ class node:
         self.left = None
         self.right = None
 
+
 class Tree:
     def __init__(self):
         self.root = None
-    
+
     def createTree(self, treeNodesArray):
         queue = []
         self.root = node(int(treeNodesArray.pop(0)))
@@ -31,7 +32,7 @@ class Tree:
                         queue.append(root.right)
                 size -= 1
             size = len(queue)
-    
+
     def recursive_preorder_traversal(self, root, path):
         if root:
             path.append(root.val)
@@ -41,30 +42,29 @@ class Tree:
 
     def iterative_preorder_traversal(self, root):
         self.path = []
-        stack = [] 
+        stack = []
         while stack or root:
             while root:
-                self.path.append(root.val)   
-                stack.append(root) 
-                root = root.left            
-            root = stack.pop()                
-            root = root.right                
-                                             
+                self.path.append(root.val)
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            root = root.right
         return self.path
-    
+
 treeNodesArray = list(input().split(','))
 preOrdertree = Tree()
 preOrdertree.createTree(treeNodesArray)
 
-#### Pre-Order Traversal :- root.val -> root.left -> root.right
+# Pre-Order Traversal :- root.val -> root.left -> root.right
 
-print(preOrdertree.recursive_preorder_traversal(preOrdertree.root, []))  
-print(preOrdertree.iterative_preorder_traversal(preOrdertree.root))  
+print(preOrdertree.recursive_preorder_traversal(preOrdertree.root, []))
+print(preOrdertree.iterative_preorder_traversal(preOrdertree.root))
 
 '''
-    Input should be of this type->    1,2,3,null,null,4,5,null,null,6,7      
+    Input should be of this type->    1,2,3,null,null,4,5,null,null,6,7
     Children of leaf node are null
-        
+
         1
        / \
       2   3
@@ -73,7 +73,9 @@ print(preOrdertree.iterative_preorder_traversal(preOrdertree.root))
            / \
           6   7
      Expected Output: 1,2,3,4,5,6,7
-     
-Stack is used to implement the iterative approach. Push nodes into the stack. We leave the inner loop when left leaf is reached. 
-Latest node is popped out of the stack and we move to its right child and repeat the above steps until we reach the right-most child. 
+
+Stack is used to implement the iterative approach. Push nodes into the stack.
+We leave the inner loop when left leaf is reached.
+Latest node is popped out of the stack and we move to its right child and
+repeat the above steps until we reach the right-most child.
 '''

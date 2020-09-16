9e3768d5e8d4f1dbf35c35d6d12e9eb1ffdaff60
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 18:12:44 +0530

    Update Swap_two_nibbles.py

--- Python/other/Swap_two_nibbles.py
+++ Python/other/Swap_two_nibbles.py
@@ -1,12 +1,14 @@
-#Python Program to swap the two nibbles in a given byte.
+# Python Program to swap the two nibbles in a given byte.
 
-#function to swap nibbles
-def swapNibbles(number): 
-    return ( (number & 0x0F)<<4 | (number & 0xF0)>>4 ) 
+# function to swap nibbles
+
+
+def swapNibbles(number):
+    return ((number & 0x0F) << 4 | (number & 0xF0) >> 4)
 
 number = int(input("Enter Number: "))
 print("After swapping the nibbles:")
-print(swapNibbles(number)) 
+print(swapNibbles(number))
 
 '''
 Sample I/O:
@@ -14,8 +16,8 @@ Sample I/O:
     Enter Number: 100
 
     Output:
-    After swapping the nibbles:                                                                                                    
-    70   
+    After swapping the nibbles:                                                                                               
+    70
 
 Time Complexity: O(1)
 Space Complexity: O(1)

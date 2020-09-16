676188c8c7fe988b09d866e59a76b60be8be4c69
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 18:24:33 +0530

    Update Depth_Of_Tree.py

--- Python/graphs/Depth_Of_Tree.py
+++ Python/graphs/Depth_Of_Tree.py
@@ -1,5 +1,6 @@
-#### Program to calculate the depth of a Tree
-#### Note that the levels start from 0(Zero).
+# Program to calculate the depth of a Tree
+# Note that the levels start from 0(Zero).
+
 
 class TreeNode:
     def __init__(self, val):
@@ -12,31 +13,29 @@ class Tree:
     def __init__(self, root):
         self.root = TreeNode(root)
 
-
     def get_node_depth(self, node, val, depth):
-        if node == None:
+        if node is None:
             return -1
 
-        if node.val ==  val:
+        if node.val is val:
             return depth
 
         l_depth = self.get_node_depth(node.left, val, depth+1)
         r_depth = self.get_node_depth(node.right, val, depth+1)
 
+        return max(l_depth, r_depth)
 
-        return max(l_depth,r_depth)
-
-    def get_maximum_depth(self,node,depth):
-        if node == None:
+    def get_maximum_depth(self, node, depth):
+        if node is None:
             return -1
 
         l_depth = 1 + self.get_maximum_depth(node.left, depth+1)
         r_depth = 1 + self.get_maximum_depth(node.right, depth+1)
 
-        return max(l_depth,r_depth)
+        return max(l_depth, r_depth)
 
 
-#### Creating a tree of the following structure:-
+# Creating a tree of the following structure:-
 
 #      1
 #    /   \
@@ -46,21 +45,23 @@ class Tree:
 
 tree = Tree(1)
 
-tree.root.left  = TreeNode(2)
+tree.root.left = TreeNode(2)
 tree.root.right = TreeNode(3)
 
-tree.root.left.left   = TreeNode(4)
-tree.root.left.right  = TreeNode(5)
-tree.root.right.left  = TreeNode(6)
+tree.root.left.left = TreeNode(4)
+tree.root.left.right = TreeNode(5)
+tree.root.right.left = TreeNode(6)
 tree.root.right.right = TreeNode(7)
 
+# Printing the depth of each node
+for i in range(1, 8):
+    print(tree.get_node_depth(tree.root, i, 0))
+    # Should print : 0 1 1 2 2 2 2
 
-### Printing the depth of each node
-for i in range(1,8):
-    print(tree.get_node_depth(tree.root, i, 0)) # Should print : 0 1 1 2 2 2 2
+# Printing the maximum depth of the tree
+print("Maximum Depth: ", tree.get_maximum_depth(tree.root, 0))
+# Should print: 2
 
-### Printing the maximum depth of the tree
-print("Maximum Depth: ", tree.get_maximum_depth(tree.root, 0)) # Should print: 2
-
-### Print -1 for the depth if the node is not present in the tree
-print("If element not present", tree.get_node_depth(tree.root, 10, 0)) # Should print: -1
+# Print -1 for the depth if the node is not present in the tree
+print("If element not present", tree.get_node_depth(tree.root, 10, 0))
+# Should print: -1

f79811fea1832fe344a8b6e55b9e213a35c2077c
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 18:01:08 +0530

    Update Stacks.py

--- Python/ds/Stacks.py
+++ Python/ds/Stacks.py
@@ -1,39 +1,38 @@
-#### Implementing stacks using linked list
+# Implementing stacks using linked list
+
 
 class Node(object):
     def __init__(self, value):
         self.value = value
         self.next = None
 
+
 class Stack(object):
     def __init__(self):
         self.head = None
         self.num_elements = 0
 
-
     def push(self, value):
         new_node = Node(value)
-        
+
         if self.head is None:
             self.head = new_node
         else:
             new_node.next = self.head
             self.head = new_node
-            
-        self.num_elements += 1
 
+        self.num_elements += 1
 
     def size(self):
         return self.num_elements
-    
+
     def is_empty(self):
         return self.num_elements == 0
-
 
     def pop(self):
         if self.is_empty():
             return None
-        
+
         value = self.head.value
         self.head = self.head.next
         self.num_elements -= 1

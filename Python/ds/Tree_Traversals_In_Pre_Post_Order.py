0c94af10013ca57a45618533c62977ff78ad2125
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-16 18:33:38 +0530

    Update Tree_Traversals_In_Pre_Post_Order.py

--- Python/ds/Tree_Traversals_In_Pre_Post_Order.py
+++ Python/ds/Tree_Traversals_In_Pre_Post_Order.py
@@ -1,4 +1,5 @@
-#### Program to traverse a given tree in pre-order, in-order and post-order fashion.
+# Program to traverse given tree in pre-order, in-order and post-order fashion
+
 
 class TreeNode:
     def __init__(self, val):
@@ -35,9 +36,7 @@ class Tree:
 
         return path
 
-
-
-#### Creating a tree of the following structure:-
+# Creating a tree of the following structure:-
 
 #      1
 #    /   \
@@ -47,26 +46,25 @@ class Tree:
 
 tree = Tree(1)
 
-tree.root.left  = TreeNode(2)
+tree.root.left = TreeNode(2)
 tree.root.right = TreeNode(3)
 
-tree.root.left.left   = TreeNode(4)
-tree.root.left.right  = TreeNode(5)
-tree.root.right.left  = TreeNode(6)
+tree.root.left.left = TreeNode(4)
+tree.root.left.right = TreeNode(5)
+tree.root.right.left = TreeNode(6)
 tree.root.right.right = TreeNode(7)
 
-#### In-Order Traversal :- left -> root -> right
+# In-Order Traversal :- left -> root -> right
 
-print(tree.inorder_traversal(tree.root, ''))   # Expected Output:- 4 2 5 1 6 3 7
+print(tree.inorder_traversal(tree.root, ''))
+# Expected Output:- 4 2 5 1 6 3 7
 
+# Pre-Order Traversal :- root -> left -> right
 
-#### Pre-Order Traversal :- root -> left -> right
+print(tree.preorder_traversal(tree.root, ''))
+# Expected Output:- 1 2 4 5 3 6 7
 
-print(tree.preorder_traversal(tree.root, ''))  # Expected Output:- 1 2 4 5 3 6 7
+# Post-Order Traversal :- left -> right -> root
 
-
-#### Post-Order Traversal :- left -> right -> root
-
-print(tree.postorder_traversal(tree.root, '')) # Expected Output:- 4 5 2 6 7 3 1
-
-
+print(tree.postorder_traversal(tree.root, ''))
+# Expected Output:- 4 5 2 6 7 3 1

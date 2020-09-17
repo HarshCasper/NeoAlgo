2e7d9398e4802ca97e282b115a2dafb91befa39e
narayan2111 <narayandwivedi076@gmail.com>
2020-09-17 20:20:33 +0530

    Formated the code A/Q to PEP-8

--- Python/cp/longest_Common_Prefix.py
+++ Python/cp/longest_Common_Prefix.py
@@ -1,56 +1,56 @@
-#Python Program to find the longest common prefix
-
+# Python Program to find the longest common prefix
 # function to find minimum length
-def findMinLength(string, size): 
-	minimum = len(string[0]) 
 
-	for i in range(1,size): 
-		if (len(string[i])< minimum): 
-			minimum = len(string[i]) 
 
-	return(minimum) 
+def findMinLength(string, size):
+    minimum = len(string[0])
+    for i in range(1, size):
+        if (len(string[i]) < minimum):
+            minimum = len(string[i])
 
-# A Function that returns the longest common prefix 
-def commonPrefix(string, size): 
+    return(minimum)
 
-	minimum_length = findMinLength(string, size) 
-	result ="" 
-	for i in range(minimum_length):  
-		same_char = string[0][i] 
-		for j in range(1,size): 
-			if (string[j][i] != same_char): 
-				return result 
-		result = result+same_char 
+# A Function that returns the longest common prefix
 
-	return (result) 
+
+def commonPrefix(string, size):
+    minimum_length = findMinLength(string, size)
+    result = ""
+    for i in range(minimum_length):
+        same_char = string[0][i]
+        for j in range(1, size):
+            if (string[j][i] != same_char):
+                return result
+        result = result+same_char
+
+    return (result)
 
 # Driver code
 if __name__ == "__main__":
-	string = []
-	size = int(input("Enter size:"))
-	print("Enter strings:")
-	for i in range(size):
-		item = input()
-		string.append(item)
-	answer = commonPrefix (string, size) 
-	if (len(answer)): 
-		print("The longest common prefix is ",answer) 
-	else: 
-		print("There is no common prefix") 
+    string = []
+    size = int(input("Enter size:"))
+    print("Enter strings:")
+    for i in range(size):
+        item = input()
+        string. append(item)
+        answer = commonPrefix(string, size)
+        if (len(answer)):
+            print("The longest common prefix is ", answer)
+        else:
+            print("There is no common prefix")
 
 '''
 Sample I/O:
-	Input:
-	Enter size:3
-	Enter Strings:
-	star
-	start
-	stardom
+Input:
+Enter size:3
+Enter Strings:
+star
+start
+stardom
 
-	Output:
-	The longest common prefix is star
+Output:
+The longest common prefix is star
 
-Time Complexity: O(size*Length of the largest string)
-Space Complexity:O(length of the largest string)
-	
+Time Complexity-O(size*Length of the largest string)
+Space Complexity-O(length of the largest string)
 '''

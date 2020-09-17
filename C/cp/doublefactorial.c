ff291891fcc8b85aba4a8b96bf6f5172d8f36290
Divyang Bagla <bagladivyang03@gmail.com>
2020-09-17 08:59:53 +0530

    added recursive solution too

--- C/cp/doublefactorial.c
+++ C/cp/doublefactorial.c
@@ -1,7 +1,3 @@
-/*Double factorial of a non-negative integer n is defined as the product of all the integers from 1 to n that have the same parity (odd or even) as n.
-It is also called as semifactorial of a number and is denoted by n!!
-*/
-
 #include <stdio.h>
 int doublefactorial(int n){
     int res = 1;
@@ -11,19 +7,56 @@ int doublefactorial(int n){
     else
          res = res*i;
     }
-    
+
 }
+
+int recdoublefactorial(int n){
+    if(n==0 || n==1)
+        return 1;
+    else
+         return n*recdoublefactorial(n-2);
+    }
+
+
 int main()
 {
     int n;
     printf("Enter the number :");
     scanf("%d",&n);
-    printf("The Double Factorial is : %d",doublefactorial(n));
-
+    int k;
+    do{
+        printf("Enter the choice below :\n1.Iterative solution \n2.Recursive Solution \n3.Exit \n");
+        scanf("%d",&k);
+        switch(k){
+        case 1:
+        printf("The Double Factorial is : %d\n",doublefactorial(n));
+        break;
+        case 2:
+        printf("The Double Factorial is : %d\n",recdoublefactorial(n));
+        break;
+        }
+    }while(k!=3);
+    
     return 0;
 }
 
 /*
-Enter the number :6                                                                                                                           
-The Double Factorial is : 48   
+Enter the number :7
+Enter the choice below :
+1.Iterative solution 
+2.Recursive Solution 
+3.Exit 
+1
+The Double Factorial is : 105
+Enter the choice below :
+1.Iterative solution 
+2.Recursive Solution 
+3.Exit 
+2
+The Double Factorial is : 105
+Enter the choice below :
+1.Iterative solution 
+2.Recursive Solution 
+3.Exit 
+3
 */

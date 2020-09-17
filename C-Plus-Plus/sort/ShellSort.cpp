6b24bddabcfa90d536a0f6e743942f6447c1612c
Saloni Shah <saloshah01@gmail.com>
2020-09-17 12:22:05 +0530

    Update Shellsort.cpp

--- C-Plus-Plus/sort/ShellSort.cpp
+++ C-Plus-Plus/sort/ShellSort.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 Shell sort is a highly efficient sorting algorithm and is based on insertion sort algorithm.
 This algorithm avoids large shifts as in case of insertion sort, if the smaller value is to the far right 
@@ -47,3 +48,52 @@ Enter the size of the array: 5
 9 5 8 1 4                                                                                                                                            5                                                                                                                                             
 Array after sorting is: 1 4 5 8 9                                                                                                                                           Array after sorting is: 1 4 5 8 9 
 */
+=======
+#include<iostream>
+using namespace std;
+int main()
+{
+    float A[10],B[10];
+    int i,j,key,m,n,temp,k;
+    cout<<"\nenter no of elements in array:";
+    cin>>n;
+    cout<<"\nenter elements of array:";
+    for(i=0;i<n;i++)
+    {
+        cin>>B[i];
+    }
+    cout<<"\nthe array elements are:";
+    for(i=0;i<n;i++)
+    {
+        cout<<B[i]<<"\t";
+    }
+    for(i=n/2;i>0;i=i/2)
+    {
+        for(j=1;j<n;j++)
+        {
+            for(k=j-i;k>=0;k=k-i)
+            {
+                if(B[k+i]>=B[k])
+                {
+                    break;
+                }
+                else
+                {
+                    key=B[k];
+                    B[k]=B[k+i];
+                    B[k+i]=key;
+                }
+            }
+        }
+    }
+    cout<<"\nthe shell sorted array is:";
+    for(i=0;i<n;i++)
+    {
+        cout<<B[i]<<"\t";
+    }
+
+
+    return 0;
+}
+				
+>>>>>>> Update Shellsort.cpp

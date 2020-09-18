25a937a238e13f9f9447d4ac7236bb9b71bd6b16
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-18 23:27:47 +0530

    Update Vigenere_Cipher.c

--- C/cryptography/Vigenere_Cipher.c
+++ C/cryptography/Vigenere_Cipher.c
@@ -4,31 +4,44 @@ Vigenère cipher is a method of encrypting alphabetic text by using a series of 
 #include <stdio.h>
 #include <string.h>
 
-char key[]="NEOALGO";
-int x=strlen(key);
+char key[100];
+int x;
+
+void encryption(char message[]) {
+	for (int i = 0; i < strlen(message); ++i) {
+		int t=(message[i] + key[i%x])%26;
+    	message[i]=('A'+t);
+	}
+}
+
+void decryption(char message[]) {
+	for (int i = 0; i < strlen(message); ++i) {
+	    int t=(message[i] - key[i%x] +26)%26;
+	    message[i]=('A'+t);
+	}
+}
 
 int main() {
-  char message[] = "TESSERACTCODINGNEOALGO";
-  int n=strlen(message);
-  //use only uppercase characters (you can modify the code for lowercase characters)
-  char new_message[n];
-  for (int i = 0; i < n; ++i) {
-    int t=(message[i] + key[i%x])%26;
-    new_message[i]=('A'+t);
-  }
-  printf("Encrypted Message: %s\n",new_message);
-  
-  char original_message[n];
-  for (int i = 0; i < n; ++i) {
-    int t=(new_message[i] - key[i%x] +26)%26;
-    original_message[i]=('A'+t);
-  }
-  printf("Decrypted Message: %s",original_message);
-  return 0;
+	char message[100];
+	printf("Enter Message To Encode: ");
+	scanf("%s", message);
+	//use only uppercase characters (you can modify the code for lowercase characters)
+	printf("\nEnter Key:");
+	scanf("%s", key);
+	x=strlen(key);
+
+	printf("Generated Key: %s\n", key);
+	encryption(message);
+	printf("Encrypted Message: %s\n",message);
+
+	decryption(message);
+	printf("Decrypted Message: %s",message);
+	return 0;
 }
 
 /*
-OUTPUT:
+OUTPUT
+Generated Key: NEOALGO
 Encrypted Message: GIGSPXOPXQOOOBTRSOLRUB
 Decrypted Message: TESSERACTCODINGNEOALGO
 */

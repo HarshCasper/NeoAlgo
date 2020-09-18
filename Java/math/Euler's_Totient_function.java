041455ef1d4bdea92bb2233f44b0bfd80eb003ec
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-09-19 01:37:42 +0530

    Update Euler's_Totient_function.java

--- Java/math/Euler's_Totient_function.java
+++ Java/math/Euler's_Totient_function.java
@@ -5,46 +5,46 @@ numberof integers between 1 and n inclusive, which are coprime to n.
 import java.util.Scanner;
 
 public class Euler_Totient {
-   public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter an integer: ");
 		int n = sc.nextInt();
-		
-		int temp=n;
+
+		int temp = n;
 		//This gives euler totient function for N
 		// Time Complexity: O(N*root(N))
 		int ans = n;
 		for (int i = 2; i <= n; ++i) {
-			if(n%i == 0){
-				ans = ans - ans/i;
-				while(n%i == 0)
-					n/=i;
+			if (n % i == 0) {
+				ans = ans - ans / i;
+				while (n % i == 0)
+					n /= i;
 			}
 		}
-		if(n>1){
-			ans = ans - ans/n;
+		if (n > 1) {
+			ans = ans - ans / n;
 		}
-		n=temp;
-		System.out.println("\nThe euler totient function for "+n+" is "+ans);
-	
+		n = temp;
+		System.out.println("\nThe euler totient function for " + n + " is " + ans);
+
 		//This gives euler totient function from 1 to N
 		// Time Complexity: O(N loglogN) - same as Sieve of Eratosthenes
-		int phi[]=new int[n+1];
+		int phi[] = new int[n + 1];
 		for (int i = 0; i <= n; ++i) {
 			phi[i] = i;
 		}
 		for (int i = 2; i <= n; ++i) {
 			if (phi[i] == i) {
-				for (int j = i; j<=n; j+=i) {
-					phi[j] = phi[j] - phi[j]/i;
+				for (int j = i; j <= n; j += i) {
+					phi[j] = phi[j] - phi[j] / i;
 				}
 			}
 		}
-		System.out.println("\nThe euler totient function for all integers from 1 to " + n +" is:\n");
+		System.out.println("\nThe euler totient function for all integers from 1 to " + n + " is:\n");
 		for (int i = 1; i <= n; ++i) {
 			System.out.println(i + " " + phi[i]);
 		}
-   }
+	}
 }
 
 /*

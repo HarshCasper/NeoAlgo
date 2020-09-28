6c61d22bbc5321080c374f8a366948a515d87893
Saloni Shah <saloshah01@gmail.com>
2020-09-28 10:57:36 +0530

    Update left_nonRepeating_char.py

--- Python/other/left_nonRepeating_char.py
+++ Python/other/left_nonRepeating_char.py
@@ -2,10 +2,13 @@
 Input the string from the user.
 Start traversing the string using two loops.
 Use the first loop to scan the characters of the string one by one.
-Use the second loop to find if the current character is occurring in the latter part of the string or not.
+Use the second loop to find if the current character is occurring
+in the latter part of the string or not.
 If it is not occurring, print that character.
 Else, continue traversing.
 """
+
+
 def firstNotRepeatingCharacter(string):
     for i in range(len(string)):
         flag = False
@@ -17,22 +20,22 @@ def firstNotRepeatingCharacter(string):
     return "_"
 
 string = input("Enter a string: ")
-index = firstNotRepeatingCharacter(string) 
-if index is None: 
-    print('No non repeating character found. All the characters are repeating.')
-else: 
+index = firstNotRepeatingCharacter(string)
+if index is None:
+    print('No non repeating character found.
+          All the characters are repeating.')
+else:
     ind = string.find(index)
-    print("Non Repeating character " + str(string[ind]) + " found at index " + str(ind)) 
-    
+    print("Non Repeating character " +
+          str(string[ind]) + " found at index " + str(ind))
+
 """
 Sample input 1:
 Input: "abaacd"
 Output: Non Repeating character b found at index 1
-
 Sample input 2:
 Input:"aabbccdd"
 Output: No non repeating character found. All the characters are repeating.
-
 Time Complexity: O(n*2) - n being the length of string
 Space Complexity: O(n)
     """

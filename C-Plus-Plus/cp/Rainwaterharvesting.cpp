f746d7f4916bfa3631de7e1b4d2f38cc9d29651c
neehit goyal <44130067+NEEHITGOYAL@users.noreply.github.com>
2020-09-29 15:02:45 +0530

    Update Rainwaterharvesting.cpp
    added problem statement

--- C-Plus-Plus/cp/Rainwaterharvesting.cpp
+++ C-Plus-Plus/cp/Rainwaterharvesting.cpp
@@ -1,3 +1,30 @@
+/*
+A very popular problem asked in interviews medium difficulty
+Apoorvaa has created an elevated roof. She wants to know how much water can she save during rain.
+
+Given n non negative integers representing the elevation map where width of every bar is 1, Find the maximum water that she can save.
+
+Explanation for the Sample input Testcase: 
+![image](https://user-images.githubusercontent.com/44130067/94519631-84921880-0248-11eb-8515-416d81534168.png)
+So the total units of water she can save is 5 units
+
+**Input Format**
+First line contains an integer n. Second line contains n space separated integers representing the elevation map.
+
+**Constraints**
+1 <= N <= 10^6
+
+**Output Format**
+Print a single integer containing the maximum unit of waters she can save.
+
+**Sample Input**
+10
+0 2 1 3 0 1 2 1 2 1
+**Sample Output**
+5
+**Explanation**
+Refer to the image for explanation. Count the number of cells having water in them.
+*/
 #include<iostream>
 using namespace std;
 int maxWater_optimized(int arr[], int n)

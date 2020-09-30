52b6fc77f4a87128dd780ed077918ee5b3650224
Yashaswi Biyahut <32020835+yashaswibiyahut@users.noreply.github.com>
2020-09-30 15:15:47 +0530

    Update floyd_warshal.c

--- C/dp/floyd_warshal.c
+++ C/dp/floyd_warshal.c
@@ -1,3 +1,10 @@
+/*Floyd-Warshall Algorithm is an algorithm 
+for finding the shortest path between all the 
+pairs of vertices in a weighted graph.
+This algorithm works for both the directed
+and undirected weighted graphs. 
+But, it does not work for the graphs with negative cycles
+*/
 #include<stdio.h>
 int i, j, k,n,arr[10][10];
 //Floyd function 

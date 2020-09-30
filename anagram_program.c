9caab986101b4d836ae8c58e455cf40d6a0da5fc
Aakriti Kinra <aakritikinra@gmail.com>
2020-10-01 00:02:34 +0530

    C program for substring recurssion

--- anagram_program.c
+++ anagram_program.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-
-void sort_strings(char str[])               //sort the strings
+void sort_strings(char str[]) //sort the strings
 {
-    int loop1, loop2;
-    char temp;
-    for(loop1=0;str[loop1]!='\0';loop1++)
+  int loop1, loop2;
+  char temp;
+  for(loop1=0;str[loop1]!='\0';loop1++)
+  {
+    for(loop2=0;str[loop2]!='\0';loop2++)
     {
-        for(loop2=0;str[loop2]!='\0';loop2++)
-        {
-            if(str[loop2]>str[loop1])
-            {
-                temp = str[loop1];          //swapping
-                str[loop1] = str[loop2];
-                str[loop2] = temp;
-            }
-        }
+      if(str[loop2]>str[loop1])
+      {
+      	temp = str[loop1]; //swapping
+        str[loop1] = str[loop2];
+        str[loop2] = temp;
+      }
     }
+  }
 }
-
 int check_anagram(char str1[], char str2[], int len)
 {
   int loop_num;
@@ -27,41 +25,39 @@ int check_anagram(char str1[], char str2[], int len)
     if(str1[loop_num]!=str2[loop_num])
         return -1;                              
   }
-  
   return 0;
 }
-
 int main() 
 {
-  	char str1[50],str2[50];
-  	int len1,len2;
-  	
-  	printf("Enter two strings:\n");
-  	scanf("%s%s",str1,str2);                            //strings to be checked
-  
-  	len1=strlen(str1);
-  	len2=strlen(str2);
-  
-  	
-  	//if length of strings is not equal they cannot be anagrams
-  	if(len1!=len2)
-  	    printf("The given strings are not anagrams");
-  	    
-  	else
-  	{
-  	    sort_strings(str1);
-  	    sort_strings(str2);
-  	    if(check_anagram(str1,str2,len1)==0)                 //len1=len2
-  	        printf("The given strings are anagrams");
-  	    else
-  	        printf("The given strings are not anagrams");
-  	}
-  	   
-	return 0;
+  char str1[50],str2[50];
+  int len1,len2;
+  printf("Enter two strings:\n");
+  scanf("%s%s",str1,str2); //strings to be checked
+  len1=strlen(str1);
+  len2=strlen(str2);
+  //if length of strings is not equal they cannot be anagrams
+  if(len1!=len2)
+      printf("The given strings are not anagrams");    
+  else
+  {
+      sort_strings(str1);
+      sort_strings(str2);
+      if(check_anagram(str1,str2,len1)==0) //len1=len2
+          printf("The given strings are anagrams");
+      else
+          printf("The given strings are not anagrams");
+  }  
+  return 0;
 }
 
 
 /*
+    TIME COMPLEXITY
+    O(nlogn)
+
+    SPACE COMPLEXITY
+    O(n)
+
     SAMPLE INPUT
     restful
     fluster
---
+++ hoct1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+int find_occ(char main_str[100], char sub_str[100], int main_len, int sub_len);
+int main()
+{
+    char word[100],sub[100];       
+    scanf("%s%s",word,sub);
+    int len_word,len_sub,occ;                           //find length of both strings
+    len_word=strlen(word);
+    len_sub=strlen(sub);
+    if(len_word=0 || len_sub==0 || len_word<len_sub)    //if either string is empty or length of substring is greater than the main string
+        printf("Enter valid strings.");
+    else
+    {
+        occ=find_occ(word, sub, len_word, len_sub);
+        printf("Number of occurences of %s in %s: %d",sub,word,occ);
+    }
+    return 0;
+}
+int find_occ(char main_str[100], char sub_str[100], int main_len, int sub_len)
+{
+    int occ=0,len_m;                                   //loop variables and variable to store occurence
+    for(len_m=0;len_m<=(main_len-sub_len);len_m++)
+    { 
+        int len_s;
+        for(len_s=0;len_s<sub_len;len_s++)
+        {
+            if(main_str[len_m+len_s]!=sub_str[len_s])
+                break;
+        }
+        if(len_s==sub_len)  
+        {
+           occ++;
+           len_s=0;
+        }
+    }
+    return occ;
+}
+/*
+Time Complexity: O(len_word*len_sub)
+Space Complexity: O(len_word)
+
+Sample Input:
+abcchifjrnccdnrr
+cc
+Output:
+Number of occurences of cc in abcchifjrnccdnrr: 2 
+*/
+

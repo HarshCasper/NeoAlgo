b3f0fda096b46a1b28662aaabe17401c71ffaa5b
narayan2111 <narayandwivedi076@gmail.com>
2020-09-30 22:52:27 +0530

    Distinct Elements Count

---
+++ Python/other/Distinct_elements_count.py
@@ -0,0 +1,55 @@
+#Python program to count distinct elements in every window of size k 
+
+
+from collections import defaultdict 
+
+def Distinct_count(array, window): 
+  
+    # Creates an empty hashmap  
+    hash_map = defaultdict(lambda:0) 
+    count_distinct = 0
+
+    for i in range(window): 
+        if hash_map[array[i]] == 0: 
+            count_distinct += 1
+        hash_map[array[i]] += 1
+  
+    # Print count of first window  
+    print(count_distinct) 
+      
+    # Traverse through the remaining array  
+    for i in range(window, size): 
+        # If there was only one occurrence, reduce distinct count.  
+        if hash_map[array[i - window]] == 1: 
+            count_distinct -= 1
+        hash_map[array[i - window]] -= 1
+      
+    # increment distinct element count  
+        if hash_map[array[i]] == 0: 
+            count_distinct += 1
+        hash_map[array[i]] += 1
+  
+        # Print count of current window  
+        print(count_distinct) 
+  
+size = int(input("Enter Size: "))
+array = [int(i) for i in input("Enter Array: ").split()]
+window = int(input("Enter window: "))
+Distinct_count(array, window) 
+
+'''
+Sample I/O:
+Input:
+Enter Size: 7
+Enter Array: 1 2 3 4 2 3 4
+Enter window: 4
+
+Output:
+4
+3
+3
+3
+
+Time Complexity: O(size)
+Space Complexity: O(Size)
+'''

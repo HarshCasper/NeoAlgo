c699d99bc06ef69c6983b703df451045a72c8de5
raksha009 <raksha009btcse19@igdtuw.ac.in>
2020-10-01 02:01:02 +0530

    Code added for Validate Stack Sequences

--- C-Plus-Plus/README.md
+++ C-Plus-Plus/README.md
@@ -96,6 +96,7 @@ _add list here_
 - [Knuth–Morris–Pratt_algorithm](cp/Knuth–Morris–Pratt.cpp)
 - [Knuth–Morris–Pratt_algorithm](cp/Knuth_Morris_Pratt.cpp)
 - [Elements Occurrence using Binary Search](cp/ElemetsOccurrence_BinarySearch.cpp)
+- [Validate Stack Sequences](ValidateStackSequences.cpp)
 
 ## Other
 
--- C-Plus-Plus/cp/Maximum_In_Array.cpp
+++ C-Plus-Plus/cp/Maximum_In_Array.cpp
@@ -1,83 +1,39 @@
-/**
- * @author omkarlanghe
- * Given an array of type positive integers, having ith element as feet and ith + 1 element as inches.
- * Eg: considering arr = [1,2] where 1 equals to feet and 2 equals to inches.
- * Find the maximum height, where height is calculated sum of feet and inches after converting feet into inches.
- * 
- * Input:
- * First line of input contains number of testcases.
- * For each testcase, first line of input N, contains size of an array.
- * Next line contains N number of elements (feet for ith index and inches for ith + 1).
- * 
- * Output:
- * Output maximum height from array.
- *
- * Example:
- * Input:
- * 2
- * 4
- * 1 2 2 1
- * 8
- * 3 5 7 9 5 6 5 5
- * 
- * Output:
- * 25
- * 93
- * 
- * Explanation:
- * Testcase 1: (1, 2) and (2, 1) are respective given heights. 
- * After converting both heigths in to inches, we get 14 and 25 as respective heights. So, 25 is the maximum height.
- * 
- * @source: https://practice.geeksforgeeks.org/problems/maximum-in-struct-array/1
+class Container_with_most_Water {
+public:
+    int calculate(int i, int j, int x1, int x2){
+        int part1 = min(x1,x2);
+        return part1 * abs(j - i);
+    }
+    
+    int maxArea(vector<int>& height) {
+        
+        int max  = 0;
+        int i = 0;    // left pointer
+        int j = height.size() - 1;    // right pointer
+        
+        while (i < j){
+            int curr = calc(i,j, height[i], height[j]);
+            if (curr > max) max = curr;
+            if (height[i] < height[j]) i++;     
+            else j--;
+        }
+        return max;  // Returning maxarea
+    
+    }
+};
+
+/*
+
+TIME COMPLEXITY
+2 POINTER APPROACH USED
+O(N) where n is the size of the array
+
+TEST CASE
+
+INPUT
+[1,8,6,2,5,4,8,3,7]
+OUTPUT
+49
 */
 
-#include <iostream>
-#include <vector>
 
-int max_height_in_array(std::vector<int>, int);
-
-int max_height_in_array(std::vector<int> arr, int size) {
-    int max = 0;
-    if ((size & 1) == 0) {  // check for even size.
-        for (int i = 0 ; i < size ; i = i+2) {
-            if (((arr[i] * 12) + arr[i+1]) > max) {
-                max = ((arr[i] * 12) + arr[i+1]);
-            }
-        }
-    } else {  // check for odd size.
-        for (int i = 0 ; i < (size - 1) ; i = i+2) {
-            if (((arr[i] * 12) + arr[i+1]) > max) {
-                max = ((arr[i] * 12) + arr[i+1]);
-            }
-        }
-
-        // special check for last remaining element at odd th index.
-        if ((arr[size-1] * 12) > max) {
-            max = (arr[size-1] * 12);
-        }
-    }
-    return (max);
-}
-
-int main() {
-    int test_cases, size, ele;
-    std::cout << "Enter test cases : " << std::endl;
-    std::cin >> test_cases;
-
-    while (test_cases--) {
-        std::vector<int> arr;
-        std::cout << "Enter the size of an array : " << std::endl;
-        std::cin >> size;
-
-        std::cout << "Enter the elements in an array : " << std::endl;
-        for (int i = 0 ; i < size ; i++) {
-            std::cin >> ele;
-            arr.push_back(ele);
-        }
-
-        int result = max_height_in_array(arr, size);
-        std::cout << "Maximum height in the given array is : " << result
-        << std::endl;
-    }
-    return (0);
-}

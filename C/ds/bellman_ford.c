db7b25ad447fb75da0c0385ecbedc143bd41505b
Rajiv Singh <rajivperfect007@gmail.com>
2020-10-01 19:44:55 +0530

    Update bellman_ford.c

--- C/ds/bellman_ford.c
+++ C/ds/bellman_ford.c
@@ -1,63 +1,71 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 //Bellman Ford Function
-int Bellman_Ford(int k, int* A, int* B, int n){
-    int i,j;
-    for(i=0;i<n;i++){
-        *(B+(k+1)*n+i)=*(B+k*n+i);
-        for(j=0;j<n;j++){
-            if(*(B+(k+1)*n+i)>*(B+k*n+j)+*(A+*(B+j)*n+*(B+i)))
-                *(B+(k+1)*n+i)=*(B+k*n+j)+*(A+*(B+j)*n+*(B+i));
+int Bellman_Ford(int k, int *A, int *B, int n)
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        *(B + (k + 1) * n + i) = *(B + k * n + i);
+        for (j = 0; j < n; j++)
+        {
+            if (*(B + (k + 1) * n + i) > *(B + k * n + j) + *(A + *(B + j) * n + *(B + i)))
+                *(B + (k + 1) * n + i) = *(B + k * n + j) + *(A + *(B + j) * n + *(B + i));
         }
     }
-    if(k<n)
-        Bellman_Ford(k+1,A,B,n);
+    if (k < n)
+        Bellman_Ford(k + 1, A, B, n);
 }
 //Main Function Began
-int main(){
-    int n,i,j,k,a;
+int main()
+{
+    int n, i, j, k, a;
     printf("Please enter the number of vertices:");
-    scanf("%d",&n);
-    int A[n][n],B[n+2][n];
+    scanf("%d", &n);
+    int A[n][n], B[n + 2][n];
     printf("\nEnter the value of Adjacency Matrix:\n");
-    for(i=0;i<n;i++){
+    for (i = 0; i < n; i++)
+    {
         printf("\n");
-        for(j=0;j<n;j++){
-            scanf("%d",&A[i][j]);
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &A[i][j]);
         }
     }
     printf("\nEnter the Source vertex number:");
-    scanf("%d",&a);
+    scanf("%d", &a);
 
     /*Calculation for the first vertex*/
-    B[0][0]=a-1;
-    B[1][0]=0;
-    for(j=1;j<n;j++){
-        if(j>a-1)
-            B[0][j]=j;
+    B[0][0] = a - 1;
+    B[1][0] = 0;
+    for (j = 1; j < n; j++)
+    {
+        if (j > a - 1)
+            B[0][j] = j;
         else
-            B[0][j]=j-1;
+            B[0][j] = j - 1;
     }
-    for(j=1;j<n;j++)
-        B[1][j]=99;
-	//calling bellman function
-    Bellman_Ford(1,A,B,n);
+    for (j = 1; j < n; j++)
+        B[1][j] = 99;
+    //calling bellman function
+    Bellman_Ford(1, A, B, n);
     //check for negative cycle
-    for(i=0;i<n;i++){
-        if(B[n+1][i]!=B[n][i]){
+    for (i = 0; i < n; i++)
+    {
+        if (B[n + 1][i] != B[n][i])
+        {
             printf("\n Negative edge-cycle Present");
             return 0;
         }
     }
     //print the output
     printf("\n\tOUTPUT:\n");
-    for(i=1;i<n;i++)
-        printf("\nWeight of vertex no.%d is %d",B[0][i]+1,B[n+1][i]);
+    for (i = 1; i < n; i++)
+        printf("\nWeight of vertex no.%d is %d", B[0][i] + 1, B[n + 1][i]);
 }
 //main ends here
 
 /* Sample Input Output
-
 Please enter the number of vertices: 5
 Enter the value of Adjacency Matrix:
 0
@@ -92,13 +100,12 @@ Enter the value of Adjacency Matrix:
 
 Enter the Source vertex number: 1
 
-    OUTPUT:
-
+OUTPUT:
 Weight of vertex no.2 is 3
 Weight of vertex no.3 is 7
 Weight of vertex no.4 is 2
 Weight of vertex no.5 is -4
 
 Time Complexity : O(V*E)
-where V is no. of vertices and E is no. of edges in the graph.
+where V is noumber of vertices and E is no. of edges in the graph.
 */

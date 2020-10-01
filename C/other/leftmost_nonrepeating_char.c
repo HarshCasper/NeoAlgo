ec2b19f9ced4300ee9dbb6b22c9471f0ef50b09e
Rajiv Singh <rajivperfect007@gmail.com>
2020-10-01 17:43:18 +0530

    Update leftmost_nonrepeating_char.c

--- C/other/leftmost_nonrepeating_char.c
+++ C/other/leftmost_nonrepeating_char.c
@@ -1,13 +1,3 @@
-/*
-PS: Given a string, find the index of the leftmost non repeating character in it.
-Example: s = "hello", output should be: 0 (index of h)
-s = "aabcdb", output should be: 3 (index of c)
-s = "xxyyzz", output should be -1 (since there is no non repeating character in this string)
-
-Time Complexity: O(n)
-Space Complexity: O(n)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,33 +7,42 @@ struct pair
     int second;
 };
 
-int find_char_index(char* str)
+int find_char_index(char *str)
 {
-    struct pair p[256]={0};
-    for (int i = 0; str[i]; i++) 
-    { 
-		(p[str[i]].first)++; 
-		p[str[i]].second = i; 
-	}
-	int index = 10001; 
-	for (int i = 0; i < 256; i++) 
-	{
-		if (p[i].first == 1) 
-			index = index>p[i].second?p[i].second:index; 
-	}
-
-	return index; 
+    struct pair p[256] = {0};
+    for (int i = 0; str[i]; i++)
+    {
+        (p[str[i]].first)++;
+        p[str[i]].second = i;
+    }
+    int index = 10001;
+    for (int i = 0; i < 256; i++)
+    {
+        if (p[i].first == 1)
+            index = index > p[i].second ? p[i].second : index;
+    }
+    return index;
 }
 
 int main()
 {
     char str[10000];
     printf("Enter a string: ");
-    scanf("%s",str);
-	int index = find_char_index(str); 
-	if (index == 10001) 
-		printf("%d",-1); 
-	else
-		printf("%d",index); 
-	return 0; 
+    scanf("%s", str);
+    int index = find_char_index(str);
+    if (index == 10001)
+        printf("%d", -1);
+    else
+        printf("%d", index);
+    return 0;
 }
+
+/*
+Given a string, find the index of the leftmost non repeating character in it.
+Example: s = "hello", output should be: 0 (index of h)
+s = "aabcdb", output should be: 3 (index of c)
+s = "xxyyzz", output should be -1 (since there is no non repeating character in this string)
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/

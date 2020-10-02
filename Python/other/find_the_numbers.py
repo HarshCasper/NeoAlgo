73c5cc2ca7b21449b88479bddb8bfae4b91e0e31
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-10-02 23:40:19 +0530

    Update find_the_numbers.py

--- Python/other/find_the_numbers.py
+++ Python/other/find_the_numbers.py
@@ -1,4 +1,4 @@
-# Python program to find the inique numbers in an array out
+# Python program to find the unique numbers in an array out
 # of which two numbers occur exactly once and are distinct.
 # You need to find the other two numbers and print them in ascending order.
 
@@ -19,3 +19,13 @@ if __name__ == '__main__':
     if x > y:
         swap(x, y)
     print("The distinct numbers are ", x, " and ", y)
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+# Input
+# Enter the number of elements: 6
+# 2 3 1 4 2 1
+
+# Ouput
+# The distinct numbers are  3  and  4

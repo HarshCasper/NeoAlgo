08e911809d821e366fe9c1bfe58271f845a8ac4f
Rajiv Singh <rajivperfect007@gmail.com>
2020-10-02 09:12:45 +0530

    Update Factorial_of_very_large_numbers.cpp

--- C-Plus-Plus/cp/Factorial_of_very_large_numbers.cpp
+++ C-Plus-Plus/cp/Factorial_of_very_large_numbers.cpp
@@ -4,36 +4,35 @@
 using namespace std;
 
 // Function to find factorial
-string Factorial(long long n){
-	if(n==0)
+string Factorial(long long n) {
+	if (n == 0)
 		return "1";
-		
-	long long i,fact=0;
-	
-	for(i=1;i<=n;i++)
-		fact+=log(i);
+
+	long long i, fact = 0;
+
+	for (i = 1; i <= n; i++)
+		fact += log(i);
 	string str;
-	str=to_string(round(exp(fact)));
+	str = to_string(round(exp(fact)));
 	return str;
 }
 
-int main(){
+int main() {
 	long long n;
-	cout<<"Enter the number: ";
-	cin>>n;
+	cout << "Enter the number: ";
+	cin >> n;
 	string str;
-	str=Factorial(n);
-	cout<<"The factorial of "<<n<<" = "<<str;
+	str = Factorial(n);
+	cout << "The factorial of " << n << " = " << str;
 	return 0;
 }
 
 /*
-	Sample input/output
-	-------------------
+	Sample input/output:
+
 	Enter the number: 51
-	The factorial of 51 = 712058632688933793172711302775443308528933084537552896.000000	
-	------------------------------------------------------------------------------------
-	
-	Time Complexity= O(n) 
+	The factorial of 51 = 712058632688933793172711302775443308528933084537552896.000000
+
+	Time Complexity= O(n)
 	Space Complexity= O(1)
 */

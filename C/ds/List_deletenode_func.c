5832f1b27fa26f95e3498c926068c7c3fd3800e8
Shraddha <42699578+shraddhavp@users.noreply.github.com>
2020-10-02 13:50:37 +0530

    Update the file with changes told
    added input output and complexities

--- C/ds/List_deletenode_func.c
+++ C/ds/List_deletenode_func.c
@@ -5,14 +5,47 @@
  *     struct ListNode *next;
  * };
  */
-/** Below is th function to delete node */
-void deleteNode(struct ListNode* node) {
+/** Below is the function to delete node */
+#include<stdio.h>
+#include<stdlib.h>
 
-node->val = node->next->val;
+struct node{
+  int data;
+  struct node *next;
+};
 
-struct ListNode* temp = node->next;
-node->next = node->next->next;
-
-temp->next = NULL;
-free(temp);
+struct node *newNode(int data){
+  struct node *n = (struct node *)malloc(sizeof(struct node *));
+  n -> data = data;
+  n -> next = NULL;
+  return n;
 }
+
+void deleteNode(struct node *ref){
+  while(ref -> next != NULL){
+    ref -> data = ref -> next -> data;//NODE TO BE DELETE POINTER GIVEN
+    ref = ref -> next;
+  }
+  free(ref);
+}
+
+void main(){
+  struct node *n = newNode(1);
+  n -> next = newNode(2);
+  n -> next -> next = newNode(3);
+  n -> next -> next -> next = newNode(4);
+  n -> next -> next -> next -> next = newNode(5);
+  n -> next -> next -> next -> next -> next = newNode(6);
+
+  deleteNode(n -> next -> next -> next); // deletion node is  4
+	
+  while(n -> next != NULL){
+    printf("%d\n", n -> data);
+    n = n -> next;
+  }
+}
+/* OUTPUT
+1 2 3 4 5 NULL
+1 2 3 5 NULL
+Time Complexity :O(1)
+Space complexity:O(n)

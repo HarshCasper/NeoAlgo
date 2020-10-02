98203288379b24df38df2f9d8e40ef29be6a8c3c
Saloni Shah <saloshah01@gmail.com>
2020-10-02 10:38:20 +0530

    Update remove_outermost_parenthesis.py

--- Python/other/remove_outermost_parenthesis.py
+++ Python/other/remove_outermost_parenthesis.py
@@ -1,10 +1,12 @@
 def removeOuterParentheses(S):
-         res, opened = [], 0
-         for c in S:
-            if c == '(' and opened > 0: res.append(c)
-            if c == ')' and opened > 1: res.append(c)
-            opened += 1 if c == '(' else -1
-         return "".join(res)
+    res, opened = [], 0
+    for c in S:
+        if c == '(' and opened > 0:
+            res.append(c)
+        if c == ')' and opened > 1:
+            res.append(c)
+        opened += 1 if c == '(' else -1
+    return "".join(res)
 
 str = input("Enter string: ")
 result = removeOuterParentheses(str)
@@ -14,13 +16,12 @@ print(result)
 """
 Time complexity: O(n)
 Space complexity: O(n)
-
 Sample Input 1:
-Input: "(()())(())"
-Output: "()()()"
-  
+Enter string: (()())(())
+Output: ()()()
+
 Sample Input 2:
-Input: "()()"
+Enter string: ()()
 Output: ""
-    
+
 """

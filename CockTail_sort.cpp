55bca7d6ca9622836f10b8882c0cab98a4af4b92
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-10-02 22:36:48 +0530

    Added code for cocktail_sort.cpp

---
+++ CockTail_sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>  
+using namespace std;   
+int temp;   
+void Cocktail(int a[], int n)  
+{  
+    int is_swapped = 1;  
+    int begin = 0,i;  
+    int end = n - 1;  
+   
+    while (is_swapped) {  
+        is_swapped = 0;  
+     for (i = begin; i < end; ++i) {  
+            if (a[i] > a[i + 1]) {  
+            temp = a[i];  
+        a[i]=a[i+1];  
+        a[i+1]=temp;                  
+        is_swapped = 1;  
+            }  
+        }  
+ if (!is_swapped)  
+            break;  
+ is_swapped = 0;  
+ for (i = end - 1; i >= begin; --i) {  
+     if (a[i] > a[i + 1])   
+    {  
+          temp = a[i];  
+      a[i]=a[i+1];  
+      a[i+1]=temp;  
+      is_swapped = 1;  
+         }  
+      }  
+        ++begin;  
+    }  
+}  
+   
+int main()  
+{  
+    int arr[] = {0, 10, 2, 8, 23, 1, 3, 45},i;  
+    int n = sizeof(arr) / sizeof(arr[0]);  
+    Cocktail(arr, n);  
+    cout <<"printing sorted array :\n";  
+     for (i = 0; i < n; i++)  
+        cout<<arr[i]<<" ";  
+    cout<<"\n";  
+    return 0;  
+}  

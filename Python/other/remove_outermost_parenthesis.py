510e94e54b12f584e7f28c9e871a3a2fbccee6a9
Saloni Shah <saloshah01@gmail.com>
2020-10-02 10:32:13 +0530

    Add files via upload

---
+++ Python/other/remove_outermost_parenthesis.py
@@ -0,0 +1,26 @@
+def removeOuterParentheses(S):
+         res, opened = [], 0
+         for c in S:
+            if c == '(' and opened > 0: res.append(c)
+            if c == ')' and opened > 1: res.append(c)
+            opened += 1 if c == '(' else -1
+         return "".join(res)
+
+str = input("Enter string: ")
+result = removeOuterParentheses(str)
+print(result)
+
+
+"""
+Time complexity: O(n)
+Space complexity: O(n)
+
+Sample Input 1:
+Input: "(()())(())"
+Output: "()()()"
+  
+Sample Input 2:
+Input: "()()"
+Output: ""
+    
+"""

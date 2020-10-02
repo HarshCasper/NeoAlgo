4ee122de8ae3b5679f7eb8108e538ded094d4130
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-10-03 01:23:19 +0530

    Update zalgorithm.cpp

--- C-Plus-Plus/cp/zalgorithm.cpp
+++ C-Plus-Plus/cp/zalgorithm.cpp
@@ -3,57 +3,57 @@ using namespace std;
 
 void getZ(string s, int z[])
 {
-    int n = s.size();
-    int left = 0, right = 0;
-    for (int i = 1; i < n; ++i)
-    {
-        if (i > right)
-        {
-            left = right = i;
-            while (right < n && s[right - left] == s[right])
-                right++;
-            z[i] = right - left;
-            right--;
-        }
-        else
-        {
-            int k = i - left;
-            if (z[k] < right - i + 1)
-            {
-                z[i] = z[k];
-            }
-            else
-            {
-                left = i;
-                while (right < n && s[right - left] == s[right])
-                    right++;
-                z[i] = right - left;
-                right--;
-            }
-        }
-    }
+	int n = s.size();
+	int left = 0, right = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if (i > right)
+		{
+			left = right = i;
+			while (right < n && s[right - left] == s[right])
+				right++;
+			z[i] = right - left;
+			right--;
+		}
+		else
+		{
+			int k = i - left;
+			if (z[k] < right - i + 1)
+			{
+				z[i] = z[k];
+			}
+			else
+			{
+				left = i;
+				while (right < n && s[right - left] == s[right])
+					right++;
+				z[i] = right - left;
+				right--;
+			}
+		}
+	}
 }
 
 int main()
 {
-    string text, pattern;
-    cout << "Enter the text: ";
-    cin >> text;
-    cout << "Enter the pattern: ";
-    cin >> pattern;
-    string concat = pattern + "$" + text;
-    int n = concat.size();
-    int m = pattern.size();
-    int z[n] = {};
-    getZ(concat, z);
-    for (int i = 0; i < n; ++i)
-    {
-        if (z[i] == m)
-        {
-            cout << "Pattern found at index " << i - m - 1 << "\n";
-        }
-    }
-    return 0;
+	string text, pattern;
+	cout << "Enter the text: ";
+	cin >> text;
+	cout << "Enter the pattern: ";
+	cin >> pattern;
+	string concat = pattern + "$" + text;
+	int n = concat.size();
+	int m = pattern.size();
+	int z[n] = {};
+	getZ(concat, z);
+	for (int i = 0; i < n; ++i)
+	{
+		if (z[i] == m)
+		{
+			cout << "Pattern found at index " << i - m - 1 << "\n";
+		}
+	}
+	return 0;
 }
 
 /*

0297a11c13b1cefb803c529afeb9d19763f68293
Prashant Kumar Gupta <47490316+imPGupta@users.noreply.github.com>
2020-10-03 10:46:41 +0530

    Update cp_sets_question.c

--- C/cp/cp_sets_question.c
+++ C/cp/cp_sets_question.c
@@ -1,5 +1,7 @@
-/* There is an array of n integers. There are also 2 disjoint sets, A and B, each containing m integers. You like all the integers in set A 
-   and dislike all the integers in set B. Your initial happiness is 0. For each i integer in the array, if i e A , you add 1 to your happiness. If i e B, you add -1 to your happiness. Otherwise, your happiness does not change. Output your final happiness at the end.
+/* There is an array of n integers. There are also 2 disjoint sets, A and B, each containing m integers.
+You like all the integers in set A and dislike all the integers in set B. Your initial happiness is 0. 
+For each i integer in the array, if i e A , you add 1 to your happiness. If i e B, you add -1 to your happiness.
+Otherwise, your happiness does not change. Output your final happiness at the end.
 
 	Input Format
 	
@@ -10,51 +12,54 @@
 	Output Format
 	
 	Output a single integer, your total happiness.
+	
+Problem Link: https://www.hackerrank.com/challenges/no-idea/problem
 */
 
 #include<stdio.h>
-int countHappiness(int arr[],int n,int m,int A[],int B[]){
-	int happiness=0,i,j;
-	
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-			
-			if(A[j]==arr[i]){
-				happiness+=1;
-				break;
-			}
-			
-			if(B[j]==arr[i]){
-				happiness-=1;
-				break;
-			}
-		}
-	}
-	return happiness;
+
+int countHappiness(int arr[], int n, int m, int A[], int B[]) {
+  int happiness = 0, i, j;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < m; j++) {
+
+      if (A[j] == arr[i]) {
+        happiness += 1;
+        break;
+      }
+
+      if (B[j] == arr[i]) {
+        happiness -= 1;
+        break;
+      }
+    }
+  }
+  return happiness;
 }
 
-int main(){
-	int i,n,m,arr[100000],A[100000],B[100000],totalHappy;
-	
-	printf("Enter the value of n and m: ");
-	scanf("%d%d",&n,&m);
-	
-	printf("Enter the elements of array: ");
-	for(i=0;i<n;i++)
-	scanf("%d",&arr[i]);
-	
-	printf("Enter the elements of set A: ");
-	for(i=0;i<m;i++)
-	scanf("%d",&A[i]);
-	
-	printf("Enter the elements of set B: ");
-	for(i=0;i<m;i++)	
-	scanf("%d",&B[i]);
-	
-	totalHappy=countHappiness(arr,n,m,A,B);
-	printf("Total happiness= %d",totalHappy);
-	
-	return 0;
+int main() {
+  int i, n, m, arr[100000], A[100000], B[100000], totalHappy;
+
+  printf("Enter the value of n and m: ");
+  scanf("%d%d", & n, & m);
+
+  printf("Enter the elements of array: ");
+  for (i = 0; i < n; i++)
+    scanf("%d", & arr[i]);
+
+  printf("Enter the elements of set A: ");
+  for (i = 0; i < m; i++)
+    scanf("%d", & A[i]);
+
+  printf("Enter the elements of set B: ");
+  for (i = 0; i < m; i++)
+    scanf("%d", & B[i]);
+
+  totalHappy = countHappiness(arr, n, m, A, B);
+  printf("Total happiness= %d", totalHappy);
+
+  return 0;
 }
 
 /*

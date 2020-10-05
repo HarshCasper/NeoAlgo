d65452d2837eea8e44c7a9dbbf80ca8b39c36528
Sharvari Raut <sharuraut7blog@gmail.com>
2020-10-05 16:02:59 +0530

    Update Armstrong_Number.cs

--- C-Sharp/math/Armstrong_Number.cs
+++ C-Sharp/math/Armstrong_Number.cs
@@ -3,11 +3,8 @@ using System;
 public class Armstrong // Class Declaration
 {
   public static void Main(string[] args) {
-    int number,
-    r,
-    sum = 0,
-    temp;
-    Console.Write("Enter the Number to check whether it is Armstrong Number or Not = ");
+    int number, r, sum = 0, temp;
+    Console.Write("Enter the Number to check whether it is an Armstrong Number or Not = ");
     number = int.Parse(Console.ReadLine()); // Getting Number From User    
     temp = number;
     while (number > 0) {
@@ -16,24 +13,23 @@ public class Armstrong // Class Declaration
       number = number / 10;
     }
     // Checking if the number is Armstrong number or not
-    if (temp == sum) Console.Write("{0} Is Armstrong Number.", temp);
-    else Console.Write("{0} Is Not Armstrong Number.", temp);
+    if (temp == sum) Console.Write("{0} Is an Armstrong Number.", temp);
+    else Console.Write("{0} Is Not an Armstrong Number.", temp);
 
   }
 }
 
 /*
 Input:
-Enter the Number to check whether it is Armstrong Number or Not = 153
+Enter the Number to check whether it is an Armstrong Number or Not = 153
 Output:
-153 Is Armstrong Number.
+153 Is an Armstrong Number.
 
 Input:
-Enter the Number to check whether it is Armstrong Number or Not = 788
+Enter the Number to check whether it is an Armstrong Number or Not = 788
 Output:
-788 Is Not Armstrong Number.
+788 Is Not an Armstrong Number.
 
 Time Complexity: O(log(n))
 Space Complexity: O(1)
 */
-

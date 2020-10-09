0e52ec24243cba7ac196adb68a3e005107c7abd0
narayan2111 <narayandwivedi076@gmail.com>
2020-10-09 11:45:57 +0530

    Added New Algorithm

--- C-Plus-Plus/other/Fast_fibonacci_last_digit.cpp
+++ C-Plus-Plus/other/Fast_fibonacci_last_digit.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 using namespace std;
 
+// Constraints for this function is 0≤𝑛≤10^7.
+
 int get_fibonacci_last_digit(long long n)
 {
     int first = 0; // first integer of fibonacci series
@@ -20,11 +22,44 @@ int get_fibonacci_last_digit(long long n)
     return result;
 }
 
+// Below algorithm is taken from Geeksforgeeks
+
+// Finds nth fibonacci number 
+long long int  fib(long long int f[], long long int n) 
+{ 
+    // 0th and 1st number of 
+    // the series are 0 and 1 
+    f[0] = 0; 
+    f[1] = 1; 
+
+    // Add the previous 2 numbers 
+    // in the series and store 
+    // last digit of result 
+    for (long long int i = 2; i <= n; i++) 
+        f[i] = (f[i - 1] + f[i - 2]) % 10; 
+
+    return f[n]; 
+} 
+
+// Returns last digit of n'th Fibonacci Number 
+int findLastDigit(int n) 
+{ 
+    long long int f[60] = {0}; 
+
+    // Precomputing units digit of 
+    // first 60 Fibonacci numbers 
+    fib(f, 60); 
+
+    return f[n % 60]; 
+} 
+
+
 int main()
 {
     int number;
     cin >> number;
-    int result = get_fibonacci_last_digit(number);
+    //int result = get_fibonacci_last_digit(number);
+    int result = findLastDigit(number);
     cout << result << '\n';
 
     return 0;
@@ -37,7 +72,7 @@ Input:
 Output:
 5
 
-Time Complexity: O(n)
-Space Complexity : O(n)
+Time Complexity:O(1) for findLastDigit() and O(n) for get_fibonacci_last_digit()
+Space Complexity : O(1) for findLastDigit() and O(n) for get_fibonacci_last_digit()
 
 */

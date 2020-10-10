182f74a628f2b6555d0e212d73594e8803203231
rjn01 <rajanpuri.07@gmail.com>
2020-10-10 12:13:46 +0530

    updated search_insert_position.py

--- Python/cp/search_insert_position.py
+++ Python/cp/search_insert_position.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Given a sorted array and a target value, return the index if the target is found. 
 If not, return the index where it would be if it were inserted in order.
@@ -9,17 +9,21 @@ Example 1:
 Input: [1,3,5,6], 5
 Output: 2
 
-'''
-#taking the input from the user
+"""
+# taking the input from the user
 
-x = list(map(int, input().strip().split()))
-target = int(input())
+x = list(map(int, input("Enter the elements in the list: ").strip().split()))
+target = int(input("Enter the Element whose index has to be found: "))
 
-#finding whther the target value is in the list or not
+# finding whether the target value is in the list or not
 
 if target in x:
-    print(x.index(target))   #if target is present then printing the index
+    print(
+        "The number is present at the index {}.".format(x.index(target))
+    )  # if target is present then printing the index
 else:
-    x.append(target)   #adding the target in the list
-    x.sort()   	#sorting the list
-    print(x.index(target))   #printing the target index
+    x.append(target)  # adding the target in the list
+    x.sort()  # sorting the list
+    print(
+        "Number not found. It can be inserted at index {}.".format(x.index(target))
+    )  # printing the target index

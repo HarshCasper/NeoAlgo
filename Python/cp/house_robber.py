32902ef0cebcf5181468eb21de434944f5caa795
Md. Shahriar Hasan <45923195+shahriar100@users.noreply.github.com>
2020-10-10 23:37:39 +0600

    update house_robber.py -- Formated (PEP 8 rules)

--- Python/cp/house_robber.py
+++ Python/cp/house_robber.py
@@ -4,23 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if nums ==[] or len(nums) == 0:
+        if nums == [] or len(nums) == 0:
             return 0
-        elif len(nums) == 1: 
+        elif len(nums) == 1:
             return nums[0]
-        
 
-        runningTotal = [-1,-1]
+        runningTotal = [-1, -1]
         runningTotal[0] = nums[0]
         runningTotal[1] = max(nums[0], nums[1])
 
-        for index in range(2, len(nums)):
-            runningTotal.append(max([nums[index] + runningTotal[index - 2], runningTotal[index - 1]]))
-
+        for i in range(2, len(nums)):
+            runningTotal.append(max([nums[i] + runningTotal[i - 2],
+                                runningTotal[i - 1]]))
 
         return runningTotal[-1]
 
 if __name__ == '__main__':
-    nums = [1,2,3,1]
+    nums = [1, 2, 3, 1]
     instance = Solution()
     print(instance.rob(nums))

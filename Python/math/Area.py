57f6c7d1b8dca62a82562ae690a4069e2073afb9
reachpranjal <pranjalpaul.git@gmail.com>
2020-10-12 01:32:58 +0530

    dynamic user input added

--- Python/math/Area.py
+++ Python/math/Area.py
@@ -1,72 +1,45 @@
+#! /usr/bin/env python3
 """
-Find the area of various geometric shapes
+Find the area of various geometric shapes at once
 """
 
 import math
 
 
-def area_rectangle(base, height):
-    """
-    Calculate the area of a rectangle
-    >> area_rectangle(10,20)
-    200
-    """
-    return base * height
+def area_rectangle(length, breadth):
+    return length * breadth
 
 
 def area_square(side_length):
-    """
-    Calculate the area of a square
-    >>> area_square(10)
-    100
-    """
-    return side_length * side_length
+    return side_length ** 2
 
 
-def area_triangle(length, breadth):
-    """
-    Calculate the area of a triangle
-    >>> area_triangle(10,10)
-    50.0
-    """
-    return 1 / 2 * length * breadth
+def area_triangle(base, height):
+    return 0.5 * base * height
 
 
 def area_parallelogram(base, height):
-    """
-    Calculate the area of a parallelogram
-    >> area_parallelogram(10,20)
-    200
-    """
     return base * height
 
 
 def area_trapezium(base1, base2, height):
-    """
-    Calculate the area of a trapezium
-    >> area_trapezium(10,20,30)
-    450
-    """
-    return 1 / 2 * (base1 + base2) * height
+    return 0.5 * (base1 + base2) * height
 
 
 def area_circle(radius):
-    """
-    Calculate the area of a circle
-    >> area_circle(20)
-    1256.6370614359173
-    """
-    return math.pi * radius * radius
+    return math.pi * (radius **2)
 
 
 def main():
+    i, j, k = [float(x) for x in input("Enter three value: ").split()]
     print("Areas of various geometric shapes: \n")
-    print(f"Rectangle: {area_rectangle(10, 20)=}")
-    print(f"Square: {area_square(10)=}")
-    print(f"Triangle: {area_triangle(10, 10)=}")
-    print(f"Parallelogram: {area_parallelogram(10, 20)=}")
-    print(f"Trapezium: {area_trapezium(10, 20, 30)=}")
-    print(f"Circle: {area_circle(20)=}")
+    print("Rectangle (length= {}, breadth= {}): {}".format(i, j, area_rectangle(i, j)))
+    print("Square (side= {}): {}".format(i, area_square(i)))
+    print("Triangle (base= {}, height= {}): {}".format(i, j, area_triangle(i, j)))
+    print("Parallelogram (base= {}, height= {}): {}".format(i, j, area_parallelogram(i, j)))
+    print("Trapezium (base1= {}, base2 = {}, height= {}): {}".format(i, j, k, area_trapezium(i, j, k)))
+    print("Circle (radius= {}): {}".format(j, area_circle(j)))
+    print(i, j, k)
 
 
 if __name__ == "__main__":
--- Python/math/GCD_Euclidean_Algorithm.py
+++ Python/math/GCD_Euclidean_Algorithm.py
@@ -4,5 +4,5 @@ def calculate_gcd(a, b):
     return a
 
 if __name__ == '__main__':
-    print(calculate_gcd(36, 20))  # Should print 4
-    print(calculate_gcd(16, 24))  # Should print 8
+    i, j = input().split()
+    print(calculate_gcd(i, j))  # 36, 20Should print 4

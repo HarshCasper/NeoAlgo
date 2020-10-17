f000283efd5825ac4f924000efa6c62b3feb964e
Saloni Shah <saloshah01@gmail.com>
2020-10-17 09:31:54 +0530

    Update TernarySearch.c

--- C/search/TernarySearch.c
+++ C/search/TernarySearch.c
@@ -27,25 +27,35 @@ int ternarySearchIterative(int left, int right, int key, int array[])
 
 int ternarySearchRecursive(int left, int right, int key, int array[]) 
 { 
+    // Find the mid1 and mid2 
     if (right >= left) { 
         int mid1 = left + (right - left) / 3; 
-        int mid2 = right - (right - left) / 3; 
+        int mid2 = right - (right - left) / 3;
+      // Check if key is present at any mid 
         if (array[mid1] == key) { 
             return mid1; 
         } 
         if (array[mid2] == key) { 
             return mid2; 
         }
+        // Since key is not present at mid, 
+        // check in which region it is present 
+        // then repeat the Search operation 
+        // in that region 
         if (key < array[mid1]) { 
+          // The key lies in between l and mid1 
             return ternarySearchRecursive(left, mid1 - 1, key, array); 
         } 
         else if (key > array[mid2]) { 
+          // The key lies in between mid2 and r 
             return ternarySearchRecursive(mid2 + 1, right, key, array); 
         } 
         else { 
+          // The key lies in between mid1 and mid2 
             return ternarySearchRecursive(mid1 + 1, mid2 - 1, key, array); 
         } 
     } 
+  // Key not found 
     return -1; 
 } 
 

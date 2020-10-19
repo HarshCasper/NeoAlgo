bcf6b3d9222316fca93343c5c1f443446b3299fa
Parjanya Aditya Shukla <46294122+parjanyaacoder@users.noreply.github.com>
2020-10-19 02:02:03 -0700

    Update TernarySearch.java

--- Java/search/TernarySearch.java
+++ Java/search/TernarySearch.java
@@ -2,20 +2,20 @@ import java.util.*;
 public class Main
 {  
    // Recursive Approach
-  static int ternarySearchRecursive (int arr[], int f, int l, int h)
+  static int ternarySearchRecursive (int arr[], int f, int left, int right)
   {
-    if (l <= h)
+    if (left <= right)
       {
-	int mid1 = l + (h - l) / 3;
-	int mid2 = h - (h - l) / 3;
+	int mid1 = left + (right - left) / 3;
+	int mid2 = right - (right - left) / 3;
 	if (arr[mid1] == f)
 	  return mid1 + 1;
 	else if (arr[mid2] == f)
 	  return mid2 + 1;
 	else if (f < arr[mid1])
-	    return ternarySearchRecursive (arr, f, l, mid1 - 1);
+	    return ternarySearchRecursive (arr, f, left, mid1 - 1);
 	else if (f > arr[mid2])
-	    return ternarySearchRecursive (arr, f, mid2 + 1, h);
+	    return ternarySearchRecursive (arr, f, mid2 + 1, right);
 	else
 
 	    return ternarySearchRecursive (arr, f, mid1 + 1, mid2 - 1);
@@ -23,24 +23,24 @@ public class Main
     return -1;
   }
     // Iterative Approach
- static int ternarySearch(int arr[],int f,int l,int h)
+ static int ternarySearch(int arr[],int f,int left,int right)
     {  int found = -1;
-        while(l<=h)
+        while(left<=right)
         {
-          int  mid1 = l + (h-l)/3;
-          int  mid2 = h - (h-l)/3; 
+          int mid1 = left + (right - left) / 3;
+	int mid2 = right - (right - left) / 3;
             if(arr[mid1]==f)
                 return mid1+1;
             else if(arr[mid2]==f)
                 return mid2+1;
             else if(f<arr[mid1])
-             h = mid1-1;
+             right = mid1-1;
             else if(f>arr[mid2])
-               l =mid2+1;
+               left =mid2+1;
             else 
               {
-                  l=mid2+1;
-                  h=mid2-1;
+                  left=mid2+1;
+                  right=mid2-1;
               }
             
         } 
@@ -52,7 +52,7 @@ public class Main
   public static void main (String[]args)
   {
     Scanner sc = new Scanner (System.in);
-    int i, j, k, l, m, n, h, mid1, mid2, f = -1, element;
+    int i,left,n, right,f = -1, element;
     System.out.println ("Enter length of array");
     n = sc.nextInt ();
     System.out.println ("Enter  array elements");
@@ -63,10 +63,10 @@ public class Main
     element = sc.nextInt ();
 
     Arrays.sort (arr);
-    l = 0;
-    h = n - 1;
+    left = 0;
+    right = n - 1;
 
-    f = ternarySearch(arr, element, l, h);
+    f = ternarySearch(arr, element, left, right);
     if (f != -1)
       System.out.println ("Element found at " + (f));
     else

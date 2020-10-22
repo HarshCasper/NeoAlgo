2555733ecb704c60cba6e691acda6febcd0fa3ba
hardik chitkara <56203317+hardikchitkara@users.noreply.github.com>
2020-10-23 00:28:20 +0530

    Update check_palindrome.cpp

--- C-Plus-Plus/math/check_palindrome.cpp
+++ C-Plus-Plus/math/check_palindrome.cpp
@@ -20,9 +20,17 @@ int main()
          cout << " The number is a palindrome.";
      else
          cout << " The number is not a palindrome.";
-     //sampleinput
+     //sample input 1
      //232
+     //sample output 1
+     // The number is a palindrome.
+     //sample input 2
      //456
+     //sample output 2
+     // The number is not a palindrome.
+     //sample input 3
      //12321
+     //sample output 3
+     // The number is a palindrome.
     return 0;
 }

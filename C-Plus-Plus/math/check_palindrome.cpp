47d09651ad58185e0f97f2c7405701cf3e625ed9
hardik chitkara <56203317+hardikchitkara@users.noreply.github.com>
2020-10-23 19:16:38 +0530

    updated comments
    added comments after the code and used the appropriate multiline comment

--- C-Plus-Plus/math/check_palindrome.cpp
+++ C-Plus-Plus/math/check_palindrome.cpp
@@ -1,3 +1,4 @@
+/*palindrome number is the number which when reversed remains the same number*/
 #include <iostream>
 using namespace std;
 
@@ -18,17 +19,18 @@ int main()
          cout << " The number is a palindrome.";
      else
          cout << " The number is not a palindrome.";
-    return 0;
-      //sample input 1
-     //232
-     //sample output 1
-     // The number is a palindrome.
-     //sample input 2
-     //456
-     //sample output 2
-     // The number is not a palindrome.
-     //sample input 3
-     //12321
-     //sample output 3
-     // The number is a palindrome.
-}
+    return 0;}
+/*
+      sample input 1
+      Enter a positive number: 232
+      sample output 1
+      The number is a palindrome.
+      sample input 2
+      Enter a positive number: 456
+      sample output 2
+      The number is not a palindrome.
+      sample input 3
+      Enter a positive number: 12321
+      sample output 3
+      The number is a palindrome.
+*/

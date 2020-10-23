e9afc3b47090bc5e341bb084c03b167515b52413
AnshikaY <46274445+AnshikaY@users.noreply.github.com>
2020-10-23 22:04:59 +0530

    Update Max_subarray_sum(DP).cpp

--- C-Plus-Plus/dp/Max_subarray_sum(DP).cpp
+++ C-Plus-Plus/dp/Max_subarray_sum(DP).cpp
@@ -10,18 +10,15 @@ int main(){
     while(t--){
         cin >> n;
         long long int a[n];
-        
         for(i=0;i<n;i++)            //input array
         cin >> a[i];
         
         long long int pre_sum[n];         
         pre_sum[0] = a[0];
-        
         for(i=1;i<n;i++)                       //array that stores prefix sum of input array
         pre_sum[i] = pre_sum[i-1] + a[i];
         
         long long int res = INT_MIN, min_pre_sum = 0;
-        
         for(i=0;i<n;i++){
             res = max(res, pre_sum[i] - min_pre_sum);               //loop through array while keeping track of minimum prefix sum so far and maximum subarray sum
             min_pre_sum = min(min_pre_sum, pre_sum[i]);

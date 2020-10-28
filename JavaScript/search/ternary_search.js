90b9216a4d8f3b0eeefb49f42b0b2b3b2beee509
ShishirBhandari <shishirbhandari54@gmail.com>
2020-10-28 20:09:41 +0545

    Implement ternary search recursive algorithm

--- JavaScript/search/ternary_search.js
+++ JavaScript/search/ternary_search.js
@@ -12,13 +12,13 @@ function ternary_search_iterative(leftIndex, rightIndex, key, array) {
         // Find two mid values
         let mid1 = leftIndex + ((rightIndex - leftIndex) / 3);
         let mid2 = rightIndex - ((rightIndex - leftIndex) / 3);
-        
+
         // If the key is in one of the mid values, return its index
         if(key == array[mid1])
             return mid1;
         else if(key == array[mid2])
             return mid2;
-        
+
         // Check which part of the array the key is present
         // and update the leftIndex and rightIndex values accordingly
         if(key < array[mid1]) {
@@ -34,5 +34,36 @@ function ternary_search_iterative(leftIndex, rightIndex, key, array) {
     }
 
     // The key does not exist
+    return -1;
+}
+
+// Search an element in an array using Ternary Search algorithm (recursive)
+// Prerequisite: Sorted array should be passed
+function ternary_search_recursive(leftIndex, rightIndex, key, array) {
+    if(rightIndex >= leftIndex) {
+        // Find the two mid values
+        let mid1 = leftIndex + ((rightIndex - leftIndex) / 3);
+        let mid2 = rightIndex - ((rightIndex - leftIndex) / 3);
+
+        // If the key is in one of the mid values, return its index
+        if(key == array[mid1])
+            return mid1;
+        else if(key == array[mid2])
+            return mid2;
+
+        // Check which part of the array the key is present
+        // and recall the function using updated values accordingly
+        if(key < array[mid1]) {
+            return ternary_search_recursive(leftIndex, mid1 - 1, key, array);
+        }
+        else if(key > array[mid2]) {
+            return ternary_search_recursive(mid2 + 1, rightIndex, key, array);
+        }
+        else {
+            return ternary_search_recursive(mid1 + 1, mid2 - 1, key, array);
+        }
+    }
+
+    // The key doesn't exists
     return -1;
 }

e476f27d044ab196a3317170c11ce2f6e4707cea
yukty2000 <yukty2000@gmail.com>
2020-12-11 11:30:22 +0530

    Issue-#1436 Add code for number of binary trees to dp folder for Winter Of Code

---
+++ C-Plus-Plus/dp/number_of_binary_trees.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define N 1000000
+using namespace std;
+typedef long long int lli;
+typedef pair<int,int> pii;
+typedef tuple<int,int,int> tii;
+// Find the number of binary trees possible given the size of the preorder sequence-n
+// O(n^2) complexity - Dynamic Programming Approach
+int main()
+{
+    int n;
+    cin>>n;
+    lli *dp=(lli *)malloc((n+1)*sizeof(lli));
+    dp[0]=1,dp[1]=1;
+    //Since number of trees that can be formed for n=0/1 is 1
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=0;
+        for(int j=0;j<i;j++)
+        {
+            dp[i]+=(dp[j]*dp[i-j-1]);
+        }
+    }
+    //Calculation is same to that of catalan number
+    /*for(int i=0;i<=n;i++)
+        cout<<dp[i]<<" ";
+    cout<<endl;*/
+    cout<<dp[n]<<endl;
+    free(dp);
+    return 0;
+}
+/* Sample input:-
+        n = 5;
+        Output = 42;
+        */

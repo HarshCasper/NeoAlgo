beb0de8d62da08b963f5de63e00f0d0506aaacf3
Kriti <69384441+itskrypton@users.noreply.github.com>
2020-12-11 18:16:55 +0530

    Update Boyer_Moore_Majority_Vote.cpp

--- C-Plus-Plus/Algorithms/Boyer_Moore_Majority_Vote.cpp
+++ C-Plus-Plus/Algorithms/Boyer_Moore_Majority_Vote.cpp
@@ -1,70 +1,41 @@
-// C++ Program for finding out 
-// Majority Element in an array  
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-/* Function to find the candidate with the Majority */
-int findCandidate(int a[], int size) 
-{ 
-    int majority_element = 0, count = 1; 
-    for (int i = 1; i < size; i++) { 
-        if (a[majority_element] == a[i]) 
-            count++; 
-        else
-            count--; 
-        if (count == 0) { 
-            majority_element = i; 
-            count = 1; 
-        } 
-    } 
-    return a[majority_element]; 
-} 
-  
-/* Function to check if the candidate 
-   occurs more than n/2 times */
-bool isMajority(int a[], int size, int cand) 
-{ 
-    int count = 0; 
-    for (int i = 0; i < size; i++) 
-  
-        if (a[i] == cand) 
-            count++; 
-  
-    if (count > size / 2) 
-        return 1; 
-  
-    else
-        return 0; 
-} 
-  
-/* Function to print the Majority Element */
-void printMajority(int a[], int size) 
-{ 
-    /* Find the candidate for Majority*/
-    int cand = findCandidate(a, size); 
-  
-    /* Print the candidate if it is Majority*/
-    if (isMajority(a, size, cand)) 
-        cout << " " << cand << " "; 
-  
-    else
-        cout << "No Majority Element"; 
-} 
-  
-/* Driver code */
-/* int main() 
-{ 
-    int a[] = { 1, 3, 3, 1, 2 }; 
-    int size = (sizeof(a)) / sizeof(a[0]); 
-  
-    // Function calling 
-    printMajority(a, size); 
-  
-    return 0; 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+ 
+// Function to find majority element
+int majorityElement(vector<int> A)
+{
+    // creation of an empty map
+    unordered_map<int, int> map;
+ 
+    // input size
+    int n = A.size();
+ 
+for (int i = 0; i < n; i++)
+    {
+        if (++map[A[i]] > n/2)
+            return A[i];
+    }
+    // return -1 if no majority element is present
+    return -1;
 }
+ 
 
-Output - No Majority Element */
+/* Test Case: 
+int main()
+{
+    vector<int> vec = { 2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2 };
+ 
+    int res = majorityElement(vec);
+    if (res != -1)
+        cout << "Majority element is " << res;
+    else
+        cout << "Majority element doesn't exists";
+ 
+    return 0;
+} */
+// The majority element is 2
 
-/* Time Complexity: O(n). The time complexity is linear.
-Auxiliary Space: O(1).  No extra space is required */
-
+// Time Complexity is O(n)
+// Space Complexity is O(n)

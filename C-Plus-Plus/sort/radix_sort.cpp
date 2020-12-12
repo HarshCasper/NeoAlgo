2c96d3fd7137c60eac41e32e95b8210766cfa94b
Yashwin <esy10@iitbbs.ac.in>
2020-12-12 10:23:47 +0530

    All changes requested are added

--- C-Plus-Plus/sort/radix_sort.cpp
+++ C-Plus-Plus/sort/radix_sort.cpp
@@ -1,14 +1,15 @@
-// Radix sort is based on counting sort
-// This sort is used for non-negative elements
-// Assuming the range is fixed, as int capacity
-// is limited in any language, this sort takes
-// Time complexity : O(n)
-// Space complexity : O(n)
-// MAX_INT contains 10 decimal values
-// If range is not fixed, this sort takes O(kn) time
-// where k is #decimal places in the number
-// This works bcz of the nature of counting sort
-// Counting sort is a stable sort
+/** 
+Radix sort is based on counting sort
+This sort is used for non-negative elements
+Assuming the range is fixed, as int capacity
+is limited in any language, this sort takes
+
+MAX_INT contains 10 decimal values
+If range is not fixed, this sort takes O(kn) time
+where k is #decimal places in the number
+This works because of the nature of counting sort
+Counting sort is a stable sort
+**/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -17,19 +18,19 @@ using namespace std;
 #define M 10
 
 void counting_sort(int arr[], int  n, int p) {
-	int ele = pow(10, p);
-	int divs = ele / 10;
+	int element = pow(10, p);
+	int divs = element / 10;
 	int count[M];
 	for(int i = 0; i < M; i++)
 		count[i] = 0;
 	for(int i = 0; i < n; i++)
-		count[(arr[i] % ele) / divs] ++;
+		count[(arr[i] % element) / divs] ++;
 	for(int i = 1; i < M; i++)
 		count[i] += count[i-1];
 	int op[n];
 	for(int i = n-1; i >= 0; i--) {
-	    op[count[(arr[i] % ele) / divs] - 1] = arr[i];
-	    count[(arr[i] % ele) / divs] --;
+	    op[count[(arr[i] % element) / divs] - 1] = arr[i];
+	    count[(arr[i] % element) / divs] --;
 	}
 	// copying the array back
 	for(int i = 0; i < n; i++)
@@ -56,3 +57,14 @@ int main() {
 	cout << '\n';
 	return 0;
 }
+
+/**
+Input : 
+8
+175 45 75 90 802 24 2 66
+Output :
+2 24 45 66 75 90 175 802
+
+Time complexity : O(n)
+Space complexity : O(n)
+**/

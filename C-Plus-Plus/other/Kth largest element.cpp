ee34b9a7a7335341ac73faded11667618a042cbf
Kumar Shashank <1711shashank@gmail.com>
2020-12-12 13:15:47 +0530

    resloved changes

--- C-Plus-Plus/other/Kth largest element.cpp
+++ C-Plus-Plus/other/Kth largest element.cpp
@@ -1,41 +1,49 @@
-//Q Kth largest element 
+/*
+Q Kth largest element 
 
-// Given an array a[] and a number K where K is smaller than size of array, 
-// the task is to find the Kth largest element in the given array.
-// It is given that all array elements are distinct.
+Given an array a[] and a number K where K is smaller than size of array, 
+the task is to find the Kth largest element in the given array.
+It is given that all array elements are distinct. */
 
-//Input 
-// n=7 k=3
-// 7 5 9 8 6 4 10
-// Output
-// 8
 
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
+//find the kth largest Element
+void kthLargestElement(int a[], int size, int k){
+
+    priority_queue <int, vector<int>, greater<int>> minHeap;
+    
+    for(int i = 0; i < size; i++){
+        minHeap.push(a[i]);
+        
+        if(minHeap.size() > k){
+            minHeap.pop();
+        }
+    }
+    cout<< minHeap.top();
+}
+
 int main()
 {
-    int n, k;
-    cin >> n >> k;
-    int a[n];
+    int size, k; 
+    cin >> size >> k;
+    int a[size];
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < size; i++){
         cin >> a[i];
     }
 
     // a={7, 5, 9, 8, 6, 4, 10}
-        
-    priority_queue<int, vector<int>, greater<int>> minh;
-    
-    for(int i = 0; i < n; i++){
-        minh.push(a[i]);
-        
-        if(minh.size() > k){
-            minh.pop();
-        }
-    }
-    cout<< minh.top();
+
+    kthLargestElement(a,size,k);  
     
     return 0;
 }
+
+// Input 
+// size=7 k=3
+// 7 5 9 8 6 4 10
+// Output
+// 8
--- C-Plus-Plus/other/Kth smallest element.cpp
+++ C-Plus-Plus/other/Kth smallest element.cpp
@@ -1,42 +1,48 @@
-//Q Kth smallest element 
+/* Q Kth smallest element 
 
-// Given an array a[] and a number K where K is smaller than size of array, 
-// the task is to find the Kth smallest element in the given array.
-// It is given that all array elements are distinct.
+Given an array a[] and a number K where K is smaller than size of array, 
+the task is to find the Kth smallest element in the given array.
+It is given that all array elements are distinct. */
 
-//Input 
-// n=7 k=3
-// 7 5 9 8 6 4 10
-// Output
-// 6
 
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n,k;
-    cin >> n >> k;
-    int a[n];
+//find the kth samllest Element
+void kthSmallestElement(int arr[], int size, int k){
 
-    for (int i = 0; i < n; i++){
-        cin >> a[i]; 
-    } 
-
-    // a={7, 5, 9, 8, 6, 4, 10}  
-
-    priority_queue<int> maxh;
+    priority_queue <int> maxHeap;
     
-    for(int i = 0; i < n; i++){
-        maxh.push(a[i]);
+    for(int i = 0; i < size; i++){
+        maxHeap.push(arr[i]);
         
-        if(maxh.size() > k){
-            maxh.pop();
+        if(maxHeap.size() > k){
+            maxHeap.pop();
         }
     }
-    
-    cout<< maxh.top();
+    cout<< maxHeap.top();
+}
+
+int main()
+{
+    int size, k; 
+    cin >> size >> k;
+    int arr[size];
+
+    for (int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+
+    // arr={7, 5, 9, 8, 6, 4, 10}
+
+    kthSmallestElement(arr, size, k);  
     
     return 0;
 }
+
+// Input 
+// size=7 k=3
+// 7 5 9 8 6 4 10
+// Output
+// 6

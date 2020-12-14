4ff34f06011ba9a216b736ed9604f64633ec1618
Atri Adhikari <atriadhikarinsec@gmail.com>
2020-12-14 11:18:17 +0530

    Changed Floyd Warshall Algorithm and Knight Walk

--- Python/graphs/Knight_Walk.py
+++ Python/graphs/Knight_Walk.py
@@ -1,6 +1,6 @@
 # Importing sys module to initalize the maxsize
 import sys
-# Importing collection model to acess deque
+# Importing collections model to access deque
 from collections import deque
 ''' Knight-Walk '''
 '''Problem Statement :Given a chess board of order N x M and
@@ -19,22 +19,17 @@ INF = 9999999
 row = [2, 2, -2, -2, 1, 1, -1, -1]
 col = [-1, 1, 1, -1, 2, -2, 2, -2]
 # Create the Node
-
-
 class Node:
     def __init__(self, x, y, dis=0):  # Distance of self from itself
         self.x = x
         self.y = y
         self.dis = dis
-
     # Since we are using the nodes as a hash we will need a
     # hashtable
     def __hash__(self):
         return hash((self.x, self.y, self.dis))
-
     def __eq__(self, other):
         return (self.x, self.y, self.dis) == (other.x, other.y, other.dis)
-
 # Enter the number of rows
 print("Enter the number of matrix : ")
 n = int(input("Row:"))
@@ -72,7 +67,6 @@ def kinghtwalk(n, src, des):
                 b = y + row[i]
                 if present(a, b, n):
                     queue.append(Node(a, b, dis + 1))
-
 # Check if the given node is valid or not
                     
                     
@@ -80,7 +74,6 @@ def present(a, b, n):
     # If either one of then is not true it mean the cell is
     # outside the board
     return not(a < 0 or y < 0 or x >= n or y >= n)
-
 KW = kinghtwalk(n, src, des)
 print("Minumun nunbers of paths are {}".format(KW))
 ''' 
@@ -97,4 +90,3 @@ Sample Output:
     Minumun nunbers of paths are 6
 '''
 # Time complexity: O(N^2)
-

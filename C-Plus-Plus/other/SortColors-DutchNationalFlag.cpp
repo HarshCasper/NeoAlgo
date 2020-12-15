183ad0fe252a8cfcbae015dc7efb35ea8221ec1b
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-15 22:35:19 +0530

    link to the problem added

--- C-Plus-Plus/other/SortColors-DutchNationalFlag.cpp
+++ C-Plus-Plus/other/SortColors-DutchNationalFlag.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 //                   
-// PROBLEM STATEMENT
+// PROBLEM STATEMENT- https://leetcode.com/problems/sort-colors/
 
 // Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 // Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.

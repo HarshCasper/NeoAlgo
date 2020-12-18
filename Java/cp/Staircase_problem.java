c6aeffbe7341aef02b0e10d0f1b266b5fd9940eb
Shubham Patel <shubhamashishpatel@gmail.com>
2020-12-18 11:34:35 +0530

    Formatted code

--- Java/cp/Staircase_problem.java
+++ Java/cp/Staircase_problem.java
@@ -1,25 +1,22 @@
-// This program finds the total number of possible combinations that can be used to
-// climb statirs . EG : for 3 stairs ,combination and output will be 1,1,1 , 1,2 , 2,1 i.e 3 . 
+/*This program finds the total number of possible combinations that can be used to
+climb statirs . EG : for 3 stairs ,combination and output will be 1,1,1 , 1,2 , 2,1 i.e 3 . */
 import java.util.Scanner;
 
 class Staircase_problem{
     public static void main(String args[]){
         int count_stairs=0;
         Scanner input = new Scanner(System.in);
-        //Asking for number of steps to find combination.
         System.out.println("Enter total number of Stairs:"); 
         count_stairs = input.nextInt();
         Staircase_problem sp = new Staircase_problem();
-        // Making object of this class and passing stair number to  the function.
         int final_steps = sp.possibilities_count(count_stairs); 
-        //Printing the number of combination which is given by the function.
         System.out.println("Total Number of possible Combinations = "+final_steps);
     }
 
     int possibilities_count(int a){
         int result = a;
-        //Using Recursion to find the total number of stairs as f(a) = f(a-1) + a(a-2) 
-        // where f(a) is the final result 
+        /*Using Recursion to find the total number of stairs as f(a) = f(a-1) + a(a-2) 
+        where f(a) is the final result */
         if(result <= 1){
             result =  1;
         }else{

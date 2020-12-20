6179c82739ae8badfd222a0a1eb85bc484ef7712
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-20 23:33:15 +0530

    bellman ford proper indentation

--- C-Plus-Plus/graphs/BellmanFord.cpp
+++ C-Plus-Plus/graphs/BellmanFord.cpp
@@ -1,21 +1,21 @@
 
-// Input:
-// First line contains two space separated integers,(N,M) 
-// N- no of vertices, M- no of edges.
-// Then M lines follow, each line has 3 space separated integers ui ,vi ,wi which denotes edge from vertex ui to vi with weight wi.
-// Find shortest distance from vertex 1 to all other vertices.
+/*Input:
+First line contains two space separated integers,(N,M) 
+N- no of vertices, M- no of edges.
+Then M lines follow, each line has 3 space separated integers ui ,vi ,wi which denotes edge from vertex ui to vi with weight wi.
+Find shortest distance from vertex 1 to all other vertices.
 
 
-// TEST CASE
-// 5 5
-// 1 2 5
-// 1 3 2
-// 3 4 1
-// 1 4 6
-// 3 5 5
+TEST CASE
+5 5
+1 2 5
+1 3 2
+3 4 1
+1 4 6
+3 5 5
 
-//OUTPUT
-// 5 2 3 7
+OUTPUT
+5 2 3 7 */
 
 
 #include<bits/stdc++.h>
@@ -27,48 +27,49 @@ vector<int>edges[1000001];
 int dist[1000001];
 
 void bellmanFord(int n){
-    for(ll i=0;i<n-1;i++){
-        int k=0;
-        while(edges[k].size()>0){
-            ll u=edges[k][1],v=edges[k][2],w=edges[k][0];
-            if(dist[v]>dist[u]+w){
-                dist[v]=dist[u]+w;
-            }
-            k++;
-        }
+  for(ll i=0;i<n-1;i++){
+    int k=0;
+    while(edges[k].size()>0){
+      ll u=edges[k][1],v=edges[k][2],w=edges[k][0];
+      if(dist[v]>dist[u]+w){
+        dist[v]=dist[u]+w;
+      }
+      k++;
     }
+  }
 }
 
 int main(){
-    tezz_chal;
-    int n,m;
-    cin>>n>>m;
-    for(int i=0;i<n;i++){
-        edges[i].clear();
-        dist[i]=1e9;
-    }
+  tezz_chal;
+  int n,m;
+  cin>>n>>m;
+  for(int i=0;i<n;i++){
+    edges[i].clear();
+    dist[i]=1e9;
+  }
     
-    for(int i=0;i<m;i++){
-        int u,v,w;
-        cin>>u>>v>>w;
-        edges[i].push_back(w);
-        edges[i].push_back(u-1);
-        edges[i].push_back(v-1);
-    }
-    dist[0]=0;
+  for(int i=0;i<m;i++){
+    int u,v,w;
+    cin>>u>>v>>w;
+    edges[i].push_back(w);
+    edges[i].push_back(u-1);
+    edges[i].push_back(v-1);
+  }
+  dist[0]=0;
 
-    bellmanFord(n);
+  bellmanFord(n);
 
-    cout<<"Shortest distance to all vertices from vertex 1 is- "<<endl;
-    for(int i=1;i<n;i++){
-       cout<<dist[i]<<" ";
-    }
+  cout<<"Shortest distance to all vertices from vertex 1 is- "<<endl;
+  for(int i=1;i<n;i++){
+  cout<<dist[i]<<" ";
+  }
 }
+/*
+Time- complexity
+    O(VE)
+    If there are 'n' vertices, then there can be n(n-1)/2 edges thus the worst case time complexity is O(n3).
+Space Complexity-
+    O(V)
 
-// Time- complexity
-//     O(VE)
-//     If there are 'n' vertices, then there can be n(n-1)/2 edges thus the worst case time complexity is O(n3).
-// Space Complexity-
-//     O(V)
-
-// Bellman Ford algorithm can handle negative weights which dijkstra couldn't. However it cannot handle negative cycles and have more complexity than Dijkstra.
+Bellman Ford algorithm can handle negative weights which dijkstra couldn't. However it cannot handle negative cycles and have more complexity than Dijkstra.
+*/

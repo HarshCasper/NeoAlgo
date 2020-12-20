48408503b2fdf011b29c113e2ebde91548fbc4b6
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-20 23:11:28 +0530

    indentation made consistent

--- C-Plus-Plus/cryptography/VernamCipher.cpp
+++ C-Plus-Plus/cryptography/VernamCipher.cpp
@@ -8,54 +8,54 @@ In this algorithm the size of the key and plain text must be same. */
 using namespace std;
 
 string encrypt(string plain,string key){
-	string enc="";
-	int n=key.length();
-    for(int i=0;i<n;i++){
-		int y = plain[i]+key[i]-130;
-		if(y>=26) {
+  string enc="";
+  int n=key.length();
+  for(int i=0;i<n;i++){
+    int y = plain[i]+key[i]-130;
+    if(y>=26) {
       y = y-26;
     }
-		enc+=char(y+65);
-	}
-	return enc;
+    enc+=char(y+65);
+  }
+  return enc;
 }
 
 string decrypt(string enc,string key){
-	string dec="";
-	int n=enc.length();
-	for(int i=0;i<n;i++){
-		if(enc[i]>=key[i]){
-			dec+=char(enc[i]-key[i]+65);
-		}
-		else{
-			dec+=char(enc[i]-key[i]+91);  
-		}
-	}
-	return dec;
-	
+  string dec="";
+  int n=enc.length();
+  for(int i=0;i<n;i++){
+    if(enc[i]>=key[i]){
+      dec+=char(enc[i]-key[i]+65);
+    }
+    else{
+      dec+=char(enc[i]-key[i]+91);  
+    }
+  }
+  return dec;
 }
+
 int main(){
-	string plain,encrypted,decrypted,key;
-	cout<<"USE UPPERCASE LETTERS"<<endl;
-	cout<<"Enter the Key: ";
-	cin>>key;
+  string plain,encrypted,decrypted,key;
+  cout<<"USE UPPERCASE LETTERS"<<endl;
+  cout<<"Enter the Key: ";
+  cin>>key;
   transform(key.begin(), key.end(), key.begin(), ::toupper);
-	
-	cout<<"\nEnter the plain text of same size as that of key: ";
-	cin>>plain;
+
+  cout<<"\nEnter the plain text of same size as that of key: ";
+  cin>>plain;
   transform(plain.begin(), plain.end(), plain.begin(), ::toupper);
 	
-	int n=key.length();
+  int n=key.length();
   if(plain.length()<key.length()){
     cout<<"Invalid";
     return 0;
   }
 	
-	encrypted = encrypt(plain,key);
-	cout<<"\nCipher text: "<<encrypted<<endl;
+  encrypted = encrypt(plain,key);
+  cout<<"\nCipher text: "<<encrypted<<endl;
 	
-	decrypted = decrypt(encrypted,key);
-	cout<<"Decrypted text: "<<decrypted<<endl;	
+  decrypted = decrypt(encrypted,key);
+  cout<<"Decrypted text: "<<decrypted<<endl;	
 }
 
 

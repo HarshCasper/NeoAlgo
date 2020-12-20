749671e487baf565d755164791ecd3a44ce68638
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-20 23:01:04 +0530

    made the specified changes

--- C-Plus-Plus/cryptography/VernamCipher.cpp
+++ C-Plus-Plus/cryptography/VernamCipher.cpp
@@ -1,7 +1,7 @@
-//Vernam Cipher
+/* Vernam Cipher
 
-// Vernam Cipher is a symmetric key cryptographic algorithm and is one of the transposition technique that converts plain text into ciphertext.
-// In this algorithm the size of the key and plain text must be same.
+Vernam Cipher is a symmetric key cryptographic algorithm and is one of the transposition technique that converts plain text into ciphertext.
+In this algorithm the size of the key and plain text must be same. */
 
 
 #include<bits/stdc++.h>
@@ -12,7 +12,9 @@ string encrypt(string plain,string key){
 	int n=key.length();
   for(int i=0;i<n;i++){
 		int y = plain[i]+key[i]-130;
-		if(y>=26) y = y-26;
+		if(y>=26) {
+      y = y-26;
+    }
 		enc+=char(y+65);
 	}
 	return enc;
@@ -26,7 +28,7 @@ string decrypt(string enc,string key){
 			dec+=char(enc[i]-key[i]+65);
 		}
 		else{
-			dec+=char(enc[i]-key[i]+91);  // enc[i]-key[i]+26+65
+			dec+=char(enc[i]-key[i]+91);  
 		}
 	}
 	return dec;
@@ -37,11 +39,11 @@ int main(){
 	cout<<"USE UPPERCASE LETTERS"<<endl;
 	cout<<"Enter the Key: ";
 	cin>>key;
-  transform(key.begin(), key.end(), key.begin(), ::toupper); // convert to uppercase if not.
+  transform(key.begin(), key.end(), key.begin(), ::toupper);
 	
 	cout<<"\nEnter the plain text of same size as that of key: ";
 	cin>>plain;
-  transform(plain.begin(), plain.end(), plain.begin(), ::toupper);  // convert to uppercase if not.
+  transform(plain.begin(), plain.end(), plain.begin(), ::toupper);
 	
 	int n=key.length();
   if(plain.length()<key.length()){
@@ -57,11 +59,11 @@ int main(){
 }
 
 
-// Output
-// USE UPPERCASE LETTERS
-// Enter the Key: TESSERACTCODING
+/* Output
+USE UPPERCASE LETTERS
+Enter the Key: TESSERACTCODING
 
-// Enter the plain text of same size as that of key: OPENSOURCESTART
+Enter the plain text of same size as that of key: OPENSOURCESTART
 
-// Cipher text: HTWFWFUTVGGWIEZ
-// Decrypted text: OPENSOURCESTART
+Cipher text: HTWFWFUTVGGWIEZ
+Decrypted text: OPENSOURCESTART */

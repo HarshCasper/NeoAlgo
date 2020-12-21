82fb587c6ee47e9988e52383df5deb5849574803
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-21 19:52:40 +0530

    made the specified changes

--- C-Plus-Plus/other/SortColors-DutchNationalFlag.cpp
+++ C-Plus-Plus/other/SortColors-DutchNationalFlag.cpp
@@ -1,36 +1,26 @@
+/*                  
+PROBLEM STATEMENT- https://leetcode.com/problems/sort-colors/
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+In simpler words sort an array containing 0,1,2.
+
+Solve the problem with a one-pass algorithm using only O(1) constant space?
+
+The bruteforce approach - Sort the vector in nlogn time
+
+                                ************************DUTCH_NATIONAL_FLAG ALGORITHM***********************************
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
-//                   
-// PROBLEM STATEMENT- https://leetcode.com/problems/sort-colors/
-
-// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
-// Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
-// In simpler words sort an array containing 0,1,2.
-
-// Solve the problem with a one-pass algorithm using only O(1) constant space?
-
-// TEST CASE-
-// Input:
-// n=6
-// nums = [2,0,2,1,1,0]
-// Output: [0,0,1,1,2,2]
-
-// Input:
-// n=3
-// nums = [2,0,1]
-// Output: [0,1,2]
-
-// The bruteforce approach - Sort the vector in nlogn time
-
-//                                 ************************DUTCH_NATIONAL_FLAG ALGORITHM***********************************
 
 void sortColors(vector<int> nums) {
         int n=nums.size();
-        int low=0,med=0,high=n-1; // mantain 3 pointers
+        int low=0,med=0,high=n-1;
         int temp;
-        while(med<=high){ // base condition-when med pointer becomes greater than high ,break
+        while(med<=high){ 
             if(nums[med]==0){
-                // swap(nums[med],nums[low]);
                 temp=nums[med];
                 nums[med]=nums[low];
                 nums[low]=temp;
@@ -41,7 +31,6 @@ void sortColors(vector<int> nums) {
                 med++;
             }
             else{
-                // swap(nums[med],nums[high]);
                 temp=nums[med];
                 nums[med]=nums[high];
                 nums[high]=temp;
@@ -64,6 +53,18 @@ void sortColors(vector<int> nums) {
       sortColors(nums);
     }
 
-  //  COMPLEXITY ANALYSIS-
-  //     Time= O(N)
-  //     space=O(1)
+/*TEST CASE-
+Input:
+n=6
+nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Input:
+n=3
+nums = [2,0,1]
+Output: [0,1,2]
+
+COMPLEXITY ANALYSIS-
+      Time= O(N)
+      space=O(1)
+*/

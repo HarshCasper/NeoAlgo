202a77cf4513adc6228c7877d9d8f4cf3bff2981
Sejal-G <sejalcjm@gmail.com>
2020-12-23 09:27:57 +0530

    Reformat the code and Resolve Changes

--- C-Plus-Plus/cryptography/Simple_Columnar_Transposition.cpp
+++ C-Plus-Plus/cryptography/Simple_Columnar_Transposition.cpp
@@ -1,9 +1,50 @@
-/*Simple Columnar Transposition Technique (Transposition Technique)
+#include<bits/stdc++.h>
+using namespace std;
 
-It is a type of transposition cipher technique which encrypts the plain text 
-message into a cipher text by rearranging the characters of the 
-plain text. The plain text is written row-wise and read column-wise.
+/* Encrypts the plain text message into a cipher text by rearranging 
+the characters of the plain text. The plain text is written row-wise 
+and read column-wise. */
+string encrypt(string s) {
+	float n = s.length();
+	int row = ceil(n/5),col = 5;
+	char arr[row][col];
+	for(int i = 0;i < row; ++i) {
+		for(int j = 0;j < col; ++j) {
+			if((i*col + j) >= s.length()) {
+				//1 is the bogus character  
+				arr[i][j] = '1' ;
+			}
+			else
+				arr[i][j] = s[i*col + j];
+		}
+	}
+	vector<int>order;  
+	//For example order of columns could be 1 0 2 3 4
+	cout<<"Enter the order of repositioning columns: ";
+	int x;
+	for(int i = 0;i < col; ++i) {
+		cin>>x;
+		order.push_back(x);
+	}
+	string encString = "";
+	for(int i = 0;i < col; ++i) {
+		for(int j = 0;j < row; ++j) {
+			encString += arr[j][order[i]];
+		}	
+	}
+	return encString;
+}
 
+int main() {
+	
+	string s;
+	cout<<"Enter the plain text: ";
+	cin>>s;
+	cout<<encrypt(s);
+
+}
+
+/*
 Example:
 Plain Text: TESSERACTCODING
 Order of Columns: 2 3 1 0 4
@@ -16,45 +57,3 @@ Cipher Text: A1NGEOL1O1
 Time Complexity: O(n)
 Space Complexity: O(1)
 */
-
-#include<bits/stdc++.h>
-using namespace std;
-
-string encrypt(string s){
-	float n = s.length();
-	int row = ceil(n/5),col=5;
-	char arr[row][col];
-	for(int i=0;i<row;i++){
-		for(int j=0;j<col;j++){
-			if((i*col+j)>=s.length()){
-				arr[i][j] = '1' ;          //1 is the boogus character  
-			}
-			else
-			arr[i][j] = s[i*col+j];
-			
-		}
-	}
-	vector<int>order;  
-	cout<<"Enter the order of repositioning columns: ";  //Example(1 0 2 3 4)
-	int x;
-	for(int i=0;i<col;i++){
-		cin>>x;
-		order.push_back(x);
-	}
-	string encString="";
-	for(int i=0;i<col;i++){
-		for(int j=0;j<row;j++){
-			encString+=arr[j][order[i]];
-		}	
-	}
-	return encString;
-}
-
-int main(){
-	
-    string s;
-	cout<<"Enter the plain text: "; //(suggested with length greater than 5)
-	cin>>s;
-	cout<<encrypt(s);
-
-}
--- C-Plus-Plus/cryptography/Simple_Columnar_Transposition_multiple_rounds.cpp
+++ C-Plus-Plus/cryptography/Simple_Columnar_Transposition_multiple_rounds.cpp
@@ -1,10 +1,59 @@
-/*Simple Columnar Transposition Technique with multiple rounds(Transposition Technique)
+#include<bits/stdc++.h>
+using namespace std;
 
-It is a type of transposition cipher technique which encrypts the plain text 
-message into a cipher text by rearranging the characters of the 
-plain text. The plain text is written row-wise and read column-wise.
-This procedure is chained and carried out k times.
+/* Encrypt the plain text message into a cipher text by 
+rearranging the characters of the plain text. The plain 
+text is written row-wise and read column-wise.This 
+procedure is chained and carried out k times. */
+string encrypt(string s,vector<int> &order) {
+	
+	float n = s.length();
+	int row = ceil(n/5),col = 5;
+	char arr[row][col];
+	for(int i = 0;i < row; ++i) {
+		for(int j = 0;j < col; ++j) {
+			if((i * col + j) >= s.length()) {
+				//1 is the bogus character  
+				arr[i][j] = '1' ;
+			}
+			else
+				arr[i][j] = s[i*col+j];
+		}
+	}
+	string encString = "";
+	for(int i = 0;i < col; ++i) {
+		for(int j = 0;j < row; ++j) {
+			encString += arr[j][order[i]];
+		}
+	}
+	return encString;
+}
 
+int main() {
+	
+	string s;
+	cout<<"Enter the plain text: "; 
+	cin>>s;
+	int rounds;
+	cout<<"Enter the number of rounds: ";
+	cin>>rounds;
+	
+	vector<int>order(5);
+	//For example order of repositioning columns could be 1 0 2 3 4
+	cout<<"Enter the order of repositioning columns: ";  
+	for(int i = 0;i < 5;i++) {
+		cin>>order[i];
+	}
+
+	//Multiple Rounds
+	for(int i = 0;i < rounds; ++i) {
+		s = encrypt(s,order);
+	}
+	cout<<s;
+	
+}
+
+/*
 Example:
 Plain Text: TESSERACTCODING
 No. of rounds: 3
@@ -18,51 +67,3 @@ Cipher Text: LAOEN1O1G1
 Time Complexity: O(n*k)
 Space Complexity: O(1)
 */
-
-#include<bits/stdc++.h>
-using namespace std;
-
-string encrypt(string s,vector<int> &order){
-	
-	float n = s.length();
-	int row = ceil(n/5),col=5;
-	char arr[row][col];
-	for(int i=0;i<row;i++){
-		for(int j=0;j<col;j++){
-			if((i*col+j)>=s.length()){
-				arr[i][j] = '1' ;          //1 is the boogus character  
-			}
-			else
-			arr[i][j] = s[i*col+j];
-			
-		}
-	}
-	string encString="";
-	for(int i=0;i<col;i++){
-		for(int j=0;j<row;j++){
-			encString+=arr[j][order[i]];
-		}	
-	}
-	return encString;
-}
-
-int main(){
-	
-    string s;int rounds;vector<int>order(5); 
-	cout<<"Enter the plain text: "; //(suggested with length greater than 5)
-	cin>>s;
-	
-	cout<<"Enter the number of rounds: ";
-	cin>>rounds;
-	
-	cout<<"Enter the order of repositioning columns: ";  //Example(1 0 2 3 4)
-	for(int i=0;i<5;i++){
-		cin>>order[i];
-	}
-	//MULTIPLE ROUNDS
-	for(int i=0;i<rounds;i++){
-		s = encrypt(s,order);
-	}
-	cout<<s;
-	
-}

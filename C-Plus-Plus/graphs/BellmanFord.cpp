08721ebece61ba34955d3cf078e4e0649814c603
Sidhan-Gupta <guptasidhant04@gmail.com>
2020-12-23 14:37:34 +0530

    made specified changes and code made robust

--- C-Plus-Plus/graphs/BellmanFord.cpp
+++ C-Plus-Plus/graphs/BellmanFord.cpp
@@ -55,9 +55,11 @@ void bellmanFord(struct Graph* graph, int src){
   int V = graph->V; 
   int E = graph->E; 
   int dist[V];
+
   for (int i = 0; i < V; i++) 
     dist[i] = INT_MAX; 
   dist[src] = 0;
+
   for(int i=0;i<V-1;i++){
     for (int j = 0; j < E; j++){
       int u = graph->edge[j].src; 
@@ -73,8 +75,8 @@ void bellmanFord(struct Graph* graph, int src){
     int v = graph->edge[i].dest; 
     int weight = graph->edge[i].weight; 
     if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) { 
-    cout<<"Graph contains negative weight cycle, hence the shortest distance is not gauranteed"<<endl; 
-    return; 
+      cout<<"Graph contains negative weight cycle, hence the shortest distance is not gauranteed"<<endl; 
+      return; 
     } 
   } 
   showDist(dist,V);

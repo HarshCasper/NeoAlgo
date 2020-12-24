6cb8e4256903ebb75594f9a140ae28d4ebd4a7ab
Satyam Dengre <59261812+satyampgt4@users.noreply.github.com>
2020-12-24 13:13:33 +0530

    Update Deque
    beginner pitch about Double Ended Queue is added ,Unnecessary lines are removed, Made formatting consistent.

--- C-Plus-Plus/ds/Deque.cpp
+++ C-Plus-Plus/ds/Deque.cpp
@@ -1,7 +1,8 @@
 /************************************************************************
-
-In this Program we will implement Double Ended Queue (de_que)
-
+Double Ended Queue (deque) is bidirectional Queue data can be insert and remove data from both(front and rear) the ends it can be use to implement stack and queue data 
+structure
+*************************************************************************
+In this Program we will implement Double Ended Queue (de_que)-:
 It contain the following operation
 1. enqueue_back(int)
 2. enqueue_front(int)
@@ -11,18 +12,11 @@ It contain the following operation
 6. display()
 7. display_reverse()
 8. size_deque()
-
-
-
-
 **********************************************************************/
-
 #include <bits/stdc++.h>
 using namespace std;
 
-
 struct node {
-
     node(int x): data(x), next(NULL), privious(NULL) {}
     int data;
     node *next;
@@ -30,141 +24,114 @@ struct node {
 };
 
 class de_que{
-
     node *head;
     node *tail;
     int n;
 public:
    // constructor of tree
-    de_que()
-    {
+    de_que(){
         head = NULL;
         tail = NULL;
-        n=0;
+        n    = 0;
     }
-
 
     void enqueue_back(int x){
-
         node *tnode = new node(x);
         //if dqueue is empty
-        if(is_empty())
-        {
+        if(is_empty()){
             head = tnode;
             tail = tnode;
         }
         //if deque is not empty
-        else
-        {
-
+        else{
             tnode->privious = tail;
-            tail->next = tnode;
-            tail = tnode;
+            tail->next      = tnode;
+            tail            = tnode;
 
         }
         n++;
-
     }
+    
     void enqueue_front(int x){
-
         node *tnode = new node(x);
         //if deque is empty
-        if(is_empty())
-        {
+        if(is_empty()){
             head = tnode;
             tail = tnode;
         }
         //if deque is not empty
-        else
-        {
-
-            tnode->next = head;
+        else{
+            tnode->next    = head;
             head->privious = tnode;
-            head = tnode;
+            head           = tnode;
 
         }
         n++;
-
     }
+    
     int dequeue_back(){
-
         //if deque is empty
-        if(is_empty())
-        {
+        if(is_empty()){
             cout<<"Deque is Empty"<<endl;
             return -9999999;
 
         }
         //if deque is not empty
-        else
-        {
+        else{
             n--;
-            int temp = tail->data;
-            tail = tail->privious;
-            tail->next = NULL;
+            int temp    = tail->data;
+            tail        = tail->privious;
+            tail->next  = NULL;            
             return temp;
         }
-
-
     }
+    
     int dequeue_front(){
         //if deque is empty
-        if(is_empty())
-        {
+        if(is_empty()){
             cout<<"Deque is Empty"<<endl;
             return -9999999;
-
         }
         //if deque is not empty
-        else
-        {
+        else{
             n--;
-            int temp = head->data;
-            head = head->next;
+            int temp       = head->data;
+            head           = head->next;
             head->privious = NULL;
             return temp;
         }
-
-
     }
-    bool is_empty()
-        {
+    
+    bool is_empty(){
             if(n==0)
                 return true;
             else
                 return false;
-
         }
-    void display()
-    {
+    
+    void display(){
         node* temp = head;
-        while(temp)
-        {
-            cout<<temp->data<<" ";
+        while(temp){
+            cout<<temp->data<<" ";            
             temp = temp->next;
         }
         cout<<endl;
     }
-    void display_reverse()
-    {
+    
+    void display_reverse(){
         //starting from tail
         node* temp = tail;
-
-        while(temp)
-        {
-            cout<<temp->data<<" ";
+        while(temp){
+            cout<<temp->data<<" ";            
             temp = temp->privious;
         }
         cout<<endl;
     }
-
-    int size_deque()
-    {
+    
+    int size_deque(){
         //return the size of de_que
         return n;
     }
-
-
 };
 
 
@@ -183,10 +150,8 @@ int main() {
     q.enqueue_back(20);
     // q will be : 15 5 6 20
     q.display();
-
-
     q.display_reverse();
-
+    
     cout<<q.dequeue_back()<<endl;
     // q will be : 15 5 6
     q.display();
@@ -196,7 +161,6 @@ int main() {
     cout<<q.dequeue_front()<<endl;
     // q will be : 5 6
     q.display();
-
     cout<<"size of deque :"<<q.size_deque()<<endl;
 
     q.enqueue_front(25);
@@ -208,8 +172,4 @@ int main() {
     q.enqueue_front(35);
     // q will be : 35 25 15 5 6 30 11
     q.display();
-
-
-
-
 }

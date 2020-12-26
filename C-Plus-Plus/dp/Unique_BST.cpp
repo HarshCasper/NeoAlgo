9d5b17dbf314950066eefd6c0189325db66d937f
Rajiv Singh <rajivperfect007@gmail.com>
2020-12-26 12:42:21 +0530

    Update Unique_BST.cpp

--- C-Plus-Plus/dp/Unique_BST.cpp
+++ C-Plus-Plus/dp/Unique_BST.cpp
@@ -1,31 +1,32 @@
 // unique BSTs using DP
 // Given n, how many structurally unique BST's that store values 1 ... n?
-#include <bits/stdc++.h> 
-using namespace std; 
+
+#include <bits/stdc++.h>
+using namespace std;
 
 int uniqueBST(int n) {
-        vector<int> count(n+1);
+	vector<int> count(n + 1);
 	// for each 'i' number of nodes
-        for(int i = 1; i <= n ; ++i) {
-            for(int j = 0 ; j < i ; ++j) {
-                // No. of trees if j is a root
-                count[i] += max(count[j], 1) * max(count[i - j - 1], 1);
-            }
-        }
-        return count[n];
-    }
+	for (int i = 1; i <= n ; ++i) {
+		for (int j = 0 ; j < i ; ++j) {
+			// No. of trees if j is a root
+			count[i] += max(count[j], 1) * max(count[i - j - 1], 1);
+		}
+	}
+	return count[n];
+}
 
-int main() 
-{ 
-    int n; 
-    cout<< "Enter value:";
-    cin>>n;
-    cout << "Number of Unique BSTs " <<uniqueBST(n) << endl; 
-  
-    return 0; 
-} 
+int main() {
+	int n;
+	cout << "Enter value:";
+	cin >> n;
+	cout << "Number of Unique BSTs " << uniqueBST(n) << endl;
 
-/* output:
+	return 0;
+}
+
+/*
+output:
 Enter value:3
 Number of Unique BSTs 5
 Time complexity : O(n)

fd5f551ecfd687927a19e1795e9a6824f1341438
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-27 13:19:45 +0530

    Update counting_sort.kt

--- Kotlin/sort/counting_sort.kt
+++ Kotlin/sort/counting_sort.kt
@@ -1,4 +1,5 @@
-fun counting_sort(list: MutableList<Int>) {
+fun counting_sort(list: MutableList<Int>) 
+{
     val countingArray = IntArray(if (list.max() == null) 0 else list.max()!! + 1)
 
     //Created a temp array to count the no of occurrences of each value in the list.
@@ -8,11 +9,15 @@ fun counting_sort(list: MutableList<Int>) {
     for (item in list) countingArray[item]++
 
     // Regenerate the list using the countingArray
+    
     var cursor = 0
-    for (index in 0 until countingArray.size) {
+    for (index in 0 until countingArray.size) 
+    {
         val value = index
         val numberOfOccurrences = countingArray[index]
+        
         if (numberOfOccurrences > 0)
             repeat(numberOfOccurrences) {list[cursor++] = value}
+        
     }
 }

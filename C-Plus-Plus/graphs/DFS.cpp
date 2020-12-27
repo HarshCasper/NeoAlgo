440eaf34ea3ff8a9cd878946f8cfcf1edaa135b7
Bharati2612 <2020pcs1009@iitjammu.ac.in>
2020-12-27 09:52:31 +0530

    Did required changes

--- C-Plus-Plus/graphs/DFS.cpp
+++ C-Plus-Plus/graphs/DFS.cpp
@@ -1,4 +1,4 @@
-/* Depth first search (DFS) algorithm starts with the initial node of the graph G, 
+/*Depth first search (DFS) algorithm starts with the initial node of the graph G, 
 and then goes to deeper and deeper until we find the goal node or the node which has no children. 
 The algorithm, then backtracks from the dead end towards the most recent node that is yet to be completely unexplored. */
 

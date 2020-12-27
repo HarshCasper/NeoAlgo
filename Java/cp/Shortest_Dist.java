3c8ad7bbad9cd7d067eb2f3720d08d8829f12276
Ahana Pal <59911272+ErzaTitania-2001@users.noreply.github.com>
2020-12-27 09:34:02 +0530

    Update Shortest_Dist.java
    Added Comment lines
    Prompted user to enter the words between which the distance is to be determined

--- Java/cp/Shortest_Dist.java
+++ Java/cp/Shortest_Dist.java
@@ -22,9 +22,11 @@ class Shortest_Dist {
 
         List < String > wd = new ArrayList(arr);
         while (t > 0) {
+            System.out.println("Enter the words between which the shortest distance is to be calculated");
             String wd1 = sc.next();
             String wd2 = sc.next();
             System.out.println(" The shortest Distance between " + wd1 + " and " + wd2 + " is : " + shortest_dist(wd, wd1, wd2));
+            //List<> wd is being allocation with elements of Collection arr
             wd = new ArrayList(arr);
 
             t--;
@@ -34,6 +36,7 @@ class Shortest_Dist {
     public static int shortest_dist(List < String > wd, String wd1, String wd2) {
         int dist = -1;
         List < String > wdcopy = new ArrayList(wd);
+        //list <> ind1. List <> ind2, for storing indexes of wd1, wd2 in List <> wd
         List < Integer > ind1 = new ArrayList <> ();
         List < Integer > ind2 = new ArrayList <> ();
 
@@ -54,6 +57,7 @@ class Shortest_Dist {
 
         for (int i = 0; i < ind1.size(); i++) {
             for (int j = 0; j < ind2.size(); j++) {
+                //calculating distance between the indexes
                 int d = (int) Math.abs(ind1.get(i) - ind2.get(j));
                 if (dist == -1)
                     dist = d;

bd1d464d62e023aac40a93d5c78ab7da4f6538a7
Rajiv Singh <rajivperfect007@gmail.com>
2020-12-27 12:43:44 +0530

    removed whitespaces

--- C-Plus-Plus/cp/Josephus.cpp
+++ C-Plus-Plus/cp/Josephus.cpp
@@ -1,6 +1,6 @@
 /**
 Josephus problem is a famous problem.
-In each iteration we kill the every kth 
+In each iteration we kill the every kth
 person in a circular arrangement of n persons.
 Find the person who is alive at the end.
 0-based indexing
@@ -10,27 +10,25 @@ link to the problem : https://en.wikipedia.org/wiki/Josephus_problem
 #include <iostream>
 using namespace std;
 
-int solution(int n, int k){
-    if(n == 1)
-      return 0;
-    return (k+solution(n-1,k))%n;
+int solution(int n, int k) {
+	if (n == 1)
+		return 0;
+	return (k + solution(n - 1, k)) % n;
 }
 
 int main() {
-    int people, gap;
-    cin >> people >> gap;
-    cout << solution(people, gap) << '\n'; 
-    return 0;
+	int people, gap;
+	cin >> people >> gap;
+	cout << solution(people, gap) << '\n';
+	return 0;
 }
 
 /**
-
-Input : 
-4 2 
+Input :
+4 2
 Output :
-0 
+0
 
 Time Complexity : O(n)
 Space Complexity : O(1)
-
 **/

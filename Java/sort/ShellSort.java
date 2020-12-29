0bf798d275623f05c63ec479c278da9bacd31e1c
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-29 10:35:16 +0530

    Update ShellSort.java

--- Java/sort/ShellSort.java
+++ Java/sort/ShellSort.java
@@ -1,38 +1,67 @@
-class ShellSort 
+import java.util.Scanner;
+
+/*
+* Shell Sort is a very efficient sorting algorithm based on insertion sort technique.
+* Shell Sort is more effective than insertion sort as
+*	sometimes in Insertion Sorting, we shift a large block to insert an item at the right place as per the sorting order,
+*	which can be avoided by using the Shell Sort algorithm.
+* In Shell Sort, the sorting is done at certain intervals/gaps and everytime, this gap is reduced.
+* This way, Shell Sort becomes nearly twice as fast as Insertion Sort.
+*/
+
+public class ShellSort 
 { 
 	static void printArray(int arr[]) 
-    { //function to print the elements of the array
-        //called twice...one before sorting and once after sorting
+	{ 
+		//function to print the elements of the array
+  
 		int n = arr.length; 
 		for (int i=0; i<n; ++i) 
 			System.out.print(arr[i] + " "); 
+		
 		System.out.println(); 
 	} 
 
-    int sort(int arr[]) 
-    //here basically we compare the elements after some gaps...a bit like insertion sort
+	int sort(int arr[]) 
 	{ 
 		int n = arr.length; 
 
-		for (int gap = n/2; gap > 0; gap /= 2) 
+		//initially gap = n/2
+		for (int gap = n/2; gap > 0; gap /= 2) 	
 		{ 
 			for (int i = gap; i < n; i += 1) 
-			{ 
+			{
+				//decreasing by gap/2
 				int temp = arr[i]; 
 				int j; 
 				for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
 					arr[j] = arr[j - gap]; 
+				//swapping the contents of arr[i] and arr[j]
 				arr[j] = temp; 
 			} 
 		} 
 		return 0; 
 	} 
+    
+    	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String args[]) 
 	{ 
-        int arr[] = {12, 34, 54, 2, 3}; //this is our array
-        //we can take a dynamic input from the user if static input is not good
-		System.out.println("Array before sorting"); 
+		//we store the number of elements in n
+		System.out.println("Kindly enter the number of elements: ");
+		int n = sc.nextInt();
+		
+		//declaring an array of n elements
+        	int arr[] = new int[n];
+		
+		//taking the input from the user
+		for(int i=0;i<n;i++)
+		{
+			System.out.print("\n" + i + "'th element : ");
+			arr[i] = sc.nextInt();
+		}
+		
+		System.out.println("\nArray before sorting"); 
 		printArray(arr); 
 
 		ShellSort ob = new ShellSort(); //creating an object of shellsort class so that the methods inside shellsort class can be used
@@ -45,8 +74,16 @@ class ShellSort
 /*
 
 Output:
+
+Kindly enter the number of elements: 5
+
+0'th element : 1
+1'th element : 2
+2'th element : 5
+3'th element : 3
+4'th element : 0
 Array before sorting
-12 34 54 2 3       
+1 2 5 3 0 
 Array after sorting
-2 3 12 34 54  
+0 1 2 3 5 
 */ 

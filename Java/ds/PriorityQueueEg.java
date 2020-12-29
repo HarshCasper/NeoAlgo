8084ae4da400103a4160f36e43eb1ce4b650f1c1
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-29 11:21:50 +0530

    Update PriorityQueueEg.java

--- Java/ds/PriorityQueueEg.java
+++ Java/ds/PriorityQueueEg.java
@@ -1,31 +1,37 @@
 import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.lang.*;
+import java.util.Scanner;
 
 //Priority Queue data structure is a special queue in which the elements are present not as per FIFO order but as per the natural elements or any xyz comparator used during queue creation.
 
 public class PriorityQueueEg {
+    
+    private static Scanner sc = new Scanner(System.in);
 
     public static void main(String args[])
     {  
+        System.out.println("Kindly enter the number of strings in the queue: ");
+        int n = sc.nextInt();
 
-        PriorityQueue<String> cities_queue = new PriorityQueue<String>();  
-
-        cities_queue.add("Sydney");  
-        cities_queue.add("Venice");  
-        cities_queue.add("New York");  
-        cities_queue.add("California");  
-        cities_queue.add("Melbourne");  
+        PriorityQueue<String> queue = new PriorityQueue<String>();  
+        
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Kindly enter the "+i+"'th element");
+            queue.add(sc.next());
+        }
 
         //print the head of the PriorityQueue
-        System.out.println("PriorityQueue Head:"+cities_queue.element());  
+        System.out.println("PriorityQueue Head : "+ queue.element());  
+        
         //Define the iterator for PriorityQueue and print its elements 
+        System.out.println("\nPriorityQueue : "); 
 
-        System.out.println("\nPriorityQueue contents:"); 
-
-        Iterator iter=cities_queue.iterator();  
-        while(iter.hasNext()){ 
-            System.out.print(iter.next() + " ");  
+        Iterator iterator = queue.iterator();  
+        while(iterator.hasNext())
+        { 
+            System.out.print(iterator.next() + " ");  
         }  
 
     }
@@ -33,7 +39,18 @@ public class PriorityQueueEg {
 }
 /*
 Output:
-PriorityQueue Head:California
-PriorityQueue contents:
-California Melbourne Sydney Venice New York 
+
+Kindly enter the number of strings in the queue:                                                                                              
+3                                                                                                                                             
+Kindly enter the 0'th element                                                                                                                 
+ram                                                                                                                                           
+Kindly enter the 1'th element                                                                                                                 
+seeta                                                                                                                                         
+Kindly enter the 2'th element                                                                                                                 
+geeta                                                                                                                                         
+PriorityQueue Head : geeta                                                                                                                    
+                                                                                                                                              
+PriorityQueue :                                                                                                                               
+geeta seeta ram   
+
 */

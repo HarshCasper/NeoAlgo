a3811fd2969ae5f12313eb637088ba6bee38f431
Akash Kumar Bhagat <akashkbhagat221199@gmail.com>
2020-12-30 20:29:40 +0530

    Typo corrected

--- Python/other/Count_Inversion.py
+++ Python/other/Count_Inversion.py
@@ -1,13 +1,13 @@
 """
 Inversion: Inversion Count for an array indicates – how far (or close)
-        the array is from being sorted. If array is already sorted then
-        inversion count is 0. If array is sorted in reverse order that
+        the array is from being sorted. If the array is already sorted then
+        inversion count is 0. If the array is sorted in the reverse order that
         inversion count is the maximum.
         Formally speaking, two elements a[i] and a[j] form an inversion
         if a[i] > a[j] and i < j.
 
 Purpose: Given an array A of length N containing distinct integers from 1
-        to N. If we make 2 sets containg N numbers, one arranged serially
+        to N. If we make 2 sets containing N numbers, one arranged serially
         another in the give array order. If we connect the i_th number from
         the 1st set to the i_th number in the second set, Find the number of
         intersection.
@@ -32,15 +32,15 @@ Purpose: Given an array A of length N containing distinct integers from 1
         Number of intersection = 3
 
 Method: Divide and Conquer / Merge-Sort
-Intution: This is a typical problem for Divide and Conquer algorithm also
-            refered to as the inversion problem.
+Intuition: This is a typical problem for Divide and Conquer algorithm also
+            referred to as the inversion problem.
         1. The idea is similar to merge sort, divide the array into two equal
             or almost equal halves in each step until the base case is reached.
         2. Create a function merge that counts the number of inversions when
             two halves of the array are merged, create two indices i and j, i
-            is the index for first half and j is an index of the second half.
+            is the index for the first half and j is an index of the second half.
             if a[i] is greater than a[j], then there are (mid – i) inversions
-            because left and right subarrays are sorted, so all the remaining
+            because the left and right subarrays are sorted, so all the remaining
             elements in left-subarray (a[i+1], a[i+2] … a[mid]) will be greater than a[j].
         3. Create a recursive function to divide the array into halves and find
             the answer by summing the number of inversions is the first half,
@@ -50,7 +50,7 @@ Intution: This is a typical problem for Divide and Conquer algorithm also
 
 Reference: https://www.geeksforgeeks.org/counting-inversions/
 
-Time Compelxity:    O(N*log_N)
+Time Complexity:    O(N*log_N)
 Space Complexity:   O(N)
 
 Argument:  List

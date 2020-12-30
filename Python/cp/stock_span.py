19fe6405578ae595a3d997dbfee4bd85c6627ecc
Rutuja Dhanawade <53823042+rutujadhanawade@users.noreply.github.com>
2020-12-30 19:36:42 +0530

    Update stock_span.py

--- Python/cp/stock_span.py
+++ Python/cp/stock_span.py
@@ -1,27 +1,27 @@
 ''' Stock Span Problem
-Given a list of prices of a stock for N number of days, find stock span for each day.
-Stock span is defined as a number of consecutive days prior to the current day when the price of a stock was less than or equal to the price at current day.
+Given a list of prices of a stock for N number of days,
+find stock span for each day.
 '''
 
+
 def calcSpan(price):
-    # Create an empty stack
     day = len(price)
     stack = []
     span = [0 for i in range(0, day)]
-    # Span value of first day is 1
+
     span[0] = 1
     stack.append(0)
-    # Calculate span values for rest of the days
+
     for i in range(1, day):
-        #  checking if previous stock prize is less
+
         while (len(stack) > 0 and price[stack[-1]] <= price[i]):
             stack.pop()
-        # updating the span value
+
         if len(stack) <= 0:
             span[i] = i + 1
         else:
             span[i] = i - stack[-1]
-        # Push element to stack
+
         stack.append(i)
 
     print(span)
@@ -39,4 +39,3 @@ sample output:
 Time complexity: O(n)
 space complexity:O(n)
 '''
-

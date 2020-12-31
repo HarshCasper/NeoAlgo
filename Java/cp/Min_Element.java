6ab2114dc7c8e91756a278204125f3237d5d4033
Ahana Pal <59911272+ErzaTitania-2001@users.noreply.github.com>
2020-12-31 21:01:46 +0530

    Update and rename GC5.java to Min_Element.java

--- Java/cp/Min_Element.java
+++ Java/cp/Min_Element.java
@@ -1,9 +1,5 @@
-/*Suppose an array sorted in an ascending order is rotated at some pivot unknown to you beforehand . Find the minimum element. You may assume no duplicate element
-Input : [3,4,5,1,2]
-Output : 1
- */
 import java.util.*;
-class GC5 {
+class Min_Element {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the elements");

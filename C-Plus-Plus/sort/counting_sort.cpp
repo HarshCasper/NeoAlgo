475281411d5af03a6bcb6010118f174f50cd8722
Sarthak Saxena <sarthaksaxena9@gmail.com>
2020-12-31 19:31:28 +0530

    Added counting_sort.cpp which implements Counting Sort for integers and updated README.md and made the necessary changes.

--- C-Plus-Plus/sort/counting_sort.cpp
+++ C-Plus-Plus/sort/counting_sort.cpp
@@ -1,12 +1,14 @@
-//
-// Created by Sarthak-9 on 30-12-2020.
-// Count Sort Algorithm for Sorting an array of small integers quickly
-//
-// Worst complexity: O(n+k)
-// Average complexity: O(n+k)
-// Space complexity: O(n+k)
-// where n is the number of elements in input array and k is the range of input.
-//
+/*
+
+ Created by Sarthak-9 on 30-12-2020.
+ Count Sort Algorithm for Sorting an array of small integers quickly.
+
+ Worst complexity: O(n+k)
+ Average complexity: O(n+k)
+ Space complexity: O(n+k)
+ where n is the number of elements in input array and k is the range of input.
+
+*/
 
 
 #include <iostream>
@@ -14,7 +16,9 @@
 using namespace std;
 
 int Max(long long int A[], long long int n){
-    long long int max = -32768;
+    long long int max = A[0];
+
+    // finds the maximum element of the array
     for (long long int i=0; i<n; i++){
         if (A[i] > max){
             max = A[i];
@@ -27,16 +31,20 @@ void CountSort(long long int A[], long long int n){
     long long int max = Max(A, n);
     int* count = new int [max + 1];
 
+    // initializes the count array with all elements to zero
     for (long long int i=0; i<max+1; i++){
         count[i] = 0;
     }
 
+    // updates the count array with frequencies of given numbers
     for (long long int i=0; i<n; i++){
         count[A[i]]++;
     }
 
     long long int i = 0;
     long long int j = 0;
+
+    // updates A with sorted elements
     while (j < max+1){
         if (count[j] > 0){
             A[i++] = j;
@@ -57,6 +65,7 @@ int main() {
     cin>>n;
     cout<<"Enter the elements"<<endl;
 
+    // inputs numbers from the user
     for(long long int i=0;i<n;i++){
         cin>>A[i];
     }
@@ -64,9 +73,25 @@ int main() {
     CountSort(A, n);
 
     cout<<"The sorted array is :"<<endl;
+
+    // prints the sorted array
     for(long long int i=0;i<n;i++){
         cout<<A[i]<<" ";
     }
     cout<<endl;
     return 0;
 }
+
+/*
+
+  Input:
+  Enter the size of array
+  10
+  Enter the elements
+  45 86 27 38 59 12 86 72 125 38
+
+  Output:
+  The sorted array is :
+  12 27 38 38 45 59 72 86 86 125
+
+*/

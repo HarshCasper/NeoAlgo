0eadbbc02fd93140e30306597a6c79b7ffdd3f0c
yukty2000 <55875848+yukty2000@users.noreply.github.com>
2021-01-01 10:50:15 +0530

    Update number_of_binary_trees.cpp

--- C-Plus-Plus/dp/number_of_binary_trees.cpp
+++ C-Plus-Plus/dp/number_of_binary_trees.cpp
@@ -1,16 +1,17 @@
-/* This program finds the number of binary trees given the length of the preorder sequence = n.
-    A tree is an acyclic graph . A binary tree is a tree where each node has 0, 1 or 2 children. 
-    Preorder traversal means traversing a tree in the following order : Root - Left child - Right child.  
+/* 
+This program finds the number of binary trees given the length of the preorder sequence = n.
+A tree is an acyclic graph . A binary tree is a tree where each node has 0, 1 or 2 children. 
+Preorder traversal means traversing a tree in the following order : Root - Left child - Right child.  
     
-    We use the dynamic programming approach where our root is fixed and the rest of the n-1 nodes
-        are split as left and right children. Hence, we calculate the number of binary trees possible
-        while exploring splits varying from 0 to n-1.
+We use the dynamic programming approach where our root is fixed and the rest of the n-1 nodes
+are split as left and right children. Hence, we calculate the number of binary trees possible
+while exploring splits varying from 0 to n-1.
     
-    Time Complexity - O(n^2)
-    Space Complexity - O(n)
+Time Complexity - O(n^2)
+Space Complexity - O(n)
     
-    Note:- The approach is similar to finding the nth catalan number.
-    */
+Note:- The approach is similar to finding the nth catalan number.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -20,12 +21,17 @@ typedef long long int lli;
 lli PossibleBinaryTrees(int number)
 {
     lli *table=(lli *)malloc((number+1)*sizeof(lli));
-    table[0]=1,table[1]=1;                                      //Since number of trees that can be formed for n=0/1 is 1
     
-    for(int loop1=2;loop1<=number;loop1++)                      //Calculating possible number of binary trees for all i<=n and storing them for future use-Tabulation Method
+    //Since number of trees that can be formed for n=0/1 is 1
+    table[0]=1,table[1]=1;    
+    
+    //Calculating possible number of binary trees for all i<=n and storing them for future use-Tabulation Method
+    for(int loop1=2;loop1<=number;loop1++)                      
     {
         table[loop1]=0;
-        for(int loop2=0;loop2<i;loop2++)                        //We loop over the possible splits from 0 to i-1
+        
+        //We loop over the possible splits from 0 to i-1
+        for(int loop2=0;loop2<loop1;loop2++)                       
         {
             table[loop1]+=(table[loop2]*table[loop1-loop2-1]);
         }
@@ -36,14 +42,17 @@ lli PossibleBinaryTrees(int number)
     return answer;
 }
 
-int main()                                                      //Driver function
+//Driver function
+int main()                                                      
 {
-    int number;                                                 //Enter length of preorder sequence
+    //Enter length of preorder sequence
+    int number;                                                 
     cin>>number;
     cout<<PossibleBinaryTrees(number)<<endl;
     return 0;
 }
-/* Sample input:-
-        n = 5;
-        Output = 42;
-        */
+/* 
+Sample input:-
+n = 5;
+Output = 42;
+*/

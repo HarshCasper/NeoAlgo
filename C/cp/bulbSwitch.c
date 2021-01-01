c65fcf9e1a2ed5965558a023ffa10233249c5c7c
Abhinav Anand <manasgmishra@gmail.com>
2021-01-01 21:19:43 +0530

    Added problem description

--- C/cp/bulbSwitch.c
+++ C/cp/bulbSwitch.c
@@ -1,5 +1,14 @@
-// Bulb Swither problem
-// link to problem: https://leetcode.com/problems/bulb-switcher/
+/*
+Bulb Swither problem
+
+There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
+On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb.
+For the nth round, you only toggle the last bulb.
+
+Return the number of bulbs that are on after n rounds.
+
+Link to problem: https://leetcode.com/problems/bulb-switcher/
+*/
 
 #include<stdio.h>
 

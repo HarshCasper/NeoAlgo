2ac972fd1944fe6c885b727d4975bd64e01e90a9
Purav Uday Desai <44437936+PuravUdayDesai@users.noreply.github.com>
2021-01-02 22:02:21 +0530

    Added InitialPermutationLogic.java under des/

---
+++ Java/cryptography/des/InitialPermutationLogic.java
@@ -0,0 +1,41 @@
+package des;
+
+/**
+ * <h1>Initial Permutation</h1>
+ * <p>
+ * 		class InitialPermutationLogic is a public class which acts as a logic class which helps to permute through initial
+ * 		permutation
+ * </p>
+ */
+public class InitialPermutationLogic
+{
+	/**
+	 * <p>
+	 * 		This is a public method which is used to permute value through initial permutation, this is done with the help of
+	 * 		table INITIAL_PERMUTATION which is present in des.DESCryptographyModels.INITIAL_PERMUTATION
+	 * </p>
+	 * 
+	 * @param  value                   This is a 64-bit value
+	 * @param  initialPermutationTable This is the initial permutation table
+	 * 
+	 * @return                         byte[] This is 64-bit permuted value
+	 */
+	public byte[] initialPermutation(byte[] value, byte[] initialPermutationTable)
+	{
+		//created an array which will hold permuted value
+		byte[] initialPermutation = new byte[64];
+		
+		//iterates 64 time
+		for (int i = 0; i < initialPermutationTable.length; i++)
+		{
+			/*
+			 * Each new element in 64-bit array is generated with the help of INITIAL_PERMUTATION table, at each iteration the value
+			 * of "INITIAL_PERMUTATION" at ith index is used as the index of "value" and the value which is retrieved from it is
+			 * used as the element in 64-bit array.
+			 */
+			initialPermutation[i] = value[initialPermutationTable[i] - 1];
+		}
+		//returning initial permuted value
+		return initialPermutation;
+	}// End of initialPermutation(byte[],byte[]) method
+}// End of class InitialPermutationLogic

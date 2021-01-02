5615814f0b1f8c757a3de3ef3a28b6158d75a153
Rutuja Dhanawade <53823042+rutujadhanawade@users.noreply.github.com>
2021-01-02 21:54:41 +0530

    Update bulb_switcher.py

--- Python/cp/bulb_switcher.py
+++ Python/cp/bulb_switcher.py
@@ -1,5 +1,17 @@
-# Bulb Swither problem
-# link to problem: https://leetcode.com/problems/bulb-switcher/
+'''
+Bulb switcher problem: There are n bulbs that are initially off.
+You first turn on all the bulbs,then you turn off every second bulb.
+On the third round,toggle every third bulb (turn on if it's off or vice-versa)
+For the ith round, you toggle every i bulb.
+For the nth round, you only toggle the last bulb.
+Return the number of bulbs that are on after n rounds.
+
+approach : By observation, A bulb is ON if it is toggled odd times
+Only when `i` has perfect square root, its number of divisors is odd.
+
+Time Complexity:  O(N)
+Space Complexity: O(1)
+'''
 
 
 def bulbSwitcher(n):
@@ -15,5 +27,4 @@ print(bulbSwitcher(N))
 '''
 Input: 5
 output: 2
-Time complexity: O(N)
 '''

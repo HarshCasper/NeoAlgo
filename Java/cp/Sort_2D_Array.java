15d797e2d28c862b567b38b7d6eb0ac2318ebb9a
Ahana Pal <59911272+ErzaTitania-2001@users.noreply.github.com>
2021-01-04 22:28:45 +0530

    Update Sort_2D_Array.java

--- Java/cp/Sort_2D_Array.java
+++ Java/cp/Sort_2D_Array.java
@@ -1,18 +1,3 @@
-/* We are given a 2D array of order N X M and a column number K ( 1<=K<=m). Our task is to sort the 2D array according to values in the Column K.
-
-Examples:
-
-Input : If our 2D array is given as (Order 4X4)
-39 27 11 42
-10 93 91 90
-54 78 56 89
-24 64 20 65
-Sorting it by values in column 3
-Output : 39 27 11 42
-24 64 20 65
-54 78 56 89
-10 93 91 90
- */
 import java.util.*;
 class Sort_2D_Array {
     public static void main(String args[]) {

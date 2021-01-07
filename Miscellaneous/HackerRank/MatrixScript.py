946763fc0ed4c6b826c16e4518834b5bf554ed23
mehabhalodiya <mehabhalodiya@gmail.com>
2021-01-07 20:06:02 +0530

    Added Matrix Script in Python

---
+++ Miscellaneous/HackerRank/MatrixScript.py
@@ -0,0 +1,19 @@
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+string = ""
+for y in range(m):
+    for x in range(n):
+        string += matrix[x][y]
+pattern = re.compile('(\w)(\W)+(\w)')
+subs = pattern.sub(r"\1 \3", string)
+print(subs) 
+

c5d4610bbe2e77486eac9efaf6b8287a20430cd8
Vaishnavi Shah <61752840+vaishnavirshah@users.noreply.github.com>
2021-01-07 11:11:30 +0530

    Updated the file with all the changes.

--- C-Plus-Plus/ds/ThreadedBinaryTree.cpp
+++ C-Plus-Plus/ds/ThreadedBinaryTree.cpp
@@ -1,3 +1,15 @@
+/*
+DESCRIPTION -
+
+Threaded binary tree are used to make faster inorder traversal without using stack or recursion.
+
+It stores the inorder successor, if the right node is empty.
+And stores the inorder predecessor if left node is empty.
+
+- There are functions to add, delete and search for an element.
+- Also the inorder for the tree. 
+*/
+
 #include <iostream>
 #include<algorithm>
 #include<cstdlib>
@@ -8,7 +20,8 @@ using namespace std;
 typedef struct  Node
 {
   int data;
-  int leftflag,rightflag; // 1 for address of child, 0 address of parent
+  // 1 for address of child, 0 address of parent
+  int leftflag,rightflag; 
   struct Node *left,*right;
 
 }Node;
@@ -16,9 +29,11 @@ void add(Node **p, int v)
 {
     Node * newnode=(Node*)malloc(sizeof(Node));
     newnode->data=v;
-    newnode->leftflag =0; // by default both the flags will be 0
+    // by default both the flags will be 0
+    newnode->leftflag =0; 
     newnode->rightflag=0; 
-    if(*p==NULL) // if its the root node it will have no inorder predecessor or successor
+    // if its the root node it will have no inorder predecessor or successor
+    if(*p==NULL) 
     {
         newnode->left=NULL;
         newnode->right=NULL;
@@ -27,7 +42,8 @@ void add(Node **p, int v)
     else
     {
         Node* temp=*p;
-        while(1){ // This loop will find the inorder predecessor or successor
+        // This loop will find the inorder predecessor or successor
+        while(1){ 
             if(temp->data > v && temp->leftflag == 1)
                 temp = temp->left;
             else if(temp->data < v && temp->rightflag == 1)
@@ -36,25 +52,32 @@ void add(Node **p, int v)
                 break;
         }
         // temp is now the inorder predecessor or successor
-        if(temp->data < v){ // This will update the new node that is created with the links to its inorder predecessor or successor
+        // This will update the new node that is created with the links to its inorder predecessor or successor
+        if(temp->data < v){ 
             newnode->right = temp->right;
-            temp->right = newnode; // points at the child
+            // points at the child
+            temp->right = newnode; 
             temp->rightflag=1;
-            newnode->left = temp; // points at the parent
+            // points at the parent
+            newnode->left = temp; 
         }else{
             newnode->left = temp->left;
-            temp->left = newnode; // points at the child
+            // points at the child
+            temp->left = newnode; 
             temp->leftflag=1;
-            newnode->right = temp; // points at the parent
+            // points at the parent
+            newnode->right = temp; 
         }
     }
 }
 void inorder(Node *p){
     while(1){
-        while(p->leftflag) // goes to left most
+        // goes to left most
+        while(p->leftflag) 
             p=p->left;
         cout<<p->data<<" ";
-        while(!p->rightflag){ // right flag must be 0 so that it goes to the parent and not the child
+         // right flag must be 0 so that it goes to the parent and not the child
+        while(!p->rightflag){
             p=p->right;
             if(p== NULL)
                 return;
@@ -63,17 +86,20 @@ void inorder(Node *p){
         p=p->right;
     }
 }
-
-int TBTsearch(Node*p,int value){ // returns 1 if present else 0
+// returns 1 if present else 0
+int TBTsearch(Node*p,int value){ 
     while(1){
-        if(p->data>value && p->leftflag){ // left flag must be 1 to make sure it goes to the child and not the parent
+         // left flag must be 1 to make sure it goes to the child and not the parent
+        if(p->data>value && p->leftflag){
             p=p->left;
         }
-        else if(p->data<value && p->rightflag){ // right flag must be 1 to make sure it goes to the child and not the parent
+        // right flag must be 1 to make sure it goes to the child and not the parent
+        else if(p->data<value && p->rightflag){ 
             p=p->right;
         }
         else{
-            break; // the element has been found or reached the end and value is not found so does not exist in the tree 
+            // the element has been found or reached the end and value is not found so does not exist in the tree 
+           break;
         }
     }
     if(p->data==value){
@@ -99,15 +125,31 @@ int main()
     
     */
     Node *root=NULL;
-    add(&root,50); 
-    add(&root,70);
-    add(&root,80);
-    add(&root,40);
-    add(&root,60);
-    inorder(root);
-    cout<<endl;
-    cout<<TBTsearch(root,10)<<endl;
-    cout<<TBTsearch(root,80)<<endl;
+    int option,element;
+    cout<<"MENU\n\nEnter 1. Insert an element\nEnter 2. Inorder of the tree\nEnter 3. Search for an element\nEnter 4. Exit\n";
+
+    while(option!=4){
+
+        cout<<"\nEnter your option: ";
+        cin>>option;
+
+        if(option == 1){
+            cout<<"Enter the element you want to insert: ";
+            cin>>element;
+            add(&root,element);
+            cout<<element<<" has been inserted into the tree.\n";
+        }else if(option ==2){
+            inorder(root);
+            cout<<endl;
+        }else if(option == 3){
+            cout<<"Enter the element you want to search: ";
+            cin>>element;
+            cout<<TBTsearch(root,element)<<endl;
+        }else if(option!=4){
+            cout<<"Enter a valid number!\n";
+        }
+    }
+    cout<<"Exiting.... ";
     return 0;
 }
 // Contributed by Vaishnavi Shah

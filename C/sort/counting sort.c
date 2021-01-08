22cf4c36e086206dc29ee5cc12fd33cfd4675e76
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-08 12:35:47 +0530

    Update counting sort.c

--- C/sort/counting sort.c
+++ C/sort/counting sort.c
@@ -1,82 +1,63 @@
-/* Counting Sort in C - Counting sort is a sorting algorithm that sorts 
-the elements of an array by counting the number of occurrences of each unique 
-element in the array. The count is stored in an auxiliary array and the sorting is done by mapping the count as an index of the auxiliary array.
-# The average time complexity for Counting Sort is O(n+k). 
-  Worst Case Complexity: O(n+k)
-  Best Case Complexity: O(n+k)
-  Overall complexity = O(max)+O(size)+O(max)+O(size) = O(max+size)
-  Counting Sort Algorithm
-  countingSort(array, size)
-  max <- find largest element in array
-  initialize count array with all zeros
-  for j <- 0 to size
-    find the total count of each unique element and 
-    store the count at jth index in count array
-  for i <- 1 to max
-    find the cumulative sum and store it in count array itself
-  for j <- size down to 1
-    restore the elements to array
-# decrease count of each element restored by 1 */
+/* Counting sort is a sorting algorithm that sorts the elements of an array by 
+counting the number of occurrences of each unique element in the array. 
+The count is stored in an auxiliary array and the sorting is done by 
+mapping the count as an index of the 
+auxiliary array.
+# The average time complexity for Counting Sort is O(n + k). 
+  The worst time complexity is O(n + k).
+# The best complexity for Counting Sort is O(n + k). */
 
 /* C Program to sort Elements Using Counting Sort */
-#include <stdio.h>
-
-void countingSort(int array[], int size) {
-  int output[10];
-
-  // Find the largest element of the array
-  int max = array[0];
-  for (int i = 1; i < size; i++) {
-    if (array[i] > max)
-      max = array[i];
-  }
-
-  // The size of count must be at least (max+1) but
-  // we cannot declare it as int count(max+1) in C as
-  // it does not support dynamic memory allocation.
-  // So, its size is provided statically.
-  int count[10];
-
-  // Initialize count array with all zeros.
-  for (int i = 0; i <= max; ++i) {
-    count[i] = 0;
-  }
-
-  // Store the count of each element
-  for (int i = 0; i < size; i++) {
-    count[array[i]]++;
-  }
-
-  // Store the cummulative count of each array
-  for (int i = 1; i <= max; i++) {
-    count[i] += count[i - 1];
-  }
-
-  // Find the index of each element of the original array in count array, and
-  // place the elements in output array
-  for (int i = size - 1; i >= 0; i--) {
-    output[count[array[i]] - 1] = array[i];
-    count[array[i]]--;
-  }
-
-  // Copy the sorted elements into original array
-  for (int i = 0; i < size; i++) {
-    array[i] = output[i];
-  }
+#include <stdio.h>  
+ 
+/*  Counting sort function  */
+void counting_sort(int A[], int k, int n)
+{
+    int i, j;
+    int B[15], C[100];
+    for (i = 0; i <= k; i++)
+        C[i] = 0;
+    for (j = 1; j <= n; j++)
+        C[A[j]] = C[A[j]] + 1;
+    for (i = 1; i <= k; i++)
+        C[i] = C[i] + C[i-1];
+    for (j = n; j >= 1; j--)
+    {
+        B[C[A[j]]] = A[j];
+        C[A[j]] = C[A[j]] - 1;
+    }
+    printf("The Sorted array is : ");
+    for (i = 1; i <= n; i++)
+        printf("%d ", B[i]);
+}
+/*  End of counting_sort()  */
+ 
+/*  The main() begins  */
+int main()
+{
+    int n, k = 0, A[15], i;
+    printf("Enter the number of input : ");
+    scanf("%d", &n);
+    printf("\nEnter the elements to be sorted :\n");
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &A[i]);
+        if (A[i] > k) {
+            k = A[i];
+        }
+    }
+    counting_sort(A, k, n);
+    printf("\n");
+    return 0;
 }
 
-// Function to print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; ++i) {
-    printf("%d  ", array[i]);
-  }
-  printf("\n");
-}
+/* 
+Output:
+Enter the number of input : 10
+ 
+Enter the elements to be sorted :
+21 4 18 15 24 6 9 1 5 4
+The Sorted array is : 1 4 4 5 6 9 15 18 21 24
+*/
 
-// Driver code
-int main() {
-  int array[] = {4, 2, 2, 8, 3, 3, 1};
-  int n = sizeof(array) / sizeof(array[0]);
-  countingSort(array, n);
-  printArray(array, n);
-}
+

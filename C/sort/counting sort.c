70bc957901464413bb4ec91c139ca2e9d983d62b
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-08 12:27:10 +0530

    Update counting sort.c

--- C/sort/counting sort.c
+++ C/sort/counting sort.c
@@ -2,11 +2,11 @@
 the elements of an array by counting the number of occurrences of each unique 
 element in the array. The count is stored in an auxiliary array and the sorting is done by mapping the count as an index of the auxiliary array.
 # The average time complexity for Counting Sort is O(n+k). 
-# Worst Case Complexity: O(n+k)
-# Best Case Complexity: O(n+k)
-# Overall complexity = O(max)+O(size)+O(max)+O(size) = O(max+size)
-# Counting Sort Algorithm
-countingSort(array, size)
+  Worst Case Complexity: O(n+k)
+  Best Case Complexity: O(n+k)
+  Overall complexity = O(max)+O(size)+O(max)+O(size) = O(max+size)
+  Counting Sort Algorithm
+  countingSort(array, size)
   max <- find largest element in array
   initialize count array with all zeros
   for j <- 0 to size
@@ -16,7 +16,7 @@ countingSort(array, size)
     find the cumulative sum and store it in count array itself
   for j <- size down to 1
     restore the elements to array
-    decrease count of each element restored by 1 */
+# decrease count of each element restored by 1 */
 
 /* C Program to sort Elements Using Counting Sort */
 #include <stdio.h>

65de265aba4fd1b25d4f1a44fa28169c03eb69db
Shloka gupta <chicken-biryani@users.noreply.github.com>
2021-01-10 23:46:42 +0530

    Queue program
    Made the required fixes in my program

---
+++ Python/ds/dynamic_queue.py
@@ -0,0 +1,122 @@
+'''
+Queue implementation using List in Python
+
+'''
+
+try:  #try catch so that the program does not crash
+    
+    queue=[]
+        
+    #For the queue to be dynamic it has been put in a loop
+    #While the loop is true the user gets 5 options:
+    #To insert element in queue,remove, display all elelements,reverse queue or exit
+    
+
+    while True:
+        op = int(input('''
+        Press--> 
+        1 to insert into queue 
+         2 to  remove from queue 
+         3 to display values of queue 
+         4 to reverse the exisiting queue
+         5 to exit '''))
+ 
+        
+        if op==1: #to insert an elelment in the queue
+
+            ele = int(input("enter elem to insert "))
+            queue.append(ele)
+        
+        elif op==2: #to remove an element from the queue
+
+            if len(queue)==0:
+                print("The queue is empty, insert values if required")
+            else:
+                ele=queue.pop(0)
+                print("Element removed is - ",ele)
+        
+        elif op==3: #to display the elements in the queue
+
+            if len(queue)==0:
+                print("The queue is empty, insert values if required")
+            else:
+                print(queue)
+        
+        elif op==4: #to reverse queue
+
+            queue.reverse()
+            print(queue)
+        
+        elif op==5: #to exit
+
+            break
+        
+        else:
+            print("invalid option")
+
+except ValueError:
+
+    print("Please enter integer only") #If user inputs an alphabet or string the program should not crash
+
+except:
+
+    print("There's been some issue please check the data you've entered")
+
+
+"""
+Sample Input- Output 
+
+dynamic_queue.py
+
+        Press-->
+         1 to insert into queue
+         2 to  remove from queue
+         3 to display values of queue
+         4 to reverse the exisiting queue
+         5 to exit 1
+enter elem to insert 3
+
+        Press-->
+         1 to insert into queue
+         2 to  remove from queue
+         3 to display values of queue
+         4 to reverse the exisiting queue
+         5 to exit 1
+enter elem to insert 4
+
+        1 to insert into queue
+         2 to  remove from queue
+         3 to display values of queue
+         4 to reverse the exisiting queue
+         5 to exit 1
+enter elem to insert 5
+
+        Press-->
+        1 to insert into queue
+         2 to  remove from queue
+         3 to display values of queue
+         4 to reverse the exisiting queue
+         5 to exit 4
+[5, 4, 3]
+
+        Press-->
+        1 to insert into queue
+         2 to  remove from queue
+         3 to display values of queue
+         4 to reverse the exisiting queue
+         5 to exit 2
+Element removed is -  5
+
+        Press-->
+        1 to insert into queue
+         2 to  remove from queue
+         3 to display values of queue
+         4 to reverse the exisiting queue
+         5 to exit 5
+
+"""
+
+"""
+Complexity - O(n)
+
+"""

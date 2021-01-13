c9e1c6e06a3be2af5b95496b7de01f90df3b2409
Meha Bhalodiya <73488906+mehabhalodiya@users.noreply.github.com>
2021-01-13 16:29:28 +0530

    Updated MatrixScript.py

--- Miscellaneous/HackerRank/MatrixScript.py
+++ Miscellaneous/HackerRank/MatrixScript.py
@@ -22,7 +22,7 @@ import os
 import random
 import re
 import sys
-
+# taking multiple integer inputs on a single line
 first_multiple_input = input().rstrip().split()
 
 n = int(first_multiple_input[0])
@@ -38,7 +38,11 @@ string = ""
 for y in range(m):
     for x in range(n):
         string += matrix[x][y]
+# for pattern matching & helps to search a pattern again without rewriting it
+# \w for word character and \W for non-word character
 pattern = re.compile('(\w)(\W)+(\w)')
+# prefix r for raw string which ignores escape and \1 is a backreference
+# \3 is for backreference to 3rd parenthesized subexpression
 subs = pattern.sub(r"\1 \3", string)
 print(subs)
 
@@ -65,4 +69,3 @@ single space ' ' for better readability.
 So, the final decoded script is:
 This is Matrix#  %!
 '''
-

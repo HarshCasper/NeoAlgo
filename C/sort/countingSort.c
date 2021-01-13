7bc89e0419721f6400207741368fc805a99c72bc
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-13 23:20:48 +0530

    Update countingSort.c

--- C/sort/countingSort.c
+++ C/sort/countingSort.c
@@ -9,8 +9,7 @@ where n is the number of elements in input array and k is the range of input.
 
 //***************************** CODE ************************************
 #include <stdio.h>   
-int countingSort(int A[], int k, int n)
-{
+int countingSort(int A[], int k, int n){
     int i, j;
     int B[15], C[100];
     for (i = 0; i <= k; i++){
@@ -22,8 +21,7 @@ int countingSort(int A[], int k, int n)
     for (i = 1; i <= k; i++){
         C[i] = C[i] + C[i-1];
     }
-    for (j = n; j >= 1; j--)
-    {
+    for (j = n; j >= 1; j--){
         B[C[A[j]]] = A[j];
         C[A[j]] = C[A[j]] - 1;
     }
@@ -34,15 +32,12 @@ int countingSort(int A[], int k, int n)
     return 0;
 }
  
-int main()
-{
-    
+int main(){
     int n, k = 0, A[15], i;
     printf("Enter the number of input : ");
     scanf("%d", &n);
     printf("\nEnter the elements to be sorted :\n");
-    for (i = 1; i <= n; i++)
-    {
+    for (i = 1; i <= n; i++){
         scanf("%d", &A[i]);
         if (A[i] > k) {
             k = A[i];

4c673528cc26f3e7bd5c9f83f6bf3c11cf0f8924
Sakshi Khachane <53635792+sakshikhachane@users.noreply.github.com>
2021-01-17 15:25:17 +0530

    Update radixSort.go

--- Go/sort/radixSort.go
+++ Go/sort/radixSort.go
@@ -25,30 +25,29 @@ func radixSort(array []int) []int {
 	unSorted := make([]int, size, size)
 
 	// Loop until the largest significant digit is reached
-	for largestNum / significantDigit > 0 {
-	    strconv.Itoa(significantDigit) 
-  
+	for largestNum/significantDigit > 0 {
+		strconv.Itoa(significantDigit)
 
 		bucket := [10]int{0}
-		
+
 		for i := 0; i < size; i++ {
-			bucket[(array[i] / significantDigit) % 10]++
+			bucket[(array[i]/significantDigit)%10]++
 		}
 
 		for i := 1; i < 10; i++ {
-			bucket[i] += bucket[i - 1] 
+			bucket[i] += bucket[i-1]
 		}
 
 		for i := size - 1; i >= 0; i-- {
-			bucket[(array[i] / significantDigit) % 10]--
-			unSorted[bucket[(array[i] / significantDigit) % 10]] = array[i]
+			bucket[(array[i]/significantDigit)%10]--
+			unSorted[bucket[(array[i]/significantDigit)%10]] = array[i]
 		}
 
 		for i := 0; i < size; i++ {
 			array[i] = unSorted[i]
 		}
-  
-		significantDigit *= 10 
+
+		significantDigit *= 10
 	}
 
 	return array
@@ -56,7 +55,7 @@ func radixSort(array []int) []int {
 
 func main() {
 
-	unsortedList :=[]int{14, 10, 9, 18, 20, 5, 2, 7}
+	unsortedList := []int{14, 10, 9, 18, 20, 5, 2, 7}
 
 	sortedList := radixSort(unsortedList)
 
@@ -65,5 +64,5 @@ func main() {
 
 /*
 Output -
-Sorted List: [2 5 7 9 10 14 18 20] 
+Sorted List: [2 5 7 9 10 14 18 20]
 */

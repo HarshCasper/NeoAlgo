dbac2cd4a07614ad43787c24627918a6fc23f347
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-01-18 19:09:14 +0530

    Update countingSort.c

--- C/sort/countingSort.c
+++ C/sort/countingSort.c
@@ -1,12 +1,3 @@
-//************************* COMPLEXITIES **********************************
-/*
-Counting sort is an algorithm for sorting a collection of objects according to keys that are small integers i.e; it is an integer sorting algorithm. It operates by counting the number of objects that have each distinct key value, and using arithmetic on those counts to determine the positions of each key value in the output sequence. 
-Worst complexity: O(n+k)
-Average complexity: O(n+k)
-Space complexity: O(n+k)
-where n is the number of elements in input array and k is the range of input.
-*/
-
 //***************************** CODE ************************************
 #include <stdio.h>   
 int countingSort(int A[], int k, int n){
@@ -46,11 +37,20 @@ int main(){
  return 0;
 }
 
-//********************************** OUTPUT ******************************
+//*************************** SAMPLE OUTPUT ******************************
 /*
 Enter the number of input : 12
 Enter the elements to be sorted : 76 54 32 90 87 55 32 65 77 88 42 10
 The Sorted array is : 10 32 32 42 54 55 65 76 77 87 88 90 
+*/
+
+//************************* COMPLEXITIES **********************************
+/*
+Counting sort is an algorithm for sorting a collection of objects according to keys that are small integers i.e; it is an integer sorting algorithm. It operates by counting the number of objects that have each distinct key value, and using arithmetic on those counts to determine the positions of each key value in the output sequence. 
+Worst complexity: O(n+k)
+Average complexity: O(n+k)
+Space complexity: O(n+k)
+where n is the number of elements in input array and k is the range of input.
 */
 
 //*********************************************************************

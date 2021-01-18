a315c523b52aec391f1ee2c23f935849dd2c2275
Amit kumar mishra <akm43300@gmail.com>
2021-01-18 19:26:18 +0530

    Updated the suggested changes

--- Java/cp/ReverseString.java
+++ Java/cp/ReverseString.java
@@ -1,9 +1,12 @@
-import java.lang.*;
+
+/* Program to reverse a string */
+
 import java.io.*;
 import java.util.*;
  
-// Class of ReverseString
-class ReverseString {
+// Main class
+
+class Program {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
@@ -12,17 +15,29 @@ class ReverseString {
 
         System.out.println("Enter string");
         String str = sc.nextLine();
-        String result = "";
-          
+
+        // revoking the function reverse
+
+        reverse(str);
+        
+    }
+
+    // Method for reversing the string
+
+    static void reverse(String s){
+
         // Reversing the string by extracting each element from the end of the string and storing in a variable result
 
-        for (int i = str.length() - 1; i >= 0; i--)
-            result += str.charAt(i);
+        String result = "";
+        for (int i = s.length() - 1; i >= 0; i--)
+            result += s.charAt(i);
 
         // Printing the reversed string
 
         System.out.println(result);
+       
     }
+
 }
 
 /* Time Complexity is O(n)

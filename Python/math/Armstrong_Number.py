1d375992c427b554ffc9ce188be8ad0aee5f56a2
Nayana <nayanajain854@gmail.com>
2021-01-21 12:54:29 +0530

    added file Armstrong_Number.py

---
+++ Python/math/Armstrong_Number.py
@@ -0,0 +1,31 @@
+# program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
+#Output
+#case 1
+#Enter a number: 407
+#407 is an Armstrong number
+#case 2
+#Enter a number: 22
+#22 is not an Armstrong number
+
+
+

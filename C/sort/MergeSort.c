8e5ae4b38cbaa25e655305c4f309c6327e6e7d81
The-Noob-Codder <77674909+The-Noob-Codder@users.noreply.github.com>
2021-01-24 09:58:58 +0530

    Update MergeSort.c
    Here the function prototype was not declared before main function so I placed those functions before main.

--- C/sort/MergeSort.c
+++ C/sort/MergeSort.c
@@ -1,48 +1,6 @@
 // MERGE SORT IN C
 #include<stdio.h>
-void main()
-{
-    int arraySize ,  index;
-    printf("Please enter the size of the array\n");
-    scanf("%d",&arraySize);
-    int arr[arraySize];
-    printf("Please enter the elements to sort\n");
-    for(index=0 ; index<arraySize ; index++)
-    {
-        scanf("%d",&arr[index]);// You would now fill the array with the numbers you want to sort
-    }
 
-    // Now the lower bound and upper bound values would be defined
-
-int lower_bound =0;
-int upper_bound = arraySize-1;
-
-// we pass three parameters to the mergeSort function
-
-mergeSort(arr,lower_bound, upper_bound);
-
-printf("SORTED ARRAY\n");
-
-for(index=0 ; index<arraySize ; index++)
-    {
-        printf("%d\t",arr[index]);
-    }
-
-}
-int mergeSort(int arr[],int lower_bound, int upper_bound)
-{
-    // We use recursion to divide the array
-
-    if(lower_bound<upper_bound)
-    {
-        int mid = (lower_bound+upper_bound)/2;
-        mergeSort(arr,lower_bound,mid);
-        mergeSort(arr,mid+1,upper_bound);
-
-        // Now we use merge function to join the array in a sorted manner
-        merge(arr , lower_bound , mid , upper_bound );
-    }
-}
 int merge(int arr[],int lower_bound , int mid , int upper_bound )
 {
     int variable_a = lower_bound ;
@@ -91,6 +49,53 @@ for(variable_c=lower_bound; variable_c<=upper_bound ; variable_c++)
     // Copying the sorted values from array_new to arr
     {
        arr[variable_c] = arr_new[variable_c];
+    }
+
+}
+
+
+int mergeSort(int arr[],int lower_bound, int upper_bound)
+{
+    // We use recursion to divide the array
+
+    if(lower_bound<upper_bound)
+    {
+        int mid = (lower_bound+upper_bound)/2;
+        mergeSort(arr,lower_bound,mid);
+        mergeSort(arr,mid+1,upper_bound);
+
+        // Now we use merge function to join the array in a sorted manner
+        merge(arr , lower_bound , mid , upper_bound );
+    }
+}
+
+
+void main()
+{
+    int arraySize ,  index;
+    printf("Please enter the size of the array\n");
+    scanf("%d",&arraySize);
+    int arr[arraySize];
+    printf("Please enter the elements to sort\n");
+    for(index=0 ; index<arraySize ; index++)
+    {
+        scanf("%d",&arr[index]);// You would now fill the array with the numbers you want to sort
+    }
+
+    // Now the lower bound and upper bound values would be defined
+
+int lower_bound =0;
+int upper_bound = arraySize-1;
+
+// we pass three parameters to the mergeSort function
+
+mergeSort(arr,lower_bound, upper_bound);
+
+printf("SORTED ARRAY\n");
+
+for(index=0 ; index<arraySize ; index++)
+    {
+        printf("%d\t",arr[index]);
     }
 
 }

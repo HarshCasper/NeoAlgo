5047651339838dd8fcea437575f959bb482e46cb
Shloka gupta <chicken-biryani@users.noreply.github.com>
2021-01-25 15:29:35 +0530

    PEP-8

--- Python/ds/dynamic_stack.py
+++ Python/ds/dynamic_stack.py
@@ -1,12 +1,11 @@
 '''
 Stack implementation using List in Python
-
 '''
 
-stack=[]
+stack = []
 
-#For the stack to be dynamic it has been put in a loop
-#While the loop is true the user gets 5 options:
+# For the stack to be dynamic it has been put in a loop
+# While the loop is true the user gets 5 options:
 # 1 to push  element in stack
 # 2 to pop element from stack
 # 3 to display elements in stack
@@ -14,47 +13,47 @@ stack=[]
 # 5 To exit
 
 
-try: # try except so that the program does not crash[optional]
+try:  # try except so that the program does not crash[optional]
 
     while True:
         op = int(input('''
-Press--> 
+Press-->
 1 to push into stack
 2 to pop from stack
-3 to diplay elements of stack 
+3 to diplay elements of stack
 4 to peek top most element
-5 to exit 
+5 to exit
+
 '''))
 
-
-        if op==1: # to push into stack
+        if op == 1:  # To push into stack
 
             ele = int(input("\nenter element  to push "))
             stack.append(ele)
 
-        elif op==2: # to pop from stack.
+        elif op == 2:  # To pop from stack.
 
-            if len(stack) ==0:
+            if len(stack) == 0:
                 print("\nStack is empty ")
             else:
-                ele=stack.pop()                        
-                print("\npopped element ",ele)
+                ele = stack.pop()
+                print("\npopped element ", ele)
 
-        elif op==3: # To display stack elements
+        elif op == 3:  # To display stack elements
 
-            if len(stack) ==0:
-                print("\nStack is empty ")
-            else:                      
-                print(stack)      
-
-        elif op==4: # To peek stack top.
-
-            if len(stack)==0:						  
+            if len(stack) == 0:
                 print("\nStack is empty ")
             else:
-                print("\nTop most element in stack is ",stack[-1])
+                print(stack)
 
-        elif op==5: # To exit from loop
+        elif op == 4:  # To peek stack top.
+
+            if len(stack) == 0:
+                print("\nStack is empty ")
+            else:
+                print("\nTop most element in stack is ", stack[-1])
+
+        elif op == 5:  # To exit from loop
 
             break
 
@@ -62,7 +61,7 @@ Press-->
 
             print("\ninvalid option")
 
-		
+
 except ValueError:
 
     print("Please enter integer only")
@@ -73,8 +72,7 @@ except:
 
 
 """
-Sample Input- Output 
-
+Sample Input- Output
 Press-->
 1 to push into stack
 2 to pop from stack
@@ -82,9 +80,7 @@ Press-->
 4 to peek top most element
 5 to exit
 1
-
 enter element  to push 2
-
 Press-->
 1 to push into stack
 2 to pop from stack
@@ -92,9 +88,7 @@ Press-->
 4 to peek top most element
 5 to exit
 1
-
 enter element  to push 3
-
 Press-->
 1 to push into stack
 2 to pop from stack
@@ -102,9 +96,7 @@ Press-->
 4 to peek top most element
 5 to exit
 1
-
 enter element  to push 4
-
 Press-->
 1 to push into stack
 2 to pop from stack
@@ -113,7 +105,6 @@ Press-->
 5 to exit
 3
 [2, 3, 4]
-
 Press-->
 1 to push into stack
 2 to pop from stack
@@ -121,9 +112,7 @@ Press-->
 4 to peek top most element
 5 to exit
 2
-
 popped element  4
-
 Press-->
 1 to push into stack
 2 to pop from stack
@@ -131,10 +120,9 @@ Press-->
 4 to peek top most element
 5 to exit
 5
-
 """
 
 """
 Time complexity - O(n)
-
 """
+

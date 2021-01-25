a9a4ab43a7815908029778d8c0778c9c6c5843db
Shloka gupta <chicken-biryani@users.noreply.github.com>
2021-01-25 13:34:32 +0530

    Vowels and Consants In PEP-8

--- Python/cp/VowelsConsants.py
+++ Python/cp/VowelsConsants.py
@@ -1,31 +1,26 @@
 """
-The program helps to count the number of 
+The program helps to count the number of
 vowels and consants in a word
 
 """
 
 
-
 def Count(word):
-    vc, cc = 0, 0 #vc-vowels cc-consonant
+    vc, cc = 0, 0
 
     for s in word:
         if (s.isalpha()):
-            if (s=='a' or s=='e' or s=='i' or s=='o' or s=='u'):
+            if (s == 'a' or s == 'e' or s == 'i' or s == 'o' or s == 'u'):
                 vc = vc + 1
             else:
                 cc = cc + 1
-	
-    return vc,cc;
+    return vc, cc
 
+TheWord = input("enter a string ").lower()
 
+vc, cc = Count(TheWord)
 
-
-TheWord= input("enter a string ").lower()
-
-vc,cc=Count(TheWord)
-
-print("vowel count = ", vc, " consonant count = ", cc) 
+print("vowel count = ", vc, " consonant count = ", cc)
 
 
 """
@@ -35,5 +30,5 @@ VowelsConsants.py
 enter a string Hello World
 vowel count =  3  consonant count =  7
 
-
 """
+

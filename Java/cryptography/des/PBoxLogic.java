9d1ab5bb38d1dfd533e974585568949682c01584
Purav Uday Desai <44437936+PuravUdayDesai@users.noreply.github.com>
2021-01-31 18:56:03 +0530

    Added PBoxLogic.java

---
+++ Java/cryptography/des/PBoxLogic.java
@@ -0,0 +1,41 @@
+package des;
+
+/**
+ * <h1>P-BOX</h1>
+ * <p>
+ * 		class PBoxLogic is a public class which acts as a logic class which helps to permute through P-BOX
+ * </p>
+ */
+public class PBoxLogic
+{
+	/**
+	 * <p>
+	 * 		This is a public method which is used to permute value through P-BOX, this is done with the help of table P_BOX which
+	 * 		is present in des.DESCryptographyModels.P_BOX
+	 * </p>
+	 * 
+	 * @param  value     This is a 32-bit value
+	 * @param  pBoxTable This is the P-BOX
+	 * 
+	 * @return           byte[] This is 32-bit permuted value
+	 */
+	public byte[] permuteByPBox(byte[] value, byte[] pBoxTable)
+	{
+		//created an array which will hold permuted value
+		byte[] permutedByPBox = new byte[32];
+		
+		//iterates 32 time
+		for (int i = 0; i < permutedByPBox.length; i++)
+		{
+			/*
+			 * Each new element in 32-bit array is generated with the help of P_BOX table, at each iteration the value of "P_BOX" at
+			 * ith index is used as the index of "value" and the value which is retrieved from it is used as the element in 64-bit
+			 * array.
+			 */
+			permutedByPBox[i] = value[pBoxTable[i] - 1];
+		}
+		
+		//returning P-BOX permuted value, it can
+		return permutedByPBox;
+	}// End of permuteByPBox(byte[],byte[]) method
+}// End of class PBoxLogic

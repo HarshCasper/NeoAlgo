05bf22b7e2c0548b7592d8e58deeb05c1eee8703
sloth-panda <achyutpanda488@gmail.com>
2021-02-08 10:11:20 +0530

    added selection sort

---
+++ JavaScript/sort/selectionSort.js
@@ -0,0 +1,40 @@
+/*
+This algorithm divides the input array into two sublists - a sorted and unsorted sublist. The sorted list is located at the start of the collection, and all elements to the right of the final sorted element are considered unsorted.
+
+Initially, the sorted list is empty, while the rest of the collection is unsorted. Selection Sort goes through the unsorted sublist and finds the smallest or largest element.
+
+The element is then swapped with the leftmost element of the unsorted sublist. Then, the sorted sublist is expanded to include that element.
+
+This is repeated, finding the fitting element, swapping it with the leftmost element of the unsorted list, and expanding the sorted list to encompass it.
+
+After each iteration, one less element needs to be checked, until the entire array or list is sorted. In other words, after the k-th iteration, the first k elements of the array or list are guaranteed to be sorted.
+
+*/
+
+function selectionSort(inputArr) {
+    let n = inputArr.length;
+
+    for (let i = 0; i < n; i++) {
+        // Finding the smallest number in the subarray
+        let min = i;
+        for (let j = i + 1; j < n; j++) {
+            if (inputArr[j] < inputArr[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            // Swapping the elements
+            let tmp = inputArr[i];
+            inputArr[i] = inputArr[min];
+            inputArr[min] = tmp;
+        }
+    }
+    return inputArr;
+}
+
+
+let inputArr = [5, 2, 4, 6, 1, 3];
+selectionSort(inputArr);
+console.log(inputArr);
+
+(6)[1, 2, 3, 4, 5, 6] /*output*/

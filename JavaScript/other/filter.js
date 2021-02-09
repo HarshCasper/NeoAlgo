2fa7a1610cc5d5e4b708ba77443f91b0ba81cfc5
sloth-panda <achyutpanda488@gmail.com>
2021-02-09 22:08:43 +0530

    updated the higher order array method files

--- JavaScript/other/filter.js
+++ JavaScript/other/filter.js
@@ -1,10 +1,13 @@
 //(Filter)
-//The filter() method takes each element in an array and it applies a conditional statement against it.If this conditional returns true, the element gets pushed to the output array. If the condition returns false, the element does not get pushed to the output array.
+/*The filter() method takes each element in an array and it applies a conditional statement against it.
+If this conditional returns true, the element gets pushed to the output array. If the condition returns
+ false, the element does not get pushed to the output array.*/
 
 //Example-1
 const numbers = [1, 2, 3, 4];
 const evens = numbers.filter(item => item % 2 === 0);
-console.log(evens); // [2, 4]
+console.log(evens);
+//Output: [2, 4]
 
 //Example-2
 const students = [
@@ -16,4 +19,5 @@ const students = [
 ];
 
 const studentGrades = students.filter(student => student.grade >= 90);
-return studentGrades; // [ { name: 'Quincy', grade: 96 }, { name: 'Alexis', grade: 100 }, { name: 'Katie', grade: 90 } ]
+return studentGrades;
+//Output: [ { name: 'Quincy', grade: 96 }, { name: 'Alexis', grade: 100 }, { name: 'Katie', grade: 90 } ]
--- JavaScript/other/map.js
+++ JavaScript/other/map.js
@@ -1,12 +1,12 @@
-
 //(Map)
 //It will basically create a new array from an existing array applying the method to each member of the array
 
 const numbers = [1, 2, 3, 4];
 const doubled = numbers.map(item => item * 2);
-console.log(doubled); // [2, 4, 6, 8]
-
+console.log(doubled);
+//Output: [2, 4, 6, 8]
 
 const numbers = [1, 2, 3, 4];
 const sub = numbers.filter(item => item - 2);
-console.log(sub); // [-1, 0, 1, 2]
+console.log(sub);
+//Output: [-1, 0, 1, 2]

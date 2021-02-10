aeb73038d1992f2f5275b2a72fb058274f4e255b
Yash202000 <69676333+Yash202000@users.noreply.github.com>
2021-02-10 19:42:08 +0530

    Update Sparse_matrix_Operation.py
    In this code I added addition and subtraction program code.

--- Python/ds/Sparse_matrix_Operation.py
+++ Python/ds/Sparse_matrix_Operation.py
@@ -1,115 +1,153 @@
-"""
-
-Input:
-
-    Enter the number of rows: 3
-
-    Enter the number of columns: 3
-
-    Enter the entries row wise:
-    1
-    1
-    0
-    0
-    0
-    0
-    0
-    2
-    0
-
-Output:
-
-    Given matrix is a sparse matrix
-    [0, 0, 2]
-    [0, 1, 1]
-    [1, 1, 2]
-
-
-Time Complexity:O(noOfRows*noOfcolumns)
-Space Complexity:O(1)
+#here we are going to take or read the matrix and convert matrix into thte sparse matrix
+from operator import itemgetter , attrgetter
 
 """
+this read_mat function will read input from user 
+and return matrix as list of inputs
+ 
+"""
+def read_mat():
+     matrix=[]
+     for i in range(int(input("enter value of row : "))):
+         l=list(map(int,input().split()))
+         matrix.append(l)
+     return matrix
+
+"""
+make_sparse will create sparse matrix 
+from given list input
+"""
+def make_sparse(matrix):
+
+    sm=[] # any nonzero elements will be stored in the sparse matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])): #considering the any seach row
+            if matrix[i][j]!=0:
+                sm.append([i,j,matrix[i][j]])
+    sm.insert(0,[len(matrix),len(matrix[0]),len(sm)])
+    return sm
+
+"""
+this function is for display sparse matrix
+ 
+"""
+def display_matrix(k):
+    print("The Output matrix : ")
+    for i in range(len(k)):
+        for j in range(len(k[0])):
+            print(k[i][j],end=" ")
+        print()
+
+"""
+core addition function to perform addition of two sparse matrix
+"""
+def addspm(sp1,sp2):
+    print("performing addition on given sparse matrices......... ")
+    count=1
+    sp3=[]
+    i,j=1,1
+    if sp1[0][0]==sp2[0][0] and sp1[0][1]==sp2[0][1]:
+        while i<len(sp1) and j<len(sp2) :
+            if sp1[i][0] > sp2[j][0] or (sp1[i][0]==sp2[j][0] and sp1[i][1]>sp2[j][1]):
+                sp3.append([sp2[j][0],sp2[j][1],sp2[j][2]])
+                j+=1
+
+            elif sp1[i][0] < sp2[j][0] or (sp1[i][0] == sp2[j][0] and sp1[i][1] < sp2[j][1]):
+                sp3.append([sp1[i][0],sp1[i][1],sp1[i][2]])
+                i+=1
+
+            else:
+                if (sp1[i][2]+sp2[j][2]):
+                    sp3.append([sp1[i][0],sp1[i][1],sp1[i][2]+sp2[j][2]])
+                    count+=1
+                    i+=1
+                    j+=1
+        while i<len(sp1):
+            sp3.append([sp1[i][0],sp1[i][1],sp1[i][2]])
+            count+=1
+            i+=1
+        while j<len(sp2):
+            sp3.append([sp2[j][0],sp2[j][1],sp2[j][2]])
+            count+=1
+            j+=1
+        sp3.insert(0,[sp1[0][0],len(sp3[1]),len(sp3)])
+        display_matrix(sp3)
+    else:
+        return "addition operation is not possible."
+
+"""
+this is second addition program 
+I was trying to enhance my aproach from 
+above code
+"""
+def addspm2(k1,k2):
+    i,j=1,1
+    sp3=[]
+    if k1[0][0]==k2[0][0] and k1[0][1]==k2[0][1]:
+        while i<len(k1) and j<len(k2):
+            if k1[i][0]==k2[j][0] and k1[i][1]==k2[j][1]:
+                sp3.append([k1[i][0],k2[j][1],k1[i][2]+k2[j][2]])
+                i+=1
+                j+=1
+            else:
+                if k1[i][0]<k2[j][0]:
+                    sp3.append([k1[i][0],k1[i][1],k1[i][2]])
+                    i+=1
+                else:
+                    sp3.append([k2[j][0],k2[j][1],k2[j][2]])
+                    j+=1
+        print(sp3)
+    else:
+        print("not possible")
 
 
-# for row and column input
+"""
+supstraction of sparse matrix
+program
+
+"""
+def substractspm(sp1,sp2):
+    print("performing substraction on given sparse matrices : ")
+    count = 1
+    sp3 = []
+    i, j = 1, 1
+    if sp1[0][0] == sp2[0][0] and sp1[0][1] == sp2[0][1]:
+        while i < len(sp1) and j < len(sp2):
+            if sp1[i][0] > sp2[j][0] or (sp1[i][0] == sp2[j][0] and sp1[i][1] > sp2[j][1]):
+                sp3.append([sp2[j][0], sp2[j][1], - sp2[j][2]])
+                j += 1
+
+            elif sp1[i][0] < sp2[j][0] or (sp1[i][0] == sp2[j][0] and sp1[i][1] < sp2[j][1]):
+                sp3.append([sp1[i][0], sp1[i][1], sp1[i][2]])
+                i += 1
+
+            else:
+                if (sp1[i][2] + sp2[j][2]):
+                    sp3.append([sp1[i][0], sp1[i][1], sp1[i][2] - sp2[j][2]])
+                    count += 1
+                    i += 1
+                    j += 1
+        while i < len(sp1):
+            sp3.append([sp1[i][0], sp1[i][1], sp1[i][2]])
+            count += 1
+            i += 1
+        while j < len(sp2):
+            sp3.append([sp2[j][0], sp2[j][1], - sp2[j][2]])
+            count += 1
+            j += 1
+        sp3.insert(0, [sp1[0][0], len(sp3[1]), len(sp3)])
+        display_matrix(sp3)
+    else:
+        return "substraction operation is not possible."
 
 
-ROW = int(input("Enter the number of rows:"))
-COLUMN = int(input("Enter the number of columns:"))
 
 
-# Initialize matrix
+def main():
+    k1=make_sparse(read_mat())
+    k2=make_sparse(read_mat())
+    
+    addspm(k1,k2)
 
-
-matrix = []
-
-print("Enter the entries row wise:")
-
-# For user input
-
-
-for i in range(ROW):          # A for loop for row entries
-    a = []
-    for j in range(COLUMN):      # A for loop for column entries
-        a.append(int(input()))
-    matrix.append(a)        # taking input of arrays
-
-# Initializing count = 0 to count the number of zeros.
-
-count = 0
-
-# Calculates number of rows and columns present in given matrix
-
-
-matrix_rows = len(matrix)
-matrix_cols = len(matrix[0])
-
-# Calculates the size of the array
-
-
-size = matrix_rows * matrix_cols
-
-
-# Count all zero element present in matrix
-
-for i in range(0, matrix_rows):
-    for j in range(0, matrix_cols):
-        if(matrix[i][j] == 0):
-            count = count + 1
-
-
-# Sparse Matrix definition
-
-def sparse_matrix():
-    size = 0
-    for i in range(ROW):
-        for j in range(COLUMN):
-            if (matrix[i][j] != 0):
-                size += 1
-
-    matrix_rows, matrix_cols = (3, size)
-    Print_Matrix = [[0 for i in range(matrix_cols)]
-                    for j in range(matrix_rows)]
-
-    k = 0
-    for i in range(ROW):
-        for j in range(COLUMN):
-            if (matrix[i][j] != 0):
-                Print_Matrix[0][k] = i
-                Print_Matrix[1][k] = j
-                Print_Matrix[2][k] = matrix[i][j]
-                k += 1
-
-    for i in Print_Matrix:
-        print(i)
-
-
-if(count == size):
-    print("Null Mtrix")
-
-elif(count > (size/2)):
-    print("Given matrix is a sparse matrix")
-    sparse_matrix()
-
-else:
-    print("Given matrix is not a sparse matrix")
+if __name__ == '__main__':
+    main()

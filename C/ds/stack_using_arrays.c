555356e3bdd047737ac2f08f0148d2d8beba35da
Rishit Bhojak <rajabhojak@gmail.com>
2021-02-14 20:43:10 +0530

    Refactor : Refactored Code

--- C/ds/stack_using_arrays.c
+++ C/ds/stack_using_arrays.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 //Implementing stacks using arrays
 struct stack{
     int size;
@@ -7,6 +8,7 @@ struct stack{
     int *arr;
 };
 
+// To check if the stack is full
 int isFull(struct stack *ptr){
     if(ptr->top == ptr->size-1)
     {
@@ -17,6 +19,8 @@ int isFull(struct stack *ptr){
         return 0;
     }
 }
+
+// To check if the stack is empty
 int isEmpty(struct stack *ptr){
     if(ptr->top == -1)
     {
@@ -28,6 +32,7 @@ int isEmpty(struct stack *ptr){
     }
 }
 
+//To inert Elements
 void push(struct stack *ptr,int val)
 {
     if(isFull(ptr))
@@ -39,8 +44,9 @@ void push(struct stack *ptr,int val)
         ptr->top++;
         ptr->arr[ptr->top]=val;
     }
-    
 }
+
+//To remove the element at the top
 int pop(struct stack *ptr)
 {
     if (isEmpty(ptr))
@@ -54,4 +60,49 @@ int pop(struct stack *ptr)
         ptr->top--;
         return val;
     }
+}
+
+// To return the topmost element i.e to peek
+int peek(struct stack *ptr)
+{
+    if(!isEmpty(ptr))
+    {
+        return ptr->arr[ptr->top];
+    }
+    else{
+        exit(EXIT_FAILURE);
+    }
+}
+
+//Main Method to execute all the functions
+int main()
+{
+    int element,choice = 0 ;
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size=10;
+    sp->top=-1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("Enter choice: \n");
+    printf("1.Push\n 2.Pop\n 3.Peek\n");
+    scanf("%d", &choice);
+    if(choice==1)
+    {
+      printf("Enter the element to insert: \n");
+      scanf("%d", &element);
+      push(sp, element);
+    }
+    else if(choice ==2)
+    {
+        pop(sp);
+    }
+    else if(choice ==3)
+    {
+         peek(sp);
+    }
+    else
+    {
+       printf("Invalid Choice");     
+    }
+    
+    return 0;
 }

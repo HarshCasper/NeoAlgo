778fe6770a953193da5b5a5abbea592f4d2b3dc5
Achalesh Lakhotiya <2019kuec2036@iiitkota.ac.in>
2021-02-14 22:35:06 +0530

    Update BrianKenighan'sAlgorithm.cpp

--- C-Plus-Plus/other/BrianKenighan'sAlgorithm.cpp
+++ C-Plus-Plus/other/BrianKenighan'sAlgorithm.cpp
@@ -1,18 +1,12 @@
-// This is a preferred approach to count no. of set bits of a digit.
+/*This is a preferred approach to count no. of set bits of a digit.
 
-//  Brian Kernighan’s Algorithm:
-<<<<<<< HEAD
+Brian Kernighan’s Algorithm:
+*/ 
 =======
-// Concept -> If we subtract 1 from a decimal number, it will flip all the bits
-// after the rightmost set bit i.e 1 including that rightmost bit too.
-// As 10 is 0000 1010, 9 is 0000 1001
-// you can see in above example rightmost set bit in 10 is at second position
-// therefore in number (10 - 1) = 9 that second position and after that 1st position
-// flips away. 
-// So, we do bitwise of n and (n-1), then we unset the rightmost unset bit and we set
-// a counter to count the number of times loop executes we set bit count.   
-// In brief, no. of times loop runs is equal to count of set bits.
->>>>>>> 562da050f4cd780afd27ff67ca30ce7ef408cb26
+/* Concept -> If we subtract 1 from a decimal number, it will flip all the bits after the rightmost set bit i.e 1 including that rightmost bit too.
+As 10 is 0000 1010, 9 is 0000 1001 you can see in above example rightmost set bit in 10 is at second position therefore in number (10 - 1) = 9 that second position and after that 1st position
+flips away.So, we do bitwise of n and (n-1), then we unset the rightmost unset bit and we set a counter to count the number of times loop executes we set bit count.   
+In brief, no. of times loop runs is equal to count of set bits.*/ >>>>>>> 562da050f4cd780afd27ff67ca30ce7ef408cb26
 
 /* Concept -> If we subtract 1 from a decimal number, it will flip the all bits after the rightmost set bit i.e 1 including that rightmost bit too.
  As 10 is 0000 1010, 9 is 0000 1001, you can see in this example rightmost set bit in 10 is at second position
@@ -51,10 +45,10 @@ int main()
 <<<<<<< HEAD
 
 
-// Sample Input : n = 9
-// Sample Output : 2
-// Explanation: Binary representation of 9 is 1001 and has 2 set bits
-//Time Complexity: O(logn)
-
+/* Sample Input : n = 9
+ Sample Output : 2
+ Explanation: Binary representation of 9 is 1001 and has 2 set bits
+Time Complexity: O(logn)
+*/
 =======
 >>>>>>> 562da050f4cd780afd27ff67ca30ce7ef408cb26

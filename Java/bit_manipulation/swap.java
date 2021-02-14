66f9a5d609f95a25489c84e7b57716c824462e94
Rohit Srivastava <62320661+rcoder23@users.noreply.github.com>
2021-02-14 10:09:37 +0530

    Updated
    Added time and space complexity .
    and Examples Too

--- Java/bit_manipulation/swap.java
+++ Java/bit_manipulation/swap.java
@@ -14,5 +14,13 @@ public class swap {
 		System.out.println(b);
 
 	}
-
 }
+
+
+Example No1:- Let a=5 and b=10 
+	after xor operation output is a=10 and b=5 
+Example No2:- a=23 and b=5
+	after xor operation output is a=5 and b=23
+
+**Time Complexity **:- O(1) 
+**Space Complexity ** :- O(1)

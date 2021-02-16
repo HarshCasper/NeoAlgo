1709bc5fb8a7c92bc32a669e3df6facb36e48f61
Achalesh Lakhotiya <2019kuec2036@iiitkota.ac.in>
2021-02-16 10:28:41 +0530

    Update BrianKenighan'sAlgorithm.cpp

--- C-Plus-Plus/other/BrianKenighan'sAlgorithm.cpp
+++ C-Plus-Plus/other/BrianKenighan'sAlgorithm.cpp
@@ -9,7 +9,8 @@ flips away.So, we do bitwise of n and (n-1), then we unset the rightmost unset b
 In brief, no. of times loop runs is equal to count of set bits.
 */
 
-/* Concept -> If we subtract 1 from a decimal number, it will flip the all bits after the rightmost set bit i.e 1 including that rightmost bit too.
+/*
+ Concept -> If we subtract 1 from a decimal number, it will flip the all bits after the rightmost set bit i.e 1 including that rightmost bit too.
  As 10 is 0000 1010, 9 is 0000 1001, you can see in this example rightmost set bit in 10 is at second position
  therefore in number (10 - 1) = 9 that second position and after that 1st position
  flips away. So, we do bitwise of n and (n-1), then we unset the rightmost unset bit and we set
@@ -23,7 +24,6 @@ using namespace std;
 int main()
 {
     int n;
-    // input the digit
     cout << "Enter a Number" << endl; 
     cin >> n;
     // declared a counter to count set bits
@@ -46,8 +46,9 @@ int main()
 
 
 
-/* Sample Input : n = 9
- Sample Output : 2
- Explanation: Binary representation of 9 is 1001 and has 2 set bits
+/* 
+Sample Input : n = 9
+Sample Output : 2
+Explanation: Binary representation of 9 is 1001 and has 2 set bits
 Time Complexity: O(logn)
 */

a50f6b28113c8cb03e687bc63d3eabd2a06ec24c
Achalesh Lakhotiya <2019kuec2036@iiitkota.ac.in>
2021-02-16 10:30:09 +0530

    Update countsetbits.cpp

--- C-Plus-Plus/other/countsetbits.cpp
+++ C-Plus-Plus/other/countsetbits.cpp
@@ -1,13 +1,14 @@
-/* Normal & Simple Approach
- Loop through all bits in an integer, check if a bit is set and if it is then increment the set bit count.
+/* 
+Normal & Simple Approach
+Loop through all bits in an integer, check if a bit is set and if it is then increment the set bit count.
 */
+
 #include <bits/stdc++.h>
 using namespace std; 
 
 int main()
 {
     int n;
-    // input the digit
     cout << "Enter a Number" << endl; 
     cin >> n;
     // declared a counter to count set bits
@@ -29,7 +30,9 @@ int main()
     }
 }
 
-/* Sample Input : n = 9
+/* 
+Sample Input : n = 9
 Sample Output : 2
 Explanation: Binary representation of 9 is 1001 and has 2 set bits
- Time Complexity: (-)(logn) (Theta of logn) */
+Time Complexity: (-)(logn) (Theta of logn) 
+*/

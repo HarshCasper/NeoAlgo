dafa1b6643ad9e271615c5bfc5f7f0774a6da80b
Himanshu <65963997+himanshu007-creator@users.noreply.github.com>
2021-02-20 14:11:35 +0530

    Update jump_search.py

--- Python/search/jump_search.py
+++ Python/search/jump_search.py
@@ -1,34 +1,37 @@
-#JUMP-SEARCH
+# JUMP-SEARCH
 # NOTE:-Works only sorted arrays.
 import math
-#main function
-def jump_search(arr,search):
+# main function
+def jump_search(arr, search):
+    
+    
     flag = 0
     interval = int(math.sqrt(len(arr)))
-    for i in range(0,len(arr),interval):
-        if (arr[i] >  search):
+    for i in range(0, len(arr), interval):
+        if arr[i]>search:
             chunk = i
             flag = 1
             break
-        if (arr[i] ==  search):
+        if arr[i]==search:
             return i
-    if (flag==0):
-        c=i
+    if flag == 0:
+        c= i
         for j in arr[i:]:
-            if j==search:
+            if j == search:
                 return c
-            c+=1
+            c+= 1
     else:
         arr_ls = arr[chunk-interval:chunk]
-        ind = [i for i,d in enumerate(arr_ls) if d==search]
-        return chunk-interval+ind[0]
-#driver code
-if __name__ == '__main__':
-    arr = list(map(int,input().split()))
-    val=int(input())
-    res = jump_search(arr, val)
-    print(res)
+        ind = [i for i, d in enumerate(arr_ls) if d== search]
+        return (chunk-interval+ind[0])
     
+    
+# driver code
+if __name__ == '__main__':
+    ary = list(map(int, input().split()))
+    val= int(input())
+    res = jump_search(ary, val)
+    print(res)    
 # Time Complexity : O(√n)
 # Auxiliary Space : O(1)
 # SAMPLE INPUT line 1   1 2 3 4 5 6 7 8 9 10

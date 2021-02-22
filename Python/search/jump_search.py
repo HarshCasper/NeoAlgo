6169e1baccd9206be2eeb2c735a28c4c60df0e42
Himanshu <65963997+himanshu007-creator@users.noreply.github.com>
2021-02-22 13:35:18 +0530

    Update jump_search.py

--- Python/search/jump_search.py
+++ Python/search/jump_search.py
@@ -10,20 +10,17 @@ import math
 
 
 # main function
-def jump_search_2(arr,search):
+def jump_search(arr, search):
     """Takes in a sorted array arr and a value to search for"""
     interval = int(math.sqrt(len(arr)))
-
-    ''' find last lowest element '''
-    for i in range(0,len(arr),interval):
+    for i in range(0, len(arr), interval):
         if arr[i] < search:
             low = i
         elif arr[i] == search:
             return i
         else:
             break
-    ''' apply linear search '''
-    l_index = [e for e,i in enumerate(arr[low:low+interval]) if i == search]
+    l_index = [e for e, i in enumerate(arr[low:low+interval]) if i == search]
     if l_index[0]:
         return low+l_index[0]
     return "Not Found"
@@ -33,5 +30,5 @@ def jump_search_2(arr,search):
 if __name__ == "__main__":
     ary = list(map(int, input().split()))
     val = int(input())
-    res = jump_search(ary, val)
-    print(res)
+    ans = jump_search(ary, val)
+    print(ans)

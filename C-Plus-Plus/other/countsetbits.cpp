a217bbf761d78fc74e89488c0c00fb81f73ce005
Achalesh <code.achalesh@gmail.com>
2021-02-23 14:52:08 +0530

    everything done

--- C-Plus-Plus/other/countsetbits.cpp
+++ C-Plus-Plus/other/countsetbits.cpp
@@ -6,33 +6,29 @@ Loop through all bits in an integer, check if a bit is set and if it is then inc
 #include <bits/stdc++.h>
 using namespace std; 
 
+unsigned int countSetBits(unsigned int n)
+{
+    unsigned int count = 0;
+    while (n) {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
+
 int main()
 {
     int n;
     cout << "Enter a Number" << endl; 
     cin >> n;
-    // declared a counter to count set bits
-    unsigned int count = 0; 
+    cout << "Number of set bits or 1s in the number is : " << countSetBits(n);
     
-    while(n) {
-        
-        // take bitwise operation with 0001;
-        count += n & 1;
-        // right shift by 1 
-        n >>= 1; 
-    }
-
-    if (count > 1){
-    cout << "Number of set bits or 1s in digit are " << count;
-    }
-    else {
-    cout << "Number of set bits or 1s in digit is " << count;   
-    }
 }
 
 /*
-Sample Input : n = 9
-Sample Output : 2
+Sample Input : Enter a Number 
+               9
+Sample Output : Number of set bits or 1s in the number is : 2
 Explanation: Binary representation of 9 is 1001 and has 2 set bits
 Time Complexity: (-)(logn) (Theta of logn)
 */

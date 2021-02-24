f2b23b9bf756c5a1269801d23ce62fbe5cae55d1
Tanmay <comparisionthing@gmail.com>
2021-02-24 14:57:05 +0530

    Updated the file

--- Java/Algorithms/Kruskals_Algorithm.java
+++ Java/Algorithms/Kruskals_Algorithm.java
@@ -1,4 +1,7 @@
-//import java.util.Scanner;
+//It is an algroithm that help us to figure out the Minimum Spanning Tree of a given graph.
+//Minimum Spanning Tree is a Spanning Tree which has minimum total weight. If we have a linked undirected graph with a weight combine with each edge.
+//Then the cost of spanning tree would be the sum of the cost of its edges.
+//It runs faster in sparse graphs
 import java.util.Arrays;
 import java.util.Scanner;
 class Edge implements Comparable<Edge>{
@@ -8,30 +11,26 @@ class Edge implements Comparable<Edge>{
 	
 	//@Override
 	public int compareTo(Edge o) {
-		return this.weight - o.weight;  //To Sort in increasing order
-	}
-}
-
+		//To Sort in increasing order
+		return this.weight - o.weight;  
+        	}
+      }
 public class Kruskals_Algorithm {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-	
-	
-		int V = scan.nextInt();
-		int E = scan.nextInt();
-        Edge input[] = new Edge[E];
-		for(int i=0;i<E;i++) {
+                int Vertices = scan.nextInt();
+		int Edges = scan.nextInt();
+        Edge input[] = new Edge[Edges];
+		for(int i=0;i<Edges;i++) {
 			input[i] = new Edge();
 			input[i].source = scan.nextInt();
 			input[i].destination = scan.nextInt();
 			input[i].weight = scan.nextInt();
 		}
-		Kruskals(input,V);
-
+		Kruskals(input,Vertices);
 	}
-    private static void Kruskals(Edge[] input, int v) {
+       private static void Kruskals(Edge[] input, int v) {
 		Arrays.sort(input);
-		
 		Edge output[] = new Edge[v-1];
 		int parent[] = new int[v];
 		for(int i=0;i<v;i++) {
@@ -50,7 +49,6 @@ public class Kruskals_Algorithm {
 				parent[sourceParent] = destParent;
 			}
 			i++;
-														
 		}
 		for(int j=0;j<v-1;j++) {
 			if(output[j].source < output[j].destination) {
@@ -59,13 +57,11 @@ public class Kruskals_Algorithm {
 				System.out.println(output[j].destination + " " + output[j].source + " "+ output[j].weight);
 			}
 		}
-		
 	}
-    private static int findParent(int source, int[] parent) {
+       private static int findParent(int source, int[] parent) {
 		if(parent[source] == source) {
 			return source;
 		}
 		return findParent(parent[source],parent);
-	}
-
-}
+	 }
+ }

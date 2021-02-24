2aa433ac35bb8b708adfdb74b42564791b1c36fd
Achalesh Lakhotiya <2019kuec2036@iiitkota.ac.in>
2021-02-24 09:59:43 +0530

    not related to implementation branch

--- C-Plus-Plus/sort/selection_sort.cpp
+++ C-Plus-Plus/sort/selection_sort.cpp
@@ -1,12 +1,3 @@
-/*
-Selection Sort Algorithm:
-The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning.
-The algorithm maintains two subarrays in a given array.
-1) The subarray which is already sorted.
-2) Remaining subarray which is unsorted.
-In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray
-*/
-
 #include<iostream>
 using namespace std;
 void swapping(int &a, int &b) {         //swap the content of a and b
@@ -51,16 +42,3 @@ int main() {
    display(arr, n);
 }
 
-/*
-Sample Input : Enter size of array:
-               n = 5
-               12
-               5
-               -9
-               8
-               6
-Sample Output : Sorted Array:
-                -9 5 6 8 12
-Time Complexity : O(n^2)
-Space Complexity : O(1)
-*/

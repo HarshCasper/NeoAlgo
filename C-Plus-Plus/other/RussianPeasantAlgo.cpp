90cb2220bdaefe009767a5ddbfcd72b7274677e3
Nayana <nayanajain854@gmail.com>
2021-02-25 19:04:10 +0530

    done the requested changes

--- C-Plus-Plus/other/RussianPeasantAlgo.cpp
+++ C-Plus-Plus/other/RussianPeasantAlgo.cpp
@@ -1,7 +1,7 @@
 /*************************************************************
  Program to multiply two numbers using Russian peasant method
  Given two integers, write a function to multiply them without using multiplication operator.
- We double the first number and halve the second number repeatedly till the second number doesn’t become 1. 
+ We double the first number and halve the second number repeatedly till the second number doesn’t become 1.
 In the process, whenever the second number becomes odd, we add the first number to result.
 
  *************************************************************/
@@ -11,24 +11,29 @@ In the process, whenever the second number becomes odd, we add the first number 
 #include <bits/stdc++.h>
 using namespace std;
 
+//implementation of logic
+int fractionalknapsack(int m, int n)
+{
+    int result;
+       while(n > 0)
+    {
+        if(n % 2 != 0)
+        {
+            result += m;
+            m << 2;
+            n >> 2;
+        }
+    }
+    return result;
+}
+
 int main()
 {
     int num1, num2, result = 0, m, n;
     cout<<"Enter two numbers: ";
     cin>>num1>>num2;
-    m = num1;
-    n =  num2;
-    while(n > 0)
-    {
-        if(n % 2 != 0)
-        {
-            result += m;
-        }
-//implementation of logic
-        m << 2;
-        n >> 2;
-    }
-    printf("%d * %d = %d", num1, num2, result);
+    result= fractionalknapsack(m, n);
+    cout<<num1<<" * "<<num2<<" = "<<result;
     return 0;
 }
 
@@ -37,3 +42,4 @@ Sample Input Output
 Enter two numbers: 12 13
 12 * 13 = 156
 */
+

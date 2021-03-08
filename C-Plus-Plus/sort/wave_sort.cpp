ec5bbfda359f7ab1c0b0f27c613419a6a7450a6f
E S Gayatri <gayatri.es20@gmail.com>
2021-03-08 23:03:19 +0530

    wave sort modified

--- C-Plus-Plus/sort/wave_sort.cpp
+++ C-Plus-Plus/sort/wave_sort.cpp
@@ -11,50 +11,51 @@ The above figure is a visual representation of the given arr and you can see we 
 3>1 and 1<2
 And it follows the condition of wave array */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> wave_sort(vector<int> &v,int n) {
-    for(int i=0;i<n;i+=2) {
+vector<int> wave_sort(vector<int> &v, int n)
+{
+	for (int i = 0; i < n; i += 2)
+	{
+		/*If the current element is lesser in value than the previous and is not the first.
+		then the values are swapped */
+		if (v[i] < v[i - 1] and i != 0)
+		{
+			swap(v[i], v[i - 1]);
+		}
 
-    	/* If the current element is lesser in value than the previous and is not the first.
-    	then the values are swapped */
-        if(v[i]<v[i-1] and i!=0) { 
-            swap(v[i],v[i-1]); 
-        }
+		/*If the current element is lesser in value than the next and is not the last.
+		then the values are swapped */
+		if (v[i] < v[i + 1] and i != n - 1)
+		{
+			swap(v[i], v[i + 1]);
+		}
+	}
 
-        /* If the current element is lesser in value than the next and is not the last.
-        then the values are swapped */
-        if(v[i]<v[i+1] and i!=n-1) { 
-            swap(v[i],v[i+1]); 
-        }
-    }
-
-    return v;
+	return v;
 }
 
-int main() {
-    
-    int n;
-    vector<int> v;
-    cin>>n;
+int main()
+{
+	int n;
+	vector<int> v;
+	cin >> n;
 
-    for(int i=0;i<n;i++) {
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	wave_sort(v, n);
+	
+	//Printing the final vector
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i] << " ";
+	}
 
-        	int temp;
-        	cin>>temp;
-        	v.push_back(temp);
-    }
-
-    wave_sort(v,n); 
-
-    //Printing the final vector
-
-    for(int i=0;i<n;i++) {
-    	
-        	cout<<v[i]<<" ";
-    }
-
-    return 0;
+	return 0;
 }
 
 /* Example 1:

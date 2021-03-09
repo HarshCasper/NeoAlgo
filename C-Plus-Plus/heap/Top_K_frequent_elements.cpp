a3301051f8f2c69c6d9414a6d75b02aee07ef1a9
tanyarajhans <61904667+tanyarajhans@users.noreply.github.com>
2021-03-09 10:36:56 +0530

    Made changes

--- C-Plus-Plus/heap/Top_K_frequent_elements.cpp
+++ C-Plus-Plus/heap/Top_K_frequent_elements.cpp
@@ -6,54 +6,54 @@ Given a non-empty array of integers, return the k most frequent elements.
 
 Steps:
 1.We will build a map and store frequency of every element in the array.
-2.Now we will iterate over the map and push all the elements along with their frequencies in a maxHeap as a pair. 
-3.Since when pushed as a pair, elements are sorted in descending order(due to maxHeap) based on the first element of the 
-pair, therefore, we will make the pair as {frequency of the element, element}.
-4.After we have pushed all the elements into the priority queue, we will run a while loop till k is greater than 0 and priority queue is not empty and pop the elements and print them.
+2.We will push all the elements of map along with their frequencies in a maxHeap as a pair. 
+3.Now, elements are sorted in descending order based on the first element of the pair.
+4.Therefore, we will make the pair as {frequency of the element, element}.
+5.We will run a loop till k is greater than 0 and maxHeap is not empty, pop the elements and print them.
 
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-
-Time Complexity: O(nlogn)
-Space Complexity: O(n)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
 void topKFrequent(vector<int>& nums, int k) {
-    map<int,int>m;
-    priority_queue<pair<int,int>> pq;
+ map<int,int>m;
+ priority_queue<pair<int,int>> pq;
         
-    for(int i: nums){
-        m[i]++;
-    }
+ for(int i: nums){
+ m[i]++;
+ }
         
-    for(auto i : m){
-        pq.push({i.second, i.first});
-    }
+ for(auto i : m){
+ pq.push({i.second, i.first});
+ }
 
-    while(k-- && !pq.empty()){
-        cout<<pq.top().second<<" ";
-        pq.pop();   
-    }
+ while(k-- && !pq.empty()){
+ cout<<pq.top().second<<" ";
+ pq.pop();   
+ }
 }
 
- // Driver code
+// Driver code
 int main() {
 
-	// Take input
-	int n; 
-    cin >> n;
-	vector<int> nums(n);
-	for (int i = 0; i < n; i++) {
-		cin >> nums[i];
-	}
-    int k;
-    cin >> k;
-    topKFrequent(nums, k);
+ // Take input
+ int n; 
+ cin>>n;
+ vector<int> nums(n);
+ for(int i = 0;i<n; i++) {
+ cin>>nums[i];
+ }
+ int k;
+ cin>>k;
+ topKFrequent(nums, k);
 }
 
 /*
+Time Complexity: O(nlogn)
+Space Complexity: O(n)
+
 Input: 
 n = 8
 nums = [1,1,7,4,2,2,2,3]

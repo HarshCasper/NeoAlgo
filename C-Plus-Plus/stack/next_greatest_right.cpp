86db8051916827f5185fdb5019d9ca6f461b3eb1
SAY-droid427 <sayanimallick0218@gmail.com>
2021-03-09 15:05:57 +0530

    Changes as requested

--- C-Plus-Plus/stack/next_greatest_right.cpp
+++ C-Plus-Plus/stack/next_greatest_right.cpp
@@ -1,15 +1,15 @@
 /*Next greatest element to the right of each element in the array
   ALGORITHM
- -->Using a stack to find out the next greatest element to the right
- -->Start the scanning of the array from the right 
- -->Declare an array and a stack
- -->If the stack is empty, then no element to the right is greater than the present element, for example, for the last element
- -->If the stack is not empty but the element present at the top of the stack is not greater than the current element then, pop the stack till either the stack is not empty or the top element of stack is greater than the current element
- -->If the stack is not empty and the element at the top of the stack is greater than the current element then push the topmost element to the stack
+ *Using a stack to find out the next greatest element to the right
+ *Start the scanning of the array from the right 
+ *Declare an array and a stack
+ *If the stack is empty, then no element to the right is greater than the present element
+ *If the stack is not empty but the element present at the top of the stack is not greater than the current element then-
+ * pop the stack till  the stack is not empty or the top element of stack is greater than the current element
+ *If the stack is not empty and the element at the top of the stack is greater than the current element then push the topmost element to the stack
 */
 #include <bits/stdc++.h>
 using namespace std;
-
 void nextLargest(int a[], int n)
 {
     vector<int> v;
@@ -40,7 +40,6 @@ void nextLargest(int a[], int n)
     for (int i = 0; i < n; i++)
         cout << v[i] << " ";
 }
-
 int main()
 {
     int n;
@@ -50,13 +49,11 @@ int main()
         cin >> a[i];
     nextLargest(a, n);
 }
-
 /*
 INPUT:
 n=4
 a[n]=1 3 2 4
 OUTPUT:
 3 4 4 -1
+Code Complexity: O(n^2)
 */
-
-

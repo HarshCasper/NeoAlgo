503797c165a41920e74897b232c3a6fdcb9cb649
Aditya Gupta <adityagupta765469@gmail.com>
2021-03-10 21:59:33 +0530

    Added CombSort

--- Kotlin/sort/CombSort/CombSort.kt
+++ Kotlin/sort/CombSort/CombSort.kt
@@ -1,1 +1,29 @@
 
+fun <T : Comparable<T>> combSort(arr: Array<T>) {
+    var arr_size = arr.size
+    if (arr_size <= 1) return  //already sorted
+    var swaps = false
+    while (arr_size > 1 || swaps) {
+        arr_size = (arr_size/1.247331).toInt()
+        if (arr_size < 1) arr_size = 1
+        var i = 0
+        swaps = false
+        while (i + arr_size < arr.size) {
+            if (arr[i] > arr[i + arr_size]) {
+                val t = arr[i]
+                arr[i] = arr[i + arr_size]
+                arr[i + arr_size] = t
+                swaps = true
+            }
+            i++
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val unsorted = arrayOf(28, 44, 46, 24, 19, 2, 17, 11, 4)
+    println("Unsorted array: ${unsorted.contentToString()}")
+    combSort(unsorted)
+    println("Sorted array: ${unsorted.contentToString()}")
+
+}

7eae6daa6f0bbb9e3cb94bfc18510e4a600b252c
bearinawolfpack <bearinawolfpack@gmail.com>
2021-03-10 11:28:54 -0500

    Adjust current test case and add additional test cases Change line 40 Input 321 to Input: 371 Change line 41 Output:- to Output: Add examples from Line 44 to Line 62

--- JavaScript/Algorithms/Armstrong.js
+++ JavaScript/Algorithms/Armstrong.js
@@ -37,6 +37,27 @@ process.stdin.on("end", () => {
 
 });
 // Example
-// Input 321
-// Output:- 371 is an Armstrong number.
+// Input: 371
+// Output: 371 is an Armstrong number.
 // Explanation 3*3*3+7*7*7+1*1*1 = 371
+
+// Example
+// Input: 321
+// Output: 321 is not an Armstrong number.
+// Explanation 3*3*3+2*2*2+1*1*1 = 44
+
+// Example
+// Input: 4321
+// Output: 4321 is not an Armstrong number.
+// Explanation 4*4*4*4+3*3*3*3+2*2*2*2+1*1*1*1 = 354
+
+// Example
+// Input: 1634
+// Output: 1634 is an Armstrong number.
+// Explanation 1*1*1*1+6*6*6*6+3*3*3*3+4*4*4*4 = 1634
+
+// Example
+// Input: 0
+// Output: 0 is an Armstrong number.
+// Explanation 0 = 0
+

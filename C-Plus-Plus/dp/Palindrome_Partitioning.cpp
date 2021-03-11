fb87cebc7ea639fe58ac431cd0cc2d1ce44a0e88
YashPaste <yashshinde2110@gmail.com>
2021-03-11 22:48:23 +0530

    Changes made

--- C-Plus-Plus/dp/Palindrome_Partitioning.cpp
+++ C-Plus-Plus/dp/Palindrome_Partitioning.cpp
@@ -1,77 +1,14 @@
 /*
 Given a string s, determine the minimum number of cuts required to make all substrings into palindrome.
-
-Example : 
-Given 
-s = "aabbc",
-Return 1, since the palindrome partitioning "aabb"|"c" can be made with just one cut. 
 */
 #include <bits/stdc++.h> 
 using namespace std; 
 
-int dp[502][502];
+bool dp[1001][1001];
 
-/* 
-Brute force: Recursion + Memoization
-
-Recursively partition a substring into 2 substrings at k places and compute the temporary answer for every cut.
-Memoize the code using a 2-dimenesional array where we look up before calling the function to avoid overlapping subproblems. 
-
-TC=> O(n^3)
-*/
-bool palindromeCheck(string String, int i, int j)
-{
-    while(i < j)
-    {
-      if(String[i] != String[j])
-        return false;  
-      i++;
-      j--;
-    }
-    return true;
-}
-
-//Recursive function to check calculate minimum cuts
-int minCutToPalindrome_Recc(string String, int i, int j)
-{
-    // If the sub problem is already stored in our table then return the value
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    // if entire string is palindrome, return true. 
-    if( i >= j || palindromeCheck(String, i, j) )
-      return 0;
-    int ans = INT_MAX, count;
-    for(int k = i; k < j; k++)
-    {
-        int left,right;
-        // Check if the sub problem has already been computed before calling
-        if(dp[i][k]!=-1){
-            left= dp[i][k];
-        } else{
-            left= minCutToPalindrome_Recc(String, i, k);
-            dp[i][k]=left;
-        }
-        if(dp[k+1][j]!=-1){
-            right= dp[k+1][j];
-        }
-        else{
-            right = minCutToPalindrome_Recc(String, k + 1, j);
-            dp[k+1][j]=right;
-        }
-      count = left+right+1;
-  
-      ans = min(ans, count);
-    }
-    // store the answer before returning
-    return dp[i][j]=ans;
-}
-/*
-Optimal soltion: Computing dp (2D matrix) where [a,b] stores whether the sub-string 'a' to 'b' is a palindrome. 
-
-*/
 int minCutToPalindrome_Opt(string s) {
         int n = s.length();
+        // Computing dp (2D matrix) where [a,b] stores whether the sub-string 'a' to 'b' is a palindrome.
         bool dp[s.length()][s.length()];
         for (int mid = 0; mid < s.length(); mid++) {
             int si = 0, ei = mid;
@@ -119,6 +56,19 @@ int main()
     string s;
     cin>>s;
     cout<<minCutToPalindrome_Opt(s)<<endl;
-    cout<<minCutToPalindrome_Recc(s,0,s.size()-1)<<endl;
     return 0; 
 } 
+
+/*
+Time complexity: O(n^2) 
+
+Sample Input 1: 
+RADARROTATOR
+Output: 1
+Explanation: We can make 1 cut and make the resulting substrings palindromes. (RADAR | ROTATOR)
+
+Sample Input 2:
+REFERENCE
+Output: 4
+Explanation: We need to cut the string at 4 places i.e, REFER | E | N | C | E
+*/

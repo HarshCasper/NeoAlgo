1a54fcfb3c8047cf811ff0ac44d78725dfac691a
ireneelizabethsabu <irene@cet.ac.in>
2021-03-11 21:17:43 +0530

    Adding cycleSort.js

---
+++ JavaScript/sort/cycleSort.js
@@ -0,0 +1,63 @@
+/*
+CYCLE SORT:
+    Selection sort is a inplace sorting algorithm which optimises the number of memory writes.
+    Time Complexity - O(n^2)
+    Space Complexity - O(1)
+*/
+
+function cycleSort(arr) {
+    const n = arr.length;
+    //start is the index of the current element.
+    for (let start = 0; start < n - 1; start++) {
+        
+        let currentKey = arr[start];
+        let pos = start;
+        for (let i = start + 1; i < n; i++) {
+            if (arr[i] < currentKey)
+                pos += 1;
+        }
+        // if pos = start => element is at original position. If so skip.
+        if (pos == start) 
+            continue;
+
+        //check for duplicates in the array. 
+        while (currentKey == arr[pos]) { 
+            pos++ ;
+        }
+        //swapping current element and element at its original position.
+        const temp = arr[pos];
+        arr[pos] = currentKey;
+        currentKey = temp;
+
+        // rotate the rest of the cycle
+        while (pos != start) {
+            pos = start;
+            //finding original position
+            for (let i = start + 1; i < n; i++) {
+                if (arr[i] < currentKey) 
+                    pos += 1;
+            }
+
+            //checking for duplicates
+            while (arr[pos] == currentKey) {
+                pos += 1;
+            }
+            const temp = arr[pos];
+            arr[pos] = currentKey;
+            currentKey = temp;
+        }
+    }
+}
+
+
+//CASE 1:
+let input = [1, 8, 7, 61, 5, 4, 11];
+cycleSort(input);
+console.log(input);
+//EXPECTED: [1,4,5,7,8,11,61]
+
+//CASE 2:
+input = [1,8,3,9,10,10,2,4];
+cycleSort(input)
+console.log(input);
+//EXPECTED: [1,2,3,4,8,9,10,10]

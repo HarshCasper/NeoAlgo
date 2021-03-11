18d8258209c8c0e462ddc68dd75b501c565df3f0
Aditya Gupta <adityagupta765469@gmail.com>
2021-03-11 13:01:00 +0530

    updated Armstrong_Number.py

--- Python/cp/Armstrong_Number.py
+++ Python/cp/Armstrong_Number.py
@@ -8,13 +8,13 @@ sum = 0
 
 # find the sum of the cube of each digit
 temp = number
-while temp>0:
+while temp > 0:
    dig = temp % 10
    sum += dig ** 3
    temp //= 10
 
 # display the result
 if number != sum:
-   print(number,"is not an Armstrong Number.")
+    print(number, "is not an Armstrong Number.")
 else:
-   print(number,"is an Armstrong Number.")
+    print(number, "is an Armstrong Number.")

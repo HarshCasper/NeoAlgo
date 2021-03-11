7bde33f06e81027ccdcacf7eda7c31b31b7478fc
Omar Hegazy <omar_hegazy@outlook.sa>
2021-03-11 20:22:39 +0200

    fix variables names

--- C-Sharp/sort/MergeSort.cs
+++ C-Sharp/sort/MergeSort.cs
@@ -8,41 +8,41 @@ namespace MergeSortAlgorithm
     class Merge_Sort
     {
         //The merge step in the sorting problem to sort the elements
-        private static void Merge(int[] arr, int[] C, int l, int mid, int r)
+        private static void Merge(int[] arr, int[] temp, int leftStart, int mid, int rightEnd)
         {
-            int i = l, j = mid + 1, k = l;
-            //Compare Elments from arr portion andt sort them in temproray array C
-            while (i < mid + 1 && j <= r)
+            int left = leftStart, right = mid + 1, index = left;
+            //Compare Elments from arr portion and sort them in temproray array temp
+            while (left < mid + 1 && right <= rightEnd)
             {
-                if (arr[j] < arr[i])
+                if (arr[right] < arr[left])
                 {
-                    C[k] = arr[j++];
+                    temp[index] = arr[right++];
                 }
                 else
                 {
-                    C[k] = arr[i++];
+                    temp[index] = arr[left++];
                 }
-                k++;
+                index++;
             }
-            // if the arr porion sorted but i didn't reach the last element place the rest in C 
-            for (; i <= mid; i++)
-                C[k++] = arr[i];
-            // if the arr porion sorted but j didn't reach the last element place the rest in C 
-            for (; j <= r; j++)
-                C[k++] = arr[j];
-            //Place the sorted elements from C into arr
-            for (i = l; i <= r; i++)
-                arr[i] = C[i];
+            // if the arr porion sorted but i didn't reach the last element place the rest in temp 
+            for (; left <= mid; left++)
+                temp[index++] = arr[left];
+            // if the arr porion sorted but j didn't reach the last element place the rest in temp 
+            for (; right <= rightEnd; right++)
+                temp[index++] = arr[right];
+            //Place the sorted elements from temp into arr
+            for (left = leftStart; left <= rightEnd; left++)
+                arr[left] = temp[left];
         }
         // recursive part in merge sort to break the array into smaller portions
-        private static void MergeSort(int[] arr, int[] C, int l, int r)
+        private static void MergeSort(int[] arr, int[] temp, int leftStart, int rightEnd)
         {
-            if (l < r)
+            if (leftStart < rightEnd)
             {
-                int mid = l + (r - l) / 2;
-                MergeSort(arr, C, l, mid);
-                MergeSort(arr, C, mid + 1, r);
-                merge(arr, C, l, mid, r);
+                int mid = leftStart + (rightEnd - leftStart) / 2;
+                MergeSort(arr, temp, leftStart, mid);
+                MergeSort(arr, temp, mid + 1, rightEnd);
+                Merge(arr, temp, leftStart, mid, rightEnd);
             }
 
         }

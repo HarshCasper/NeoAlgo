6c033b3353970bb0d2da90a46b8ae9179f7bf0bd
Swetha Ramachandran <67588774+Swetha126@users.noreply.github.com>
2021-03-11 10:51:40 +0530

    Update Palindrome-doublylinkedlist.py

--- Python/ds/Palindrome-doublylinkedlist.py
+++ Python/ds/Palindrome-doublylinkedlist.py
@@ -77,14 +77,17 @@ if __name__ == "__main__":
 
 '''
 
-Sample Input/Output :-
-Test case 1:
+Sample Input/Output :
+
+- Test case 1:
 Enter the string : clouds
 Not a Palindrome
-Test case 2:
+
+- Test case 2:
 Enter the string : madam
 Is a Palindrome
-Test case 3:
+
+- Test case 3:
 Enter the string : books
 Not a Palindrome
 

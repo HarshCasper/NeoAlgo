7c849670b247418a2c1d1ac0fb344129f2a54830
Shabi <kaushalshabi@gmail.com>
2021-03-11 10:17:26 +0530

    Added implementation of Merge Sort in Python

---
+++ Python/sort/Merge_Sort.py
@@ -0,0 +1,74 @@
+# Python program for implementation of Merge Sort.
+
+# Merge sort is one of the most prominent divide-and-conquer sorting algorithms in the modern era.
+# It can be used to sort the values in any traversable data structure such as a list.
+
+
+def mergeSort(arr):
+	if len(arr) > 1:
+
+		# Finding the mid of the array
+		mid = len(arr)//2
+
+		# Dividing the array elements
+		L = arr[:mid]
+
+		# into 2 halves
+		R = arr[mid:]
+
+		# Sorting the first half
+		mergeSort(L)
+
+		# Sorting the second half
+		mergeSort(R)
+
+		i = j = k = 0
+
+		# Copy data to temp arrays L[] and R[]
+		while i < len(L) and j < len(R):
+			if L[i] < R[j]:
+				arr[k] = L[i]
+				i += 1
+			else:
+				arr[k] = R[j]
+				j += 1
+			k += 1
+
+		# Checking if any element was left
+		while i < len(L):
+			arr[k] = L[i]
+			i += 1
+			k += 1
+
+		while j < len(R):
+			arr[k] = R[j]
+			j += 1
+			k += 1
+
+# Code to print the list
+
+
+def printList(arr):
+	for i in range(len(arr)):
+		print(arr[i], end=" ")
+	print()
+
+
+# Driver Code
+if __name__ == '__main__':
+	arr = [12, 11, 13, 5, 6, 7]
+	print("Given array is", end="\n")
+	printList(arr)
+	mergeSort(arr)
+	print("Sorted array is: ", end="\n")
+	printList(arr)
+
+
+# Time-Complexity:
+# The algorithm works in O(n.logn)
+# This is because the list is being split in log(n) calls and the merging process takes linear time in each call
+
+
+# Author:
+# Shabi Kaushal (msbuddhu)
+# Date: 10.03.2021

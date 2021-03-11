35372b2b7d24cd0a2201936997e6d84f5407cbf6
kushal1311001 <121910302059@gitam.in>
2021-03-11 12:15:33 +0530

    added Number_of_K-Length_Sublists_with_Average_at_Least _Target.py file

--- Python/cp/Number_of_K-Length_Sublists_with_Average_at_Least _Target.py
+++ Python/cp/Number_of_K-Length_Sublists_with_Average_at_Least _Target.py
@@ -1,23 +1,25 @@
-'''
-Number of K-Length Sublists with Average at Least Target
+# Number of K-Length Sublists with Average at Least Target
 
-Given a list of integers nums, and integers k and target
-return the number of sublists whose length is k and its average value ≥ target.
+# Given a list of integers nums, and integers k and target
+# return the number of sublists whose length is k and its average value ≥ target.
 
-Constraints:-
+# Constraints:-
 
-n ≤ 100,000 where n is the length of nums
+# n ≤ 100,000 where n is the length of nums
 
-Approach : First i will take the sum of [0:k] elements and check it's avg.
-If avg greater than target ans count is increased
-then in the loop starting from kth index adding the new element 
-and removeing the initial added elements simultaneously
+# Approach : First i will take the sum of [0:k] elements and check it's avg.
+# If avg greater than target ans count is increased
+# then in the loop starting from kth index adding the new element 
+# and removeing the initial added elements simultaneously
 
 
-Time Complexity:  O(N)
-Space Complexity: O(N)
-'''
+# Time Complexity:  O(N)
+# Space Complexity: O(N)
 
+
+nums= [int(num) for num in input("Enter the list items : ").split()] #input list
+k=int(input()) #input the k-length of sublist
+target=int(input()) #target value( min avg)
 
 def solution(nums, k, target):
     p=sum(nums[0:k]) #taking first [0:k] sum
@@ -34,38 +36,23 @@ def solution(nums, k, target):
             c+=1
 
     return c #final result is returned
-
-
-nums= [int(num) for num in input("Enter the list items : ").split()] #input list
-
-k=int(input()) #input the k(length of sublist)
-
-target=int(input()) #target value( min avg)
-
 print(solution(nums, k, target)) #calling the function
 
+# ST-1:- Input:-
 
-'''
-ST-1:-
+# nums = [0, 9, 4, 5, 6]
+# k = 3
+# target = 5
 
-Input:-
+# output:-
+# 2
 
-nums = [0, 9, 4, 5, 6]
-k = 3
-target = 5
+# ST-2:- Input:-
 
-output:-
-2
+# nums = [84, 45, 2, 1, 894, 156, 68, 48, 7, 7, 54, 45, 3, 8, 98, 4, 3, 35, 87, 68, 4, 5, 5, 4, 8, 68, 7, 3554, 8]
+# k = 4
+# target = 13
 
-ST-2:-
+# output:-
+# 24
 
-Input:-
-
-nums = [84, 45, 2, 1, 894, 156, 68, 48, 7, 7, 54, 45, 3, 8, 98, 4, 3, 35, 87, 68, 4, 5, 5, 4, 8, 68, 7, 3554, 8]
-k = 4
-target = 13
-
-output:-
-24
-
-'''

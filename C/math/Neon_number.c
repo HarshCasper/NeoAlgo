c55da3a96df1ccd403ded63bbc98d15a9929a9cc
Madhumita Moi <madhumoi0266@gmail.com>
2021-03-11 19:43:11 +0530

    git commit -m "Changes added"

--- C/math/Neon_number.c
+++ C/math/Neon_number.c
@@ -1,39 +1,37 @@
-// C Program to check whether a number is Neon or not. A neon number is a number where the sum of digits of square of the number is equal to the number.
+// C Program to check whether a number is Neon or not. A neon number is a number where the sum of digits of square of the number is equal to the number. 
 
 #include <stdio.h>
 #include <math.h>
-int neon(int n)
+void neon(int n)
 {
-    int i, sum = 0, t = 1;
-    t = pow(n, 2);
-    while (t != 0)
-    {
-        sum = sum + (t % 10);
-        t = t / 10;
-    }
-    if (sum == n)
-    {
-        printf("Neon number.\n");
-    }
-    else
-    {
-        printf("Not a neon number\n");
-    }
-    return 0;
+int i,sum=0,t=1;
+t=pow(n,2);
+while(t!=0)
+{
+sum=sum+(t%10);
+t=t/10;
+}
+if(sum==n)
+{
+printf("Neon number.\n");
+}
+else
+{
+printf("Not a neon number\n");
+}
 }
 int main()
 {
-    int n;
-    printf("Enter a number to check wheather it is neon or not- ");
-    scanf("%d", &n);
-    neon(n);
-    return 0;
+	int n;
+printf("Enter a number ");
+scanf("%d", &n);
+neon(n);
 }
 /* Sample Input/output
 Example 1-
-Enter a number to check wheather it is neon or not- 9
+Enter a number 9
 Neon number.
 Example 2-
-Enter a number to check wheather it is neon or not- 144
+Enter a number 144
 Not a neon number
 */

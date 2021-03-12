3f08312a969eda1254244ce4d721945b4777d9f0
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2021-03-12 12:29:32 +0530

    Update tkt_auction.cpp
    resolved changes requested :))

--- C-Plus-Plus/cp/tkt_auction.cpp
+++ C-Plus-Plus/cp/tkt_auction.cpp
@@ -1,5 +1,6 @@
 /*
-@Author: Suvraneel Bhuin    (@Suvraneel)
+Author: @Suvraneel
+	Suvraneel Bhuin
 
 Problem Statement:
 * There are n concert tickets available, each with a certain price. Then, m customers arrive, one after another.
@@ -18,21 +19,12 @@ If a customer cannot get any ticket, print −1.
 Constraints:
 *   1   ≤   n,m    ≤2⋅105
 *   1   ≤  hi,ti   ≤109
-
-Example:
-Input:
-5 3
-5 3 7 8 5
-4 8 3
-Output:
-3
-8
--1
 */
 
 
 #include <iostream>
-using namespace std;                            //For cin and cout
+//For cin and cout
+using namespace std;
 
 //Swap function --> simply exchanges the values of inputs
 void swap(int *xp, int *yp) 
@@ -47,7 +39,6 @@ int partition(int array[], int lo, int hi)
     int i = (lo - 1);
     // Select the pivot element
     int pivot = array[hi];
-
     //Traverses the array - whenever an element < pivot occurs, swap it with (i+1)th element 
     //This leads to elements < pivot on the left & elements > pivot on the right wrt the pivot
     for (int j = lo; j < hi; j++) {
@@ -56,7 +47,6 @@ int partition(int array[], int lo, int hi)
             swap(&array[i], &array[j]);
         }
     }
-
     //Place the pivot element at i+1 (between smaller and larger elements)
     swap(&array[i + 1], &array[hi]);
     //Returns the pivot position
@@ -64,58 +54,77 @@ int partition(int array[], int lo, int hi)
 }
 
 void quickSort(int array[], int lo, int hi) {
-
     //If starting index crosses end index, then there're no elements to sort further, thus return
     if( lo >= hi ){
         return;
     }
-    
     // Select pivot position and put all the elements smaller 
     // than pivot on left and greater than pivot on right
     int pivot = partition(array, lo, hi);
-
     // Sort the elements on the left of pivot
     quickSort(array, lo, pivot - 1);
-
     // Sort the elements on the right of pivot
     quickSort(array, pivot + 1, hi);
 }
   
 //Main driver program
 int main() {
-    int n, m, i, j;                            // n= no. of tickets; m = no. of customers
-    int sold = -1;                             //implies initially no customer had any ticket
-	cin >> n >> m;
-    int h[n], t[n];
-    for (j = 0; j < n; j++)
-        {
-            cin >> h[j];                       //accepts price of each ticket
-        }
-
-    for (i = 0; i < m; i++)
-        {
-            cin >> t[i];                       //accepts maximum bid by each customer
-        }
-    quickSort(h, 0, n-1);                      //calls sorting algorithm on ticket price array
-
-    for ( i = 0; i < m; i++)                   //loop for examining each ticket
+    //tkt = no. of tickets; cust = no. of customers
+    int tkt, cust, i, j;
+    //implies initially no customer had any ticket
+    int sold = -1;
+    cin >> tkt >> cust;
+    int price[tkt], bid[cust];
+    for (j = 0; j < tkt; j++)
     {
-        for ( j = n-1; j >= 0; j--)            //loop for considering each customer
+        //accepts price of each ticket
+        cin >> price[j];                       
+    }
+    for (i = 0; i < cust; i++)
+    {
+        //accepts maximum bid by each customer
+        cin >> bid[i];                       
+    }
+    //calls sorting algorithm on ticket price array
+    quickSort(price, 0, tkt-1);
+    //loop for considering each customer
+    for ( i = 0; i < cust; i++)
+    {
+	//loop for examining each ticket starting from expensive so as to confirm maximum bid
+        for ( j = tkt-1; j >= 0; j--)            
         {
-            if (t[i]>=h[j])                    //for each ticket sold
-            {   
-                sold = h[j];                   //tracks successful bids
-                h[j] = INT_MAX;                //set the price as max integer possible
-                                               //so that noone else can buy the same ticket 
+	    //for each ticket sold
+            if (bid[i]>=price[j])
+            {
+		//track successful bids
+                sold = price[j];
+                //set the price as max integer possible so that noone else can buy the same ticket
+                price[j] = INT_MAX;                                     
                 break;
             }
-        
         }
-
-        cout << sold << endl;                  //outputs successful bids for each customer
-                                               //(-1) indicates the customer was unable to acquire a ticket
-        sold = -1;                             //resets the parameter for the next ticket
+        //output successful bids for each customer
+        cout << sold << endl;                  
+        //(-1) indicates the customer was unable to acquire a ticket
+        //reset the 'sold' parameter for the next ticket
+        sold = -1;
     }
     
 	return 0;   
 }
+
+/*
+Time complexity:    Sorting = O(n)          where n = no. of tickets
+                    Auction = O(mn)         where m = no. of customer
+Space Complexity:   O(n)
+
+Sample Input:
+5 3
+5 3 7 8 5
+4 8 3
+
+Standard Output:
+3
+8
+-1
+*/

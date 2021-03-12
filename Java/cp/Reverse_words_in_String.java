814c2be53dc0b60ae37573366f75bf326deeb804
Ruchi Sahu <sahu2101ruchi@gmail.com>
2021-03-12 09:30:26 -0500

    Formated the code as requested

--- Java/cp/Reverse_words_in_String.java
+++ Java/cp/Reverse_words_in_String.java
@@ -3,23 +3,29 @@
 * We can reverse each word of a string.
 * In other words, Reverse text without reversing individual word in a String.
 */
-    import java.util.Scanner;
-    public class Reverse_String {
-  	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please Enter the number of words your String have....");
-		int n = scan.nextInt();
-		String in[] = new String[n];
-		System.out.println("Please enter your String here...");
-		for (int i = 0; i <= n-1; i++) {
-			in[i] = scan.next();
-		}
-		System.out.println();
-		for (int i = n - 1; i >= 0; i--) {
-			System.out.print(in[i] + " ");
-		}
-   	}
-	  }
+import java.util.Scanner;
+public class Reverse_String {
+/*
+This is an constructor called from main function
+*/
+public Reverse_String(String in[],int n){
+System.out.println();
+for (int i = n - 1; i >= 0; i--) {
+System.out.print(in[i] + " ");
+}
+}
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+System.out.println("Please Enter the number of words your String have....");
+int n = scan.nextInt();
+String in[] = new String[n];
+System.out.println("Please enter your String here...");
+for (int i = 0; i <= n-1; i++) {
+in[i] = scan.next();
+}
+Reverse_String r1=new Reverse_String(in,n);
+}
+}
 /*
 Time complexity is O(n-1)
 Space complexity is O(1)

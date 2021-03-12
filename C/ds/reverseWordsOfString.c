26ed52aeba21e8db92ff8f8091aed8a023399373
taruni-always <nayakantitaruni@gmail.com>
2021-03-12 11:32:53 +0530

    made the requested changes

--- C/ds/reverseWordsOfString.c
+++ C/ds/reverseWordsOfString.c
@@ -6,15 +6,15 @@
 typedef struct stack {
     int capacity, top;
     char *arr;
-}stack;
+}stack;     /* creating a structure to implement stack */
 
 int isEmpty(stack *s) {
     return s -> top == -1;
-}
+}   /* to check if the stack is empty */
 
 int isFull(stack *s) {
     return s -> top == s -> capacity - 1;
-}
+}    /* to check if the stack is full */
 
 stack * createStack(int size) {
     struct stack *s = (struct stack *) malloc(sizeof(struct stack));
@@ -37,7 +37,7 @@ void reverseWords(char sentence[100], int length) {
     for ( int  i = 0; i < length; i ++){
         char letter = sentence[i];
         if ( (! isspace(letter)) && (letter != '\n') && (letter != '\0')) {
-            push(s, letter);
+            push(s, letter);    /* anything other than an alphabet or number indicates that a word has ended */
         }
         else {
             while ( !(isEmpty(s)) ) {
@@ -51,9 +51,17 @@ void reverseWords(char sentence[100], int length) {
 int main() {
     int size, i = 0;
     char sentence[100];
-    printf("Enter a word:\n");
+    printf("Enter a sentence:\n");
     fgets(sentence, 100, stdin);
     printf("The sentence after reversing individual words is:\n");
-    reverseWords(sentence, strlen(sentence));
+    reverseWords(sentence, strlen(sentence));    /* calling the reverseWords function */
     return 0;
 }
+
+/* 
+Sample Input1: Get me food
+Sample Output1: teG em doof
+
+Sample Input2: happy birthday
+Sample Output2: yppah yadhtrib
+*/
--- Python/ds/reverseWordsOfString.py
+++ Python/ds/reverseWordsOfString.py
@@ -4,12 +4,13 @@ Reversing individual words of a string using stack data structure
 
 
 def reverseWords(string: str):
+    ''' Takes a sentence of words (a string) and prints the individual words in a reverse order '''
     stack = []
     for letter in string:
         if letter.isalnum():
             stack.append(letter)     # equivalent to stack.push()
         else:
-            while (stack != []):
+            while stack != []:
                 print(stack.pop(), end="")
             print(letter, end="")
     while stack:
@@ -20,3 +21,12 @@ def reverseWords(string: str):
 sentence = input("Enter a sentence:\n")
 print("The sentence after reversing individual words is:")
 reverseWords(sentence)
+
+
+'''
+Sample Input1: Get me food
+Sample Output1: teG em doof
+
+Sample Input2: happy birthday
+Sample Output2: yppah yadhtrib
+'''

1217399579e1eb4ff52f6762b0467afa20329b95
Shubham Agarwal <to.shubham.ag@gmail.com>
2021-03-12 10:32:55 +0530

    improved styling

--- Go/dp/longest_common_subsequence.go
+++ Go/dp/longest_common_subsequence.go
@@ -1,9 +1,9 @@
-/* Given two sequences, find the length of longest subsequence present 
-	in both of them. A subsequence is a sequence that appears in the 
-	same relative order, but not necessarily contiguous. 
-	For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc 
-	are subsequences of “abcdefg”. So, the answer will be the longest
-	length i.e. 5
+/* Given two sequences, find the length of longest subsequence present
+in both of them. A subsequence is a sequence that appears in the
+same relative order, but not necessarily contiguous.
+For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc
+are subsequences of “abcdefg”. So, the answer will be the longest
+length i.e. 5
 */
 
 package main
@@ -11,39 +11,38 @@ package main
 import "fmt"
 
 //function to find the maximum
-func max(a, b int) int{
+func max(a, b int) int {
 	if a > b {
 		return a
 	} else {
 		return b
 	}
-}  
+}
 
 //longest common subsequence function
 func lcs(X, Y string, m, n int) int {
 
-	//creating 2d array 
+	//creating 2d array
 	L := make([][]int, m+1)
 	for itr := 0; itr <= m; itr++ {
 		L[itr] = make([]int, n+1)
 	}
 
 	//bottom up fashion
-	for i := 0; i <= m; i++ {  
+	for i := 0; i <= m; i++ {
 		for j := 0; j <= n; j++ {
-			if i == 0 || j == 0 { 
+			if i == 0 || j == 0 {
 				L[i][j] = 0
-			} else if X[i - 1] == Y[j - 1] {
-				L[i][j] = L[i - 1][j - 1] + 1
+			} else if X[i-1] == Y[j-1] {
+				L[i][j] = L[i-1][j-1] + 1
 			} else {
-				L[i][j] = max(L[i - 1][j], L[i][j - 1]) 
+				L[i][j] = max(L[i-1][j], L[i][j-1])
 			}
 		}
-	}  
+	}
 
-    return L[m][n];  
-}  
-
+	return L[m][n]
+}
 
 func main() {
 	var str1 string

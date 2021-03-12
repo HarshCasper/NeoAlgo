49990dc3495776612415d50b0c3cd55da9463794
Saviour1001 <harshghodkar@gmail.com>
2021-03-12 11:51:32 +0530

    Updated with the required changes
    Made all the asked changes after getting reviewed

--- C-Plus-Plus/README.md
+++ C-Plus-Plus/README.md
@@ -30,7 +30,6 @@
 - [Josephus Problem](cp/Josephus.cpp)
 - [Kadane's Algorithm for max sum subArray](cp/Kadane_Alogorithm.cpp)
 - [Knuth–Morris–Pratt algorithm](cp/Knuth_Morris_Pratt.cpp)
-- [Long Factorials](cp/long_factorials.cpp)
 - [Longest Common Prefix](cp/longestCommonPrefix.cpp)
 - [Longest Subarray with Zero Sum](cp/ZeroSum.cpp)
 - [Maximum in Array](cp/Maximum_In_Array.cpp)
@@ -189,7 +188,7 @@ _add list here_
 - [Longest increasing subsequence](dp/longest_increasing_subsequence.cpp)
 - [Matrix Chain Multiplication](dp/Matrix_Chain_Multiplication.cpp)
 - [Maximum Subarray Sum (Constant Space)](<dp/Max_subarray_sum_constant_space(DP).cpp>)
-- [[Maximum Subarray Sum (Constant Space)](<dp/Max_subarray_sum_constant_space(DP).cpp>)]
+- [Maximum Subarray Sum (Constant Space)](<dp/Max_subarray_sum_constant_space(DP).cpp>)
 - [Maximum Sum Rectangle](dp/max_sum_rectangle.cpp)
 - [Maximum Square Matrix With All 1s](dp/maxSquareMatrixWithOnes.cpp)
 - [Minimum Sum Partition](dp/Minimum_Sum_Partition.cpp)
--- C-Plus-Plus/cp/long_factorials.cpp
+++ C-Plus-Plus/cp/long_factorials.cpp
@@ -5,35 +5,50 @@ We used to muiltiply numbers and would add the summation and get the final resul
 This program here uses a vector to store each particular digit in a single index of the vector. 
 The carry is forwarded as basic maths and in the end the complete result gets stored in the vector*/
 
+//The length of vector should be >= length of the resultant factorial
+
+// Time Complexity = O(N)
+
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
 
-int main() {
-    int num;
+void factorial(int val)
+{
     int carry = 0;       
-    cin >> num;                  //Input the number whose factorial is to be calculated 
-    vector <int> arr(10000, 0);  //The size of the vector should be more than the digits present 
-    arr[0] = 1;                  //in the factorial of the number.  
+    vector <int> arr(10000, 0);   
+    arr[0] = 1;                   
     int k = 0;                         
 
-    for(int i = 1; i <= num; i++) {
+    for(int i = 1; i <= val; i++) {
         for(int j = 0;j <= k; j++) {      
             arr[j] = arr[j] * i + carry;
             carry = arr[j] / 10;
             arr[j] = arr[j] % 10;
         }
-        while(carry) {        //The carries are forwarded in order to get the final result
+        while(carry) {        
             k++;
             arr[k] = carry % 10;
             carry /= 10;
         }   
     }
-    for(int i = k; i >= 0; i--) {  //Each digit of the final answer is stored in a single index 
-        cout << arr[i];            //of the vector    
+    for(int i = k; i >= 0; i--) {  
+        cout<<arr[i];
     }
     cout << "\n";
+}
+int main() {
+    int num;
+    cin>>num;
+    factorial(num);
     return 0;
 }
+
+/*Sample test cases 
+1. Input: 5
+   Output: 120
+2. Input: 50
+   Output: 30414093201713378043612608166064768844377641568960512000000000000 
+ */

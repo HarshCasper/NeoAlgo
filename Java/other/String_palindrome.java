360db465312c49f6c833508ff1eaa6fa079ee12e
krishantraj <65496822+krishantraj@users.noreply.github.com>
2021-03-12 18:09:20 +0530

    added a seperste function for codelogic and description

--- Java/other/String_palindrome.java
+++ Java/other/String_palindrome.java
@@ -1,28 +1,22 @@
 //PROGRAM TO CHECK  whether the string is a Palindrome or not IN JAVA.
 
-/*This is a program to check if the user entered string is palindrome or not . A palindrome is a string that is the same irrespective of wheather you read it in forward or backward direction. For example, "dad" is the same whether you read it in forward or reverse direction.*/
+/*This is a program to check if the user entered string is palindrome or not . 
+ * A palindrome is a string that is the same irrespective of wheather you read it in forward or backward direction.
+ *  For example, "dad" is the same whether you read it in forward or reverse direction.*/
 
 import java.util.Scanner;
 
 public class String_palindrome {
+	// function to check if the string is palindrome or not
+	static void checkpalindrome(String string, String reverse_string) {
 
-	static void checkpalindrome() {
-		// declaring an empty reverse_string variable and a string variable to be taken  as input
-	
-		String string, reverse_string = "";
-
-		Scanner sc = new Scanner(System.in);
-    // taking input of the string to be checked
-		System.out.println("Enter a string:");
-		string = sc.nextLine();
-
-    // assigning the length of the string to integger type length variable.
+		// assigning the length of the string to integger type length variable.
 		int length = string.length();
 
-// reversing the string and storing it in the variable reverse_string. 
+//reversing the string and storing it in the variable reverse_string. 
 		for (int i = length - 1; i >= 0; i--) {
 			reverse_string = reverse_string + string.charAt(i);
-		} 
+		}
 
 		if (string.equals(reverse_string)) {
 
@@ -35,15 +29,21 @@ public class String_palindrome {
 	}
 
 	public static void main(String[] args) {
+		String string, reverse_string;
 
-		checkpalindrome();
+		Scanner sc = new Scanner(System.in);
+
+		// taking input of the string to be checked
+		System.out.println("Enter a string:");
+		string = sc.nextLine();
+
+		checkpalindrome(string, "");
 
 	}
 }
 
 /*
- * OUTPUT:
- * 1)when entered a non palindromic string
+ * OUTPUT: 1)when entered a non palindromic string
  * 
  * Enter a string: NEOALGO NEOALGO is not a palindrome string
  * 

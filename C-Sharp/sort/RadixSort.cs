d82fb9fa4e5b184dd0c7787748e3947a60749464
Aditya Gupta <adityagupta765469@gmail.com>
2021-03-12 16:44:03 +0530

    updated RadixSort.cs

--- C-Sharp/sort/RadixSort.cs
+++ C-Sharp/sort/RadixSort.cs
@@ -1,6 +1,11 @@
 using System;
-  
-class RadixSort{
+
+/*  Radix sort is a sorting technique that sorts the elements by 
+    first grouping the individual digits of the same place value. 
+    Then, sort the elements according to their increasing/decreasing order.
+*/
+
+ class RadixSort{
   static void radixSort(int[] arr){
     int size = arr.Length;
     int max_element = arr[0];
@@ -18,9 +23,7 @@ class RadixSort{
  
   static void print_arr(int[] arr){ 
     int size = arr.Length; 
-    for (int i=0; i<size; i++){
-      Console.Write(arr[i] + " "); 
-      }
+   
     Console.Write("\n"); 
   } 
   
@@ -50,11 +53,49 @@ class RadixSort{
 
 
   public static void Main(string[] args){
-    int[] a = {65, 3,91, 80, 45, 106,41, 1};
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] a = new int[n];
+   
+   int i,j;
+    for(i=0; i<n;i++){
+        Console.Write("Element {0} : ", i);
+        a[i] = Convert.ToInt32(Console.ReadLine());
+    }
     Console.Write("Unsorted array: ");
-    print_arr(a);
+    for (j=0; j<n; j++){
+      Console.Write(a[j] + " "); 
+    } 
+      
+    Console.Write("\n");
+    
     radixSort(a);
+    
     Console.Write("Sorted array: ");
-    print_arr(a);  
+     for (j=0; j<n; j++){
+      Console.Write(a[j] + " "); 
+      } 
   }
 }
+
+/* Input
+    6                                                                                         
+    Element 0 : 87                                                                            
+    Element 1 : 12                                                                            
+    Element 2 : 4                                                                             
+    Element 3 : 76                                                                            
+    Element 4 : 1                                                                             
+    Element 5 : 23 
+*/
+
+/* Output
+    Unsorted array: 87 12 4 76 1 23 
+    Sorted array: 1 4 12 23 76 87 
+*/
+
+/* Time Complexity = O(d(n+k)) 
+        where k= largest element in the array
+              n= number of elements in the array
+              d = base of numbers(decimal base system =10)
+*/
+
+

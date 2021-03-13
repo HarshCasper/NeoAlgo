f9d21989c2a1f2b189ebc311c1df6a9ca62b8324
mansi-9 <64132543+mansi-9@users.noreply.github.com>
2021-03-13 23:19:46 +0530

    #2894 resolved gssoc 2021
    #2894 resolved (gssoc 2021)

--- C-Plus-Plus/cp/MinSwapToSortArray.cpp
+++ C-Plus-Plus/cp/MinSwapToSortArray.cpp
@@ -1,25 +1,28 @@
-/*minimum no. of swaps needed to sort the array in ascending
-order having integer in range 1 to N */
+/*
+minimum no. of swaps needed to sort the array in ascending
+order having integer in range 1 to N 
+*/
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	int swap=0,n,i;
-	cin>>n;
-	int a[n];
-	for(i=0;i<n;i++)cin>>a[i];
-	for(i=0;i<n-1;i++)
+    int swap=0,n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)cin>>a[i];
+    for(i=0;i<n-1;i++)
+    {
+        int j=a[i],temp;
+	if((i+1)!=a[i])
 	{
-		int j=a[i],temp;
-		if((i+1)!=a[i])
-		{
-			temp=a[i];
-			a[i]=a[j-1];
-			a[j-1]=temp;
-			swap++;
-		}
+	    temp=a[i];
+	    a[i]=a[j-1];
+	    a[j-1]=temp;
+	    swap++;
 	}
-	cout<<swap;
+    }
+    cout<<swap;
 }
 /*
 input:

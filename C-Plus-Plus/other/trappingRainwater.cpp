5b7cdf327c6291f93f6b568d57502c9b42be507d
Rishika <agarwalrishika30@gmail.com>
2021-03-13 09:03:26 +0530

    Added trapping rainwater problem #2873

--- C-Plus-Plus/other/trappingRainwater.cpp
+++ C-Plus-Plus/other/trappingRainwater.cpp
@@ -1,64 +1,67 @@
-#include <bits/stdc++.h>
+/* C++ program to solve Trapping Rainwater problem.An array of non-negative integers are given which
+represent an elevation height each of width 1, the
+goal is to compute total water trapped after raining*/
+
+#include<bits/stdc++.h>
 using namespace std;
 
-int maxWater(int arr[], int n)
-{
-
-	// indices to traverse the array
-	int left = 0;
-	int right = n-1;
-
-	// To store Left max and right max 
-	// for two pointers left and right
-	int left_max = 0;
-	int right_max = 0;
-
-	// To store the total amount 
-	// of rain water trapped
+int trapWater(int arr[], int n){
+	// Initialize output
 	int result = 0;
-	while (left <= right)
-	{
+	// Maximum element on left and right
+	int left_max = 0, right_max = 0;
+	// Indices to traverse the matrix
+	int left =0, right = n-1;
 
-	// We need check for minimum of left 
-	// and right max for each element
-	if(right_max <= left_max)
-	{
-
-		// Add the difference between 
-		// current value and right max at index r
-		result += max(0, right_max-arr[right]);
-
-		// Update right max
-		right_max = max(right_max, arr[right]);
-
-		// Update right pointer
-		right -= 1;
+	while(left <= right){
+		if(arr[left] < arr[right]){
+			if(arr[left] > left_max)
+				// Update max in left
+				left_max = arr[left];
+			else
+				// Water trapped on current index is max - current height
+				result += left_max - arr[left];
+			left++;
+		}else{
+			if(arr[right] > right_max)
+				// Update max in right
+				right_max = arr[right];
+			else
+				result += right_max - arr[right];
+			right--;
+		}
+		return result;
 	}
-	else
-	{ 
-
-		// Add the difference between 
-		// current value and left max at index l
-		result += max(0, left_max-arr[left]);
-
-		// Update left max
-		left_max = max(left_max, arr[left]);
-
-		// Update left pointer
-		left += 1;
-	}
-	}
-	return result;
 }
 
-// Driver code
-int main() {
-  int n;
-  int arr[n];
-  cin>>n;
-  for(int i=0;i<n;i++){
-    cin>>arr[i];
-  }
-	cout << maxWater(arr, n) << endl;
-	return 0;
+int main(){
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	cout << "Maximum water that can be accumulated is "<< trapWater(arr,n);
 }
+
+/*
+
+Input -
+
+n = 5
+arr[n] = {3,0,2,0,4}
+
+Sample Output -
+
+Maximum water that can be accumulated is 7
+
+Explanation -
+
+Three units of water can be stored at indexes 1 and 3 and 
+1 unit at index 2
+
+Time Complexity : O(n)
+Space Complexity : O(1) 
+
+*/
+

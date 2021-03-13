a1ea257f097efb86bbab16008df5604a57a519c7
Mohim Singla <69247094+Mohim-Singla@users.noreply.github.com>
2021-03-13 22:34:14 +0530

    Covering other possible cases in the program
    issue #2966

--- C-Plus-Plus/stack/Check_for_balanced_parenthesis.cpp
+++ C-Plus-Plus/stack/Check_for_balanced_parenthesis.cpp
@@ -2,71 +2,102 @@
 Check for balanced parenthesis
 ==============================
 Given an expression containing parenthesis, check if it is well-formed or balanced.
-Example of balanced parenthesis are: (), ((())), (a+b), (a/b)*(b/a)
 
 Application: Stack data structure
 Time Complexity: O(n)
 Space Complexity: O(n)
+
+Author: @gargvader
+Edit by: @Mohim-Singla
 */
 
 #include<iostream>
 #include <stack>
 using namespace std;
 
-bool isBalanced(string str) {
-	// Size of the string
-	int n = str.size();
-
-	// Stack to store open parenthesis
-	stack<int> s;
-	// Open parenthesis -> 1
-
-	// Loop through characters in the string
-	for (int i = 0; i < n; i++) {
-		// Open parenthesis is always pushed into the stack
-		if (str[i] == '(') {
-			s.push(1);
-		} else if (str[i] == ')') {
-			// Closed parenthesis encountered must be balanced by an open parenthesis already
+void parenthcheck(string s)
+{
+    stack<char> oper;
+    for(int i=0;i<s.length();i++)
+    {
+        // Open parenthesis is always pushed into the stack
+        if (s[i]=='(' || s[i]=='[' || s[i]=='{')
+        {
+            oper.push(s[i]);
+        }
+        else if (s[i]==')' || s[i]==']' || s[i]=='}')
+        {
+            // Closed parenthesis encountered must be balanced by an open parenthesis already
 			// present in the stack
-			if (!s.empty()) {
-				// Stack contains open parenthesis, one of which has been balanced
+            if (oper.empty())
+            {
+                cout<<"Unbalanced Parenthesis";
+                return;
+            }
+            else
+            {
+                // Stack contains open parenthesis, one of which has been balanced
 				// Pop one parenthesis out
-				s.pop();
-			} else {
-				// Stack contains no open parenthesis. So closed parenthesis cannot be balanced
-				return 0;
-			}
-		}
-	}
+                if ((s[i]==')' && oper.top()=='(') || (s[i]==']' && oper.top()=='[') || (s[i]=='}' && oper.top()=='{'))
+                    oper.pop();
+                else
+                {
+                    // Stack contains no open parenthesis. So closed parenthesis cannot be balanced
+                    cout<<"Unbalanced Parenthesis";
+                    return;
+                }
 
-	// Check if we have open parenthesis remaining
-	if (s.size()) {
-		return 0;
-	} else {
-		return 1;
-	}
+
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    // Check if we have open parenthesis remaining
+    if (oper.empty())
+    {
+        cout<<"Balanced Parenthesis";
+    }
+    else
+    {
+        cout<<"Unbalanced Parenthesis";
+    }
 }
 
 int main() {
 
 	// Input the string
-	string str; cin >> str;
-
-	if (isBalanced(str)) cout << "Yes";
-	else cout << "No";
+	string s;
+    cout<<"enter string: ";
+    cin>>s;
+    parenthcheck(s);
 }
 
 /*
+-------------------------------
+Test case 1:
 Input:
 ((a+b)+(c-d+f))
 
 Output:
-Yes
+Balanced Parenthesis
 
+-------------------------------
+Test case 2:
 Input:
-((a+b)+(c-d+f)))
+{(a+b)+(c-d+f)]}
 
 Output:
-No
+Unbalanced Parenthesis
+---------------------------------
+
+Test case 3:
+Input:
+(a+b)*{c*[a+b*(c+d)}]
+Output:
+Unbalanced Parenthesis
+
 */

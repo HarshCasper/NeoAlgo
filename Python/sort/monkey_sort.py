fbe8dd9c39e47401e03c058fc7a29ce9e0dd0934
Novice :) <72334601+noviicee@users.noreply.github.com>
2021-03-13 13:55:58 +0530

    Update monkey_sort.py

--- Python/sort/monkey_sort.py
+++ Python/sort/monkey_sort.py
@@ -1,16 +1,17 @@
 """
 Implementation of the MonkeySort (BogoSort) algorithm in Python.
-For implementing the Monkey Sort Algorithm, we need to import a built-in module named as 'random' in Python.
-A demonstration on how the random module will be used in Monkey Sort.
-random_demo.py->follow: https://github.com/noviicee/Search_and_Sort/blob/main/Sorting%20Algos/Monkey%20Sort/random_demo.py
-The monkey sort works somewhat like what an actual monkey would do if it is asked to sort a given set of (let's say) cards.
+For implementing the this algorithm, we need to import a built-in module named as 'random'.
+A demonstration on how the random module will be used in Monkey Sort:
+random_demo.py
+->follow: https://github.com/noviicee/Search_and_Sort/blob/main/Sorting%20Algos/Monkey%20Sort/random_demo.py
+The monkey sort works somewhat like what an actual monkey would do if it is asked to sort a given set of cards.
 """
 
 import random
 
 
 def is_sorted(data) -> bool:
-# Determine whether the data is sorted
+	# Determine whether the data is sorted
 
     return all(data[i] <= data[i + 1] for i in range(len(data) - 1))
 
@@ -20,8 +21,7 @@ def monkey_sort(arr):
 
 	while not is_sorted(arr):
 		random.shuffle(arr)
-		print("You can see these are the unordered lists that we get just by trying to sort it's elements by shuffling them")
+		print("These are the unordered lists that we get by trying to sort the elements by shuffling them")
 		print(arr)
 	print("\nThe final sorted list:")
 	print(arr)
-

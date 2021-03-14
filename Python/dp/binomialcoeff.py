0e0fe5e1039c0570719611193263823ced8765da
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-14 19:11:04 +0530

    Create binomialcoeff.py

---
+++ Python/dp/binomialcoeff.py
@@ -0,0 +1,42 @@
+''' Definition: 
+A binomial coefficient C(n, k) can be defined as the coefficient of x^k 
+in the expansion of (1 + x)^n.
+Input:
+Enter the value of n and k :
+4 2
+Output:
+6
+Explanation:
+4C2=4!/(2!*(4-2)!)
+=>(4*3*2*1)/(2*1*2*1)
+=>6
+'''
+#Time Complexity: O(n*k)
+#Space Complexity: O(n*k)
+def binomialCoeffMemorization(n,k,dp):
+    if n==k or k==0:#if n is equal to k or if k is equal to 0 then it is return 1 as nCn and nC0 is 1
+        return 1
+    #print(n)
+    if(dp[n][k]!=-1): #Checks whether dp[n][k] is already present
+        return dp[n][k]#Returns dp[n][k] if present
+    else:#Storing dp[n-1][k-1]+dp[n-1][k] to dp[n][k]
+        dp[n][k]=binomialCoeffMemorization(n-1,k-1,dp)+binomialCoeffMemorization(n-1,k,dp)
+    return dp[n][k]
+def binomialCoefTabulation(n,k):
+    dp2=[[-1 for i in range(k+1)] for j in range(n+1)]
+    for i in range(n+1):
+        for j in range(k+1):
+            if(j<=i):
+                if(j==0 or i==j):
+                    dp2[i][j]=1;
+            else:
+                dp2[i][j]=dp2[i-1][j-1]+dp2[i-1][j];
+    print("Tabulation: {}C{} = {}".format(n,k,binomialCoeffMemorization(n,k,dp)))
+
+            
+    
+n,k=map(int,input("Enter the value of n and k : ").split())
+dp=[[-1 for i in range(k+1)] for j in range(n+1)]
+#print(dp)
+print("Memorization: {}C{} = {}".format(n,k,binomialCoeffMemorization(n,k,dp)))
+binomialCoefTabulation(n,k)

f514857db6537db97faf89283dd4c765b67f6e66
Avishake Maji <56298861+Avishake007@users.noreply.github.com>
2021-03-14 19:43:52 +0530

    Update binomial_coefficient.py

--- Python/dp/binomial_coefficient.py
+++ Python/dp/binomial_coefficient.py
@@ -16,8 +16,7 @@ Explanation:
 def binomialCoeffMemorization(n,k,dp):
     if n==k or k==0:#if n is equal to k or if k is equal to 0 then it is return 1 as nCn and nC0 is 1
         return 1
-    #print(n)
-    if(dp[n][k]!=-1): #Checks whether dp[n][k] is already present
+    elif dp[n][k]!=-1: #Checks whether dp[n][k] is already present
         return dp[n][k]#Returns dp[n][k] if present
     else:#Storing dp[n-1][k-1]+dp[n-1][k] to dp[n][k]
         dp[n][k]=binomialCoeffMemorization(n-1,k-1,dp)+binomialCoeffMemorization(n-1,k,dp)
@@ -26,11 +25,11 @@ def binomialCoefTabulation(n,k):
     dp2=[[-1 for row in range(k+1)] for col in range(n+1)]
     for row in range(n+1):
         for col in range(k+1):
-            if(col<=row):
-                if(col==0 or row==col):
-                    dp2[row][col]=1;
+            if col<=row :
+                if col==0 or row==col:
+                    dp2[row][col]=1
             else:
-                dp2[row][col]=dp2[row-1][col-1]+dp2[row-1][col];
+                dp2[row][col]=dp2[row-1][col-1]+dp2[row-1][col]
     print("Tabulation: {}C{} = {}".format(n,k,binomialCoeffMemorization(n,k,dp)))
 
             

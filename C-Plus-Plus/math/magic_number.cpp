786ea213d423dedd6109071e4b0ced5a4dea6a60
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-03-14 13:28:54 +0530

    requested changes done

--- C-Plus-Plus/math/magic_number.cpp
+++ C-Plus-Plus/math/magic_number.cpp
@@ -1,13 +1,10 @@
 /*
-A number is said to be a magic number,if the sum of its digits
-are calculated recursively till a single digit comes by adding 
-the sum of digits after every addition. 
-If single digit comes out to be 1 then the number is a Magic Number.
+A number is said to be Magic Number, when the recursive sum of the digits is 1.
 For example 1729
     1+7+2+9=19
     1+9=10
     1+0=1
-So, it comes out be 1. Hence, 1729 is a Magic number.
+So, it comes out be 1. Hence, 1729 is a Magic Number.
 */
 
 #include <bits/stdc++.h>
@@ -49,16 +46,10 @@ int main()
 /*
 Sample I/0
 
-1.
-    Input:
-    Enter the Number to check whether it is Magic Number or Not:
-    1234
-    Output:
-    1234 is Magic Number.
-2.
-    Input:
-    Enter the Number to check whether it is Magic Number or Not:
-    785
-    Output:
-    785 is Not Magic Number.
+Input:
+Enter the Number to check whether it is Magic Number or Not:
+1234
+Output:
+1234 is Magic Number.
+
 */

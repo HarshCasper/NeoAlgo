8ac1150efa564c71e03557a56a6b03ccaa633b80
Manu Gupta <manugupta425@gmail.com>
2021-03-15 14:15:42 +0530

    Update Bidirectional_search_using_BFS_in_undirected_graph.cpp

--- C-Plus-Plus/graphs/Bidirectional_search_using_BFS_in_undirected_graph.cpp
+++ C-Plus-Plus/graphs/Bidirectional_search_using_BFS_in_undirected_graph.cpp
@@ -4,7 +4,7 @@ target vertex of a graph.
 In this algorithm, we perform the search operation from two sides, i,e. from the source
 vertex(Forward Search) and the target vertex(Backward Search) simultaneously.
 It divides the graph into two smaller sub-graphs, one starting at the source
-vertex and other at the target vertex. 
+vertex and other at the target vertex.
 As the searching operation proceeds, the two graphs grow and the search terminates when two graphs
 intersect.
 To perform the searching operation, we implement Breadth-first search on both source vertex and
@@ -27,8 +27,13 @@ void bfs(queue<int>& q, vector<bool>& visited, vector<int>& parent, vector<int> 
     int current = q.front();
     q.pop();
 
-    for (auto i : graph[current]) {
-        if (!visited[i]) {
+    for (auto i : graph[current])
+    {
+
+        // If adjacent vertex is not visited earlier
+        // mark it visited by assigning true value
+        if (!visited[i])
+        {
             parent[i] = current;
             visited[i] = true;
             q.push(i);
@@ -40,13 +45,15 @@ void printShortestPath(vector<int>& parentF, vector<int>& parentB, int sv, int t
     vector<int> path;
     path.push_back(inode);
     int i = inode;
-    while (i != sv) {
+    while (i != sv)
+    {
         path.push_back(parentF[i]);
         i = parentF[i];
     }
     reverse(path.begin(), path.end());
     i = inode;
-    while (i != tv) {
+    while (i != tv)
+    {
         path.push_back(parentB[i]);
         i = parentB[i];
     }
@@ -56,9 +63,11 @@ void printShortestPath(vector<int>& parentF, vector<int>& parentB, int sv, int t
     cout << endl;
 }
 
-int bidirectional_search(int sv, int tv, vector<int> graph[], int n, vector<bool>& visitedF,
+// Helper Method for bidirectional searching
+int bidirectional_search_helper(int sv, int tv, vector<int> graph[], int n, vector<bool>& visitedF,
     vector<bool>& visitedB, vector<int>& parentF, vector<int>& parentB)
 {
+    // queue for front and backward search
     queue<int> qF, qB;
 
     qF.push(sv);
@@ -67,20 +76,28 @@ int bidirectional_search(int sv, int tv, vector<int> graph[], int n, vector<bool
     qB.push(tv);
     visitedB[tv] = true;
 
+    // parent of source is set to -1
     parentF[sv] = -1;
+
+    // parent of target is set to -1
     parentB[tv] = -1;
 
-    while (!qF.empty() && !qB.empty()) {
+    while (!qF.empty() && !qB.empty())
+    {
+
         // Forward Breadth first search
-        bfs(qF, visitedF, parentF, graph); 
+        bfs(qF, visitedF, parentF, graph);
 
         // Backward Breadth first search
-        bfs(qB, visitedB, parentB, graph); 
+        bfs(qB, visitedB, parentB, graph);
 
+        // checks for intersecting vertex
         int inode = -1;
         int i, flag = 0;
-        for (i = 0; i < n; i++) {
-            if (visitedF[i] && visitedB[i]) {
+        for (i = 0; i < n; i++)
+        {
+            if (visitedF[i] && visitedB[i])
+            {
                 flag = 1;
                 break;
             }
@@ -88,12 +105,45 @@ int bidirectional_search(int sv, int tv, vector<int> graph[], int n, vector<bool
         if (flag == 1)
             inode = i;
 
+        // If intersecting vertex is found
+        // that means there exist a path
         if (inode != -1)
             return inode;
     }
     return -1;
 }
 
+// Method for bidirectional searching
+void bidirectional_search(int vertices, vector<int> graph[], int sv, int tv)
+{
+    // visited array for forward search
+    vector<bool> visitedF(vertices, false);
+
+    // visited array for backward search
+    vector<bool> visitedB(vertices, false);
+
+    // parent array for forward search
+    vector<int> parentF(vertices);
+
+    // parent array for backward search
+    vector<int> parentB(vertices);
+
+    int intersection_node = bidirectional_search_helper(
+        sv, tv, graph, vertices, visitedF, visitedB, parentF, parentB);
+
+    // If intersection node does not exist then the path between the source vertex and target vertex
+    // doesn't exist
+    if (intersection_node == -1)
+    {
+        cout << "Path between " << sv << " and " << tv << " does not exists" << endl;
+    }
+    else
+    {
+        cout << "Path exist between " << sv << " and " << tv << endl;
+        cout << "Intersection at: " << intersection_node << endl;
+        printShortestPath(parentF, parentB, sv, tv, intersection_node);
+    }
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -104,39 +154,22 @@ int main()
 
     vector<int> graph[vertices];
 
-    for (int i = 0; i < edges; i++) {
+    for (int i = 0; i < edges; i++)
+    {
         int a, b;
         cin >> a >> b;
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
     // source vertex
-    int sv; 
+    int sv;
     // target vertex
-    int tv; 
+    int tv;
 
     cin >> sv >> tv;
-     
-    // visited array for forward search 
-    vector<bool> visitedF(vertices, false); 
-    // visited array for backward search
-    vector<bool> visitedB(vertices, false); 
 
-    // parent array for forward search
-    vector<int> parentF(vertices); 
-    // parent array for backward search
-    vector<int> parentB(vertices); 
+    bidirectional_search(vertices, graph, sv, tv);
 
-    int intersection_node
-        = bidirectional_search(sv, tv, graph, vertices, visitedF, visitedB, parentF, parentB);
-    if (intersection_node == -1) {
-        cout << "Path between " << sv << " and " << tv << " does not exists" << endl;
-    }
-    else {
-        cout << "Path exist between " << sv << " and " << tv << endl;
-        cout << "Intersection at: " << intersection_node << endl;
-        printShortestPath(parentF, parentB, sv, tv, intersection_node);
-    }
     return 0;
 }
 
@@ -172,3 +205,4 @@ Space Complexity : O(bd/2)
 { b = branching factor of tree
   d = distance of goal vertex from source }
 */
+

e70deab755f495943301bb48f0d8005678988242
Nanda Kishor M Pai <nandakishormpai@gmail.com>
2021-03-15 22:36:44 +0530

    Corrected errors 2

--- Python/cp/Paint_Fence.py
+++ Python/cp/Paint_Fence.py
@@ -3,7 +3,7 @@ ___Paint Fence Algorithm___
 
 Problem:
 Given a fence with n posts and k colors
-find out the number of ways of painting the fence 
+Find out the number of ways of painting the fence
 such that at most 2 adjacent posts have the same color.
 
 Time Complexity  O(n)
@@ -14,7 +14,7 @@ n : number of posts
 k : number of colors
 
 Output:
-result: number of ways of painting 
+result: number of ways of painting
         considering the condition above
 
 
@@ -22,9 +22,9 @@ result: number of ways of painting
 
 
 def paint_fence(n, k):
-    if n<2 or k<(n/2):
-        return -1      #Here Number of posts is not enough or 
-                       #Number of colors are not enough to obey the condition
+    if n<2 or k<(n/2) :
+        return -1       # Here Number of posts is not enough or
+                        # Number of colors are not enough to obey the condition
     total = [0]*(n-1)
     same = [0]*(n-1)
     diff = [0]*(n-1)
@@ -35,8 +35,7 @@ def paint_fence(n, k):
     for i in range(1, n-1):
         same[i] = diff[i-1]
         diff[i] = total[i-1]*(k-1)
-        total[i] = same[i]+diff[i]
-    
+        total[i] = same[i]+diff[i]    
     return total[n-2]
 
 
@@ -44,12 +43,13 @@ if __name__ == "__main__":
     post = int(input("Enter n, Number of Posts\n"))
     color = int(input("Enter k, Number of Colors\n"))
     result = paint_fence(post, color)
-    if result == -1:
+    if result == -1 :
         print("Painting is not possible following the condition")
     else:
         print("Number of Possiblities:", result)
 
-'''
+
+"""
 Sample Input/Output:
 
 Enter n,Number of Posts
@@ -69,5 +69,4 @@ Enter n,Number of Posts
 Enter k,Number of Colors
 3
 Number of Possiblities: 180
-
-'''
+"""

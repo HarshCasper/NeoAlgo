66069448f74e5356e220b7024691a031e560b7af
Naman Garg <66183578+Naman-12911@users.noreply.github.com>
2021-03-15 22:32:57 +0530

    Update password_genretor.py

--- Python/other/password_genretor.py
+++ Python/other/password_genretor.py
@@ -10,26 +10,24 @@ if __name__ == '__main__':
     # The numbers for password
     digit_1 = string.digits
     # The symbols for password
-    symbol = string.punctuation        # you can use punctuation  function here for the symbols.
+    symbol = string.punctuation     
 
     password_len = int(input(("Entre lenght of the Password That You Want:")))
     if password_len < 4:  # Condtions for not printing password
         print("Make your password strong. please entre greater to 4")
     else:
-        all = accsi_1 + accsi_2 + digit_1 + symbol
-        temp = random.sample(all, password_len)
+        all_string_type = accsi_1 + accsi_2 + digit_1 + symbol
+        temp = random.sample(all_string_type, password_len)
         password = "".join(temp)
         print("Your password is:")
         print(password)
 
-'''
-Sample input
-Entre lenght of the Password That You Want: 10
 
-sample output
-WI%pP[,x53
+# Sample input
+# Entre lenght of the Password That You Want: 10
 
-'''
+# sample output
+# WI%pP[,x53
 
 # time complexity o(n)
 # space complexity o(1)

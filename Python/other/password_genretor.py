87d4e2256a195c2a9163eaf14ed8a20c450f211a
Naman Garg <66183578+Naman-12911@users.noreply.github.com>
2021-03-15 22:22:03 +0530

    Update password_genretor.py

--- Python/other/password_genretor.py
+++ Python/other/password_genretor.py
@@ -1,27 +1,20 @@
-import string
+port string
 import random
 if __name__ == '__main__':
     # The lower case for password
     accsi_1 = string.ascii_lowercase
 
-    #the upper case for password
+    # the upper case for password
     accsi_2 = string.ascii_uppercase
 
-    #The numbers for password
+    # The numbers for password
     digit_1 = string.digits
     # The symbols for password
-    symbol = ('@,#,$,&,*')          # you can use punctuions function here for the symbols.
+    symbol = string.punctuation        # you can use punctuation  function here for the symbols.
 
-    password_len = int(input(("Entre Password That You Want")))
-    if password_len == 0: # condtions for not printing password
-        print("Make your password storng. please entre greater to 4")
-    if password_len == 1:
-        print("Make your password storng. please entre greater to 4")
-    if password_len == 2:
-        print("Make your password storng. please entre greater to 4")
-    if password_len == 3:
-        print("Make your password storng. please entre greater to 4")
-
+    password_len = int(input(("Entre lenght of the Password That You Want:")))
+    if password_len < 4:  # Condtions for not printing password
+        print("Make your password strong. please entre greater to 4")
     else:
         all = accsi_1 + accsi_2 + digit_1 + symbol
         temp = random.sample(all, password_len)
@@ -29,5 +22,14 @@ if __name__ == '__main__':
         print("Your password is:")
         print(password)
 
+'''
+Sample input
+Entre lenght of the Password That You Want: 10
+
+sample output
+WI%pP[,x53
+
+'''
+
 # time complexity o(n)
-# space compplexity o(1)
+# space complexity o(1)

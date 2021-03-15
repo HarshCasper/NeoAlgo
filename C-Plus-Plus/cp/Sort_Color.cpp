a322e8822e5b2ece7923fa6ea99fbbc521e739cc
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2021-03-15 22:06:20 +0530

    Added question and test cases

--- C-Plus-Plus/cp/Sort_Color.cpp
+++ C-Plus-Plus/cp/Sort_Color.cpp
@@ -1,3 +1,7 @@
+//Given an array nums with n objects colored red, white, or blue, 
+//sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
 #include <bits/stdc++.h> 
 #define MAX 100
 using namespace std;
@@ -65,3 +69,21 @@ int main(){
 	sc.display();
 	return 0;	
 }
+/*{
+	Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+}*/

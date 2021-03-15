f7c0cd16261dce41ec56fe0da78c659f6e83fe75
Sakshi Gupta <55478753+sakshi2707@users.noreply.github.com>
2021-03-15 18:28:52 +0530

    Made requested changes
    fixes #3054

--- Java/sort/CycleSort.java
+++ Java/sort/CycleSort.java
@@ -1,12 +1,12 @@
 /*
-Cycle sort is an in-place sorting Algorithm, unstable sorting algorithm, a comparison sort that is theoretically optimal in terms of the total number of writes to the original array.
-It is optimal in terms of number of memory writes. It minimizes the number of memory writes to sort (Each value is either written zero times, if it’s already in its correct position, or written one time to its correct position.)
-It is based on the idea that array to be sorted can be divided into cycles. Cycles can be visualized as a graph. We have n nodes and an edge directed from node i to node j if the element at i-th index must be present at j-th index in the sorted array.
-We one by one consider all cycles. We first consider the cycle that includes first element. We find correct position of first element, place it at its correct position, say j. We consider old value of arr[j] and find its correct position, we keep doing this till all elements of current cycle are placed at correct position, i.e., we don’t come back to cycle starting point.
-Time Complexity : O(n^2)
-Worst Case Complexity : O(n^2)
-Average Case Complexity: O(n^2)
-Best Case Complexity: O(n^2)
+Cycle sort is an in-place sorting Algorithm, unstable sorting algorithm, a comparison sort that is theoretically optimal in terms of the total number of writes 
+to the original array.It is optimal in terms of number of memory writes. It minimizes the number of memory writes to sort (Each value is either written zero 
+times, if it’s already in its correct position, or written one time to its correct position.)It is based on the idea that array to be sorted can be divided into 
+cycles. Cycles can be visualized as a graph. We have n nodes and an edge directed from node i to node j if the element at i-th index must be present at j-th 
+index in the sorted array.We one by one consider all cycles. We first consider the cycle that includes first element. We find correct position of first element,
+place it at its correct position, say j. We consider old value of arr[j] and find its correct position, we keep doing this till all elements of current cycle are 
+placed at correct position, i.e., we don’t come back to cycle's starting point.
+
 This sorting algorithm is best suited for situations where memory write or swap operations are costly.
 */
 import java.util.*; 
@@ -95,9 +95,19 @@ public class CycleSort {
 
 /*
 Sample Input/ Output:-
+
 Enter size of array:7
 Enter array elements:
 9 8 7 6 5 4 3
 After sort : 
 3 4 5 6 7 8 9 
+
+Complexity:-
+
+Space Complexity : O(1)
+Time Complexity : O(n^2) , n is size of array
+Worst Case Complexity : O(n^2)
+Average Case Complexity: O(n^2)
+Best Case Complexity: O(n^2)
+
 */

2d4a7447a64aae089730fbea55065dc85b7980ed
SACHIN KHANDELWAL <75352195+iamsachin45@users.noreply.github.com>
2021-03-16 20:24:12 +0530

    multithreading in java

---
+++ Multithreading.java
@@ -0,0 +1,50 @@
+package connection;
+
+import java.util.Scanner;
+
+
+public class Multithreading 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		Thread3 th1 = new Thread3();
+		Thread4 th2 = new Thread4();
+		th1.start();
+		th2.start();
+		
+		
+	}
+	
+
+}
+
+class Thread3 extends Thread
+{
+	
+	public void run()
+	{
+		for(int i=0;i<3;i++)
+		{
+		System.out.println("Hello from thread1");
+		}
+	}
+}
+class Thread4 extends Thread
+{
+	public void run()
+	{
+		for(int i=0;i<3;i++)
+		{
+		System.out.println("Hello from thread2");
+		}
+	}
+}
+//OUTPUT  -- 
+//Hello from thread1
+//Hello from thread2
+//Hello from thread2
+//Hello from thread2
+//Hello from thread1
+//Hello from thread1
+

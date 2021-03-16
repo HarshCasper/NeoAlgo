8f486987dcd8b881d679c03a1945c33d6f5e53d6
ritik <ritik.rkgupta2000@gmail.com>
2021-03-16 11:03:56 +0530

    fixed issue 2509 5th attempt

--- C-Plus-Plus/ds/LevelOrderTraversal.cpp
+++ C-Plus-Plus/ds/LevelOrderTraversal.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include<queue>
- using namespace std;
-//Now first I need to create the structure of node in a binary tree
+using namespace std;
+ 
+//Creating strucuture for child
 struct child
 {
    int data;
@@ -16,6 +17,7 @@ struct child
    }
 }; 
 
+//Function to build tree Recursively 
 child*buildTree()
 {
 	int d;
@@ -24,18 +26,27 @@ child*buildTree()
   {
 		return NULL;
 	}
+	//Inserting value into root
 	child*root=new child(d);
+	//Inserting value into left Subtree
 	root->LeftPtr=buildTree();
+	//Inserting value into Right Subtree
 	root->RightPtr=buildTree();
 	return root;
 }
+
 //Function for implementing level order traversal using Queue
  void levelOrder(child*root)
  { 
      if(root==NULL)
      return;
+     //If the root is NULL,simply return
      queue<child*>q;
-     q.push(root);
+     //push the root value into the Queue
+	 q.push(root);
+     /*Create a t pointer and initially point it to the front of queue
+       If there is any value on left or Right push it on Queue and simply 
+	    pop it out on every iteration untill queue becomes empty*/
      while(!q.empty())
      {
          child*t=q.front();
@@ -48,38 +59,36 @@ child*buildTree()
      }
       
  }
-
-//Main Function starts
- int main()
- {
+  
+   int main()
+ { //Input Values here
     cout<<"Input the values of nodes of the tree"<<endl;
     child*root=buildTree();
     cout<<"Level Order Traversal for the following Tree will be:"<<endl;
     levelOrder(root);
- 
     return 0;
-
- }
- //Main Function ends
+}
+  
 
  /*
     
    Time Complexity: O(n)
    
-        1
-       /
-      2
-     /
-    3
-   /
-  4 
- /
-5
-Output:
-1
-2
-3
-4
-5
+                1
+               /
+              2
+             /
+            3
+           /
+          4 
+         /
+       5
+   
+   Output:
+   1
+   2
+   3
+   4
+   5
    
    */

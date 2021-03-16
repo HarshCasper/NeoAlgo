abe9ac010d6543d091d81dd27507feaafbd4f6b4
Harshit Pandey <harshitpandey251@gmail.com>
2021-03-16 11:45:15 +0530

    Queue using Stack

--- C-Plus-Plus/ds/Queue_using_stack.cpp
+++ C-Plus-Plus/ds/Queue_using_stack.cpp
@@ -1,6 +1,6 @@
 /*
 A queue can be implemented using two stacks. 
-Let queue to be implemented be q and stacks used to implement q be s1 and s2.
+Let queue to be implemented be q and stacks used to implement q be stack1 and stack2.
 */
 
 #include <iostream>
@@ -12,69 +12,45 @@ class Queue{
 
 public:
 
-	//main data structure
-	stack<int> s1;				
-	//helper
-	stack<int> s2;				
+	stack<int> stack1;				
+	stack<int> stack2;				
 
 	void push(int x){ 
-		s1.push(x);
+		stack1.push(x);
 	}
 
 	void pop(){
-
-		//copy s1 to s2
-
-		while(!s1.empty()){
-			s2.push(s1.top());
-			s1.pop();
+		while(!stack1.empty()){
+			stack2.push(stack1.top());
+			stack1.pop();
 		}
-
-		s2.pop();
-
-		//copy s2 to s1
-
-		while(!s2.empty()){
-			s1.push(s2.top());
-			s2.pop();
+		stack2.pop();
+		while(!stack2.empty()){
+			stack1.push(stack2.top());
+			stack2.pop();
 		}
-
 	}
 
 	int front(){
-		//copy s1 to s2
-
-		while(!s1.empty()){
-			s2.push(s1.top());
-			s1.pop();
+		while(!stack1.empty()){
+			stack2.push(stack1.top());
+			stack1.pop();
 		}
-
-		int x = s2.top();
-
-		//copy s2 to s1
-
-		while(!s2.empty()){
-			s1.push(s2.top());
-			s2.pop();
+		int x = stack2.top();
+		while(!stack2.empty()){
+			stack1.push(stack2.top());
+			stack2.pop();
 		}
 
 		return x;
-
 	}
-
 	int size(){
-		return s1.size();
+		return stack1.size();
 	}
-
 	bool empty(){
-		return s1.empty();
+		return stack1.empty();
 	}
-
-
 };
-
-
-
 
 int main()
 {
@@ -82,29 +58,21 @@ int main()
 
 	int n;
 	cin >> n;
-
 	for(int i=1;i<=n;i++){
 		q.push(i);
 	}
 
-
 	cout<<q.front()<<endl;
-
 	cout<<q.size()<<endl;
-
 	q.pop();
 	cout<<q.front()<<endl;
-	
 	cout<<q.empty()<<endl;
-
 
 	while(!q.empty()){
 		cout<<q.front()<<endl;
 		q.pop();
 	}
 
-
-	
 	return 0;
 }
 

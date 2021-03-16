ff23e6c138a504ea3470dfd283cde3f604bc710b
ritik <ritik.rkgupta2000@gmail.com>
2021-03-16 18:29:18 +0530

    fixed issue 2509 6 attempt

--- C-Plus-Plus/ds/LevelOrderTraversal.cpp
+++ C-Plus-Plus/ds/LevelOrderTraversal.cpp
@@ -1,94 +1,105 @@
+/*
+    Level Order Traversal
+    
+	Given a binary tree with unique values,print all the elements in 
+    a level Order i.e in a BFS( Breadth First Search) order.
+    
+	Sample Input:
+        Enter the values of nodes of the tree  :  1 2 3 4 5 6 -1 -1 -1 -1 -1 -1 -1  
+    
+	Sample Output:
+        Level Order Traversal for the following Tree will be   :  1 2 3 4 5 6
+	        
+	@author : hritikgupta92
+    @created : 16-03-2021
+        
+*/
+
 #include <iostream>
 #include<queue>
 using namespace std;
  
-//Creating strucuture for child
-struct child
-{
-   int data;
-   child*LeftPtr;
-   child*RightPtr;
+//  Basic Definition of node in a Binary Tree
+
+struct child{
+    int data;
+    child*LeftPtr;
+    child*RightPtr;
  
-   child(int value)
-   {
-     data=value;
-     LeftPtr=NULL;
-     RightPtr=NULL;
-   }
+    child(int value)
+	{
+        data=value;
+        LeftPtr=NULL;
+        RightPtr=NULL;
+    }
 }; 
 
-//Function to build tree Recursively 
+//  Function to build tree Recursively 
+
 child*buildTree()
 {
-	int d;
+    int d;
 	cin>>d;
-	if(d==-1)
-  {
+	
+	//  Base Case
+	if(d==-1){
 		return NULL;
-	}
-	//Inserting value into root
+    }
+
+	//  Recursive Case 
 	child*root=new child(d);
-	//Inserting value into left Subtree
+
+	//  Inserting value into left Subtree
 	root->LeftPtr=buildTree();
-	//Inserting value into Right Subtree
+
+	//  Inserting value into Right Subtree
 	root->RightPtr=buildTree();
+
 	return root;
 }
 
-//Function for implementing level order traversal using Queue
+//  Function for implementing level order traversal using Queue
+ 
  void levelOrder(child*root)
  { 
-     if(root==NULL)
+     //  Base Case
+	 if(root==NULL)
      return;
-     //If the root is NULL,simply return
-     queue<child*>q;
-     //push the root value into the Queue
+    
+	 queue<child*>q;
+    
+	 //  push the root value into the Queue
 	 q.push(root);
-     /*Create a t pointer and initially point it to the front of queue
-       If there is any value on left or Right push it on Queue and simply 
-	    pop it out on every iteration untill queue becomes empty*/
+      
+	 //  Recursive Case 
      while(!q.empty())
      {
-         child*t=q.front();
+          child*t=q.front();
           cout<<t->data<<endl;
-         if(t->LeftPtr)
-         q.push(t->LeftPtr);
-         if(t->RightPtr)
-         q.push(t->RightPtr);
-         q.pop();
+           
+		   if(t->LeftPtr)
+           q.push(t->LeftPtr);
+           if(t->RightPtr)
+           q.push(t->RightPtr);
+         
+		   q.pop();
      }
       
  }
   
-   int main()
- { //Input Values here
-    cout<<"Input the values of nodes of the tree"<<endl;
-    child*root=buildTree();
-    cout<<"Level Order Traversal for the following Tree will be:"<<endl;
-    levelOrder(root);
-    return 0;
+int main()
+{ 
+    //  Input Values here
+    cout<<"Enter the values of nodes of the tree: "<<endl;
+     
+	child*root=buildTree();
+    
+	cout<<"Level Order Traversal for the following Tree will be:"<<endl;
+     
+	levelOrder(root);
+     
+	return 0;
 }
   
 
- /*
-    
-   Time Complexity: O(n)
-   
-                1
-               /
-              2
-             /
-            3
-           /
-          4 
-         /
-       5
-   
-   Output:
-   1
-   2
-   3
-   4
-   5
-   
-   */
+  

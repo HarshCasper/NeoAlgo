e97a3894999121b07c39efd21699fa8fdd1921cf
hritikgupta92 <58954579+hritikgupta92@users.noreply.github.com>
2021-03-17 00:54:26 +0530

    Update LevelOrderTraversal.cpp

--- C-Plus-Plus/ds/LevelOrderTraversal.cpp
+++ C-Plus-Plus/ds/LevelOrderTraversal.cpp
@@ -1,115 +1,116 @@
 /*
-    Level Order Traversal
-    
-	Given a binary tree with unique values,print all the elements in 
-      a level Order i.e in a BFS( Breadth First Search) order.
-    
-      Sample Input 1:
-      Enter the values of nodes of the tree  :  1 2 3 4 5 6 -1 -1 -1 -1 -1 -1 -1  
-    
-      Sample Output 1:
-      Level Order Traversal for the following Tree will be   :  1 2 3 4 5 6
-	
-	
-      Sample Input 2:
-      Enter the values of nodes of the tree  :  3 4 6 -1 -1 7 -1 -1 5 -1 -1  
-
-      Sample Output 2:
-      Level Order Traversal for the following Tree will be   :  3 4 5 6 7
 
 
-      @author : hritikgupta92
-      @created : 16-03-2021
-        
+This program is about Traversing elements of a binary Tree in a Level Order way.Here we will enter the values for
+the nodes of the tree. 
+It will simply print the elements of the tree level by level by simply storing the  
+addresses of the nodes in a queue and printing the node whose address is on the front 
+,and calling the same function for left and right subtree recursively and the process 
+is iterated untill  queue doesn't gets emptied.
+
+
 */
-
 #include <iostream>
+
 #include<queue>
+
 using namespace std;
- 
+
 //  Basic Definition of node in a Binary Tree
 
-struct child{
+struct child {
     int data;
     child*LeftPtr;
     child*RightPtr;
- 
-    child(int value)
-    {
-        data=value;
-        LeftPtr=NULL;
-        RightPtr=NULL;
+
+    child(int value) {
+        data = value;
+        LeftPtr = NULL;
+        RightPtr = NULL;
     }
-}; 
+};
 
 //  Function to build tree Recursively 
 
-child*buildTree()
-{
-     int d;
-     cin>>d;
-	
-     //  Base Case
-     if(d==-1){
-     return NULL;
-     }
+child * buildTree() {
+    int d;
+    cin >> d;
 
-     //  Recursive Case 
-     child*root=new child(d);
+    if (d == -1) {
+        return NULL;
+    }
 
-     //  Inserting value into left Subtree
-     root->LeftPtr=buildTree();
+    child*root = new child(d);
 
-     //  Inserting value into Right Subtree
-     root->RightPtr=buildTree();
+    root->LeftPtr = buildTree();
 
-     return root;
- }
+    root->RightPtr = buildTree();
 
- //  Function for implementing level order traversal using Queue
- 
- void levelOrder(child*root)
- { 
-         //  Base Case
-	 if(root==NULL)
-         return;
-    
-	 queue<child*>q;
-    
-	 //  push the root value into the Queue
-	 q.push(root);
-      
-	 //  Recursive Case 
-         while(!q.empty())
-         {
-              child*t=q.front();
-              cout<<t->data<<endl;
-           
-              if(t->LeftPtr)
-              q.push(t->LeftPtr);
-          
-	        if(t->RightPtr)
-              q.push(t->RightPtr);
-         
-              q.pop();
-         }
-      
- }
-  
-int main()
-{ 
-    //  Input Values here
-    cout<<"Enter the values of nodes of the tree: "<<endl;
-     
-    child*root=buildTree();
-    
-    cout<<"Level Order Traversal for the following Tree will be:"<<endl;
-     
-    levelOrder(root);
-     
-    return 0;
+    return root;
+}
+
+//  Function for implementing level order traversal using Queue
+
+void levelOrder(child * root) {
+
+    if (root == NULL)
+        return;
+
+    queue <child*> q;
+
+    q.push(root);
+
+    while (!q.empty()) {
+        child*t = q.front();
+        cout << t -> data << endl;
+
+        if (t->LeftPtr)
+            q.push(t->LeftPtr);
+        if (t->RightPtr)
+            q.push(t->RightPtr);
+
+        q.pop();
+    }
 
 }
- 
 
-  
+int main() {
+
+    cout << "Enter the values of nodes of the tree: " << endl;
+
+    child*root = buildTree();
+
+    cout << "Level Order Traversal for the following Tree will be:" << endl;
+
+    levelOrder(root);
+
+    return 0;
+}
+
+/*  
+Output:-
+Enter the values of nodes of the tree:
+3
+4
+6
+-1
+-1
+7
+-1
+-1
+5
+-1
+-1
+
+
+Level Order Traversal for the following Tree will be:
+3
+4
+5
+6
+7
+
+Time Complexity:- O(n)
+Space Complecity:- O(n) 
+
+*/

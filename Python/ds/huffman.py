94155dc51b7c37ebe9ee97d09b2e5bdca62ada72
devara khurma venkata kavyasree <61260505+kavyasree-123@users.noreply.github.com>
2021-03-16 14:43:52 +0530

    updated

--- Python/ds/huffman.py
+++ Python/ds/huffman.py
@@ -1,16 +1,16 @@
-''' Huffman coding in python issue-3306
-Huffman coding works on data stream that makes up the file to be compressed'''
-from collections import defaultdict
-#  importing defaultdict is valuable option for handling missing keys in dictionaries.
-from heapq import heappop, heapify, heappush
-''' A heap queue is created by using python's inbuilt library named heapq
+'''Huffman coding in python issue-3306
+Huffman coding works on data stream that makes up the file to be compressed
+A heap queue is created by using python's inbuilt library named heapq
 heappop - This function is used to remove and return the smallest element from heap
 heapify -This function converts a regular list to a heap
 heappush-This function is used to insert the element mentioned in its arguments'''
+from collections import defaultdict
+#  importing defaultdict is valuable option for handling missing keys in dictionaries.
+from heapq import heappop, heapify, heappush
+
+
 def Huffman(code):
     getheap = [[ab, [xyz, "\t "]] for xyz, ab in code.items()]
-    
-    
 #  getting a loop to access all elements
     while len(getheap) > 1 :
         heapify(getheap)
@@ -23,13 +23,13 @@ def Huffman(code):
             allelements[1] = '0' + allelements[1]
         heappush(getheap, [least[0] + heighest[0]] + least[1:] + heighest[1:])
 #  need to push additon of least and heighest in an order itself to ger correct results
-    return sorted(heappop(getheap)[1:], key = lambda text:(len(text[-1]), text))
+    return sorted(heappop(getheap)[1:], key=lambda text: (len(text[-1]), text))
 # getting a lambda function for length of inputs directed into keyword key
 
 
-print("element first printed in row")
-print("weights printed in second row")
-print("huffcode  are printed last of row")
+print("element_first_printed_in_row.")
+print("weights_printed_in_second_row.")
+print("huffcode_are_printed_last_of_row.")
 code = defaultdict(int)
 inputs = "testing code with inputs"
 #  give the inputs in string format
@@ -38,25 +38,25 @@ for element in inputs:
 huffman = Huffman(code)
 #  for  loop to print the result for text input
 for text in huffman:
-    print(text[1], text[0], code[text[0]])
-''' input testing code with inputs.
+    print(text[1], text[0], "-", code[text[0]])
+'''input testing code with inputs.
 output:
-huffcode first printed in row                                                                                                                                               
-element printed in second row                                                                                                                                               
-weights are printed last of row                                                                                                                                             
-00        t 4                                                                                                                                                               
-100         4                                                                                                                                                               
-011       i 3                                                                                                                                                               
-1011      e 2                                                                                                                                                               
-1101      n 2                                                                                                                                                               
-1111      s 2                                                                                                                                                               
-0100      u 1                                                                                                                                                               
-0101      w 1                                                                                                                                                               
-10100     c 1                                                                                                                                                               
-10101     d 1                                                                                                                                                               
-11000     g 1                                                                                                                                                               
-11001     h 1                                                                                                                                                               
-11100     o 1                                                                                                                                                               
-11101     p 1
-time complexity is O(logn). 
-space complexity is  O(n log k).'''
+element_first_printed_in_row.                                                                                                                                                        
+weights_printed_in_second_row.                                                                                                                                                       
+huffcode_are_printed_last_of_row.                                                                                                                                                  
+00        t-4                                                                                                                                                                      
+011        -3                                                                                                                                                                      
+100       i-3                                                                                                                                                                      
+1011      e-2                                                                                                                                                                      
+1101      n-2                                                                                                                                                                      
+1111      s-2                                                                                                                                                                      
+0100      u-1                                                                                                                                                                      
+0101      w-1                                                                                                                                                                      
+10100     c-1                                                                                                                                                                      
+10101     d-1                                                                                                                                                                      
+11000     g-1                                                                                                                                                                      
+11001     h-1                                                                                                                                                                      
+11100     o-1                                                                                                                                                                      
+11101     p-1
+time_complexity_is_O(logn)andspace_complexity_is_O(n log k)'''
+

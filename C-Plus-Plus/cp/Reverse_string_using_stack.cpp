c5d503dd809bb268cfd8ebfc3b1098a8c062a940
Harshit Pandey <harshitpandey251@gmail.com>
2021-03-17 14:29:26 +0530

    Update Reverse string using Stack

--- C-Plus-Plus/cp/Reverse_string_using_stack.cpp
+++ C-Plus-Plus/cp/Reverse_string_using_stack.cpp
@@ -1,14 +1,10 @@
-/* 
-Given a string,print reverse of the string using stack
-Approach:-
-In stack,we know LIFO principle works.
-First,i create an empty stack,then i push each character of string in stack
-then,using for loop i popped out every single character.
-*/
+//Given a string,print reverse of the string using stack
+//Approach:-
+//In stack,we know LIFO principle works.
+//First,i create an empty stack,then i push each character of string in stack
+//then,using for loop i popped out every single character.
 
-#include<iostream>
-#include<string>
-#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
 
 //function to reverse the string
@@ -17,15 +13,13 @@ void reverse(string &s)
 	int i;
 	stack<int> str;
 
-	for(char ch:s)
-	{
+	for(char ch:s){
         str.push(ch);
-    	}
-    	for(i=0;i<s.size();i++)
-    	{
+    }
+    for(i=0;i<s.size();i++){
         s[i]=str.top();
         str.pop();
-    	} 
+    } 
 }
 
 int main()
@@ -34,7 +28,6 @@ int main()
 	string s;
 	cout<<"Enter a string you want to reverse: ";
 	cin>>s;
-
 	reverse(s);
     cout<<"String After Reverse: ";
 	cout<<s;
@@ -45,14 +38,10 @@ int main()
 Test Case 1:
 Input :
 ***********Program to reverse a string using stack***********
-
 Enter a string you want to reverse: Harshit
-
 Output :
 String After Reverse: tihsrah
-
 
 Time Complexity : O(n), where n is the length of the input string.
 Space Complexity : O(n) for stack.
 */
-

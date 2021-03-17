20f75cbfda98ad9e6c4bcf8f36565a2ccf5c35da
XZANATOL <abdelrahmanbedox@gmail.com>
2021-03-17 05:10:05 +0200

    Adding Requested Changes

--- Python/math/Arctan.py
+++ Python/math/Arctan.py
@@ -1,11 +1,12 @@
-#Arctan Algorithm
-#Written by XZANATOL
+# Arctan Algorithm
+# Written by XZANATOL
 
 """
-Algorithm that demonstrates how to calculate Arctan and obtain angle in the desired mode. \
-(degrees/radians)
+Algorithm that demonstrates how to calculate Arctan and obtain angle in the \
+desired mode. (degrees/radians)
 """
-pi = 3.141592653589793238462643383279502884197169399375105 #Constant
+pi = 3.141592653589793238462643383279502884197169399375105  # Constant
+
 
 def conv_to_deg(angle):
     """Converts radian angles to degrees"""
@@ -14,35 +15,37 @@ def conv_to_deg(angle):
 
 def conv_if_neg(x):
     """Returns abs of an angle if it's negative"""
-    if x<0:
+    if x < 0:
         return abs(x), True
-    return x,False
+    return x, False
+
 
 def arctan(val):
     """Arctan power series that obtains the angle in radians"""
-    val = val - (val**3)/3 + (val**5)/5 - (val**7)/7 + (val**9/9) - (val**11/11)
+    val = val - (val**3)/3 + (val**5)/5 - (val**7)/7 + (val**9/9) - \
+        (val**11/11)
     return val
 
 
-#Start Checkpoint
+# Start Checkpoint
 if __name__ == "__main__":
-    #Get Inputs
+    # Get Inputs
     val = float(input("Enter value to obtain its angle > "))
     mode = input("Enter display mode (deg/rad) > ")
 
-    val, neg_flag = conv_if_neg(val) #Step 1 - Check if negative
-    comp_flag = False #Step 2 - Complementary angles
-    identity_flag = False #Step 3 - Identity angle
+    val, neg_flag = conv_if_neg(val)  # Step 1 - Check if negative
+    comp_flag = False  # Step 2 - Complementary angles
+    identity_flag = False  # Step 3 - Identity angle
     if val > 1:
         comp_flag = True
         val = val**-1
     elif val > (2-3**0.5):
         identity_flag = True
-        val = ((3**0.5 * val) -1) / (3**0.5 + val)
+        val = ((3**0.5 * val) - 1) / (3 ** 0.5 + val)
 
-    angle = arctan(val) #Step 4 - Substitute in the power series
+    angle = arctan(val)  # Step 4 - Substitute in the power series
 
-    #Validate flags
+    # Validate flags
     if comp_flag:
         angle = pi/2 - angle
     if identity_flag:
@@ -50,7 +53,7 @@ if __name__ == "__main__":
     if neg_flag:
         angle *= -1
 
-    #Validate display mode
+    # Validate display mode
     if mode == "deg":
         angle = conv_to_deg(angle)
 
@@ -74,6 +77,7 @@ Enter display mode (deg/rad) > deg
 
 Output:
 Angle > 74.47588924544092
+=============
 
 Time Complexity: O(1)
 Explain: None of the functions use loops or depend on how large the entered number is.

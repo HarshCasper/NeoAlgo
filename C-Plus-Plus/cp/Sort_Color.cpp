b638494df9feb2437dab2ba80da7f3953f824d1e
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2021-03-18 00:41:15 +0530

    Added link to the question as well as examples

--- C-Plus-Plus/cp/Sort_Color.cpp
+++ C-Plus-Plus/cp/Sort_Color.cpp
@@ -1,7 +1,34 @@
-//Given an array nums with n objects colored red, white, or blue, 
-//sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+/*Given an array nums with n objects colored red, white, or blue, 
+sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
-//We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is 0, 1, or 2.
+
+link to the problem: https://leetcode.com/problems/sort-colors/
+*/
+
 #include <bits/stdc++.h> 
 #define MAX 100
 using namespace std;

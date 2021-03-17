bd6d91290a5f38a13c051874f1bec98f3d46471e
bitaashna <57864526+bitaashna@users.noreply.github.com>
2021-03-17 11:19:12 +0530

    Update max_sum_subarray_of_size_k.cpp

--- C-Plus-Plus/sliding_window/max_sum_subarray_of_size_k.cpp
+++ C-Plus-Plus/sliding_window/max_sum_subarray_of_size_k.cpp
@@ -1,41 +1,37 @@
 /*
 Problem statement :
-An array of integers of size 'n' and a number 'k' is given.
-We have to detemine the maximum sum of subarray of size k
-
+An array of integers of size 'n' and a number 'k' is given. We have to detemine the maximum sum of subarray of size k
 => Input format:
    size of array (n)
    n elemnts of array
    k
-
 => Output format:
    maximum sum
 */
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Returns maximum sum in a subarray of size k
-int max_sum_subarray(int n,int arr[],int k) {
-    int sum=0;
-    int maxx=INT_MIN;
-    /*two pointers are needed to represent start and end of the window
-      let's denote i as the start and j as the end then window size (k) =j-i+1*/
-    int i=0,j=0;
-    //pointer should be moved unless j reach the end index of arrray
-    while(j<n)
-    {   //Compute sum of each window
-        sum=sum+arr[j];
-        if(j-i+1<k)
+int max_sum_subarray(int n, int arr[], int k)
+{
+    int sum = 0;
+    int maxx = INT_MIN;
+    /*two pointers are needed to represent start and end of the window.let's denote i as the start and j as the end then window size (k) =j-i+1*/
+    int i = 0, j = 0;
+   	//pointer should be moved unless j reach the end index of arrray
+    while (j < n)
+    {
+    	//Compute sum of each window
+        sum = sum + arr[j];
+        if (j - i + 1 < k)
             j++;
-        else if(j-i+1==k)
-        {   //compute maximum of sum and previous sum
-            maxx=max(sum,maxx);
-            /*Compute sums of remaining windows by
-              removing first element of previous
-              window */
-            sum=sum-arr[i];
-            //slide the  window
+        else if (j - i + 1 == k)
+        {
+        	//compute maximum of sum and previous sum
+            maxx = max(sum, maxx);
+            /*Compute sums of remaining windows by removing first element of previous window */
+            sum = sum - arr[i];
+           	//slide the  window
             i++;
             j++;
         }
@@ -43,16 +39,17 @@ int max_sum_subarray(int n,int arr[],int k) {
     return maxx;
 }
 
-int main() {
-    //taking input from user
-    int n,k;
-    cin>>n;
+int main()
+{
+   	//taking input from user
+    int n, k;
+    cin >> n;
     int arr[n];
-    //taking array element from user
-    for(int i=0; i<n; i++)
-        cin>>arr[i];
-    cin>>k;
-    cout<<max_sum_subarray(n,arr,k);
+   	//taking array element from user
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> k;
+    cout << max_sum_subarray(n, arr, k);
 }
 
 /*
@@ -63,7 +60,6 @@ Input:-
 3
 Output:
 19
-
 Time complexity: O(n)
 Space complexity: O(1)
 */

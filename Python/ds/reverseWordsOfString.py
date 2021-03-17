b0b3aaf83c6360af8e1a8a26885522799625add0
taruni-always <nayakantitaruni@gmail.com>
2021-03-17 11:31:12 +0530

    created reverseWordsOfString.py

---
+++ Python/ds/reverseWordsOfString.py
@@ -0,0 +1,32 @@
+'''
+Reversing individual words of a string using stack data structure
+'''
+
+
+def reverseWords(string: str):
+    ''' Prints the individual words in a reverse order '''
+    stack = []
+    for letter in string:
+        if letter.isalnum():
+            stack.append(letter)  # equivalent to stack.push()
+        else:
+            while stack != []:
+                print(stack.pop(), end="")
+            print(letter, end="")
+    while stack:
+        print(stack.pop(), end="")
+    print()
+
+
+sentence = input("Enter a sentence:\n")
+print("The sentence after reversing individual words is:")
+reverseWords(sentence)
+
+# Sample Input1: Get me food
+# Sample Output1: teG em doof
+
+# Sample Input2: happy birthday
+# Sample Output2: yppah yadhtrib
+
+# Time complexity = O(n) [since each pop or push takes O(1) times]
+# Space Complexity = O(n)

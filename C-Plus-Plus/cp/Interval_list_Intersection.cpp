327a167596fc61de470ab26379d87558e3406243
Anuj Agrawal <anujagr2001@gmail.com>
2021-03-19 00:15:03 +0530

    Update Interval_List_Intesection

--- C-Plus-Plus/cp/Interval_list_Intersection.cpp
+++ C-Plus-Plus/cp/Interval_list_Intersection.cpp
@@ -17,142 +17,129 @@ the intersection of [1, 3] and
 [2, 4] is [2, 3].
 */
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> IntervalList(int I1,int J1,int I2,int J2)
+vector<int> IntervalList(int I1, int J1, int I2, int J2)
 {
     vector<int> U;
-    if(J2==I1)
-    {
+    if (J2 == I1) {
         U.push_back(J2);
         U.push_back(J2);
         return U;
     }
-    if((I2<I1)  &&  (J2>I1)  &&  (J2<J1))
-    {
+    if ((I2 < I1) && (J2 > I1) && (J2 < J1)) {
         U.push_back(I1);
         U.push_back(J2);
         return U;
     }
-    if((I2>=I1)  &&  (J2<=J1))
-    {
+    if ((I2 >= I1) && (J2 <= J1)) {
         U.push_back(I2);
         U.push_back(J2);
         return U;
-    }   
-    if((I2>I1)  &&  (J2>=J1)  &&  (I2<J1))
-    {
+    }
+    if ((I2 > I1) && (J2 >= J1) && (I2 < J1)) {
         U.push_back(I2);
         U.push_back(J1);
         return U;
     }
-    if((I1>=I2)  &&  (J1<=J2))
-    {
+    if ((I1 >= I2) && (J1 <= J2)) {
         U.push_back(I1);
         U.push_back(J1);
         return U;
-    } 
-    if(J1==I2)
-    {
+    }
+    if (J1 == I2) {
         U.push_back(J1);
         U.push_back(J1);
         return U;
-    }    
+    }
     return U;
 }
-vector<vector<int>> intervalIntersection(
-    vector<vector<int>>& firstList, 
-    vector<vector<int>>& secondList) {
-        int X,Y,i,j,I1,J1,I2,J2;
-        X=firstList.size();
-        Y=secondList.size();
-        vector<vector<int> > Ans;
-        if((X==0)  ||  (Y==0))
+vector<vector<int> > intervalIntersection(
+    vector<vector<int> >& firstList,
+    vector<vector<int> >& secondList)
+{
+    int X, Y, i, j, I1, J1, I2, J2;
+    X = firstList.size();
+    Y = secondList.size();
+    vector<vector<int> > Ans;
+    if ((X == 0) || (Y == 0))
         return Ans;
-            I1=firstList[0][0];
-            J1=firstList[0][1];
-            I2=secondList[0][0];
-            J2=secondList[0][1];
-        i=0;
-        j=0;
-        while((i<X) && (j<Y))
-        {
-            vector<int> U=IntervalList(I1,J1,I2,J2);
-            if(U.size()!=0)
-            {
+    I1 = firstList[0][0];
+    J1 = firstList[0][1];
+    I2 = secondList[0][0];
+    J2 = secondList[0][1];
+    i = 0;
+    j = 0;
+    while ((i < X) && (j < Y)) {
+        vector<int> U = IntervalList(I1, J1, I2, J2);
+        if (U.size() != 0) {
             Ans.push_back(U);
-            }
-            if(J1<J2)
-            {
+        }
+        if (J1 < J2) {
             ++i;
-                if(i<X)
-                {
-            I1=firstList[i][0];
-            J1=firstList[i][1];
-                }
-            }
-            else if(J1==J2)
-            {
-            ++i;
-                if(i<X)
-                {
-            I1=firstList[i][0];
-            J1=firstList[i][1];
-                }
-            ++j;
-                if(j<Y)
-                {
-            I2=secondList[j][0];
-            J2=secondList[j][1];
-                }
-            }
-            else
-            {
-            ++j;
-                if(j<Y)
-                {
-            I2=secondList[j][0];
-            J2=secondList[j][1];
-                }
+            if (i < X) {
+                I1 = firstList[i][0];
+                J1 = firstList[i][1];
             }
         }
-        return Ans;
+        else if (J1 == J2) {
+            ++i;
+            if (i < X) {
+                I1 = firstList[i][0];
+                J1 = firstList[i][1];
+            }
+            ++j;
+            if (j < Y) {
+                I2 = secondList[j][0];
+                J2 = secondList[j][1];
+            }
+        }
+        else {
+            ++j;
+            if (j < Y) {
+                I2 = secondList[j][0];
+                J2 = secondList[j][1];
+            }
+        }
+    }
+    return Ans;
 }
 int main()
-{  
-  int N,i,S,E;
-    cout<<"Enter the size of first list : ";
-    cin>>N;
-    vector<vector<int>> firstList;
-    for(i=0;i<N;i++)
-    {
+{    
+    int N, i, S, E, M;
+    cout << "Enter the size of first list : ";
+    cin >> N;
+    vector<vector<int> > firstList;
+    for (i = 0; i < N; i++) {
         vector<int> Temp;
-        cin>>S>>E;
+        cin >> S >> E;
         Temp.push_back(S);
         Temp.push_back(E);
         firstList.push_back(Temp);
     }
-    cout<<"Enter the size of second list : ";
-    cin>>N;
-    vector<vector<int>> secondList;
-    for(i=0;i<N;i++)
-    {
+    cout << "Enter the size of second list : ";
+    cin >> M;
+    vector<vector<int> > secondList;
+    for (i = 0; i < M; i++) {
         vector<int> Temp;
-        cin>>S>>E;
+        cin >> S >> E;
         Temp.push_back(S);
         Temp.push_back(E);
         secondList.push_back(Temp);
     }
-    cout<<"The Intersection of given two list : "<<endl;
-    vector<vector<int>> Ans=intervalIntersection(firstList,secondList);
-    for(i=0;i<Ans.size();i++)
-        cout<<Ans[i][0]<<" "<<Ans[i][1]<<endl;
+    cout << "The Intersection of given two list : " << endl;
+    vector<vector<int> > IntersectingList = intervalIntersection(firstList, secondList);
+    for (i = 0; i < IntersectingList.size(); i++)
+        cout << IntersectingList[i][0] << " " << IntersectingList[i][1] << endl;
     return 0;
 }
 
-
 /*
+Time Complexity: O(N + M)
+Space Complexity: O(M + N)
+where N and M are the size of first
+and second List respectively.
+
 Sample Input 
 Enter the size of first list :
 4
@@ -176,4 +163,3 @@ The Intersection of given two list :
 24 24
 25 25
 */
-

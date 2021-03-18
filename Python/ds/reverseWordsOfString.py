b47c4f524f710458266735e9b7876446406f0410
taruni-always <nayakantitaruni@gmail.com>
2021-03-18 16:36:30 +0530

    made requested changes

--- Python/ds/reverseWordsOfString.py
+++ Python/ds/reverseWordsOfString.py
@@ -1,7 +1,12 @@
 '''
 Reversing individual words of a string using stack data structure
+Algorithm/Approach:
+We iterate through the string character-by-character.
+If the current character is not a space, or not a newline character or not a null character,
+we push it into the stack
+Otherwise, we pop the elements from the stack until it's empty. 
+(Because a space or newline or null character marks the end of a word.)
 '''
-
 
 def reverseWords(string: str):
     ''' Prints the individual words in a reverse order '''

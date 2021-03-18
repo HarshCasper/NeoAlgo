507ae394fe27587749b50459fac8969bdcc00b77
Kanchi <kanchipardhi@gmail.com>
2021-03-18 14:10:04 +0530

    added find_duplictae_element.cpp file

---
+++ C-Plus-Plus/Bit--Manipulation/find_duplicate_element.cpp
@@ -0,0 +1,55 @@
+//finding duplicate element (twice ocurring)
+// Time Complexity - O(n^2)
+// Space Complexity - O(n^2)
+
+/*Sample Input:
+
+Example 1:- 
+7
+3 2 1 3 4 5 4 
+Example 2:-
+10
+1 5 6 4 5 4 1 2 3 2
+
+Sample Output:
+
+Example 1:- 
+Repeated Element in array = 3
+Repeated Element in array = 4
+Example 2:-
+Repeated Element in array = 1
+Repeated Element in array = 5
+Repeated Element in array = 4
+Repeated Element in array = 2
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    
+    cin>>n;  
+    int arr[n]  ;  
+    
+    for(int i=0;i<n ; i++){
+        cin>>arr[i];
+    }  
+
+    for (int i = 0; i < n; i++)  
+    {  
+        for (int j = i+1; j < n; j++)  
+        {  
+            int res = arr[i] ^ arr[j];  
+
+            if (res == 0)  
+            {  
+                cout<< "Repeated Element in array = "<<arr[i]<<endl;  
+            }  
+        }  
+    }  
+    return 0;  
+}  

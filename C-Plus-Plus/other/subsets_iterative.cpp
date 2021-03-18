9fe9c9f3efe28e68d8ac2f08e4f8f250e23ea3f7
Dhruv Panwar <60705641+dhruvinfo28@users.noreply.github.com>
2021-03-18 18:16:19 +0530

    Requested changes done
    Driver code in main
    Primary logic of code in some other function
    Multiline comments
    Indentation and code formatted

--- C-Plus-Plus/other/subsets_iterative.cpp
+++ C-Plus-Plus/other/subsets_iterative.cpp
@@ -1,24 +1,32 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int main(){
-    int n; cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    //Number of possible subsets are 2^n
-    //If n=2, then:
-    //00 01 10 11 , four sets
-    //00 Indicates that both 0 and 1 index are not set hence empty
-    //01 Indicating that first index is unset and zeroth index is set
-    for(int i=0;i<pow(2,n);i++){
+void generate_subsets(int arr[], int n) {
+    /*
+    Number of possible subsets are 2^n
+    If n=2, then:
+    00 01 10 11 , four sets
+    00 Indicates that both 0 and 1 index are not set hence empty
+    01 Indicating that first index is unset and zeroth index is set
+    */
+    for (int i = 0; i < pow(2, n); i++) {
         //For checking set bits in the binary pattern and then printing the corresponding element
-        for(int j=0;j<n;j++){
-            if(i & (1<<j)){
-                cout<<arr[j]<<" ";
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                cout << arr[j] << " ";
             }
-        }cout<<endl;
+        }
+        cout << endl;
     }
+}
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    generate_subsets(arr, n);
 }
 /*
 Input:

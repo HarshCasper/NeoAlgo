642ab0141530c414a4d48af0a52b4f6e53cc9a58
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-03-18 10:56:07 +0530

    Update celebrity_problem.cpp

--- C-Plus-Plus/graphs/celebrity_problem.cpp
+++ C-Plus-Plus/graphs/celebrity_problem.cpp
@@ -1,49 +1,55 @@
 // C++ program to solve the Celebrity Problem
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-
-int find_celebrity(int person[][9000], int n) {
+int find_celebrity(int person[][9000], int n)
+{
     int low = 0;
     int high = n - 1;
-    while(1) {
-        if (low == high) 
+    while (1)
+    {
+        if (low == high)
             break;
-        
-        if (person[low][high] == 1) 
+
+        if (person[low][high] == 1)
             low++;
-        
-        else 
+
+        else
             high--;
     }
-    for (int i = 0; i < n; i++) {
-        if (low == i) 
+    for (int i = 0; i < n; i++)
+    {
+        if (low == i)
             continue;
-        
-        if (person[low][i] == 1 or person[i][low] == 0) 
-            return -1;   
+
+        if (person[low][i] == 1 or person[i][low] == 0)
+            return -1;
     }
     return low;
 }
 
-
-int main() {
+int main()
+{
     int n;
-    cout<<"How many person are present in the party? ";
+    cout << "How many person are present in the party? ";
     cin >> n;
     int person[n][9000];
-    cout<<"Enter thier co-relation graph: ";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    cout << "Enter thier co-relation graph: ";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cin >> person[i][j];
         }
     }
     int celeb = find_celebrity(person, n);
-    if (celeb >= 0) {
-        cout<<"Celebrity is present at the "<<celeb<<"th index of the given co-relation graph";
+    if (celeb >= 0)
+    {
+        cout << "Celebrity is present at the " << celeb << "th index of the given co-relation graph";
     }
-    else {
-        cout<<"Celebrity is not present in the party.";
+    else
+    {
+        cout << "Celebrity is not present in the party.";
     }
 }
 

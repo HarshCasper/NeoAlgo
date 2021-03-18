7133f1dcf2aa0b094c4865490236fcae002f4bd1
Amanduttbhagat <bhagataman980@gmail.com>
2021-03-17 22:25:07 -0500

    Updated Code

--- Java/cp/Jump_Game.java
+++ Java/cp/Jump_Game.java
@@ -1,30 +1,29 @@
-/* Jump Game :
- * We need to find the minimum number of jumps to reach the end of the array starting from the first element of the array,
- * jumps value would be (-1) if we are not able to reach the end of the array
- *
- *
- * example : array = {2,3,0,1,4,1,1,2,0,3,5}
- *           minimum jumps = 5  {2 to 3, 3 to 4, 4 to 2, 2 to 3, 3 to 5}
- *
- *
- * Explanation : (Greedy Solution)
- *
- *  1. we will check if array's length is equal to or less than 1 or not
- *     i.e  if array's length is equal to 1 then we have only one element in the array and it is already at the end
- *          and program will return 0
- *
- *  2. Now, we will check if the first element of array is 0 or not,
- *     i.e if it is 0 then we can't make any jump and program will return -1
- *
- *  3. Now, we will use the for loop from i = 1 till array's length
- *     when (i == array.length - 1) this is true then we have reached the end and we will return jumps
- *
- *  4. i + array[i] is the number of steps we take from the current position and we will compare it with
- *     max_Jump_Reach and then we will use Math.max which will compare both and return the maximum and assigns
- *     it to the max_Jump_Reach again
- *
- *  5. we have used checks-- so that we can iterate the for loop till the checks == 0 and find the maximum
- *     value for max_Jump_Reach and afterwards we will update checks by (max_Jump_Reach - i)
+/*
+  Jump Game :
+  We need to find the minimum number of jumps to reach the end of the array starting from the first
+  element of the array, jumps value would be (-1) if we are not able to reach the end of the array
+
+  Example : array = {2,3,0,1,4,1,1,2,0,3,5}
+            minimum jumps = 5  {2 to 3, 3 to 4, 4 to 2, 2 to 3, 3 to 5}
+
+  Explanation : (Greedy Solution)
+
+   1. We will check if array's length is equal to or less than 1 or not
+          i.e  if array's length is equal to 1 then we have only one element in the
+          array and it is already at the end and program will return 0
+
+   2. Now, we will check if the first element of array is 0 or not,
+          i.e if it is 0 then we can't make any jump and program will return -1
+
+   3. Now, we will use the for loop from i = 1 till array's length when (i == array.length - 1)
+      this is true then we have reached the end and we will return jumps
+
+   4. (i + array[i]) is the number of steps we take from the current position and we will compare it with
+      max_Jump_Reach and then we will use Math.max which will compare both and return the maximum and assigns
+      it to the max_Jump_Reach again
+
+   5. We have used checks-- so that we can iterate the for loop till the checks == 0 and find the maximum
+      value for max_Jump_Reach and afterwards we will update checks by (max_Jump_Reach - i)
  */
 
 import java.util.Scanner;
@@ -100,21 +99,22 @@ public class JumpGame {
 	}
 }
 
-/* Test Cases :
- *  1.  Input : 7
- *              2 1 3 4 1 2 3
- *     Output : minimum number of Jumps required : 3
- *
- *  2.  Input : 4
- *              1 0 0 3
- *     Output : minimum number of Jumps required : -1
- *
- *  3.  Input : 1
- *              0
- *     Output : minimum number of Jumps required : 0
- *
- *
- * Time Complexity is O(n)
- *
- * Space Complexity is O(n)
- */
+/*
+  Test Cases :
+   1.  Input : 7
+               2 1 3 4 1 2 3
+      Output : minimum number of Jumps required : 3
+
+   2.  Input : 4
+               1 0 0 3
+      Output : minimum number of Jumps required : -1
+
+   3.  Input : 1
+               0
+      Output : minimum number of Jumps required : 0
+
+
+  Time Complexity is O(n)
+
+  Space Complexity is O(n)
+*/

5e8a406cd132938b8a1607a5d39523c2680828ad
DEEPIKA-SUK <64724039+DEEPIKA-SUK@users.noreply.github.com>
2021-03-19 19:28:36 +0530

    Quotient-Remainder Sort

--- C-Plus-Plus/sort/Quotient-Remainder_Sort.cpp
+++ C-Plus-Plus/sort/Quotient-Remainder_Sort.cpp
@@ -1,72 +1,83 @@
-/*Implementation of Quotient-Remainder Sorting algorithm (non-negative numbers only) in C++*/ 
+/*
+Implementation of Quotient-Remainder Sorting algorithm (non-negative numbers only) in C++
+*/
+
 #include <bits/stdc++.h>
+#include <vector>
+#include <list>
+#include <utility>
+
 using namespace std;
-#define ll long long int
 
 // Function for QR-Sort Algorithm implementation.
-vector < ll > func(vector < ll > a, ll n) {
-  vector < ll > ans;
-  /*Make a vector (v) of list of pair of type { arr[i] / n , arr[i]}.The index of vector represents remainder.*/
-  vector < list < pair < ll, ll >>> v(n);
-  for (ll i = 0; i < n; i++) {
-    ll r = a[i] % n;
-    v[r].push_back(make_pair(a[i] / n, a[i]));
-  }
-  /*Add delimiter {-1,-1} for each index in the vector*/
-  for (ll i = 0; i < n; i++) {
-    v[i].push_back(make_pair(-1, -1));
-  }
-  /*Run the loop until the size of output array is not equal to input array. */
-  while (ans.size() != n) {
-    for (ll i = 0; i < n; i++) {
-      while (!v[i].empty()) {
-        pair < ll, ll > p = v[i].front();
-        v[i].pop_front();
-        if (p.first == -1) {
-          /*Delimiter found break the loop*/
-          break;
-        } else {
-          if (p.first == 0) {
-            ans.push_back(p.second); /*Add the array element in the output array*/
-          } else {
-            v[p.first % n].push_back(make_pair(p.first / n, p.second));
-          }
+vector < int64_t > func(vector < int64_t > a, int64_t n) {
+    vector < int64_t > ans;
+    // Vector (v) of list of pair of type { arr[i] / n , arr[i]}.
+    //  The index of vector represents remainder.
+    vector < list < pair < int64_t, int64_t >>> v(n);
+    for (int64_t i = 0; i < n; i++) {
+        int64_t r = a[i] % n;
+        v[r].push_back(make_pair(a[i] / n, a[i]));
+    }
+    // Add delimiter {-1,-1} for each index in the vector
+    for (int64_t i = 0; i < n; i++) {
+        v[i].push_back(make_pair(-1, -1));
+    }
+    // Run the loop until the size of output array is not equal to input array.
+    while (ans.size() != n) {
+        for (int64_t i = 0; i < n; i++) {
+            while (!v[i].empty()) {
+                pair < int64_t, int64_t > p = v[i].front();
+                v[i].pop_front();
+                if (p.first == -1) {
+                    // Delimiter found break the loop
+                    break;
+                } else {
+                    if (p.first == 0) {
+                        // Add the array element in the output array
+                        ans.push_back(p.second);
+                    } else {
+                        int64_t r1 = p.first % n;
+                        int64_t q1 = p.first / n;
+                        v[r1].push_back(make_pair(q1, p.second));
+                    }
+                }
+            }
         }
-      }
+        // Adding delimiter for next pass.
+        for (int64_t i = 0; i < n; i++) {
+            v[i].push_back(make_pair(-1, -1));
+        }
     }
-    /* Adding delimiter for next pass.*/
-    for (ll i = 0; i < n; i++) {
-      v[i].push_back(make_pair(-1, -1));
-    }
-  }
-  return ans;
+    return ans;
 }
 
 int main() {
-  ll n;
-  cout << "Enter input size" << endl;
-  cin >> n;
-  vector < ll > a(n);
-  vector < ll > ans;
-  cout << "Enter input array:" << endl;
-  for (ll i = 0; i < n; i++) {
-    cin >> a[i];
-  }
-  cout << endl;
-  if (n != 1) {
-    ans = func(a, n);
-  } else {
-    ans.push_back(a[0]);
-  }
-  cout << "Output array:" << endl;
-  for (ll i = 0; i < n; i++) {
-    cout << ans[i] << " ";
-  }
-  cout << endl;
-  return 0;
+    int64_t n;
+    cout << "Enter input size" << "\n";
+    cin >> n;
+    vector < int64_t > a(n);
+    vector < int64_t > ans;
+    cout << "Enter input array:" << "\n";
+    for (int64_t i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << "\n";
+    if (n != 1) {
+        ans = func(a, n);
+    } else {
+        ans.push_back(a[0]);
+    }
+    cout << "Output array:" << "\n";
+    for (int64_t i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+    return 0;
 }
 
-/* Sample example:
+/*
+   Sample example:
    Enter input size:
    5
    Enter input array:
@@ -78,4 +89,5 @@ int main() {
          T(n)= { O(1) if n=1,
                  O(n*log(m base n) +n) if n>1
                }
-   Space Complexity = O(2*n) */
+   Space Complexity = O(2*n)
+*/

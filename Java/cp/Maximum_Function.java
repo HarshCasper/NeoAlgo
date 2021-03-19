c22e4cfdfd12cc95c2edc942569c1142faccb327
Amanduttbhagat <bhagataman980@gmail.com>
2021-03-19 06:38:10 -0500

    Updated Code

--- Java/cp/Maximum_Function.java
+++ Java/cp/Maximum_Function.java
@@ -1,16 +1,17 @@
 /*
-Maximum Function (in Java) :
-   A sequence is given A1,A2,…,An (atleast 3 elements you need to give)
-   Find the maximum value of the expression |Ax − Ay|+|Ay − Az|+|Az − Ax|
-   over all triples of pairwise distinct valid indices (x,y,z).
+ - Maximum Function (in Java) :
+     A sequence is given A1,A2,…,An (atleast 3 elements you need to give)
+     Find the maximum value of the expression |Ax − Ay|+|Ay − Az|+|Az − Ax|
+     over all triples of pairwise distinct valid indices (x,y,z).
 
-Approach :
-  1. We need to sort the given array
-  2. To find maximum difference of two elements we will subtract 1st element with the last element of the array (|Ax − Ay|)
-  3. Next, we can take middle element as 2nd element of the array (Az)
-  4. That means, Ax = 1st element of the sorted array, Ay = last element, Az = 2nd element
-  5. Therefore, adding |Ax − Ay|+|Ay − Az|+|Az − Ax| will give maximum result everytime
+ - Approach :
+     1. We need to sort the given array
+     2. To find maximum difference of two elements we will subtract 1st element with the last element of the array (|Ax − Ay|)
+     3. Next, we can take middle element as 2nd element of the array (Az)
+     4. That means, Ax = 1st element of the sorted array, Ay = last element, Az = 2nd element
+     5. Therefore, adding |Ax − Ay|+|Ay − Az|+|Az − Ax| will give maximum result everytime
  */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -38,14 +39,14 @@ class MaximumFunction {
 }
 /*
  - Test Cases :
-   1.  Input : 10
-               7 5 3 9 1 2 7 3 8 9
-      Output : Result = 16
+    1.  Input : 10
+                7 5 3 9 1 2 7 3 8 9
+       Output : Result = 16
 
  - Complexity Analysis :
- * Time Complexity : O(n)
-       We are using only one for loop, so it iterates maximum 'n' times while traversing for loop
+    * Time Complexity : O(n)
+         We are using only one for loop, so it iterates maximum 'n' times while traversing for loop
 
- * Space Complexity : O(n)
-       Auxiliary space : O(1)  +  input space : O(n)
+    * Space Complexity : O(n)
+         Auxiliary space : O(1)  +  input space : O(n)
  */

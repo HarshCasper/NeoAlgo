a02199716f69aecee5cf6adfc94281291cf80f83
Kanchi <kanchipardhi@gmail.com>
2021-03-19 22:54:54 +0530

    cpp code

--- C-Plus-Plus/Bit--Manipulation/Find_duplicate_elements.cpp
+++ C-Plus-Plus/Bit--Manipulation/Find_duplicate_elements.cpp
@@ -1,60 +1,54 @@
-//finding duplicate elements 
-/*elements occuring twice in array is considered duplicate
-here and rest occur only once
+//finding duplicate elements
+/*Some integral elements are taken in input, which may occur once or twice ,the repeated 
+element is considered as duplicate and this code prints the repeated elements in output
 */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
     int n;
-    cin>>n; 
+    cout << "enter total number of integral elements" << endl;
+    cin >> n;
 
-    int arr[n] ;  
+    int arr[n];
+    cout << "enter the integral elements" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-    for(int i=0;i<n ; i++){
-    cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int x = arr[i] ^ arr[j];
+            if (x == 0)
+            {
+                cout << "Repeated Element = " << arr[i] << endl;
+            }
+        }
+    }
+
+    return 0;
 }
-
-        for (int i = 0; i < n; i++){ 
-    
-            for (int j = i+1; j < n; j++){  
-
-                int x = arr[i] ^ arr[j];  
-
-                if (x == 0){  
-                cout<< "Duplicate Element in array = "<<arr[i]<<endl;  
-                }
-
-            } 
-
-        }   
-return 0;  
-}  
-
-
-// Time Complexity - O(n^2)
-// Space Complexity - O(n^2)
 
 /*Sample Input:
 
-Example 1:- 
-7
-3 2 1 3 4 5 4 
-Example 2:-
+enter total number of integral elements
 10
+enter the integral elements
 1 5 6 4 5 4 1 2 3 2
 
 Sample Output:
 
-Example 1:- 
-Repeated Element in array = 3
-Repeated Element in array = 4
-Example 2:-
-Repeated Element in array = 1
-Repeated Element in array = 5
-Repeated Element in array = 4
-Repeated Element in array = 2
+Repeated Element  = 1
+Repeated Element  = 5
+Repeated Element  = 4
+Repeated Element  = 2
 
 */
+
+// Time Complexity - O(n^2)
+// Space Complexity - O(n^2)

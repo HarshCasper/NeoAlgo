897d9f9f5550d830afcc991dad52d89166e1085c
AkanshaUniyal <78859329+AkanshaUniyal@users.noreply.github.com>
2021-03-19 13:07:02 +0530

    Update tribonacci.java

--- Java/math/tribonacci.java
+++ Java/math/tribonacci.java
@@ -6,9 +6,9 @@
 
 /*    Approach:
 
-   A term is the sum of preceding three terms in this series. The sum of three preceding variables will be stored in "sum" variable.
-   The first of the three variable terms will be overrided by second variable term which in turn will be overrided by third one and the third term will be 
-   overrided by the sum. This process will continue till n terms.  */ 
+   The sum of three preceding variables will be stored in "sum" variable.
+   Variable "first" will be overrided by "second" which in turn will be overrided by "third" which will be overrided by "sum".
+   This process will continue till n terms.  */ 
 
  import java.util.Scanner;
  public class tribonacci 
@@ -17,27 +17,32 @@
     { 
 
         Scanner scan = new Scanner(System.in);
-        long num;
-	long first=0,second=0,third=1,sum=0,count=0;
+	    
+	long num,first=0,second=0,third=1,sum=0,count=0;
+	    
         System.out.println("Enter the number upto which the series is to be printed");
         num=scan.nextLong();
 
         while(num>0)	
         {
             count++;
+		
             if(count==1)	
-            System.out.print(0);
+            	System.out.print(0);
             else if(count==2)
-            System.out.print(", "+0);
+           	 System.out.print(", "+0);
             else if(count==3)
-            System.out.print(", "+1);
+            	System.out.print(", "+1);
             else    //Applicable to all the terms other than the first three terms of the series.
             {
-            sum=first+second+third;	
-            System.out.print(", "+sum);
-            first=second;second=third;third=sum;  //Storing the values of succeeding terms in respective variables.
+            	sum=first+second+third;	
+            		System.out.print(", "+sum);
+		    //Storing the values of succeeding terms in respective variables.
+            	first=second;
+		second=third;
+		third=sum;  
             }
-         num--;    //Decreasing the value of total number of terms by one after each iteration.
+             num--;    //Decreasing the value of total number of terms by one after each iteration.
         }
     }
  }
@@ -49,12 +54,6 @@
  Enter the number upto which the series is to be printed
  23
  0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, 121415
-
- Test Case 2:
-
- Enter the number upto which the series is to be printed
- 5
- 0, 0, 1, 1, 2
 
  Space Complexity: O(1)
  Time Complexity: O(n)

3d07ee366fbd9818143f26e1207e37c2e25082e1
Ritika Verma <verma.ansi22@gmail.com>
2021-03-19 15:31:03 +0530

    Added string Concatenation

---
+++ C-Plus-Plus/other/n_string_concatenate.cpp
@@ -0,0 +1,43 @@
+/*
+String Concatenation 
+Problem Statement : Take n string from user and concatenate them together.
+Input Format : First line takes number of strings to be concatenated.
+               Next n lines contains the strings to be concatenated.
+Output Format : Print a single line that contains the concatenated string.
+Note : No space is there in between a single string.
+Sample Test Case :
+Input : 4
+        "This"
+        "is"
+        "Concatenated"
+        "String."
+Output : "ThisisConcatenatedString."
+Approach : Take n strings as input using loop.
+           Take a string variable and update it using append() function each time a new sting is entered.
+           Print the string.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    //n stores the no. of string
+	int n; 
+    //takes inut from the user
+	cin>>n; 
+    //f_str contains the final string  
+	string f_str="";
+    //loop for taking n strings from user and updating the final string 
+	for(int i=0;i<n;i++)
+    {
+    //variable to take string as input
+    string a;
+    cin>>a;
+    //f_str.append() appends the string each time a new string is entered 
+    f_str.append(a);
+    }
+    //prints the final string
+    cout<<f_str;
+	return 0;
+}

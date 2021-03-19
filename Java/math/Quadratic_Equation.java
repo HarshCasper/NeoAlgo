93f8f95c50a4a799d09c8f500270e00fbf475256
nagasaisriya <nagasaisriya@gmail.com>
2021-03-20 00:24:53 +0530

    Resolved changes_in_quadratic

--- Java/math/Quadratic_Equation.java
+++ Java/math/Quadratic_Equation.java
@@ -1,7 +1,5 @@
-
-/*
-Problem:
-Solving Quadratic equations in Java-
+/* 
+Problem: Solving Quadratic equations in Java-
 A program that prints all real solutions to quadratic equation ax^2+bx+c=0, if discriminant is negative displays a message "roots are imaginary". 
 */
 import java.util.*;
@@ -24,52 +22,37 @@ public class Quadratic_Equation {
     System.out.print("Enter c: ");
     c = sc.nextDouble();
     System.out.println();
-    // If a is 0, then equation is not quadratic, but
-    // linear
+    // If a is 0, then equation is not quadratic, but linear
     if (a == 0) {
-      System.out.println("Its a linear equation,Not a quadratic equation!");
-      return;
-    }
+        System.out.println("Its a linear equation,Not a quadratic equation!");
+        return;
+      }
     // calculate the discriminant (b^2 - 4ac)
     double discriminant = b * b - 4 * a * c;
-
-    // check if discriminant is greater than 0
+    // discriminant is greater than 0, real and distinct roots
     if (discriminant > 0) {
-
-      // two real and distinct roots
-      root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-      root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-      System.out.println("Roots are real and distinct");
-      System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
-    }
-
-    // check if determinant is equal to 0
+        root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        System.out.println("Roots are real and distinct");
+        System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+      }
+    // discriminant is equal to 0, real and equal roots
     else if (discriminant == 0) {
-
-      // two real and equal roots
-      // discriminant is equal to 0
-
-      root1 = root2 = -b / (2 * a);
-      System.out.println("Equal roots");
-      System.out.format("root1 = root2 = %.2f;", root1);
-    }
-
-    // if discriminant is less than zero
+        root1 = root2 = -b / (2 * a);
+        System.out.println("Equal roots");
+        System.out.format("root1 = root2 = %.2f;", root1);
+      }
+    // discriminant is less than zero, imaginary roots
     else {
-
-      // roots are complex number
-      double real = -b / (2 * a);
-      double imaginary = Math.sqrt(-discriminant) / (2 * a);
-      System.out.println("Imaginary roots!");
-      System.out.format("root1 = %.2f+%.2fi", real, imaginary);
-      System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
-    }
-
+        double real = -b / (2 * a);
+        double imaginary = Math.sqrt(-discriminant) / (2 * a);
+        System.out.println("Imaginary roots!");
+        System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+        System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
+      }
   }
-
   public static void main(String[] args) {
     quadraticroots();
-
   }
 }
 
@@ -110,4 +93,6 @@ Enter c: 1
 Equal roots
 root1 = root2 = 1.00;
 
+Time Complexity: O(1)
+Space Complexity: O(1)
 */

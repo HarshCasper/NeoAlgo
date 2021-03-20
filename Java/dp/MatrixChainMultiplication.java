73c15e7e3ba8972606cbe066dd63a671b886a142
Shreyash Lata <shreyeslata@gmail.com>
2021-03-21 00:48:30 +0530

    Add Matrix Chain Multiplication with changes2

--- Java/dp/MatrixChainMultiplication.java
+++ Java/dp/MatrixChainMultiplication.java
@@ -16,18 +16,21 @@ public class MatrixChainMultiplication {
 		if (j <= i + 1) {
 			return 0;
 		}
-		// minMult : stores the minimum number of scalar multiplications (i.e., cost)
-		// needed to compute matrix `M[i+1] … M[j] = M[i…j]`
-		int minMult = Integer.MAX_VALUE;
-		// take the minimum over each possible position at which the
-		// sequence of matrices can be split
 		/*
+     minMult : stores the minimum number of scalar multiplications (i.e., cost)
+		 needed to compute matrix `M[i+1] … M[j] = M[i…j]`
+    */
+		int minMult = Integer.MAX_VALUE;
+		/*
+       take the minimum over each possible position at which the
+		   sequence of matrices can be split
+
             (M[i+1]) × (M[i+2]………………M[j])
             (M[i+1]M[i+2]) × (M[i+3…………M[j])
             …
             …
             (M[i+1]M[i+2]…………M[j-1]) × (M[j])
-         */
+    */
 		for (int k = i + 1; k <= j - 1; k++) {
 			int cost = matrixChain(dimension, i, k);
 			cost += matrixChain(dimension, k, j);
@@ -45,10 +48,12 @@ public class MatrixChainMultiplication {
 		int n;
 		Scanner sc = new Scanner(System. in );
 		System.out.print("Enter the number of matrices you want to multiply: ");
-		// Matrix `M[i]` has dimension `dimension[i-1] × dimension[i]` for `i = 1…n`
-		// Storing value 1 more than the entered number as dimensions
-		// As dimensions of 3 matrices are  `10 × 30` matrix, `30 × 5` matrix, `5 × 60` matrix
-		// in case of int[] dimension = { 10, 30, 5, 60 };
+		/*
+       Matrix `M[i]` has dimension `dimension[i-1] × dimension[i]` for `i = 1…n`
+		   Storing value 1 more than the entered number as dimensions
+		   As dimensions of 3 matrices are  `10 × 30` matrix, `30 × 5` matrix, `5 × 60` matrix
+		   in case of int[] dimension = { 10, 30, 5, 60 };
+    */
 		n = sc.nextInt();
 		int[] dimension = new int[n + 1];
 		System.out.print("Enter the dimensions in a continuous non repeating form (for 'n' matrices 'n+1' dimensions should be entered): ");
@@ -72,8 +77,8 @@ public class MatrixChainMultiplication {
                         Let the input 4 matrices be A, B, C and D.  The minimum number of
                         multiplications are obtained by putting parenthesis in following way
                         ((AB)C)D --> 10*20*30 + 10*30*40 + 10*40*30
-*/
-/*  Time and space complexity for this approach:
+
+    Time and space complexity for this approach:
     Time Complexity: O(n^3)
     Auxiliary Space: O(n^2)
 */

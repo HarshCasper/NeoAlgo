6067f4c84a57b3839e4721a8d4443d9a2dbfea7c
Mohim Singla <69247094+Mohim-Singla@users.noreply.github.com>
2021-03-21 21:45:34 +0530

    Made program with user inputs

--- C-Plus-Plus/ds/Check_for_Binary_search_tree.cpp
+++ C-Plus-Plus/ds/Check_for_Binary_search_tree.cpp
@@ -19,42 +19,42 @@ node* lchild;
 node* rchild;
 };
 //Function to create tree
-node* create_node(int data)
+node* create_node (int data)
 {
-    node* temp=new node;
-    temp->data=data;
-    temp->lchild=temp->rchild=NULL;
+    node* temp = new node;
+    temp -> data = data;
+    temp -> lchild = temp -> rchild = NULL;
     return temp;
 }
 
-node* create(node* root, int data)
+node* create (node* root, int data)
 {
-    if(root==NULL)
+    if(root == NULL)
     {
-        root = create_node(data);
+        root = create_node (data);
         return root;
     }
     else
     {
-        if (data<root->data)
+        if (data < root->data)
         {
-            root->lchild = create(root->lchild,data);
+            root -> lchild = create (root -> lchild, data);
         }
         else
         {
-            root->rchild=create(root->rchild, data);
+            root -> rchild = create (root -> rchild, data);
         }
         return root;
     }
 }
 //BST Check Function
-bool check(node* root, int minv, int maxv)
+bool check (node* root, int minv, int maxv)
 {
-    if(root==NULL)
+    if (root == NULL)
     {
         return true;
     }
-    else if (root->data>minv && root->data<maxv && check(root->lchild, minv, root->data) && check(root->rchild, root->data, maxv))
+    else if (root -> data > minv && root -> data < maxv && check (root -> lchild, minv, root -> data) && check (root -> rchild, root -> data, maxv))
     {
         return true;
     }
@@ -65,43 +65,38 @@ bool check(node* root, int minv, int maxv)
 }
 
 //Displaying output
-void check_output(node* root, int minv, int maxv)
+void check_output (node* root, int minv, int maxv)
 {
-    bool ans=check(root,INT_MIN,INT_MAX);
-    if(ans)
+    bool ans = check (root, INT_MIN, INT_MAX);
+    if (ans)
     {
-        cout<<"True"<<endl;
+        cout << "True" << endl;
     }
     else
     {
-        cout<<"False"<<endl;
+        cout << "False" << endl;
     }
 }
 
-int main()
+int main ()
 {
     //creating a BST
-    node* root=NULL;
-    root=create(root, 16);
-    root=create(root, 4);
-    root=create(root, 10);
-    root=create(root, 20);
-    root=create(root, 11);
-    root=create(root, 25);
-    root=create(root, 8);
-    root=create(root, 6);
-    root=create(root, 14);
-    root=create(root, 2);
-    root=create(root, 21);
-    root=create(root, 31);
-    root=create(root, 7);
-    root=create(root, 32);
+    node* root = NULL;
+    int counts;
+    cout << "Enter number of nodes: " ;
+    cin >> counts;
+    for (int i = 0; i < counts; i++)
+    {
+        int datas;
+        cin >> datas;
+        root = create (root, datas);
+    }
 
-    check_output(root,INT_MIN,INT_MAX);
+    check_output (root, INT_MIN, INT_MAX);
 
     //converting bst to binary tree
-    root->lchild->data=18;
-    check_output(root,INT_MIN,INT_MAX);
+    root -> lchild -> data = 18;
+    check_output (root, INT_MIN, INT_MAX);
     return 0;
 }
 
@@ -110,7 +105,8 @@ int main()
 ---------------------------
 Test case:
 Input:
-A BST
+10
+1 2 34 4 5 56 7 18 9 23
 A non-BST
 
 Output:

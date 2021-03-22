df87e0716974ccce13fb589d2783e2a7072d2da3
sakshi(P) <66208261+Sakshi1007@users.noreply.github.com>
2021-03-23 00:21:50 +0530

    Requested changes are made.
    @HarshCasper plz review.

--- C-Plus-Plus/cp/Space_Array.cpp
+++ C-Plus-Plus/cp/Space_Array.cpp
@@ -1,6 +1,8 @@
+/* PROBLEM LINK: https://www.codechef.com/MARCH21C/problems/SPACEARR
+*/
+
 /*
 The rules of the game are as follows:
-
 1-Initially, a sequence a1,a2,…,aN is given.
 2-The players alternate turns.
 3-In each turn, the current player must choose
@@ -9,25 +11,6 @@ an index i and increment ai, i.e. change ai to ai+1.
 integers 1 through N such that ai=pi holds for each valid
  i, the current player loses.
 5-Otherwise, the game continues with the next turn.
-*/
-
-/*
-Input:
-The first line of the input contains a single integer T denoting the number of test cases.
-The description of T test cases follows.
-The first line of each test case contains a single integer N.
-The second line contains N space-separated integers a1,a2,…,aN.
-Output:
-For each test case, print a single line containing the string "First"
-if the first player wins or "Second"
-if the second player wins (without quotes).*/
-
-/*
-Constraints
-1=T=2·104
-1=N=2·105
-The sum of N over all test cases doesn't exceed 2·105
-1=ai=N for each valid i
 */
 
 // Code
@@ -47,13 +30,13 @@ int main() {
 		int sum = 0, c = 0;
 		for (int i = 0; i < n; i++) {
 			if (arr[i] > (i + 1)) {           //Cheking if index is graeter or less than element of array  at that position
-				c = 1;
+				c = 1;                    // true, increase value of c  i.e. count
 				break;
 			}
-			sum += abs(arr[i] - (i + 1));
+			sum += abs(arr[i] - (i + 1));    //if not,take the sum of difference of index and array at that index.
 		}
-		if (c == 1)
-			cout << "Second" << endl;
+		if (c == 1)                             //c is 1 i.e. arrayy is greater than element of array
+			cout << "Second" << endl;        //second player wins.
 		else {
 			if (sum % 2 == 0)                  // if sum is even.
 				cout << "Second" << endl;      // second is winner.
@@ -82,16 +65,8 @@ Second
 Second
 */
 
-/*
-Explanation:
-if array at that index is greater than the array then Second Player will be the winner.
-if not the we will take the sum of difference of index and array at that index.
-sum will be odd then first will win otherwise second will win
-*/
-
 /*Time complexity: 0.06 sec using above algo, "MAX time complexity can be 1 sec "
 Memory used: 6.5 MB uisng above algo, MAx space complexity for his problem is: 50000 Bytes"
 */
 
-/* PROBLEM LINK: https://www.codechef.com/MARCH21C/problems/SPACEARR
-*/
+

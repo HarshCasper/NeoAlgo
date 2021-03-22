d3101c80ff2daddd9817ee5d53b17bf1945accbc
keshav2508 <60262928+keshav2508@users.noreply.github.com>
2021-03-22 09:27:13 +0530

    Update Binary_Insertion_Sort.cpp

--- C-Plus-Plus/sort/Binary_Insertion_Sort.cpp
+++ C-Plus-Plus/sort/Binary_Insertion_Sort.cpp
@@ -7,37 +7,7 @@ We can reduce it to O(log n) by using binary search.
 
 #include<bits/stdc++.h>
 using namespace std;
-#define int               long long int
-#define ff                first
-#define ss                second
-#define pb                push_back
-#define mp                make_pair
-#define pii               pair<int,int>
-#define vi                vector<int>
-#define vvi               vector<vector<int>>
-#define mii               map<int,int>
-#define pqb               priority_queue<int>
-#define pqs               priority_queue<int,vi,greater<int> >
-#define mod               1000000007
-#define inf               1e18
-#define ps(x,y)           fixed<<setprecision(y)<<x
-#define heap(arr,n,type)  type *arr=new type[n];
-#define w(x)              int x; cin>>x; while(x--)
-#define endl              '\n'
-#define fio               ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define rep(var,s,e)      for(int var=s;var<e;var++)
-#define repc(var,s,e)      for(int var=s;var<=e;var++)
-#define setbits(x)        __builtin_popcountll(x)
-#define numbits(x)        (64 - __builtin_clzll(x))
-#define zerobit(x)        __builtin_ctzll(x)
-#define log2(x)           (63 - __builtin_clzll(x))
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-int lcm(int a, int b) { return a * (b / gcd(a, b)); }
 
-bool comp(pair<pii,pii> x, pair<pii,pii> y){
-    if(x.ff.ff<y.ff.ff) return true;
-    else return false;
-}
 //searching element in array a by binary search algoritm
 int binarySearch(int a[], int element, int low, int high)
 {
@@ -78,15 +48,18 @@ void insertionSort(int arr[], int n)
  
 signed main()
 {
-    //Let's take this unsorted array
-    int arr[] = { 35, 23, 12, 17, 12, 72, 31, 46, 180, 88, 54 };
-    int n = sizeof(a) / sizeof(a[0]), i;
+    //Taking Input
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
  
     //Applying Sort
     //calling sort function
     insertionSort(a, n);
     cout<<"Sorted Array is :-";
-    for (i = 0; i < n; i++){ 
+    for (int i = 0; i < n; i++){ 
         cout<<arr[i]<<" ";
     }
     return 0;

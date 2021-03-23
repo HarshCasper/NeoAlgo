36bb5e09154257d49b19b0533a526c7cb78a8ef0
Tarun Kamboj <kambojtarun02@gmail.com>
2021-03-23 17:46:37 +0530

    Next Prime Number Modified

--- Python/math/Next_Prime_Number.py
+++ Python/math/Next_Prime_Number.py
@@ -1,25 +1,33 @@
 '''
 Introduction 
 
-The code below calculates and returns the next prime number when user inputs any number.
+The code below calculates and returns the next prime number from the number given by user.
+
+Time Complexity : O(n²)
+Space Complexity : O(1)
+
 '''
 
-# taking input from user
-n = int(input("Enter a number between 0 and 1000000 : "))
+def next_prime(n):
+	# looping from next number to find if it prime or not
+	x = n+1
+	while True:
+		flag = False
+		# checking whether x is divisible by numbers from 2 to it's half
+		for y in range(2,x//2+1):
+			if x%y==0:
+				x+=1
+				flag = True
+				break
+		if flag==False:
+			return(x)
 
-# looping from next number to find if it prime or not
-for x in range(n+1,9999999):
-	# checking whether x is divisible by numbers from 2 to it's half
-    for y in range(2,x//2+1):
-        if x%y==0:
-            break  # break if x is divisble cause it will not be a prime number then
-    else:
-        print("Next prime number is ",x) # if x is not divisible then printing x
-        break  # break cause we found the result in form of x
+n = int(input("Enter a number : "))
+print('Next prime number is ', next_prime(n))
 
 '''
 Sample output
 
->>>Enter a number between 0 and 1000000 : 17
-Next prime number is 19
+Enter a number : 23
+Next prime number is  29
 '''

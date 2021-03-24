75d5d5b485bace965792796b13818b357499340f
Naga srihith Penjarla <57094227+nagasrihith608@users.noreply.github.com>
2021-03-24 12:03:38 +0530

    Formatted the file with few more comments

--- C/cryptography/MonoAlphabeticCipher.c
+++ C/cryptography/MonoAlphabeticCipher.c
@@ -1,6 +1,6 @@
 /* Monoalphabetic substitution cipher basically replaces each letter of a plaintext by another letter or symbol, depending only on the letter.
    The same plaintext letter is always replaced by the same ciphertext letter and the same letter can be replaced by letter itself.
-   Example if a is there we replace it with B to Z .....so on.*/
+   Example if a is there we replace it with B to Z and so on for the others.*/
 
 #include<stdio.h>
 #include<string.h>
@@ -8,11 +8,14 @@
 void monoalphebaticcipher(char input[100],int size){
      
      //this where we store output 
-     char output[size-1];    
+     char output[size-1]; 
+     //this is the list which we use to cipher 
      char cipherer[26] = {'Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'};     
      //the list we use to check which alphebatic we recieved in plain text
      char list[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};  
+     //we use this char to compare the space charecter
      char space = ' ';
+     //this is where we iterate and check what alphabetic is in input and then replace ii with its corresponding cipher
      for(int i=0;i<size-1;i++){     
          for(int j=0;j<26;j++){
              if(input[i]==list[j])
@@ -30,18 +33,17 @@ void monoalphebaticcipher(char input[100],int size){
 
 int main(void){
 
-     //we take input here
+     //we store input here
      char input[100];
      printf("Give the plain text here in small letters :");
+     //we take input here
      fgets(input,100,stdin);
      int size = strlen(input);
-     //we store the cipher text here
      monoalphebaticcipher(input,size);
      return 0;
 }
 /*
   Sample I/O :
-  
   Give the plain text here in small letters :hello world
   The Cipher text :SVOOL DLIOW
 */

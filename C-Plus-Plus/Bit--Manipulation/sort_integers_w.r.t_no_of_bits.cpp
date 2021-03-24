84b967230c66aa4e0597d11104b9919663b2cfcf
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-03-24 10:38:35 +0530

    Create sort_integers_w.r.t_no_of_bits.cpp

---
+++ C-Plus-Plus/Bit--Manipulation/sort_integers_w.r.t_no_of_bits.cpp
@@ -0,0 +1,70 @@
+// C++ program to sort integers w.r.t the number of set bits in its binary equivalence
+#include <bits/stdc++.h>
+using namespace std;
+
+/* We modify the compare method to sort the numbers w.r.t thier number of set bits */
+static bool compare(const int &a, const int &b)
+{
+    /* count stores the number of set bits
+        and temp variables hold the values of a and b temporarily */
+    int count_a_1 = 0, count_b_1 = 0, temp_a = a, temp_b = b;
+    // Count number of set bits in a
+    while (temp_a)
+    {
+        temp_a = temp_a & (temp_a - 1);
+        count_a_1++;
+    }
+    // Count number of set bits in b
+    while (temp_b)
+    {
+        temp_b = temp_b & (temp_b - 1);
+        count_b_1++;
+    }
+
+    // If thier counts are equal, check thier magnitude.
+    if (count_a_1 == count_b_1)
+        return a < b;
+    return count_a_1 < count_b_1;
+}
+
+int main()
+{
+    int n, ele;
+    cout << "How many numbers do you want to sort w.r.t the number of set bits? ";
+    cin >> n;
+    if (n == 0)
+    {
+        cout << "There are no numbers to sort!!";
+        return 0;
+    }
+    vector<int> arr;
+    cout << "Enter the numbers:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ele;
+        arr.push_back(ele);
+    }
+    // Call the sort method, with our modified compare method
+    sort(arr.begin(), arr.end(), compare);
+    cout << "\nThe sorted array is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+/*
+
+Time Complexity: O(logn)
+Space Complexity: O(n)
+
+SAMPLE INPUT AND OUTPUT
+
+How many numbers do you want to sort w.r.t the number of set bits? 5
+Enter the numbers: 13 2 9 34 22 
+
+The sorted array is: 2 9 34 13 22  
+
+*/

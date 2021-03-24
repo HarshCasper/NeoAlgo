e689387153c8403fe25417e34bcf53e47562b89f
Anish Kumar Singh <anishsingh935>
2021-03-24 11:54:41 +0530

    made some changes

--- C-Plus-Plus/stack/Check_for_balanced_parenthesis.cpp
+++ C-Plus-Plus/stack/Check_for_balanced_parenthesis.cpp
@@ -8,53 +8,55 @@ Author: @gargvader
 Edit by: @Mohim-Singla
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
- 
+
 // function to check if brackets are balanced
 bool areBracketsBalanced(string expr)
-{ 
+{
     stack<char> s;
     char x;
- 
+
     // Traversing the Expression
     for (int i = 0; i < expr.length(); i++)
     {
-        if (expr[i] == '(' || expr[i] == '['
-            || expr[i] == '{')
+        if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{')
         {
             // Push the element in the stack
             s.push(expr[i]);
             continue;
         }
- 
+
         // IF current current character is not opening
         // bracket, then it must be closing. So stack
         // cannot be empty at this point.
         if (s.empty())
             return false;
- 
-        switch (expr[i]) {
+
+        switch (expr[i])
+        {
         case ')':
-             
+
             // Store the top element in a
             x = s.top();
             s.pop();
             if (x == '{' || x == '[')
                 return false;
             break;
- 
+
         case '}':
- 
+
             // Store the top element in b
             x = s.top();
             s.pop();
             if (x == '(' || x == '[')
                 return false;
             break;
- 
+
         case ']':
- 
+
             // Store the top element in c
             x = s.top();
             s.pop();
@@ -63,18 +65,18 @@ bool areBracketsBalanced(string expr)
             break;
         }
     }
- 
+
     // Check Empty Stack
     return (s.empty());
 }
- 
+
 // Driver code
 int main()
 {
     string expr;
-	cout<<"Enter a expression "<<endl;
-	cin>>expr;
- 
+    cout << "Enter a expression " << endl;
+    cin >> expr;
+
     // Function call
     if (areBracketsBalanced(expr))
         cout << "Balanced Parenthesis";

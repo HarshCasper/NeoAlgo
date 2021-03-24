9f924a089ea0d942e4fba6442bbf68ba9d130a89
Bhawana001 <bhawana.ece18@itbhu.ac.in>
2021-03-24 21:03:31 +0530

    Fixed

--- C-Plus-Plus/stack/Sort_stack_using_recursion.cpp
+++ C-Plus-Plus/stack/Sort_stack_using_recursion.cpp
@@ -3,109 +3,88 @@ Problem Statement:
  Given a stack, sort the given stack using recursion such that the greatest element is on the top. 
 */
 
-/*
-Input:
-
-The first line of input contains a single integer n--number of the element in the stack. 
-The second line contains n space-separated integers: ith integer represent ith stack element.
-
-Output:
-
-Print sorted stack.
-*/
-
-/*
-For example-
-
-Input: n=4
-       3 2 4 1
-Output: 4 3 2 1      
-
-*/
-
-#include<iostream>
-#include<algorithm>
-#include<stack>
+#include <iostream>
+#include <algorithm>
+#include <stack>
 
 using namespace std;
 
 void printStack(stack<int> st)
 {
-    while(!st.empty()) 
-    {
-        int x=st.top();
-         st.pop();
-         
-         cout<<x<<" ";
-    }
-    
-      return;
+  while (!st.empty())
+  {
+    int x = st.top();
+    st.pop();
+
+    cout << x << " ";
+  }
+
+  return;
 }
 
 void insert(stack<int> &st, int temp)
 {
-    // base condition
-    if(st.size()==0 || st.top()<temp)
-      {  
-          st.push(temp); 
-          return;
-      }
-     
-     int val= st.top();
-     st.pop();
-     
-     insert(st,temp);
-     st.push(val);
-     
-     return;
-}
+  // base condition
+  if (st.size() == 0 || st.top() < temp)
+  {
+    st.push(temp);
+    return;
+  }
 
+  int val = st.top();
+  st.pop();
+
+  insert(st, temp);
+  st.push(val);
+
+  return;
+}
 
 void sortStack(stack<int> &st)
 {
-    
-    // base condition
-    if(st.size()==0)
-     return ;
-   
-   int temp=st.top();
-     st.pop();
-   
-   // recursion
-   sortStack(st);
-   
-   // function to insert element in stack
-    insert(st,temp);
-   
-   return ;
+
+  // base condition
+  if (st.size() == 0)
+    return;
+
+  int temp = st.top();
+  st.pop();
+
+  // recursion
+  sortStack(st);
+
+  // function to insert element in stack
+  insert(st, temp);
+
+  return;
 }
 
 int main()
 {
-    // input number of element in stack
-    int n;
-    cin>>n;
-    
-    stack<int> st;
-    
-    // input stack elements
-    for(int i=0;i<n;i++)
-    {
-        int x;
-        cin>>x;
-        
-        st.push(x);
-    }
-    
-    // function to sort stack
-     sortStack(st);
-     
-    // function to print stack element
-     printStack(st);
-     
-    cout<<endl;
+  // input number of element in stack
+  int n;
+  cin >> n;
 
-    return 0;
+  stack<int> st;
+
+  // input stack elements
+  for (int i = 0; i < n; i++)
+  {
+    int x;
+    cin >> x;
+
+    st.push(x);
+  }
+
+  // function to sort stack
+  sortStack(st);
+
+  // function to print stack element
+  printStack(st);
+
+  cout << endl;
+
+  return 0;
 }
 
 /*
@@ -126,4 +105,3 @@ int main()
   Output: 41 32 11 3 2              
 
 */
-

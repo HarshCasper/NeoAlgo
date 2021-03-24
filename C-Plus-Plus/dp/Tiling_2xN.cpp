94629a5ffc3169f9a368ab41d849a58ea3a58d34
Aditya Anand <anand.aditya0803@gmail.com>
2021-03-24 23:45:06 +0530

    Updating Requested Changes

--- C-Plus-Plus/dp/Tiling_2xN.cpp
+++ C-Plus-Plus/dp/Tiling_2xN.cpp
@@ -4,38 +4,36 @@
     A tile can either be placed horizontally i.e., as a 1 x 2 tile or vertically
    i.e., as 2 x 1 tile. */
 
-/* Algorithm =>
-    Approach used : Dynamic Programming
-    Test cases handled explicitly:
-    Case 1: if n == 1 , only 1 configuration of tile is possible
-    Case 2: if n == 2 , only 2 configurations are possible
-    Case 3: if n > 2, then we got 2 possible subcases:
-        Subcase 1: if we place length wise, call dp[i-2];
-        Subcase 2: if we place width wise, call dp[i-1];
-    at last return, dp[n] for number of ways. */
-
 #include <iostream>
 #include <climits>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-long long numberOfWays(long long n){
-    int N = 1000000007;
-    vector<long long> dp(n + 1, 0);
+long long numberOfWays(long long length){
+    int mod = 1000000007;
+    vector<long long> dp(length + 1, 0);
+
+    // Case 1: if n == 1 , only 1 configuration of tile is possible
     dp[1] = 1;
+
+    // Case 2: if n == 2 , only 2 configurations are possible
     dp[2] = 2;
-    for (int i = 3; i <= n; i++){
-        dp[i] = ((dp[i - 2] % N) + (dp[i - 1] % N) % N);
+    
+    /* Case 3: if n > 2, then we got 2 possible subcases:
+        Subcase 1: if we place length wise, call dp[i-2];
+        Subcase 2: if we place width wise, call dp[i-1];  */
+    for (int i = 3; i <= length; i++){
+        dp[i] = ((dp[i - 2] % mod) + (dp[i - 1] % mod)) % mod;
     }
-    return dp[n] % N;
+    return dp[length];
 }
 
 int main(){
-    long long n;
+    long long length;
     cout << "Enter the length of the floor: " << endl;
-    cin >> n;
-    cout << "Number of ways to tile floor of size 2 x " << n << " are: " << numberOfWays(n) << endl;
+    cin >> length;
+    cout << "Number of ways to tile floor of length 2 x " << length << " are: " << numberOfWays(length) << endl;
 }
 
 /*  Sample Input:

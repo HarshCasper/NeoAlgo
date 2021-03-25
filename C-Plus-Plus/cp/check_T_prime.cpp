dadf01e08fd54a834de9c595eeab4a67e105cfdb
ayushjha952 <60105854+ayushjha952@users.noreply.github.com>
2021-03-25 08:14:02 +0530

    Changes made to issue(#3869)

--- C-Plus-Plus/cp/check_T_prime.cpp
+++ C-Plus-Plus/cp/check_T_prime.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 int Check_T_Prime(int n)
 {
-    //Because 2 and 3 are prime(2 factors) and 1 having only 1 factor.
     if (n <= 3)
     {
         return 0;
@@ -27,10 +26,9 @@ int Check_T_Prime(int n)
         //to check if number is perfect square or not.
         if (square_root * square_root == n)
         {
-
             for (i = 2; i <= sqrt(square_root); i++)
             {
-
+                
                 /* If the square root of the 
                 user input number (num) is prime then it's T-prime otherwise it's not.
                 Because for T-prime number should have 3 factors(one the number itself)
@@ -42,15 +40,11 @@ int Check_T_Prime(int n)
                     break;
                 }
             }
-            //If it is perfect square and square root of perfect square is prime.
             if (perfect_square_prime == 0)
                 return 1;
-            //If it is perfect square and square root of perfect square not prime.
             else
                 return 0;
         }
-
-        //If it is not perfect square and square root of perfect square not prime.
         else
             return 0;
     }
@@ -59,17 +53,14 @@ int Check_T_Prime(int n)
 int main()
 {
 
-    //To deal with large numbers
     long long int num;
     cout << "Enter the number: " << endl;
     cin >> num;
 
     int result = Check_T_Prime(num);
 
-    //If functions returns 1 it is T-Prime.
     if (result == 1)
         cout << num << " is T-prime. " << endl;
-    //If function returns 0 it is not T-prime.
     else
         cout << num << " is not T-prime. " << endl;
 }

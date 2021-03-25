a0d366b1a7a9ed0f65439d535ac2a5fe41ac52d4
rutujaingole <72335505+rutujaingole@users.noreply.github.com>
2021-03-25 22:48:19 +0530

    Updated the commit with the required changes

--- C-Plus-Plus/computational_geometry/Convex_Hull_Graham_Scan.cpp
+++ C-Plus-Plus/computational_geometry/Convex_Hull_Graham_Scan.cpp
@@ -14,7 +14,7 @@
 
 
 
-#include <iostream> //header files for the code
+#include <iostream> 
 #include<algorithm>
 #include<vector>
 #include<stack>
@@ -24,12 +24,15 @@
 #define y second
 
 using namespace std;
-typedef struct point//structure for the coordinates
+
+typedef struct point                    //structure for the coordinates
 {
-double x,y;  // comparison is done first on y coordinate and then on x coordinate.
-} point;
+   double x,y;                         // comparison is done first on y coordinate and then on x coordinate.
+} 
+point;
 
 vector <point>plane;
+
 stack<point> hull;
 
 double area(point a, point b, point c)
@@ -37,34 +40,32 @@ double area(point a, point b, point c)
     return (a.x - b.x) * (b.y - c.y) - (a.y - b.y) * (b.x - c.x);
 
 }
-double dis(point a, point b) //returns euclidean distance between two points.
+double dis(point a, point b)          //returns euclidean distance between two points.
 {
     return sqrt((a.x = b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
-bool cmp(point a, point b) // for comparing points based on their orientation.
+bool cmp(point a, point b)           // for comparing points based on their orientation.
 {
-    if (area(plane[0], a, b) < -e)
+    if (area(plane[0], a, b) < -e){
     return 0;
-    if (area(plane[0], a, b) > e)
+    }
+    if (area(plane[0], a, b) > e){
         return 1;
+    }
     return dis(a, plane[0]) < dis(b, plane[0]);
 
 }
 
-
-
 int main()
 
 {
-
-
-    cout<<"enter the desired number of points\n";
-    int n; // the number of coordinates to be entered
+   cout<<"enter the desired number of points\n";
+    int n;                         // the number of coordinates to be entered
     point tmp;
     cin >> n;
     cout<<"enter the points\n";
     plane.resize(n);
-    for (int i = 0; i < n; i++)  //entering the desired points for determining the convex hull
+    for (int i = 0; i < n; i++)    //entering the desired points for determining the convex hull
     {
 
         cin >> plane[i].x >> plane[i].y;
@@ -95,15 +96,7 @@ int main()
 
 
 
-/*Time Complexity: O(nLogn), if total points = n
-    Explanation:
-    -> Finding the first element takes at most O(n).
-    -> Sorting the points takes at most O(nLogn) - since we are using quick sort.
-    -> Pushing and poping the points take at most O(1).
-    -> To process the coordinates one by one would take at most O(n).
-    -> Printing the convex hull coordinates takes O(n).
-    -> Writing the coordinates onto the files takes O(n).
-    -> So, Total time complexity = O(n) + O(nLogn) + O(1) + O(n) + O(n) + O(n) = O(nLogn)*/
+/*Time Complexity: O(nLogn), if total points = n*/
 /*Worst case time complexity: Θ(N log N)
 Average case time complexity: Θ(N log N)
 Best case time complexity: Θ(N log N)
@@ -131,21 +124,4 @@ The following vertices form the convex hull:
 3,1
 0,0
 0,3
-
-Test Case 2:
-enter the desired number of points
-6
-enter the points
-0 0
-5 0
-5 5
-0 5
-1 1
-2 2
-
-The following vertices form the convex hull:
-0,5
-5,5
-5,0
-0,0
 */

99385f6beb70b578efbecb28e7b637100ea8dd25
Anuj Agrawal <anujagr2001@gmail.com>
2021-03-26 15:20:01 +0530

    Update IntervalListIntersection.cpp

--- C-Plus-Plus/cp/Interval_list_Intersection.cpp
+++ C-Plus-Plus/cp/Interval_list_Intersection.cpp
@@ -5,16 +5,11 @@ where firstList[i] = [starti, endi]
 and secondList[j] = [startj, endj].
 Each list of intervals is pairwise
 disjoint and in sorted order.Return
-the intersection of these two 
-interval lists.A closed interval 
-[a, b] (with a < b) denotes the set
-of real numbers x with a <= x <= b.
-The intersection of two closed 
-intervals is a set of real numbers
-that are either empty or represented
-as a closed interval. For example,
-the intersection of [1, 3] and 
-[2, 4] is [2, 3].
+the intersection of these two inte-
+-rval lists.The intersection of two
+closed intervals is a set of real
+numbers that are either empty or 
+represented as a closed interval.
 */
 
 #include <bits/stdc++.h>

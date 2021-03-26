ef5e7cbb5ff65cc8d9d170b04c4a744e79edb529
Nirbhay <mauryanirbhay1002@gmail.com>
2021-03-26 19:49:33 +0530

    dynamic inputting added

--- C-Plus-Plus/other/job_sheduling_with_deadlines.cpp
+++ C-Plus-Plus/other/job_sheduling_with_deadlines.cpp
@@ -43,10 +43,19 @@ using namespace std;
 void JSD_algo(int[],int[],int[],int);
 int main()
 {
-  int jobs[]={1,2,3,4,5,6};
-  int profit[]={100,85,74,65,60,43};
-  int deadlines[]={4,1,3,1,2,2};
-  int n=sizeof(jobs)/sizeof(jobs[0]);
+  int n,k;
+  cout<<"enter the no. of jobs:"<<endl;
+  cin>>n;
+  int jobs[n],profit[n],deadlines[n];
+  cout<<"enter the profit in decreasing order:"<<endl;
+  for(k=0;k<n;k++)
+      cin>>profit[k];
+  cout<<"enter the respective deadline:"<<endl;
+  for(k=0;k<n;k++)
+      cin>>deadlines[k];
+  cout<<"enter the respective job serial no.:"<<endl;
+  for(k=0;k<n;k++)
+      cin>>jobs[k];
   JSD_algo(jobs,profit,deadlines,n);
 }
 
@@ -54,8 +63,8 @@ void JSD_algo(int jobs[],int p[],int d[],int n)
 {
   int profit=0,i,max_dl,j;
   max_dl=*max_element(d,d+n); /* finding max deadline*/
-  int sub_job[max_dl]={0};
-  for(i=0;i<n;i++)
+  int sub_job[max_dl]={0}; /* no. of maximum jobs can be sheduled will be equal to max deadline */
+  for(i=0;i<n;i++)   
      {
          for(j=d[i]-1;j>=0;j--)
          {
@@ -76,10 +85,18 @@ void JSD_algo(int jobs[],int p[],int d[],int n)
 /* 
 Time complexity:O(n^2) becase we are using loop inside a loop .(nxn)
 space complexity:O(1)
-
-
-ans:
+--------------------------------------------------------------------------------------------------------
+input:
+enter the no. of jobs:
+6
+enter the profit in decreasing order:
+100 85 74 65 60 43
+enter the respective deadline:
+4 1 3 1 2 2
+enter the respective job serial no.:
+6 5 2 3 1 4
+output:
 sheduled jobs:
-2 5 3 1
+5 1 2 6
 max profit:319
 */

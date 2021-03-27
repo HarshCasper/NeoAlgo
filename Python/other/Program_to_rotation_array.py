c269bdb04fff1d4cf0a82c6320efb3b738bc296c
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-03-27 21:16:20 +0530

    Modify the code as requested

--- Python/other/Program_to_rotation_array.py
+++ Python/other/Program_to_rotation_array.py
@@ -16,8 +16,19 @@ ending_element = input("Enter the ending element:\n")
 array_rotation()
 
 '''
->> Time & Space Complexity is O(n) 
->>
+>>Time & Space Complexity is O(n) 
+
+INPUT
+
+>>Enter the no. elements:
+  1 2 3 4 5 6 7
+>>Stating from which element:
+  2  
+>>Enter the ending element:
+  7
+
+OUTPUT 
+>>rest arry = ['3', '4', '5', '6', '7', '1', '2']
 
 
 '''

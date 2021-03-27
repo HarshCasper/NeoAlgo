76985b5a4d6647f66e75cb3aabefc7a5dde038d7
CSAURABH <578saurabh@gmail.com>
2021-03-27 17:40:27 +0530

    Added multi-line comments and added more description about the problem

--- C-Plus-Plus/other/Median_in_row_wise_sorted_matrix.cpp
+++ C-Plus-Plus/other/Median_in_row_wise_sorted_matrix.cpp
@@ -1,8 +1,40 @@
-// Find median in a row wise sorted Matrix in C++.
+/* Find median in a row wise sorted Matrix in C++.
 
-// Formula for calculating the median.
-   // median = (1 + r * c) / 2
+   We are given a row-wise sorted matrix of size r*c,
+   we need to find the median of the matrix given.
+   It is assumed that r*c is always odd.
 
+   Example 1 : 
+
+   Input 1 : 1 3 4
+             2 6 9
+             3 6 9
+   
+   Output 1 : Median is 4
+
+
+   Example 2 : 
+
+   Input 2 : 1 3 4
+             2 5 6
+             7 8 9
+   
+   Output 2 : Median is 5
+  
+
+   Formula for calculating the median.
+   median = (1 + r * c) / 2
+
+   Algorithm :-
+   1) First, we need to find the minimum and maximum elements from the matrix.
+   2) we can apply binary search on this range.
+      The mid element can be calculated and number 
+      of elements smaller or equal to mid can be calculated, 
+      we have used upper_bound() function for this.
+   3) Based on the value of our counter,
+      the min and max can be adjusted accordingly based 
+      on what we do for binary search.
+*/
 
 //CODE :-
 #include<bits/stdc++.h>

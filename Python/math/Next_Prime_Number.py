0039615a9593bc81d1217d4ab7eba5c122e778c6
Tarun Kamboj <kambojtarun02@gmail.com>
2021-03-28 11:49:08 +0530

    Modified Next Prime Number Script

--- Python/math/Next_Prime_Number.py
+++ Python/math/Next_Prime_Number.py
@@ -1,10 +1,11 @@
 '''
-Introduction 
+INTRODUCTION
 
 The code below calculates and returns the next prime number from the number given by user.
-
-Time Complexity : O(n²)
-Space Complexity : O(1)
+It takes a number n from user and returns a number x such that
+1. x is greater than n
+2. x is a prime number and 
+3. there are no prime numbers between n and x 
 
 '''
 
@@ -26,8 +27,12 @@ n = int(input("Enter a number : "))
 print('Next prime number is ', next_prime(n))
 
 '''
-Sample output
+OUTPUT
 
 Enter a number : 23
 Next prime number is  29
+
+
+TIME COMPLEXITY : O(n²)
+SPACE COMPLEXITY : O(1)
 '''

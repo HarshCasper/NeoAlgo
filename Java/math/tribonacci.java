fea79f018828a15ef7ec97ff6ecfcac2879d308b
AkanshaUniyal <78859329+AkanshaUniyal@users.noreply.github.com>
2021-03-28 18:04:37 +0530

    Update tribonacci.java

--- Java/math/tribonacci.java
+++ Java/math/tribonacci.java
@@ -51,6 +51,7 @@
  Enter the number upto which the series is to be printed
  23
  0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, 121415
+
  Space Complexity: O(1)
  Time Complexity: O(n)
  */

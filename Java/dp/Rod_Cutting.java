15541f4da28c109da9980d0f6b451af9c56f06f6
Krishnaraj Yadav <krishnay.75676@gmail.com>
2021-03-28 20:34:19 +0530

    #2489-changes applied

--- Java/dp/Rod_Cutting.java
+++ Java/dp/Rod_Cutting.java
@@ -1,34 +1,23 @@
 /*Problem Description
-
 You are given a rod of length N inches and an array of prices that contains prices of all pieces of size smaller than N. 
 Determine the maximum value obtainable by cutting up the rod and selling the pieces.
-
 */
-
-// importing required package 
 import java.util.Scanner;
-
 class Helper {
-	
     // this method - getValue() will return the final answer to main method 
     public int getValue(int price[], int n) {
-
         // m variable will help in getting the size array length by using price length 
         int m = price.length ;
         int size[] = new int[m];
         for(int i = 0 ; i < m  ; i++)
-            size[i] = i + 1;
-
+            size[i] = i + 1 ;
         // call solve() method for solving the problem
         int ans = solve(price , size, m , n);
         return ans ;
     }
-    
     //solve() method will act as solving the problem
-
     int solve(int[] price , int[] size , int m , int n  )
     {
-
         // Create 2-D array to store the value of profit 
         int dp[][] = new int[m+1][n+1] ;
         
@@ -42,7 +31,6 @@ class Helper {
                     dp[i][j] = 0;
                     
                 }
-
                 /*  if size is less than the current max size then 
                   get the max price from size or neglect if not needed
                   */
@@ -50,7 +38,6 @@ class Helper {
                 {
                     dp[i][j] = Math.max(price[i-1] + dp[i][j - size[i-1] ] , dp[i-1][j] ) ;
                 }
-
                 // if size is greater than current max size , no value is taken from price array
                 else
                 {
@@ -62,41 +49,37 @@ class Helper {
     }
     
 }
-
 // main class which contains main method
-public class Rod_Cutting {
-	
+public class Rod_Cutting {	
 	 public static void main(String args[]) {
-
 	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter number of testcase");
 	        int test = sc.nextInt();
 	        while (test-- > 0) {
+	        	System.out.println("Enter length of rod");
 	            int n = sc.nextInt();
 	            int[] arr = new int[n];
-
                 // get values of price 
+	            System.out.println("Enter price of each length");
 	            for (int i = 0; i < n; i++) 
 	            	arr[i] = sc.nextInt();
-
             //Creating object of another class 
 	            Helper help = new Helper();
 	            System.out.println(help.getValue(arr, n));
 	        }
-	    }
-	 
+	    }	 
 }
-
 /* Input
-
-N = 8
+Enter number of testcase
+1
+Enter length of rod
+8
+Enter price of each length
 1 5 8 9 10 17 17 20
-
 Output
 22
-
-Explanation
+Explanation :
 The maximum value we get is 22 by cutting in two pieces of lengths 2 and 6 . So , 22 is answer.
-
 Time Complexity : O(N^2)
 Space Complexity : O(N)
 */

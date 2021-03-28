5d5116ad86f62a7bf572f3bf0a7c477e9b7d6141
git-ayush-pradhan <ap.ayush.pradhn@gmail.com>
2021-03-28 17:31:07 +0530

    Updated merge string alternatively

--- C-Plus-Plus/cp/Merge_string_alternatively.cpp
+++ C-Plus-Plus/cp/Merge_string_alternatively.cpp
@@ -1,4 +1,5 @@
 /** 
+
 Problem:
 You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
 If a string is longer than the other, append the additional letters onto the end of the merged string.
@@ -11,27 +12,24 @@ For each test case we enter two strings to be merged alternatively
 Output Format:
 For each test case we get a alternatively merged string.
 
-Sample input:
-2
-abc pqr
-abcd pq
-
-Sample Output:
-apbqcr
-apbqcd
-
 Explanation:
 First we alternatively add both the strings to the resultant string till the length of the smallest string (both strings can also be equal length). 
 After that we add the substring of the remaining longer string to resultant string if the length of both strings are not equal.
+
 **/
 
 #include<bits/stdc++.h>
 using namespace std;
 
 string mergeAlternately(string word1, string word2) {
+
+    /* This function will take 2 string as input and return the resultant merged string formed alternatively from the inputted strings */
+
     string ans="";
-    int less=min(word1.length(),word2.length()),i=0,j=0;
-    while(i<less || j<less){
+    int less=min(word1.length(),word2.length());    // takes the size of the smallest string among word1 and word2
+    int i=0,j=0; 
+
+    while(i<less || j<less){    // alternatively add word by word from each string to resultant string
         if(j>=i){
             ans+=word1[i];
             i++;
@@ -41,7 +39,8 @@ string mergeAlternately(string word1, string word2) {
             j++;
         }
     }
-    if(word1.length()!=word2.length()){
+
+    if(word1.length()!=word2.length()){     // if both strings are of unequal sizes then add the rest of substring to the resultant string
         if(word1.length()>less){
             ans+=word1.substr(less,word1.length());
         }
@@ -49,6 +48,7 @@ string mergeAlternately(string word1, string word2) {
             ans+=word2.substr(less,word2.length());
         }
     }
+
     return ans;
 }
 
@@ -61,3 +61,19 @@ int main(){
         cout<<mergeAlternately(word1,word2)<<endl;
     }
 }
+
+/*
+
+Time complexity: O(N) where N is the length of the longest string.
+Space complexity: O(M) where M is the summation of length of both strings.
+
+Sample input:
+2
+abc pqr
+abcd pq
+
+Sample Output:
+apbqcr
+apbqcd
+
+*/

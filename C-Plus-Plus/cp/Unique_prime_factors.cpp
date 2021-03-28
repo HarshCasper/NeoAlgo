7a23ccf13be76eca2e4360d0ad21425a112d6d11
Kirti Goyal <lci2020039@iiitl.ac.in>
2021-03-28 10:37:36 +0530

    Changes done

--- C-Plus-Plus/cp/Unique_prime_factors.cpp
+++ C-Plus-Plus/cp/Unique_prime_factors.cpp
@@ -1,9 +1,10 @@
-//This is a use of Sieve of Eratosthenes algorithm.
-//In this We can find all unique prime factor of any number n.For example if n is 12 then output will be 2,3
-//In this first we store all prime factor of numbers till n and then print prime factors of n.
+/* This is a use of Sieve of Eratosthenes algorithm.
+In this We can find all unique prime factor of any number n.For example if n is 12 then output will be 2,3
+In this first we store all prime factor of numbers till n and then print prime factors of n. */
 
 #include<bits/stdc++.h>
 using namespace std;
+
 void unique_prime(int num) {
   int arr[num+1];
   for(int i = 1 ; i <= num ; i++) {
@@ -21,26 +22,23 @@ void unique_prime(int num) {
     }
   }
   //print vector when key is n
+  cout<<"All Unique prime factors of "<<num<<" are: ";
   for(int i=0 ; i < (m[num].size()) ; i++){
-    cout<<m[num][i]<<"  ";
+    cout <<m[num][i]<<"  ";
   }
   return;
 }
+
 int main(){
   int n;
+  cout<<"Enter the number: ";
   cin>>n;
   unique_prime(n);
   
 }
 
-/* Sample Input: 24
-   Sample output: 2  3
-
-   Test case_1 Input: 111
-   Test case_1 Output: 3  37
-
-   Test case_2 Input:5446
-   Test case_2 Output: 2  7  389
+/* Sample Input:5446
+   Sample output: 2  7  389
 */
 /* Time complaxity : O(n*n)
    Space Complaxity : O(n*n)

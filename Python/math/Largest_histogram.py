48771174b6b4aa53d3b63df2e6254d3c671f1e43
Abhiram Ravipati <abhiramravipati9@gmail.com>
2021-03-29 16:52:23 +0530

    formatted the code

--- Python/math/Largest_histogram.py
+++ Python/math/Largest_histogram.py
@@ -8,32 +8,35 @@ Time Complexity :- O(n)
 Space Complexity:- O(n)
 '''
 
-#Getting the input from user
-a = []
-n=int(input("Enter the no.of Rectangles :"))
-for i in range(n):
-    x=int(input("Enter the heights of Rectangles : "))
-    a.append(x)
-    
 def Largest_Rectangle(histogram):
-    a=[]
-    area=0
-    i=0
+    heights = []
+    area = 0
+    i = 0
     histogram.append(0)
-    
-    while i<len(histogram):
+    while i < len(histogram):
         #if stack is empty or rectangle with greater height is found then push the index into stack
-        if len(a) == 0 or histogram[a[-1]] < histogram[i]:
-                          a.append(i)
-                          i += 1
+        if len(heights) == 0 or histogram[heights[-1]] < histogram[i]:
+            heights.append(i)
+            i += 1
         #if rectangle of smaller height is found pop the top element from the stack and calculate area              
         else:
-            top = a.pop()
-            area = max(area,histogram[top]*(i-a[-1] - 1 if a else i))
+            top = heights.pop()
+            area = max(area, histogram[top] * (i - heights[-1] - 1 if heights else i))
     return area
 
-#Printing the area of largest rectangle
-print('Largest Rectangular area in the Histogram is :',Largest_Rectangle(a))
+def main():
+    #Getting the input from user
+    heights = []
+    n = int(input("Enter the no.of Rectangles :"))
+    for i in range(n):
+        height = int(input("Enter the heights of Rectangles : "))
+        heights.append(height)
+
+    #Printing the area of largest rectangle
+    print('Largest Rectangular area in the Histogram is :', Largest_Rectangle(heights))
+
+if __name__ == "__main__":
+    main()
 
 '''
 Sample Input/Output :-

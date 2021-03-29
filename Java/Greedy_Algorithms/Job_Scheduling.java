62307734c78768d6ec2a10987888c2291913c2e4
Krishnaraj Yadav <krishnay.75676@gmail.com>
2021-03-29 17:26:53 +0530

    Applied changes

--- Java/Greedy_Algorithms/Job_Scheduling.java
+++ Java/Greedy_Algorithms/Job_Scheduling.java
@@ -4,10 +4,11 @@ Each job takes 1 unit of time to complete and only one job can be scheduled at a
 We earn the profit if and only if the job is completed by its deadline.
 The task is to find the maximum profit and the number of jobs done.
 */
-//Solution :
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
+
 public class Job_Scheduling_Greedy {
     // this Job class will store the id, profit, deadline property 
 	static class Job {
@@ -18,41 +19,43 @@ public class Job_Scheduling_Greedy {
             this.deadline = deadline;
         }
 	};
+	
 	// solve() will do the task of getting the profit
 	static int[] solve (Job[] arr , int n )
 	{
 		// Sort the array in descending order according to profit
 		Arrays.sort(arr , (a,b) -> b.profit - a.profit );
-        int res[]  = new int[2] ;
-        int totalProfit = 0;
-        int count = 0 ;        
-        Boolean[] slots = new Boolean[n];
-        Arrays.fill(slots, false);
-       /* iterate through the jobs with minDeadLine to remaining jobs and
-         check if there is any slote available and also claculate the profit
-         */
-        for (int i = 0; i < n; i++) {
-        	int minDeadLine = Math.min( arr[i].deadline - 1 , n- 1) ;
-            for (int j = minDeadLine ; j >= 0; j--) 
-            {
-                if (slots[j] == false) {
-                    count ++ ;
-                    slots[j] = true;
-                    totalProfit += arr[i].profit ;
-                    break;
-                }
-            }
-        }
-        // store the count and profit in res array and return the result
-        res[0] = count ;
-        res[1] = totalProfit ;
-        return res ;
+		int res[]  = new int[2] ;
+		int totalProfit = 0;
+		int count = 0 ;        
+		Boolean[] slots = new Boolean[n];
+		Arrays.fill(slots, false);
+	       /* iterate through the jobs with minDeadLine to remaining jobs and
+		 check if there is any slote available and also claculate the profit
+	       */
+		for (int i = 0; i < n; i++) {
+		    int minDeadLine = Math.min( arr[i].deadline - 1 , n- 1) ;
+		    for (int j = minDeadLine ; j >= 0; j--) 
+		    {
+			if (slots[j] == false) {
+			    count ++ ;
+			    slots[j] = true;
+			    totalProfit += arr[i].profit ;
+			    break;
+			}
+		    }
+		}
+		// store the count and profit in res array and return the result
+		res[0] = count ;
+		res[1] = totalProfit ;
+		return res ;
 	}
+	
  	public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter number of testcase");
-		int test = Integer.parseInt(br.readLine().trim());
-		while(test-->0){
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    System.out.println("Enter number of testcase");
+	    int test = Integer.parseInt(br.readLine().trim());
+	    while(test-->0){
             // input the no. of jobs
             System.out.println("Enter number of jobs");
             String input[] = br.readLine().trim().split(" ");
@@ -71,6 +74,7 @@ public class Job_Scheduling_Greedy {
         }
     }
 }
+
 /* Input
 Enter number of testcase
 1
@@ -78,10 +82,16 @@ Enter number of jobs
 4
 Enter values of jobs
 1 4 20 2 1 10 3 1 40 4 1 30
+
+Output :
 Maximum Profit : 2 60
+
 Explanation
 Sort all the jobs according to descending in terms of profit and loop the jobs and check if slot is available or not
 and according insert value to res array and output the result.
+
 Time Complexity : O(NLogN)
 Space Complexity : O(N)
 */
+
+

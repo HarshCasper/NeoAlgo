48a2bda88baf34298af749513fc2efb51527b8c8
devara khurma venkata kavyasree <61260505+kavyasree-123@users.noreply.github.com>
2021-03-29 18:44:52 +0530

    updated rabin karp code effectively unique one

--- C/ds/rabin karp algo.c
+++ C/ds/rabin karp algo.c
@@ -1,86 +1,68 @@
-/*  C implementation of Rabin Karp Algorithm issue-2943*/
-#include<stdio.h> 
-#include<string.h> 
-  
-#define d 256 
-  
-
-void search(char pat[], char txt[], int q) 
-{ 
-    int M = strlen(pat); 
-    int N = strlen(txt); 
-    int i, j; 
-    int p = 0; 
-    int t = 0; 
-    int h = 1; 
-  
-    /*The value of h would be "pow(d, M-1)%q"*/ 
-    for (i = 0; i < M-1; i++) 
-        h = (h*d)%q; 
-  
-    
-    
-    for (i = 0; i < M; i++) 
-    { 
-        p = (d*p + pat[i])%q; 
-        t = (d*t + txt[i])%q; 
-    } 
-  
-
-    for (i = 0; i <= N - M; i++) 
-    { 
-  
-        /* Check the hash values of current window of text*/ 
-         /* and pattern. If the hash values match then only */
-      /*check for characters on by one */
-        if ( p == t ) 
-        { 
-            /* Check for characters one by one */
-            for (j = 0; j < M; j++) 
-            { 
-                if (txt[i+j] != pat[j]) 
-                    break; 
-            } 
-  
-             
-            if (j == M) 
-                printf("Pattern found at index %d \n", i); 
-        } 
-  
-        
-        if ( i < N-M ) 
-        { 
-            t = (d*(t - txt[i]*h) + txt[i+M])%q; 
-  
-            /*We might get negative value of t, converting it to positive */
-            if (t < 0) 
-            t = (t + q); 
-        } 
-    } 
-} 
-  
-/* Driver Code */
-int main() 
-{ 
-    char txt[] = "THIS IS A TEST TEXT"; 
-    char pat[] = "TEST"; 
-    
-      // A prime number 
-    int q = 101;  
-    
-      // function call 
-    search(pat, txt, q); 
-    return 0;
+/* RABIN KARP ALGO in c issue - 2943
+Rabin-Karp is  pattern searching algorithm to find the pattern in a more efficient way, 
+finds the hash value. When the hash value is matched, then only it tries to check each character */
+#include <stdio.h>
+#include<string.h>
+void hash(char word[], char format[], int max, int len1 ,int pos, int num, int  initial){
+ /* C hash value calculation for pattern and text*/
+  for (int index = 0; index < len1; index++) {
+    initial = (num *  initial +format[index]) % max;
+/* getting initial updated with hash value*/
+    pos = (num * pos + word[index]) % max;
+/* getting pos updated with hash value*/
+  }
 }
-
-/*
-Input:  txt[] = "THIS IS A TEST TEXT"
-        pat[] = "TEST"
-Output: Pattern found at index 10
-
-Input:  txt[] =  "AABAACAADAABAABA"
-        pat[] =  "AABA"
-Output: Pattern found at index 0
-        Pattern found at index 9
-        Pattern found at index 12*/
-// time complexity O(n)
+/* checking whether the pattern is present or not*/
+int checking(char word[], char format[], int len2 , int len1 ,int pos, int  initial){
+    int Index;
+  for ( int index = 0; index <= len2 - len1; index++) {
+ /* we check both size and intial positions */
+    if (pos ==  initial) {
+      for (Index = 0; Index < len1; Index++) {
+        if (format[index + Index] != word[Index])
+          break;
+      }
+ /* checking the len1 and the Indexes are equal or not */
+      if (Index == len1)
+        printf( "PATTERN IS AT POSITION %d", index + 1 );
+/*printing the position of word present in format text*/
+    }
+  }
+}
+int main() {
+   char format[1000];
+   printf("enter the string format ");
+   /* giving input text in string format */
+   gets(format);
+    char word[1000];
+    printf("give pattern ");
+    /*giving the pattern for checking word in text*/
+    gets(word);
+  int max = 100,index=0,Index=0;
+ /* getting the length stored in len1 for word*/ 
+  int len1 = strlen(word);
+/*getting the length stored in len2 for format string*/  
+  int len2 = strlen(format);
+  int pos= 0, initial = 0, valuehash = 1, num=10;
+  for (index = 0; index < len1- 1; index++){
+    valuehash = (valuehash * num) % max;
+  }
+    hash(word, format, max , len1,pos, num, initial );
+    checking(word, format, len2, len1, pos,  initial);
+ /*we are checking the intial with lenght1 and 2, if is less make it postive by adding max*/  
+for (index = 0; index <= len2 - len1; index++) {
+    if ( initial < 0)
+         initial= ( max +initial);
+ if (index <len2 - len1) {
+       initial = (num * ( initial - format[index] * valuehash) +format[index +len1]) % max;
+ /* we have final output in form of integer to specify the positin*/
+    }
+   }
+}
+/* input :
+enter the string format abcdehu                                                                                                                                                      
+give pattern ab  
+output: PATTERN IS AT POSITION 1 
+time complexity  O(n+m) for searching at instance
+space complexity O(1)
+*/ 

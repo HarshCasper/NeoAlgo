595bd4fa0afd703ac6e6b20d22770961a616e177
kushal1311001 <121910302059@gitam.in>
2021-03-29 21:32:22 +0530

    added python file

--- Python/cp/Minimize_Amplitude_After_K_Removals.py
+++ Python/cp/Minimize_Amplitude_After_K_Removals.py
@@ -48,7 +48,7 @@ while i < len(nums) - p + 1:
 # printing max possible from list
 print("The manimum Possible difference is : " , m)
 
-"""Test Case=
+"""Test Case for the given sum
 Input:-
 
 nums = [84, 45, 2, 1, 894, 156, 68, 48]
@@ -58,4 +58,4 @@ output:-
 39
 
 Time Complexity:  O(nlogn)
-Space Complexity: O(1)"""
+Space Complexity: O(1) """

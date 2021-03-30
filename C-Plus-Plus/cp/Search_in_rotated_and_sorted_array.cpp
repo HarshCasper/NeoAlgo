4d86c607d9363635d7b3b2e8dcecd090481fe644
Swarnima Shishodia <swarnimashishodia11@gmail.com>
2021-03-30 08:36:42 +0530

    Update Search_in_rotated_and_sorted_array.cpp

--- C-Plus-Plus/cp/Search_in_rotated_and_sorted_array.cpp
+++ C-Plus-Plus/cp/Search_in_rotated_and_sorted_array.cpp
@@ -3,19 +3,6 @@ Input:
 The first line of the input contains an integer T, denoting the total number of test cases. Then T test cases follow. Each test case consists of three lines. First line of each test case contains an integer N denoting the size of the given array. Second line of each test case contains N space separated integers denoting the elements of the array A. Third line of each test case contains an integer K denoting the element to be searched in the array.
 Output:
 Corresponding to each test case, output the index of the element found in the array.  If element is not present, then output -1.
-INPUT:
-1
-5
-3 2 1 4 5
-1
-OUTPUT
-2
-INPUT
-1
-4
-2 1 3 4
-5
--1
 */
 
 #include <iostream>
@@ -39,7 +26,7 @@ int main()
 	   pos=-1; //If element is not present print -1
 	   while(l<=r)
 	   {
-	       mid=(l+r)/2;  //find mid
+	       mid=l + ((r - l) / 2);;  //find mid
 	       if(arr[mid]==x) //if element is found set pos=mid
 	       {
 	       pos=mid;
@@ -64,3 +51,22 @@ int main()
 	}
 	return 0;
 }
+
+/*
+INPUT:
+1
+5
+3 2 1 4 5
+1
+OUTPUT
+2
+INPUT
+1
+4
+2 1 3 4
+5
+-1
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/

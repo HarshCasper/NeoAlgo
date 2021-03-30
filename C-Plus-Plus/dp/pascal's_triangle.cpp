ca6d0ff79fb6dbbe73dc104fd4c9629212a3bc31
piyush9311 <56592802+piyush9311@users.noreply.github.com>
2021-03-30 11:36:30 +0530

    changes made in Pascal's Triangle Using DP as mentioned

--- C-Plus-Plus/dp/pascal's_triangle.cpp
+++ C-Plus-Plus/dp/pascal's_triangle.cpp
@@ -1,10 +1,13 @@
+/*
+Given the number of rows , print the pascal's triangle having that many rows.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     long int n;
     cout << "Enter the number of rows" << endl;
-    cin >> n;
+    cin >> n; // Enter the number of rows in the triangle
     long int dp[n][n];
     cout << "The Pascal's Triangle is : " << endl;
     for (int row = 0; row < n; row++)
@@ -24,8 +27,11 @@ int main()
 /*Piyush Kumar
    Time Complexity - O(n^2)
    Space Complexity - O(n^2) auxiliary space
-   Sample Input - 4 
-   Sample Output - 
+   Sample Input -
+   Enter the number of rows
+   4 
+   Sample Output -
+   The Pascal's Triangle is :  
        1
       1 1
      1 2 1

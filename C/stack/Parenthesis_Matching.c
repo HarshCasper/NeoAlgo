b0af2f1cdf9b7f309f7ce73401f91f56ed7a0a7b
Kanak <62026983+kanak22@users.noreply.github.com>
2021-03-31 23:48:13 +0530

    Update Parenthesis_Matching.c

--- C/stack/Parenthesis_Matching.c
+++ C/stack/Parenthesis_Matching.c
@@ -1,3 +1,6 @@
+/* This program is of parenthesis matching which is an application of stack in C */
+/* TIME COMPLEXITY: O(n) */
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -37,16 +40,16 @@ int main()
 
 }
 
-//function to check parathesis
+//function to check parenthesis
 int check(char exp[] )
 {
         //declaration
         int i,n;
         char temp;
         n=strlen(exp);
-        //loop for traversing through the expression
-        //storing(pushing) parathesis into stack
-        //poping if the match of that parathesis is found
+        /*loop for traversing through the expression
+        storing(pushing) parenthesis into stack
+        poping if the match of that parenthesis is found*/
         for(i=0;i <n;i++)
         {
                 if(exp[i]=='(' || exp[i]=='{' || exp[i]=='[')
@@ -55,15 +58,17 @@ int check(char exp[] )
                 {         //stack empty
                         if(top==-1)
                         {
+                                //when there are more parenthesis on the right
                                 printf("Right parentheses are more than left parentheses\n");
                                 return 0;
                         }
                         else
                         {
                                 temp=pop();
-                                //calling function match to match parathesis
+                                //calling function match to match parenthesis
                                 if(!match(temp, exp[i]))
                                 {
+                                        //to print the parenthesis  which gets mismatched
                                         printf("Mismatched parentheses are : ");
                                         printf("%c and %c\n",temp,exp[i]);
                                         return 0;
@@ -75,25 +80,30 @@ int check(char exp[] )
         //if stack is empty
         if(top==-1)
         {
+                //if stack gets empty
                 printf("Balanced Parentheses\n");
                 return 1;
         }
         else
         {
+                //if stack isn't empty
                 printf("Left parentheses more than right parentheses\n");
                 return 0;
         }
 }
 
-//function to match parathesis
+//function to match parenthesis
 int match(char a,char b)
 {
+        //if a=[ and b=]
         if(a=='[' && b==']')
-                return 1;
+              {  return 1;}
+        //if a={ and b=}
         if(a=='{' && b=='}')
-                return 1;
+              {  return 1;}
+        //if a=( and b=)
         if(a=='(' && b==')')
-                return 1;
+              {  return 1;}
         return 0;
 }
 
@@ -102,6 +112,7 @@ void push(char item)
 {
         if(top==(MAX-1))
         {
+                //printing message if stack is full
                 printf("Stack Overflow\n");
                 return;
         }
@@ -114,8 +125,27 @@ char pop()
 {
         if(top==-1)
         {
+                //printing message if stack is empty
                 printf("Stack Underflow\n");
                 exit(1);
         }
         return(stack[top--]);
 }
+
+/* sample input/output */
+/*  1.
+Enter an algebraic expression : (((5+7)*6)/2)
+Balanced Parentheses
+Valid expression
+
+Press any key to continue . . .
+
+    2.
+Enter an algebraic expression : [a(b+c}]
+Mismatched parentheses are : ( and }
+Invalid expression
+
+Press any key to continue . . .
+
+
+*/

7c7b558160dfb31bcc18bbecda8cf083d48c480e
Yashmita <55138349+yashmita@users.noreply.github.com>
2021-03-31 13:41:27 +0530

    Update tiling_problem.py

--- Python/dp/tiling_problem.py
+++ Python/dp/tiling_problem.py
@@ -1,25 +1,24 @@
 '''
 Tiling problem 2xN
 Problem Statement:
-Given a 2 x n board and tiles of dimension 2 x 1,
-count the number of ways to tile the given board using the 2 x 1 tiles.
+Given a 2 x n board and tiles of dimension 2 x 1, count the number of ways to tile the given board using the 2 x 1 tiles.
 A tile can either be placed horizontally, as a 1 x 2 tile or vertically, as 2 x 1 tile.
 '''
 
 
 def numberOfWays(num):
 	# base cases
-    if num < 1:
-        return 0
-    if num < 2:
-        return 1
+	if num < 1:
+		return 0
+	if num < 2:
+		return 1
 	# temporary list to store the number of ways to fill 2xi grid
-    ways = [None] * (num + 1)
-    ways[1] = ways[2] = 1
+	ways = [None] * (num + 1)
+	ways[1] = ways[2] = 1
 	# storing the ways in the temporary list using dynamic programming
-    for i in range(3, num+1):
-        ways[i] = ways[i-1] + ways[i-2]
-    return ways[num]
+	for i in range(3, num+1):
+		ways[i] = ways[i-1] + ways[i-2]
+	return ways[num]
 
 
 n = int(input())  # taking input from the user

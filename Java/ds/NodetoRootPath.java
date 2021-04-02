45329bd5da18dddb258570946bc5bbff8287f427
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-04-02 13:22:40 +0530

    Updated NodeToRootPath.java
    Corrected all the  faults that were mentioned by the reviewers

--- Java/ds/NodetoRootPath.java
+++ Java/ds/NodetoRootPath.java
@@ -1,77 +1,150 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
-public class NodetoRootPath {
+public class nodetorootpath_ {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
 
-    public static class Node {
-        int data;
-        Node left;
-        Node right;
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
 
-        Node(int data) {
-            this.data = data;
-            left = null;
-            right = null;
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
         }
-    }
 
-    // to check if the data is present in the tree or not
-    public static boolean find(Node node, int data) {
-        // write your code here
-        if (node == null)
-            return false;
-
-        boolean res = node.data == data;
-        return res || find(node.left, data) || find(node.right, data);
-    }
-
-    static ArrayList<Integer> path = new ArrayList<>();
-
-    public static boolean Helper(Node node, int data) {
-
-        if (node == null)
-            return false;
-
-        boolean res = (node.data == data) || Helper(node.left, data) || Helper(node.right, data);
-        if (res) {
-            path.add(node.data);
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
         }
 
-        return res;
+        top.state++;
+      } else {
+        st.pop();
+      }
     }
 
-    public static ArrayList<Integer> node2RootPath(Node node, int data) {
-        // write your code here
-        path = new ArrayList<>(); // reinitialisation of the arraylist
-        Helper(node, data);
+    return root;
+  }
 
-        return path;
-
+  public static void display(Node node) {
+    if (node == null) {
+      return;
     }
 
-    public static void main(String[] args) throws Exception {
-        Node root = new Node(60);
-        root.left = new Node(50);
-        root.right = new Node(70);
-        root.left.left = new Node(30);
-        root.left.right = new Node(52);
-        root.right.left = new Node(65);
-        root.right.right = new Node(75);
-        root.right.left.left = new Node(62);
-        root.right.left.right = new Node(68);
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
 
-        int data = 62;
+    display(node.left);
+    display(node.right);
+  }
 
-        boolean found = find(root, data);
-        System.out.println(found);
-        ArrayList<Integer> path = node2RootPath(root, data);
-        System.out.println(path);
+  // to check if the data is present in the tree or not
+  public static boolean find(Node node, int data) {
+    // write your code here
+    if (node == null)
+      return false;
+
+    boolean res = node.data == data;
+    return res || find(node.left, data) || find(node.right, data);
+  }
+
+  static ArrayList<Integer> path = new ArrayList<>();
+
+  public static boolean Helper(Node node, int data) {
+
+    if (node == null)
+      return false;
+
+    boolean res = (node.data == data) || Helper(node.left, data) || Helper(node.right, data);
+    if (res) {
+      path.add(node.data);
     }
+
+    return res;
+  }
+
+  public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+    // write your code here
+    path = new ArrayList<>(); // reinitialisation of the arraylist
+    Helper(node, data);
+
+    return path;
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    boolean found = find(root, data);
+    System.out.println(found);
+
+    ArrayList<Integer> path = nodeToRootPath(root, data);
+    System.out.println(path);
+  }
 
 }
 
+// I/P:
+// 19
+// 50 25 12 n n 37 30 n n n 75 62 n 70 n n 87 n n
+// 30
 // O/P:
 // true
-// [62, 65, 70, 60]
+// [30, 37, 25, 50]
 
-// Time Complexity : O(n) in Worst Case
+// Worst Case Time Complexity : O(n)
 // Space Complexity : O(1)

0f7cd57c9ba398c35ee01aca6c8da0814e84fb31
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-04-02 17:09:39 +0530

    made the required changes

--- Java/graphs/Floyd_Warshall_Algorithm.java
+++ Java/graphs/Floyd_Warshall_Algorithm.java
@@ -6,75 +6,78 @@ It breaks the problem down into smaller subproblems, then combines the answers t
 
 import java.util.Scanner;
 
-public class FloydWarshall {
+public class Floyd_Warshall_Algorithm {
 
-	public static void display (int v, int[][] answerMatrix) {
+	public static void display(int v, int[][] answerMatrix) {
 
 		System.out.println("Matrix displaying minimum costs between the pairs of vertices.");
-		
-		//display final matrix after all the updations of shortest cost path from vertex i to j
+
+		// display final matrix after all the updations of shortest cost path from
+		// vertex i to j
 		// value at i,j represents minimum cost from vertex i to vertex j.
-		for (int i=0; i<v; i++) {
-			for (int j=0; j<v; j++) {
-				System.out.print (answerMatrix[i][j] + " ");
+		for (int i = 0; i < v; i++) {
+			for (int j = 0; j < v; j++) {
+				System.out.print(answerMatrix[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
 
-	public static void floydWarshallAlgorithm (int v, int[][] graph) {
+	public static void floydWarshallAlgorithm(int v, int[][] graph) {
 
-		// answer matrix where we will be storing minimum cost between two pair of vertices.
-		int[][] answerMatrix=new int[v][v];
-		
-		//just copy down values of graph into ans matrix.
-		for (int i=0; i<v; i++) {
-			for (int j=0; j<v; j++) {
+		// answer matrix where we will be storing minimum cost between two pair of
+		// vertices.
+		int[][] answerMatrix = new int[v][v];
+
+		// just copy down values of graph into ans matrix.
+		for (int i = 0; i < v; i++) {
+			for (int j = 0; j < v; j++) {
 				answerMatrix[i][j] = graph[i][j];
 			}
 		}
-		
-		//update the matrix by keeping each vertex as a main rout part once.
-		for (int k=0; k<v; k++) {
-			//k represents that we are keeping kth vertex constant.
-			//means that path from i to j must pass from k.
-			
-			for (int i=0; i<v; i++) {
-				for (int j=0; j<v; j++) {
-					
-					int oc = answerMatrix[i][j];    //old cost, already present at i,j
-					int nc = answerMatrix[i][k] + answerMatrix[k][j];   //new cost, cost from i to k plus cost from k to j. 
+
+		// update the matrix by keeping each vertex as a main rout part once.
+		for (int k = 0; k < v; k++) {
+			// k represents that we are keeping kth vertex constant.
+			// means that path from i to j must pass from k.
+
+			for (int i = 0; i < v; i++) {
+				for (int j = 0; j < v; j++) {
+
+					int oc = answerMatrix[i][j]; // old cost, already present at i,j
+					int nc = answerMatrix[i][k] + answerMatrix[k][j]; // new cost, cost from i to k plus cost from k to
+																		// j.
 					// Since we are assuming that path from to j passes through k.
-					
-					if (nc<oc) {
+
+					if (nc < oc) {
 						answerMatrix[i][j] = nc;
 					}
 				}
 			}
-			
+
 		}
 
-		display (v, ans);
+		display(v, answerMatrix);
 	}
 
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Enter no. of vertices in the Graph.");
-		int v=sc.nextInt();
-		
-		int[][] graph=new int[v][v];
+		Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter costs of edges in the Graph.");
-		
-		for (int i=0; i<v; i++) {
-			for (int j=0; j<v; j++) {
-				graph[i][j]=sc.nextInt();
+		System.out.println("Enter no. of vertices in the Graph.");
+		int v = sc.nextInt();
+
+		int[][] graph = new int[v][v];
+
+		System.out.println("Enter costs of edges in the Graph.");
+
+		for (int i = 0; i < v; i++) {
+			for (int j = 0; j < v; j++) {
+				graph[i][j] = sc.nextInt();
 			}
 		}
-		
-        floydWarshallAlgorithm (v, graph);  
+
+		floydWarshallAlgorithm(v, graph);
 	}
 }
 

e2ac2f5407bfa6c44106092fb53c824cf1e5dbdd
Anuj Agrawal <anujagr2001@gmail.com>
2021-04-02 12:26:52 +0530

    Update variable and Format properly

--- C-Plus-Plus/cp/ReconstructOriginalDigitsFromEnglish.cpp
+++ C-Plus-Plus/cp/ReconstructOriginalDigitsFromEnglish.cpp
@@ -1,76 +1,69 @@
 /*
 Given a non-empty string containing
-an out-of-order English representation
-of digits 0-9, output the digits in 
-ascending order.
+an out-of-order English representa-
+tion of digits 0-9,output the digits
+in ascending order.
 Note:
 Input contains only lowercase English
 letters.
 Input is guaranteed to be valid and 
 can be transformed to its original 
 digits.That means invalid inputs such
-as "abc" or "zerone" are not permitted.
+as "abc" or "zeroe" are not permitted.
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 string EnglishToDigit(string English)
 {
-	long long int X,i,Z,U,Max;
-    string S,Ans;
-    vector<pair<string,int>> M;
-    X=English.length();
-    int A[26]={0};
-    for(i=0;i<X;i++)
-    {
-        A[English[i]-'a']++;
+    long long int L, i, Z, Max;
+    string Temp, Digit;
+    vector<pair<string, int> > M;
+    L = English.length();
+    int count[26] = { 0 };
+    for (i = 0; i < L; i++) {
+        count[English[i] - 'a']++;
     }
-    M.push_back({"six",6});
-    M.push_back({"zero",0});
-    M.push_back({"seven",7});
-    M.push_back({"eight",8});
-    M.push_back({"five",5});
-    M.push_back({"four",4});
-    M.push_back({"three",3});
-    M.push_back({"two",2});
-    M.push_back({"one",1});
-    M.push_back({"nine",9});
-    for(auto it=M.begin();it!=M.end();it++)
-    {
-        Max=INT_MAX;
-        S=it->first;
-        Z=S.length();
-        U=0;
-        for(i=0;i<Z;i++)
-        {
-            if(A[S[i]-'a']<Max)
-            {
-                Max=A[S[i]-'a'];
+    M.push_back({ "six", 6 });
+    M.push_back({ "zero", 0 });
+    M.push_back({ "seven", 7 });
+    M.push_back({ "eight", 8 });
+    M.push_back({ "five", 5 });
+    M.push_back({ "four", 4 });
+    M.push_back({ "three", 3 });
+    M.push_back({ "two", 2 });
+    M.push_back({ "one", 1 });
+    M.push_back({ "nine", 9 });
+    for (auto it = M.begin(); it != M.end(); it++) {
+        Max = INT_MAX;
+        Temp = it->first;
+        Z = Temp.length();
+        for (i = 0; i < Z; i++) {
+            if (count[Temp[i] - 'a'] < Max) {
+                Max = count[Temp[i] - 'a'];
             }
         }
-        for(i=0;i<Max;i++)
-        {
-            Ans+=('0'+it->second);
+        for (i = 0; i < Max; i++) {
+            Digit += ('0' + it->second);
         }
-        for(i=0;i<Z;i++)
-        {
-            A[S[i]-'a']-=Max;
+        for (i = 0; i < Z; i++) {
+            count[Temp[i] - 'a'] -= Max;
         }
     }
-    sort(Ans.begin(),Ans.end());
-    return Ans;
+    sort(Digit.begin(), Digit.end());
+    return Digit;
 }
 
 int main()
 {
-	string S,Ans;
-	cout<<"Enter the String"<<endl;
-	cin>>S;
-	Ans=EnglishToDigit(S);
-	cout<<"Coverted String"<<endl;
-	cout<<Ans<<endl;
-	return 0;
+    string English, Digit;
+    cout << "Enter the String" << endl;
+    cin >> English;
+    Digit = EnglishToDigit(English);
+    cout << "Coverted String" << endl;
+    cout << Digit << endl;
+    return 0;
 }
 
 /*
@@ -87,4 +80,3 @@ Output:
 Coverted String
 012
 */
-

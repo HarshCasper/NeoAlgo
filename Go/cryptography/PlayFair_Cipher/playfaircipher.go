35ba5779f3fd0864571c9ce601bbf93faddb12c2
Naga srihith Penjarla <57094227+nagasrihith608@users.noreply.github.com>
2021-04-03 10:30:39 +0300

    formatted

--- Go/cryptography/PlayFair_Cipher/playfaircipher.go
+++ Go/cryptography/PlayFair_Cipher/playfaircipher.go
@@ -39,23 +39,15 @@ func keyTable() {
      var hash[100] int;
      //marking the alphabets in key
      for i=0;i<len(key);i++ {
-
          if(key[i] != 'j'){
-
             for j=0;j<26;j++ {
-
                 if(key[i] == alph[j]){
-
                    index[j] = 2;
                    hash[c] = j;
                    c++;
-
                 }
-
             }
-
          }
-
      }
 
      //marking alphabetic j
@@ -65,9 +57,7 @@ func keyTable() {
 
      //adding the marked alphabets  
      for k:=0;k<len(key);k++ {
-
          if(index[hash[k]] == 2) {
-
             index[hash[k]] -= 1;
             keyMatrix[i][j] = string(key[k]);
             j++;
@@ -75,32 +65,22 @@ func keyTable() {
             if(j==5){
               i++;
               j=0;
-
             }
-
          }
-
      }
 
      //adding the remaining alphabets
      for  k:=0;k<26;k++ {
-
          if(index[k] == 0){
-
             keyMatrix[i][j] = string(alph[k]);
             j++;
 
             if(j==5){
-
                i++;
                j=0;
-
             }
-
          }
-
      }
-
 }
 
 /* This function searches for the characters of the digraph in key matrix
@@ -109,78 +89,53 @@ func search(x,y string) {
 
      //replacing j with i   
      if(x == "j'") {
-
          x = "i";
-
      }else if(y =="j") {
-
          y = "i";
-
      }
 
      //returning the positions of characters through list    
      for i:=0;i<5;i++ {
-
          for j:=0;j<5;j++ {
-
              if(keyMatrix[i][j] == x) {
-
                 list[0] = i;
                 list[1] = j;
-
              }else if(keyMatrix[i][j] == y) {
-
                 list[2] = i;
                 list[3] = j;
-
              }
          }
-
       }
-
-
 }
 
 // This function ciphers the plane text 
 func cipher() {
 
      i := 0;
-
      //cipher the plain text with the key matrix we built
      for i<len(plaintext) {
 
          search(string(plaintext[i]),string(plaintext[i+1]))
 
          if(list[0] == list[2]) {
-
             output += string(keyMatrix[list[0]][(list[1]+1)%5]);
             output += string(keyMatrix[list[0]][(list[3]+1)%5]);
-
          } else if(list[1] == list[3]){
-
             output += string(keyMatrix[(list[0]+1)%5][list[1]]);
             output += string(keyMatrix[(list[2]+1)%5][list[1]]);
-
          }else{
-
             output += string(keyMatrix[list[0]][list[3]]);
             output += string(keyMatrix[list[2]][list[1]]);
-
          }
-
          i+=2;
      }
-
-
 }
 
 // This function will encript the plaintext to cipher in playfail cipher
 func playfaircipher() {
 
     if(len(plaintext)%2 != 0 ){
-
          plaintext += "z";
-
     }
 
     //generating key table
@@ -221,4 +176,3 @@ func main() {
      The cipher text is : kbekdv 
 
 */
-

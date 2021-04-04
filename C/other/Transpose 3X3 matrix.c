694f8bab15102420b1aa68db1c0f6689433c37bc
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-04-04 20:34:57 +0530

    Create Transpose 3X3 matrix.c

---
+++ C/other/Transpose 3X3 matrix.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ 
+void main()
+{
+    static int array[10][10];
+    int i, j, m, n;
+ 
+    printf("Enter the order of the matrix \n");
+    scanf("%d %d", &m, &n);
+    printf("Enter the coefiicients of the matrix\n");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            scanf("%d", &array[i][j]);
+        }
+    }
+    printf("The given matrix is \n");
+    for (i = 0; i < m; ++i)
+    {
+        for (j = 0; j < n; ++j)
+        {
+            printf(" %d", array[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Transpose of matrix is \n");
+    for (j = 0; j < n; ++j)
+    {
+        for (i = 0; i < m; ++i)
+        {
+            printf(" %d", array[i][j]);
+        }
+        printf("\n");
+    }
+}

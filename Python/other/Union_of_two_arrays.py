25ce2d39c4e26581f80f1c8950d7d512dedda63c
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-04-04 16:13:41 +0530

    modified:   Python/other/Union_of_two_arrays.py

--- Python/other/Union_of_two_arrays.py
+++ Python/other/Union_of_two_arrays.py
@@ -1,19 +1,26 @@
+# Find array union of two arrays
 def union_of_arrays(arr1, arr2):
+    # let's sort both arrays
     arr1.sort()
     arr2.sort()
-
     
+    # iterators for arr1, arr2
     firstelement = 0
     secondelement  = 0
+    # result
     union = []
 
+    # then go along both arrays
     while firstelement < len(arr1) and secondelement < len(arr2):
+        # if an element is lower than another, increment iterator in that array
         if arr1[firstelement] < arr2[secondelement]:
             firstelement += 1
         elif arr1[firstelement] > arr2[secondelement]:
             secondelement += 1
         else:
-            union.append(arr1[firstelement])  
+            # if we find equal elements
+            union.append(arr1[firstelement]) # add element to the result
+            # increment iterators of both arrays 
             firstelement += 1
             secondelement += 1
 
@@ -28,6 +35,7 @@ if __name__ == "__main__":
 '''
 Time Complexity : O((n+m)log(n+m))
 Space Complexity : O(n+m)
+
 INPUT:
     Enter the list 1: 
     1 2 3 2 0
@@ -36,5 +44,4 @@ INPUT:
 
 OUTPUT  
     [1, 2, 2, 3]
-
 '''

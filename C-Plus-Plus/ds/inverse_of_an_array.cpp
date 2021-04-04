d7ed121db9238b935e537c65cc33b9523c1b8f72
Amit Pandey <76877421+Op-panda@users.noreply.github.com>
2021-04-04 09:06:42 +0530

    Update inverse_of_an_array.cpp

--- C-Plus-Plus/ds/inverse_of_an_array.cpp
+++ C-Plus-Plus/ds/inverse_of_an_array.cpp
@@ -1,20 +1,11 @@
 /*
 INVERSE OF AN ARRAY   
-/*
 If the array elements are swapped with their corresponding indices,
 the array finally results is inverse of an array. 
 */
 
-
 #include <bits/stdc++.h>
 using namespace std;
-
-// ALGORITHM -->
-// create two arrays, ori and inv.
-// give input for ori array.
-// create a temporary variable then, and store the element from the original array.
-// In inv array for each value of temp, store the index of ori array.
-// Display the inverse array.
 
 void inverse(int a[], int n, int b[])
 {
@@ -45,19 +36,15 @@ int main()
     }
     return 0;
 }
-// Driver code ends
-
 
 /*
-time complexity -: O(n)
- 
+time complexity -: O(n) 
 Sample input:
 2
 5
 4 0 2 3 1
 3
 0 1 2
-
 Sample Output
 1 4 2 3 0
 0 1 2

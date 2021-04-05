d5c2589da868148b17d9c54b7104a592a7a012eb
tushar khandelwal <tusharkhandelwal315@gmail.com>
2021-04-05 20:59:04 +0530

    requested changes done

--- JavaScript/math/gcd_of_two_no.js
+++ JavaScript/math/gcd_of_two_no.js
@@ -3,21 +3,18 @@ A program to find Greatest Common Divisor(GCD) of two Numbers.
 GCD of two integers is the largest positive integer that divides each of the integers.
 */
 
-//Take input from the user
-var number1=prompt("Enter 1st Number:");
-var number2=prompt("Enter 2nd Number:");
+var number1 = 10;
+var number2 = 15;
 
 //Smaller of the two number
-var smaller_number=Math.min(number1,number2);
+var smaller_number = Math.min(number1, number2);
 
 //Iterating from smaller number till 1
-for(var i=smaller_number;i>=1;i--)
-{
-	if(number1%i==0 && number2%i==0)
-	{
-		console.log("HCF: "+i);
-		break;
-	}
+for (var i = smaller_number; i >= 1; i--) {
+  if (number1 % i == 0 && number2 % i == 0) {
+    console.log("HCF: " + i);
+    break;
+  }
 }
 
 /*

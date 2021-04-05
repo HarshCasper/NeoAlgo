6becb886035bf6bf18a53a17d8ec98466d05feed
Ruchi Sahu <sahu2101ruchi@gmail.com>
2021-04-05 12:03:41 -1100

    Left Aligned

--- Java/cp/Prime_range.java
+++ Java/cp/Prime_range.java
@@ -1,41 +1,41 @@
-  /*
+/*
   A positive integer greater than 1 which has no other
   factors except 1 and the number itself is called a prime number.
   2, 3, 5, 7 etc. are prime numbers as they do not have any other factors.
-  */
-  import java.util.Scanner;
-  public class Prime 
+*/
+import java.util.Scanner;
+public class Prime
+{
+  public Prime(int first_range, int second_range)
   {
-      public  Prime(int first_range, int second_range)
+    for (int i = first_range; i <= second_range; i++)
+    {
+      int put = 1;
+      for (int j = 2; j < i / 2; j++)
       {
-            for(int i=first_range;i<=second_range;i++)
-            {
-                 int put=1;
-                 for(int j=2;j<i/2;j++)
-                 {
-                      if(i%j==0)
-                      {
-                          put=0;
-                          break;
-                      }
-                }
-                if(put==1)
-                {
-                    System.out.println(i);
-                }
-            }
-      }
-  public static void main(String[] args)
-  {
-      Scanner scan=new Scanner(System.in);
-      System.out.println("Please enter range..");
-      int first_range=scan.nextInt();
-      int second_range=scan.nextInt();
-      Prime out=new Prime(first_range,second_range);
-  }
-  }
+        if (i % j == 0)
+        {
+          put = 0;
+          break;
+				}
+			}
+      if (put == 1)
+      {
+        System.out.println(i);
+			}
+		}
+	}
+public static void main(String[] args)
+{
+  Scanner scan = new Scanner(System. in );
+  System.out.println("Please enter range..");
+  int first_range = scan.nextInt();
+  int second_range = scan.nextInt();
+  Prime out = new Prime(first_range, second_range);
+	}
+}
 
-  /*
+/*
   Time complexity is O(n)
   Space complexity is O(n)
 

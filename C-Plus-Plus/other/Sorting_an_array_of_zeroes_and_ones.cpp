6eb892909781c66f4e3dfc150bf8d9f6efab7846
iaditichine <58857630+iaditichine@users.noreply.github.com>
2021-04-05 19:07:50 +0530

    Update Sorting_an_array_of_zeroes_and_ones.cpp

--- C-Plus-Plus/other/Sorting_an_array_of_zeroes_and_ones.cpp
+++ C-Plus-Plus/other/Sorting_an_array_of_zeroes_and_ones.cpp
@@ -3,74 +3,69 @@
 Given an array of size n which contains only 0's and 1's
 in any order,we have to modify the array in such a way
 that 0's are on left side and 1's on the right side.
-Test case-1:
-Input-1:
-N = 5
-arr[] = {0, 0, 1, 1, 0}
-Output-1: 0 0 0 1 1
 
 */
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 //Function to sort an array of 0's and 1's
 void sort()
 {
 	
-    int n;
-    cin >> n;
-    int a[n];
-    //Input elements in array
-    for (int i = 0; i < n; i++)
+    int size_of_array;
+    cin >> size_of_array;
+    int array[size_of_array];
+    for (int i = 0; i < size_of_array; i++)
     {
-        cin >> a[i];
+        cin >> array[i];
     }
     /*  Two counters to count number of 0's and 1's respectively
 	    which are initialized to 0*/
-    int c0 = 0, c1 = 0;
-    for (int i = 0; i < n; i++)
+    int counter_for_0 = 0, counter_for_1 = 0;
+    for (int i = 0; i < size_of_array; i++)
     {
-    	//checking whether the array element is equal to 0
-        if (a[i] == 0)
+    	
+        if (array[i] == 0)
         {
-            c0++;
+            counter_for_0++;
             continue;
         }
 
-        c1++;
+        counter_for_1++;
     }
-    while (c0--)
+    while (counter_for_0--)
     {
-    	//printing 0 for c0 times
+    	
         cout << 0 << " "; 
     }
-    while (c1--)
+    while (counter_for_1--)
     {
-    	//printing 1 for c1 times
+    	
         cout << 1 << " "; 
     }
     cout << endl;
 }
 int main()
 {
-    int T;
-    cin >> T;
-    while (T--)
-        sort(); 
+    
+    sort(); 
     return 0;
 }
 /*
-Test case-1:
+
 Input:
-N = 4
-Arr[] = {0, 0, 0, 0}
+size_of_array= 4
+array[] = {0, 0, 0, 0}
 Output: 0 0 0 0
 There are no 1's in the given array,
 so the modified array is 0 0 0 0.
 
-Test case-2:
+
 Input:
-N==6
-arr[]={0,1,1,0,1,0}
+size_of_array==6
+array[]={0,1,1,0,1,0}
 Output:0 0 0 1 1 1
+
+Space Complexity-O(N)
+Time Complexity-0(1)
 */

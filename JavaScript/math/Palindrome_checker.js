b16bfa3d2423fe8def0b55fc981739957dc36e14
TUSHAR KHANDELWAL <56471793+tk315@users.noreply.github.com>
2021-04-05 21:31:25 +0530

    Requested changes done

--- JavaScript/math/Palindrome_checker.js
+++ JavaScript/math/Palindrome_checker.js
@@ -3,21 +3,21 @@ Write a program to check whether a given number is palindrome or not.
 Palindrome is a number which reads the same backward as forward.
 */
 
-// take input from the user
-let no = parseInt(prompt('Enter a no which needs to be checked for palindrome: '));
+function palindromechecker(no) {
+  let temp = no;
+  let reverse = 0;
 
-let temp = no;
-let reverse=0;
+  //To reverse the number
+  while (temp !== 0) {
+    reverse = reverse * 10 + (temp % 10);
+    temp = parseInt(temp / 10);
+  }
 
-//To reverse the number
-while(temp!==0)
-{
-    reverse=reverse*10+temp%10;
-    temp=parseInt(temp/10);
+  if (no === reverse) console.log("Entered Number is a Palindrome");
+  else console.log("Entered Number is not a Palindrome");
 }
 
-if(no===reverse)console.log("Entered Number is a Palindrome");
-else console.log("Entered Number is not a Palindrome");
+palindromechecker(131);
 
 /*
 

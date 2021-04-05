a38f098e4c18a9eb7d7c74129cb466f727dbe2e7
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-04-05 23:51:36 +0530

    Create Transpose 3X3 matrix.java

---
+++ Java/other/Transpose 3X3 matrix.java
@@ -0,0 +1,31 @@
+public class Transpose {
+
+    public static void main(String[] args) {
+        int row = 2, column = 3;
+        int[][] matrix = { {2, 3, 4}, {5, 6, 4} };
+
+        // Display current matrix
+        display(matrix);
+
+        // Transpose the matrix
+        int[][] transpose = new int[column][row];
+        for(int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        // Display transposed matrix
+        display(transpose);
+    }
+
+    public static void display(int[][] matrix) {
+        System.out.println("The matrix is: ");
+        for(int[] row : matrix) {
+            for (int column : row) {
+                System.out.print(column + "    ");
+            }
+            System.out.println();
+        }
+    }
+}

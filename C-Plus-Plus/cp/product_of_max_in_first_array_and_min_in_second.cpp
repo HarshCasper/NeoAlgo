e228f477c525bb1cfa0f9b9f971319b824921247
Your Name <you@example.com>
2021-04-05 21:58:26 +0530

    made new changes

--- C-Plus-Plus/cp/product_of_max_in_first_array_and_min_in_second.cpp
+++ C-Plus-Plus/cp/product_of_max_in_first_array_and_min_in_second.cpp
@@ -1,12 +1,11 @@
-/*Given two arrays:arr1[] and arr2[] of different size i.e.,no_of_ele_in_arr1 amd no_of_ele_in_arr2 respectively.
+/*Given two arrays:arr1[] and arr2[] of different sizes.
 Task :calculate the product of max element of first array i.e.,arr1[] and min element of second array i.e.,arr2[]
-Example:we are given two arrays arr1={1,2,3,4} and arr2={5,6,7,8,9} ,so the maximum element in array arr1 is 4 and minimum element in array arr2 is 5.Hence,the product of max element in array arr1 and min element in array arr2 is 4*5=20.
 
 Approach:
-->take two arrays as input.
-->using std library of cpp we will find the maximum element in array arr1 and minimum element in array arr2
-->after getting the max and min element we will multiply them.
-->return the product.
+-take two arrays as input.
+-using std library of cpp we will find the maximum element in array arr1 and minimum element in array arr2
+-after getting the max and min element we will multiply them.
+-return the product.
 */
 
 
@@ -44,11 +43,11 @@ int main()
     
     
     int prod=find_product(arr1,arr2,no_of_ele_in_arr1,no_of_ele_in_arr2);
-    cout<<prod;
+    cout<<prod<<endl;
     return 0;
 }
 
-/*Sample Input1 : 
+/*Sample Input : 
 enter the no. of elements in array 1:
 3
 enter the elements of array 1:
@@ -58,23 +57,12 @@ enter the no. of elements in array 2:
 enter the elements of array 2:
 5 7 3 1 9
 
-Sample Output1 :
+Sample Output :
 product of max element of first array and min element of second array: 3
--------------------------------------------------------  
-Sample Input2 : 
-enter the no. of elements in array 1:
-5
-enter the elements of array 1:
-7, -3, 5, -8, 4
-enter the no. of elements in array 2:
-5
-enter the elements of array 2:
-9, 8, 6, -4, 2
 
-Sample Output2 :
-product of max element of first array and min element of second array: -28 
  
 Time Complexity : O(n)
 Auxiliary Space : O(1) 
+
 */
   

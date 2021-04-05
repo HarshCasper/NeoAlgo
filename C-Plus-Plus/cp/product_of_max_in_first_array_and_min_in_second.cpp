33f1c2dd76046a53625f16e30c60eca1cdb35aae
Your Name <you@example.com>
2021-04-05 20:54:46 +0530

    I had done required changes

--- C-Plus-Plus/cp/product_of_max_in_first_array_and_min_in_second.cpp
+++ C-Plus-Plus/cp/product_of_max_in_first_array_and_min_in_second.cpp
@@ -1,47 +1,80 @@
-/*Given two arrays, the task is to calculate the product of max element of first array and min element of second array*/
+/*Given two arrays:arr1[] and arr2[] of different size i.e.,no_of_ele_in_arr1 amd no_of_ele_in_arr2 respectively.
+Task :calculate the product of max element of first array i.e.,arr1[] and min element of second array i.e.,arr2[]
+Example:we are given two arrays arr1={1,2,3,4} and arr2={5,6,7,8,9} ,so the maximum element in array arr1 is 4 and minimum element in array arr2 is 5.Hence,the product of max element in array arr1 and min element in array arr2 is 4*5=20.
 
-/*Given two arrays, the task is to calculate the product of max element of first array and min element of second array*/
+Approach:
+->take two arrays as input.
+->using std library of cpp we will find the maximum element in array arr1 and minimum element in array arr2
+->after getting the max and min element we will multiply them.
+->return the product.
+*/
+
+
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int find_product(int a[], int b[], int n, int m)
+int find_product(int arr1[], int arr2[], int no_of_ele_in_arr1, int no_of_ele_in_arr2)
     {
-        //main logic 
-        int num1=*max_element(a, a+ n);
-        int num2=*min_element(b, b+ m);
+        
+        int num1=*max_element(arr1, arr1+ no_of_ele_in_arr1);
+        int num2=*min_element(arr2, arr2+ no_of_ele_in_arr2);
         int r=num1*num2;
         return r;
     }
-//main function
+
 int main()
 {
-    //initialising and declaring two array 
-    int a[] = { 7, 3, 5, 8, 4};
-    int b[] = { 9, 8, 6, 4, 2};
+    int no_of_ele_in_arr1,no_of_ele_in_arr2,arr1[no_of_ele_in_arr1],arr2[no_of_ele_in_arr2];
+    cout<<"enter the no. of elements in array 1"<<endl; 
+    cin>>no_of_ele_in_arr1;
+    cout<<"enter the elements of array 1"<<endl;
+    for(int i=0;i<no_of_ele_in_arr1;i++)
+    {
+        cin>>arr1[i];
+    }
     
-    //calculating the number of elements in the two array
-    int n = sizeof(a) / sizeof(a[0]);
-    int m = sizeof(b) / sizeof(b[0]);
-    int prod=find_product(a,b,n,m);
+    cout<<"enter the no. of elements in array 2"<<endl; 
+    cin>>no_of_ele_in_arr2;
+    cout<<"enter the elements of array 2"<<endl;
+    for(int i=0;i<no_of_ele_in_arr2;i++)
+    {
+        cin>>arr2[i];
+    }
+    
+    
+    int prod=find_product(arr1,arr2,no_of_ele_in_arr1,no_of_ele_in_arr2);
     cout<<prod;
     return 0;
 }
 
-/* Input1 : a[] = { 7, 3, 5, 8, 4},  
-           b[] { 9, 8, 6, 4, 2}
+/*Sample Input1 : 
+enter the no. of elements in array 1:
+3
+enter the elements of array 1:
+1 2 3
+enter the no. of elements in array 2:
+5
+enter the elements of array 2:
+5 7 3 1 9
 
-  Output : 16
-  max element in first array 
-  is 8 and min element in second array 
-  is 2. The product of these two is 16.
-  
-  Input1 : a[] = { 7, -3, 5, -8, 4},  
-           b[] = { 9, 8, 6, -4, 2}
+Sample Output1 :
+product of max element of first array and min element of second array: 3
+-------------------------------------------------------  
+Sample Input2 : 
+enter the no. of elements in array 1:
+5
+enter the elements of array 1:
+7, -3, 5, -8, 4
+enter the no. of elements in array 2:
+5
+enter the elements of array 2:
+9, 8, 6, -4, 2
 
-  Output : -28
-  max element in first array 
-  is 7 and min element in second array 
-  is -4. The product of these two is -28.
+Sample Output2 :
+product of max element of first array and min element of second array: -28 
+ 
+Time Complexity : O(n)
+Auxiliary Space : O(1) 
 */
   

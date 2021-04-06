2b05f74992b20653c82d6658a4462be968c3dba3
Diksha Joshi <58946705+dikshajoshi18@users.noreply.github.com>
2021-04-06 18:37:52 +0530

    Update kadane's_algo.js

--- JavaScript/Algorithms/kadane's_algo.js
+++ JavaScript/Algorithms/kadane's_algo.js
@@ -1,62 +1,31 @@
-
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-
-
-function main() {
-    let t = parseInt(readLine());
-    for(let i=0;i<t;i++)
-    {
-        let input_line = readLine().split(' ');
-        let N = parseInt(input_line[0]);
-        input_line = readLine().split(' ').map(x=>parseInt(x));
-        let arr = new Array(N);
-        for(let i=0;i<N;i++){
-            arr[i]=input_line[i];
-        }
-        let obj = new Solution();
-        let ans = obj.kadanesalgo(arr, N);
-        if(ans==0)
-            ans=0;
-        console.log(ans);
-    }
-}
-
-
-
 class Solution {
     
-    kadanesalgo(arr, N){
+    kadanesalgo(arr){
         let maxh = 0, maxf = arr[0];
-        
+        let N=arr.length;     
 
         for(let i=0;i<N;i++) 
         {
-            //Updating max sum till current index.
             maxh+=arr[i];
             //Storing max sum so far by choosing maximum between max 
             //sum so far and max sum till current index.
             maxf=Math.max(maxh,maxf);
-            
-            //If max sum till current index is negative, we do not need to add
-            //it to result so we update it to zero.
+           
             if(maxh<0)
                 maxh=0;
         }
-         //returning the result.
+
         return maxf;
     } 
 }
+
+
+var arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+let obj = new Solution();
+let ans = obj.kadanesalgo(arr);
+if(ans===0)
+    ans=0;
+console.log(ans);
 // solved example
 // Input:
 // N = 4

f032eaf7aecab16ef1dd50fc0278e29f80d3b535
Yashmita <55138349+yashmita@users.noreply.github.com>
2021-04-07 00:27:41 +0530

    Update loop_detection_in_LL.py

--- Python/ds/loop_detection_in_LL.py
+++ Python/ds/loop_detection_in_LL.py
@@ -1,14 +1,14 @@
 '''
 PROBLEM STATEMENT:
-Given a linked list, the task is to find whether the linked list consists of 
+Given a linked list, the task is to find whether the linked list consists of
 a loop or not.
 The user will enter the elements in a linked list and will specify whether
 the tail of the linked list is pointed to some other node or to NULL.
 If the tail is pointing to NULL, the user will input -1 in 'pos' variable,
 otherwise, the user will input the index of the node to which the tail pointer
 will point.
-'pos' variable is only used to create a linked list as per the data entered 
-by the user, and is not passed to the function for detecting a loop, 
+'pos' variable is only used to create a linked list as per the data entered
+by the user, and is not passed to the function for detecting a loop,
 it is being detected by using Floyd's cycle detection algorithm.
 '''
 
@@ -19,37 +19,41 @@ class Node:
 		self.val = val
 		self.next = None
 
+
 class LinkedList:
 	# Constructor to initialize head and variable pointer
 	def __init__(self):
 		self.head = None
 		self.var = None
 
+
 	# Function to create a linked list
 	def push(self, newval):
 		newnode = Node(newval)
-		if self.var :
+		if self.var:
 			self.var.next = newnode
 			self.var = newnode
-		else :
+		else:
 			self.head = newnode
 			self.var = newnode
 		return newnode
 
+
 	# Function to create a loop
-	def CreateLoop(self, pos, tail):
+	def CreateLoop(self, idx, last):
 		temp = self.head
-		if pos >= 0 :
-			while pos > 0 :
+		if idx >= 0:
+			while idx > 0:
 				temp = temp.next
-				pos -= 1
-			tail.next = temp
+				idx -= 1
+			last.next = temp
+
 
 	# Function for detecting loop using Floyd's cycle detection algorithm
 	def LoopDetect(self):
 		slow = self.head
 		fast = self.head
-		while slow and fast and fast.next :
+		while slow and fast and fast.next:
 			slow = slow.next
 			fast = fast.next.next
 			if slow == fast:
@@ -58,40 +62,37 @@ class LinkedList:
 		print("Loop not found!")
 
 
-
 # Driver Code
-n = int(input("Enter the size of linked list: ")) 
+n = int(input("Enter the size of linked list: "))
 ll = LinkedList()
 print("Enter elements:")
 for i in range(n):
 	# tail stores the last node of the linked list
-    tail = ll.push(int(input()))
+	tail = ll.push(int(input()))
 # if the user wants to enter a linked list with a loop, pos specifies the index
 # of the node which will be pointed by the tail
 # For no loop, pos will take -1
-pos = int(input("Enter the index of the node which has to be pointed by tail, else enter -1: "))
+pos = int(input("Index of node which is connected to tail, else enter -1: "))
 ll.CreateLoop(pos, tail)
 ll.LoopDetect()
 
 
-'''
-TEST CASES:
-1.
-Input:
-n = 5
-1 2 3 4 5
-pos = 2
-Output:
-Loop Detected!
+# TEST CASES:
+# 1.
+# Input:
+# n = 5
+# 1 2 3 4 5
+# pos = 2
+# Output:
+# Loop Detected!
 
-2.
-n= 4
-6 8 2 1
-pos = -1
-Output:
-Loop not found!
+# 2.
+# n= 4
+# 6 8 2 1
+# pos = -1
+# Output:
+# Loop not found!
 
-TIME COMPLEXITY: O(n), to traverse the linked list
-where 'n' denotes the length of the linked list.
-SPACE COMPLEXITY: O(1), no extra space used.
-'''
+# TIME COMPLEXITY: O(n), to traverse the linked list
+# where 'n' denotes the length of the linked list.
+# SPACE COMPLEXITY: O(1), no extra space used.

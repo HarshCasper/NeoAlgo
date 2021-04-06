518df76edde000c75449bebead3cd35cbb851d65
iaditichine <58857630+iaditichine@users.noreply.github.com>
2021-04-06 18:07:22 +0530

    Update Sorting_an_array_of_zeroes_and_ones.cpp

--- C-Plus-Plus/other/Sorting_an_array_of_zeroes_and_ones.cpp
+++ C-Plus-Plus/other/Sorting_an_array_of_zeroes_and_ones.cpp
@@ -11,10 +11,11 @@ using namespace std;
 //Function to sort an array of 0's and 1's
 void sort()
 {
-	
     int size_of_array;
+    cout<<"Enter size of array: ";
     cin >> size_of_array;
     int array[size_of_array];
+    cout<<"Enter array elements: ";
     for (int i = 0; i < size_of_array; i++)
     {
         cin >> array[i];
@@ -24,47 +25,36 @@ void sort()
     int counter_for_0 = 0, counter_for_1 = 0;
     for (int i = 0; i < size_of_array; i++)
     {
-    	
         if (array[i] == 0)
         {
-            counter_for_0++;
+		    counter_for_0++;
             continue;
         }
-
         counter_for_1++;
     }
+    cout<<"Sorted array of 0's and 1's is: ";
     while (counter_for_0--)
     {
-    	
-        cout << 0 << " "; 
+	    cout << 0 << " "; 
     }
     while (counter_for_1--)
     {
-    	
         cout << 1 << " "; 
     }
     cout << endl;
 }
+
 int main()
 {
-    
     sort(); 
     return 0;
 }
 /*
-
 Input:
-size_of_array= 4
-array[] = {0, 0, 0, 0}
-Output: 0 0 0 0
-There are no 1's in the given array,
-so the modified array is 0 0 0 0.
-
-
-Input:
-size_of_array==6
-array[]={0,1,1,0,1,0}
-Output:0 0 0 1 1 1
+Enter size of array: 6
+Enter array elements: 0 1 0 1
+Output:
+Sorted array of 0's and 1's is: 0 0 1 1
 
 Space Complexity-O(N)
 Time Complexity-0(1)

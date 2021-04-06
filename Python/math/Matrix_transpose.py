53cfa87f133ea476d2a39f0d0d901f9e312efa53
aaadddiii <adithya.kurien@gmail.com>
2021-04-06 18:56:30 +0530

    some changes

--- Python/math/Matrix_transpose.py
+++ Python/math/Matrix_transpose.py
@@ -1,3 +1,7 @@
+# Transpose of a matrix is a matrix obtained by changing rows to columns and columns
+# to rows.In other words, transpose of Arr[][] is obtained by changing Arr[i][j] to Arr[j][i].
+# The below program finds transpose of matrix[][] and stores the result in result[][]
+
 Row = int(input("Enter the number of rows : "))
 Col = int(input("Enter the number of columns : "))
 
@@ -18,20 +22,19 @@ for i in range(Row):
         print(result[i][j], end=" ")
     print()
 
-"""
-Sample I/O :
 
-Enter the number of rows : 3
-Enter the number of columns : 3
-Enter the entries rowwise:
-1 2 3
-4 5 6
-7 8 9
-The Transpose of the matrix is
-1 4 7
-2 5 8
-3 6 9
+# Sample I/O :
 
-Time complexity : O(n^2)
-Space complexity : O(n^2)
-"""
+# Enter the number of rows : 3
+# Enter the number of columns : 3
+# Enter the entries rowwise:
+# 1 2 3
+# 4 5 6
+# 7 8 9
+# The Transpose of the matrix is
+# 1 4 7
+# 2 5 8
+# 3 6 9
+
+# Time complexity : O(n^2)
+# Space complexity : O(n^2)

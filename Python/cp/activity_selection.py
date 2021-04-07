de25e4f8ac575e6f22e5aacd1c43092ff0cb343e
Shahina <56913578+shahina-bano@users.noreply.github.com>
2021-04-07 23:07:06 +0530

    Add files via upload

---
+++ Python/cp/activity_selection.py
@@ -0,0 +1,79 @@
+'''
+Activity Selection Problem :
+Given "row" number of activities with start and end time,
+select activities in such manner that we can perform maximum
+number of activities among them. Note, activities are not sorted
+'''
+
+
+def final_activities():
+    row = int(input())
+    # 2d list is used to enter the start and end time of jobs
+    Activity = []
+    print("Enter elements of matrix:")
+    for x in range(row):
+        arr = []
+        for y in range(0, 2):
+            arr.append(int(input()))
+        Activity.append(arr)
+    # sorting the 2d list according to the second key
+    Activity.sort(key=lambda sec: sec[1])
+
+    ptr = 0
+    # matrix for activities selceted
+    activity_selected = []
+    # initial activity is always selected
+    activity_selected.append(Activity[ptr])
+
+    # If the starting time of next activity is greater than/equal
+    # to the end time of previous, then it will be selected
+    for itr in range(1, row):
+        if Activity[ptr][1] <= Activity[itr][0]:
+            activity_selected.append(Activity[itr])
+            ptr = itr
+    # printing the selected activities
+    print("Following activities are selected :")
+    print(activity_selected)
+
+
+if __name__ == "__main__":
+    final_activities()
+
+
+'''
+Test Cases:
+1.
+Input:
+4
+1
+2
+2
+4
+3
+5
+4
+6
+
+Output:
+Enter elements of matrix:
+Following activities are selected :
+[[1, 2], [2, 4], [4, 6]]
+
+2.
+Input:
+4
+1
+2
+2
+3
+3
+4
+4
+6
+
+Output:
+Enter elements of matrix:
+Following activities are selected :
+[[1, 2], [2, 3], [3, 4], [4, 6]]
+
+'''

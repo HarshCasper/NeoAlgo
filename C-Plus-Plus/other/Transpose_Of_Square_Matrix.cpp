a51bcca0da9828b36593289f67576a3d16de71d5
somya khandelwal <somyakhandelwal570@gmail.com>
2021-04-07 10:07:21 +0530

    Updated

--- C-Plus-Plus/other/Transpose_Of_Square_Matrix.cpp
+++ C-Plus-Plus/other/Transpose_Of_Square_Matrix.cpp
@@ -1,53 +1,56 @@
-// Transpose of a square matrix is obtained by changing the row to columna and vice-versa.
+// Transpose of a square matrix is obtained by changing the rows to columns and vice-versa.
 #include <bits/stdc++.h>
 using namespace std;
 
-#define N 3
 
-void transpose(int a[][N]){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
+
+void transpose(int *a,int N){
+    int i,j;
+    for( i=0;i<N;i++){
+        for( j=i+1;j<N;j++){
             int temp;
-            temp = a[j][i];
-            a[j][i] = a[i][j];
-            a[i][j] = temp;
+            temp = *(a+ j *N + i);
+            *(a+ j *N + i) = *(a+ i *N + j);
+            *(a+ i *N + j) = temp;
         }
     }
 }
 
 int main() {
-    // int n;
-    // cout<<"Enter the dimension of the matrix:";
-    // cin>>n;
+    int N;
+    cout<<"Enter the dimension of the matrix:";
+    cin>>N;
 
     int a[N][N];
-    cout<<"Enter the elements of the matrix:"<<endl;
-	for(int i=0;i<N;i++){
-	    for(int j=0;j<N;j++){
-	        cin>>a[i][j];
-	    }
-	}
+    cout<<"Enter the elements of the matrix:";
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cin>>a[i][j];
+        }
+    }
 
-    transpose(a);
+    transpose(a[0],N);
 
-	cout<<"The transpose of the given matrix is:"<<endl;
-	for(int i=0;i<N;i++){
-	    for(int j=0;j<N;j++){
-	        cout<<a[i][j]<<" ";
-	    }
-	    cout<<endl;
-	}
+    cout<<"The transpose of the given matrix is:"<<endl;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
-	return 0;
+    return 0;
 }
 
 /* Sample I/O
 
+Enter the dimension of the matrix:3
 Enter the elements of the matrix:1 2 3 4 5 6 7 8 9
 The transpose of the given matrix is:
 1 4 7
 2 5 8
 3 6 9
+
 
 Time Compexity: O(n^2)
 Space Complexity: O(1)

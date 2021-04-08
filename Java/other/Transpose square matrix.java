cef614b494b039b3666b1a9e79d558080e8b6cc5
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-04-08 19:57:41 +0530

    Update Transpose square matrix.java

--- Java/other/Transpose square matrix.java
+++ Java/other/Transpose square matrix.java
@@ -1,30 +1,40 @@
-public class MatrixTransposeExample{  
-public static void main(String args[]){  
-//creating a matrix  
-int original[][]={{1,3,4},{2,4,3},{3,4,5}};    
-    
-//creating another matrix to store transpose of a matrix  
-int transpose[][]=new int[3][3];  //3 rows and 3 columns  
-    
-//Code to transpose a matrix  
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-transpose[i][j]=original[j][i];  
-}    
-}    
-  
-System.out.println("Printing Matrix without transpose:");  
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-System.out.print(original[i][j]+" ");    
-}    
-System.out.println();//new line    
-}    
-System.out.println("Printing Matrix After Transpose:");  
-for(int i=0;i<3;i++){    
-for(int j=0;j<3;j++){    
-System.out.print(transpose[i][j]+" ");    
-}    
-System.out.println();//new line    
-}    
-}}  
+import java.util.Scanner;  
+public class MatrixTransposeExample2   
+{  
+    public static void main(String args[])    
+    {  
+    int i, j;  
+    System.out.println("Enter total rows and columns: ");  
+    Scanner s = new Scanner(System.in);  
+    int row = s.nextInt();  
+    int column = s.nextInt();  
+    int array[][] = new int[row][column];  
+    System.out.println("Enter matrix:");  
+    for(i = 0; i < row; i++)  
+    {  
+        for(j = 0; j < column; j++)   
+            {  
+            array[i][j] = s.nextInt();  
+            System.out.print(" ");  
+            }  
+    }  
+    System.out.println("The above matrix before Transpose is ");  
+    for(i = 0; i < row; i++)  
+        {  
+            for(j = 0; j < column; j++)  
+            {  
+            System.out.print(array[i][j]+" ");  
+            }  
+            System.out.println(" ");  
+        }  
+    System.out.println("The above matrix after Transpose is ");  
+    for(i = 0; i < column; i++)  
+        {  
+            for(j = 0; j < row; j++)  
+            {  
+                System.out.print(array[j][i]+" ");  
+            }  
+            System.out.println(" ");  
+        }  
+    }  
+}  

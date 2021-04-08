3d651714c14b2a04c108c66ee6d092b09a201a43
Diksha Joshi <58946705+dikshajoshi18@users.noreply.github.com>
2021-04-08 15:33:21 +0530

    Kadane's Algorithm

---
+++ JavaScript/Algorithms/kadane's_algo.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+
+
+class Solution {
+    
+    kadanesalgo(arr){
+        let maxh = 0, maxf = arr[0];
+        let N=arr.length        
+
+        for(let i=0;i<N;i++) 
+        {
+            maxh+=arr[i];
+            //Storing max sum so far by choosing maximum between max 
+            //sum so far and max sum till current index.
+            maxf=Math.max(maxh,maxf);
+           
+            if(maxh<0)
+                maxh=0;
+        }
+
+        return maxf;
+    } 
+}
+
+
+var array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+let obj = new Solution();
+let ans = obj.kadanesalgo(arr);
+if(ans==0)
+    ans=0;
+console.log(ans);
+// solved example
+// Input:
+// N = 4
+// arr[] = {-1,-2,-3,-4}
+// Output:
+// -1
+// Explanation:
+// Max subarray sum is -1 
+// of element (-1)
+
+  

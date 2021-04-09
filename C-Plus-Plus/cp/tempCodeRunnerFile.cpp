6710251e04e5c51be1fcd28856e6c5a98b55dd07
Aditya <lit2020023@iiitl.ac.in>
2021-04-09 15:39:25 +0530

    Rain Water Trapping Problem

--- C-Plus-Plus/cp/tempCodeRunnerFile.cpp
+++ C-Plus-Plus/cp/tempCodeRunnerFile.cpp
@@ -6,72 +6,85 @@
     could be stored at that particulat index of building.
     This process has to be done from both left and the 
     right sides. 
-    The complexity of the whole process will be O(n)
-    where, n stands for total number of buildings in each test-case.
+    The complexity of the whole process will be O(t*n)
+    where, t stands for total number of test cases and n 
+    stands for total number of buildings in each test-case
 
 */
 #include<bits/stdc++.h>
 using namespace std;
 
-// Function to return amount of water trapped
+// Function to return amount of water trapped in each test case
 void solve()
 {
-    long long int no_of_buildings;
     //taking input of number of buildings
-    cin>>no_of_buildings;
-    // Making an array containing heights of buildings
-    long long int height[no_of_buildings];
-    for(long long int i=0;i<no_of_buildings;i++){
-        
-        //Taking input of height of ith-building
-        cin>>height[i];
-    }
+    long long int n;
+    cin>>n;
+    // Making an array v containing heights of buildings
+    long long int v[n];
+    for(long long int i=0;i<n;i++)cin>>v[i];
 
     // Making a left and a right array. 
-    long long int left_arr[no_of_buildings],right_arr[no_of_buildings];
-    left_arr[0]=height[0];
-    right_arr[no_of_buildings-1]=height[no_of_buildings-1];
+    long long int left_arr[n],right_arr[n];
+    left_arr[0]=v[0];
+    right_arr[n-1]=v[n-1];
     // Computing subsequent max elements from left sidewards.
-    for(long long int i=1;i<no_of_buildings;i++)
+    for(long long int i=1;i<n;i++)
     {
-        left_arr[i]=max(height[i],left_arr[i-1]);
+        left_arr[i]=max(v[i],left_arr[i-1]);
     }
     // Computing subsequent max elements from right sidewards.
-    for(long long int i=no_of_buildings-2;i>=0;i--)
+    for(long long int i=n-2;i>=0;i--)
     {
-        right_arr[i]=max(right_arr[i+1],height[i]);
+        right_arr[i]=max(right_arr[i],v[i+1]);
     }
-    long long int total_water=0;
+    long long int sum=0;
 
     // To find the amount of water getting collected inbetween the Buildings:
     // We find minimum between the right and the left array at same indices, 
     // Then Subtract the amount of space taken by the building at that particular index.
 
-    for(long long int i=0;i<no_of_buildings;i++)
+    for(long long int i=0;i<n;i++)
     {
         //Take sum of collected water at each index.
-        total_water+=min(left_arr[i],right_arr[i])-height[i];
+        sum+=min(left_arr[i],right_arr[i])-v[i];
         // cout<<sum<<"\n";
     }
-    cout<<total_water<<endl;
+    cout<<sum<<"\n";
 }
+
 // Driver Code
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // Calling void function
-    solve();
+    // Taking input of number of test cases
+    long long int t;
+    cin>>t;
+    while(t--)
+    {    
+        // Calling void function
+        solve();
+    }
     return 0;
 }
 /*
     Input:
+    3
+    3
+    2 0 2
     5
     3 0 2 0 4
+    12
+    0 1 0 2 1 0 1 3 2 1 2 1
     ---------------
     Output:
+    2
     7
+    6
     ---------------
-    Time-Complexity: O(n)
+    Time-Complexity: O(n^2)
     Space-Compleixty: O(1) 
+
 */
+

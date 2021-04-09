1dc120d84eba383fba901d498a816018bbaa2481
ishwari20 <56400099+ishwari20@users.noreply.github.com>
2021-04-09 16:08:41 +0530

    Update checkPath.cpp

--- C-Plus-Plus/graphs/checkPath.cpp
+++ C-Plus-Plus/graphs/checkPath.cpp
@@ -18,27 +18,24 @@ bool path(int V, int** mat, bool* visited, int v1, int v2){
         }
         // visited[i]=true;
         if(mat[v1][i]==1){
-			bool small=path(V, mat, visited, i, v2);
+	    bool small=path(V, mat, visited, i, v2);
             if(small==true){
                 return true;
             }
         }
     }    
-    
     return false;
 }
 
 int main() {
     int V, E;
     cin >> V >> E;
-
-
-	  int** mat= new int*[V];
-	  for(int i=0; i<V; i++){
-          mat[i]=new int[V];
-         for(int j=0; j<V; j++){
-             mat[i][j]=0;
-         }
+    int** mat= new int*[V];
+    for(int i=0; i<V; i++){
+    	mat[i]=new int[V];
+    	for(int j=0; j<V; j++){
+            mat[i][j]=0;
+      	}
     }
     for(int i=0; i<E; i++){
         int f,g;
@@ -62,14 +59,15 @@ int main() {
     else{
         cout<<"false";
     }
-    
-	
-    
     return 0;
 }
 
 
 /*
+Time Complexity: O(V+E)
+
+Space Complexity: O(V^2)
+
 Sample Input 1 :
 4 4
 0 1
@@ -80,7 +78,5 @@ Sample Input 1 :
 Sample Output 1 :
 true
 
-Time Complexity: O(V+E)
 
-Space Complexity: O(V^2)
 */

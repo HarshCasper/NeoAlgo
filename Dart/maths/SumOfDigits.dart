88cc6798cf32ef1a10c769631cca70c0efa79681
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-04-09 23:06:56 +0530

    code for sum of digits added

---
+++ Dart/maths/SumOfDigits.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+// Calculate factorial of number
+int sum_digits(int number) {
+  int sum = 0;
+
+  // Extract individual digits and add to sum
+  while (number > 0) {
+    int digit = number % 10;
+    number ~/= 10;
+    sum += digit;
+  }
+  return sum;
+}
+
+// Main Function, with driver code
+void main() {
+  print("Enter a number:");
+  int input = int.parse(stdin.readLineSync()!);
+
+  // Call factorial function
+  int sum_of_digits = sum_digits(input);
+  print("Sum of digits in $input is $sum_of_digits");
+}
+/**
+Enter a number:
+401
+Sum of digits in 401 is 50
+
+Enter a number:
+123
+Sum of digits in 123 is 6
+*/

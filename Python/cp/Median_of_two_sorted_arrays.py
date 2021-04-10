a6840625336540eb2aa34c7e19bc62de3d7ac36a
Amit kumar mishra <akm43300@gmail.com>
2021-04-10 11:47:27 +0530

    Update

--- Python/cp/Median_of_two_sorted_arrays.py
+++ Python/cp/Median_of_two_sorted_arrays.py
@@ -1,12 +1,13 @@
 
-# Given two sorted arrays nums1 and nums2 of size m and n 
+# Given two sorted arrays nums1 and nums2 of size m and n
 # respectively, return the median of the two sorted arrays.
- 
+
 class Solution:
-    
+
+
     # function to find median of two arrays
 
-    def findMedianSortedArrays(self, nums1, nums2):
+    def findMedianSortedArrays(nums1, nums2):
         newlist = sorted(nums1 + nums2)
 
         # checking if length of merged array is even
@@ -15,10 +16,10 @@ class Solution:
             first = newlist[len(newlist) // 2]
             second = newlist[len(newlist) // 2 - 1]
             return (first + second) / 2
-        else:
 
         # checking if length of merged array is odd
 
+        else:  
             return newlist[len(newlist) // 2]
 
     nums1 = []
@@ -26,18 +27,18 @@ class Solution:
     n = int(input('Enter length of first array: '))
     m = int(input('Enter length of second array: '))
 
-    print ('Enter the elements of first array: ')
+    print('Enter the elements of first array: ')
     for i in range(n):
         x = int(input())
         nums1.append(x)
 
-    print ('Enter the elements of second array: ')
+    print('Enter the elements of second array: ')
     for i in range(m):
         y = int(input())
         nums2.append(y)
 
-    print ('Median of two arrays is: ' + str(findMedianSortedArrays(0,
-            nums1, nums2)))
+    print('Median of two arrays is: ' + 
+        str(findMedianSortedArrays(nums1, nums2)))
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)
@@ -65,7 +66,7 @@ class Solution:
 # Enter length of second array:
 # 3
 # Enter the elements of first array:
-# 1 5 6 7 8 
+# 1 5 6 7 8
 # Enter the elements of second array:
 # 2 4 4
 

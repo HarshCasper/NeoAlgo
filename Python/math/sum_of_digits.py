c2e4ef35c088ba8df903a6063c06c4b060b3d033
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2021-04-10 15:19:36 +0530

    Update sum_of_digits.py

--- Python/math/sum_of_digits.py
+++ Python/math/sum_of_digits.py
@@ -2,11 +2,11 @@
 
 # Extracting Each digits
 def sum_of_digits(num):
-    sum = 0
-    while(num != 0):
-        sum = sum + (num % 10)
+    s = 0
+    while num != 0:
+        s = s + (num % 10)
         num = num // 10
-    return sum
+    return s
 
 
 print("Enter the number: ", end="")

1d83bd2f61dc210b16b1b3c36fa4b13c51f95009
Amit kumar mishra <akm43300@gmail.com>
2021-04-10 14:33:56 +0530

    Update

--- Python/cp/Median_of_two_sorted_arrays.py
+++ Python/cp/Median_of_two_sorted_arrays.py
@@ -1,41 +1,40 @@
 
 # Given two sorted arrays nums1 and nums2 of size m and n
-# respectively, return the median of the two sorted arrays.
+# respectively, return the median of the two sorted arrays.  
 
-class Solution:
+# function to find median of two arrays
+def findMedianSortedArrays(nums1, nums2):
+    newlist = sorted(nums1 + nums2)
 
+    # checking if length of merged array is even
+    if len(newlist) % 2 != 1:
+        first = newlist[len(newlist) // 2]
+        second = newlist[len(newlist) // 2 - 1]
+        return (first + second) / 2
 
-    # function to find median of two arrays
-    def findMedianSortedArrays(nums1, nums2):
-        newlist = sorted(nums1 + nums2)
+    # checking if length of merged array is odd
+    if len(newlist) % 2 == 1:
+        return newlist[len(newlist) // 2]
 
-        # checking if length of merged array is even
-        if len(newlist) % 2 != 1:
-            first = newlist[len(newlist) // 2]
-            second = newlist[len(newlist) // 2 - 1]
-            return (first + second) / 2
+# Driver's code
+# creating an empty lists
+nums1 = []
+nums2 = []
+n = int(input('Enter length of first array: '))
+m = int(input('Enter length of second array: '))
 
-        # checking if length of merged array is odd
-        if len(newlist) % 2 == 1:
-            return newlist[len(newlist) // 2]
+print('Enter the elements of first array: ')
+for i in range(n):
+    x = int(input())
+    nums1.append(x)
 
-    nums1 = []
-    nums2 = []
-    n = int(input('Enter length of first array: '))
-    m = int(input('Enter length of second array: '))
+print('Enter the elements of second array: ')
+for i in range(m):
+    y = int(input())
+    nums2.append(y)
 
-    print('Enter the elements of first array: ')
-    for i in range(n):
-        x = int(input())
-        nums1.append(x)
-
-    print('Enter the elements of second array: ')
-    for i in range(m):
-        y = int(input())
-        nums2.append(y)
-
-    print('Median of two arrays is: ' +
-          str(findMedianSortedArrays(nums1, nums2)))
+print('Median of two arrays is: ' +
+      str(findMedianSortedArrays(nums1, nums2)))
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)

225bbfb70a2af4a1a6e598528a0540cad5f31e08
yogita saini <yogitasaini2110@gmail.com>
2021-04-10 19:30:18 +0530

    Update HCF.java

--- Java/math/HCF.java
+++ Java/math/HCF.java
@@ -1,6 +1,8 @@
+/*program for calculating HCF of two number*/
 import java.util.Scanner;
 
-public class hcf {
+public class hcf
+ {
 
 	   public static void main(String args[]){
 		   
@@ -8,15 +10,28 @@ public class hcf {
 	      
 	      Scanner sc = new Scanner(System.in);
 	      
-	      System.out.println("Enter first number : ");
-	      dividend = sc.nextInt();
-	      System.out.println("Enter second number : ");
-	      divisor = sc.nextInt();
+	        System.out.println("Enter first number : ");
+	        dividend = sc.nextInt();
 
-	      for(i = 1; i <= dividend|| i <= divisor; i++) {
+	        System.out.println("Enter second number : ");
+	        divisor = sc.nextInt();
+
+	      for(i = 1; i <= dividend|| i <= divisor; i++) 
+              {
 	         if( dividend % i == 0 && divisor% i== 0 )
 	         hcf = i ;
 	      }
 	      System.out.println("HCF of given two numbers is : "+hcf);
 	   }
-	}
+}
+/*
+ * Example-
+ * 1.
+ * Enter first number : 4
+ * Enter second number : 4
+ * HCF of given two number is : 4
+ * 
+ *  2.
+ *  Enter first number : 0
+ * Enter second number : 16
+ * HCF of given two number is : 16 */

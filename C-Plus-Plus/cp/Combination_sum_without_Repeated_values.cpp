be5c45b5f4db5cf551928f823ea97df81e7eda57
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2021-04-10 16:35:28 +0530

    Update Combination_sum_without_Repeated_values.cpp

--- C-Plus-Plus/cp/Combination_sum_without_Repeated_values.cpp
+++ C-Plus-Plus/cp/Combination_sum_without_Repeated_values.cpp
@@ -6,29 +6,30 @@ Note that Same element from given array should not occur more then one time
 #include <bits/stdc++.h>
 using namespace std;
 void find(vector<int> arr,vector<vector<int>>& ans,vector<int>& r,int sum,int start)
+{
+    if(sum<0)return;
+    else if(sum ==0)
     {
-        if(sum<0)return;
-        else if(sum ==0)
-        {
-            ans.push_back(r);
-            return;
-        }
-        for(int i=start;i<arr.size();i++)
-        {
-            if(i>start && arr[i]==arr[i-1])continue;
-            if(sum-arr[i] < 0)break;
-            r.push_back(arr[i]);
-            find(arr,ans,r,sum-arr[i],i+1);
-            r.pop_back();
-        }
+        ans.push_back(r);
+        return;
     }
-    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
-        vector<vector<int>> ans;
-        vector<int> r;
-        sort(arr.begin(),arr.end());
-        find(arr,ans,r,target,0);
-        return ans;
+    for(int i=start;i<arr.size();i++)
+    {
+        if(i>start && arr[i]==arr[i-1])continue;
+        if(sum-arr[i] < 0)break;
+        r.push_back(arr[i]);
+        find(arr,ans,r,sum-arr[i],i+1);
+        r.pop_back();
     }
+}
+vector<vector<int>> combinationSum2(vector<int>& arr, int target)
+{
+    vector<vector<int>> ans;
+    vector<int> r;
+    sort(arr.begin(),arr.end());
+    find(arr,ans,r,target,0);
+    return ans;
+}
     
 int main()
 {

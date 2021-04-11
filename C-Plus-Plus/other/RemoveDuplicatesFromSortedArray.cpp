b90d2ae9f23403eb67e19d01f5154e4a13bdf5af
PSoni8 <46897694+PSoni8@users.noreply.github.com>
2021-04-11 20:30:14 +0530

    Requested Change

--- C-Plus-Plus/other/RemoveDuplicatesFromSortedArray.cpp
+++ C-Plus-Plus/other/RemoveDuplicatesFromSortedArray.cpp
@@ -1,10 +1,9 @@
-// Given a sorted array,remove the duplicates such that each element appears only once and return the new length using two pointers.
-
-// Function removeDuplicates returns modified size of vector which is passed in this function initially.
+/* 
+Given a sorted array,remove the duplicates such that each element appears only once and return the new length using two pointers.
+Function removeDuplicates returns modified size of vector which is passed in this function initially.
+ */
 
 #include<bits/stdc++.h>
-#include<iostream>
-#include<vector>
 
 using namespace std;
 
@@ -27,8 +26,8 @@ int removeDuplicates(vector<int> &nums) {
 	        std::swap(nums[++p1],nums[p2++]);
 	    }
 	}
-nums.erase(nums.begin()+p1+1,nums.end());
-return nums.size();
+nums.erase (nums.begin() + p1 + 1 , nums.end()) ;
+return nums.size() ;
 }
 
 int main(){
@@ -44,19 +43,14 @@ int main(){
     cout<<"Answer is"<<ans;
 }
 
-/*Time Complexity: O(n)
+/*
+Time Complexity: O(n)
 Space Complexity: O(1)
 
 Sample input & output :
 
-sample 1: Input - nums = [1,1,2] ; Output: 2, nums = [1,2]
 Enter size of array 3
 Enter array 1 1 2
 Answer is 2
 
-sample 2:
-Input - nums = [0,0,1,1,1,2,2,3,3,4] ; output: 5, nums = [0,1,2,3,4]
-Enter size of array 10
-Enter array 0 0 1 1 1 2 2 3 3 4
-Answer is 5
 */

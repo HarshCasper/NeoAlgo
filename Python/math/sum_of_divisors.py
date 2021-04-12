359be7ff5dcc458f9046974c9a54ae855b9ae9fe
tushar khandelwal <tusharkhandelwal315@gmail.com>
2021-04-12 17:26:13 +0530

    sum of divisors in python

---
+++ Python/math/sum_of_divisors.py
@@ -0,0 +1,55 @@
+"""
+Python program to get sum of divisors.
+The Sum of divisors refers to the sum of numbers in the range [1 to n] which perfectly divides n.
+"""
+
+import math
+
+#Function to get the sum of divisors
+
+def sum_of_divisors(n):
+
+    #Initializing i as 1 and sum as 0
+
+    i = 1
+    sum = 0
+    while True:
+        if i > math.sqrt(n):
+            break
+
+        #Adding i to sum if it is a factor 
+
+        if n % i == 0 and i == math.sqrt(n):
+            sum += i
+
+        #Adding i and n/i to sum if i is a factor     
+        
+        elif n % i == 0:
+            sum += i
+            sum += n // i
+        i += 1
+    return sum
+
+#Taking Input
+
+n = int(input("Enter the number :"))
+
+#Printing Output
+
+print("The sum of the divisors is: {} ".format(sum_of_divisors(n)))
+
+"""
+Time Complexity - O(sqrt(n)), where 'n' is the number
+Space Complexity - O(1)
+
+SAMPLE INPUT AND OUTPUT
+
+SAMPLE I
+
+INPUT
+Enter the number: 36
+
+OUTPUT
+The sum of the divisors is: 91
+
+"""

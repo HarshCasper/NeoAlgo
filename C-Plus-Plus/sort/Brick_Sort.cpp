caf5dd1d67c7c75def8dbb8d2bd672b9dd268691
keshav2508 <60262928+keshav2508@users.noreply.github.com>
2021-04-14 00:03:34 +0530

    Create Brick_Sort.cpp

---
+++ C-Plus-Plus/sort/Brick_Sort.cpp
@@ -0,0 +1,77 @@
+/*
+In our program in each iteration we first do bubble sort on odd indexed elements and then a bubble sort on the even indexed elements.
+Will we get a sorted result if we first perform a bubble sort on even indexed element first and then on the odd indexed element
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// A function to sort the algorithm using Odd Even sort
+void oddEvenSort(int arr[], int n)
+{
+    // Initially array is unsorted
+    bool isSorted = false; 
+    while (isSorted==false)
+    {
+        isSorted = true;
+ 
+        //Bubble sort on odd indexed element
+        for (int i=1; i<=n-2; i=i+2)
+        {
+            if (arr[i] > arr[i+1])
+             {
+                //swap arr[i] and arr[i+1]
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+                
+                isSorted = false;
+              }
+        }
+ 
+        // Bubble sort on even indexed element
+        for (int i=0; i<=n-2; i=i+2)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                //swap arr[i] and arr[i+1]
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+              
+                isSorted = false;
+            }
+        }
+    }
+ 
+    return;
+}
+
+signed main()
+{
+    //Taking Input
+    int n; cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    //Applying Sort
+    //calling sort function
+    oddEvenSort(arr, n);
+    cout<<"Sorted Array is :-";
+    for (int i = 0; i < n; i++){ 
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+/* Sample Input
+11
+35 23 12 17 12 72 31 46 180 88 54
+*/
+/* Sample Output
+Sorted Array is:- 12 12 17 23 31 35 46 54 72 88 180
+*/
+
+// Time Complexity -O(n^2)
+//Space Complexity -O(1)

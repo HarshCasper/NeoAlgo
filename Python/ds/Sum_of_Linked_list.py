7722e7c7973608c9af3542b48dbac84d763fd402
Akhil Bhalerao <55273506+iamakkkhil@users.noreply.github.com>
2021-04-14 00:32:33 +0530

    Update Sum_of_Linked_list.py

--- Python/ds/Sum_of_Linked_list.py
+++ Python/ds/Sum_of_Linked_list.py
@@ -1,77 +1,82 @@
-# Program to calculate sum of linked list.
-  
+"""
+Program to calculate sum of linked list.
+
+In the sum_ll function we traversed through all the functions of the linked list and calculate
+the sum of every data element of every node in the linked list.
+"""
+
+
 # A node class
 class Node:
-      
+
     # To create a new node
     def __init__(self, data):
         self.data = data
         self.next = None
-  
-  
+
+
 # Class Linked list
 class LinkedList:
-      
+
     # create a empty linked list
     def __init__(self):
         self.head = None
-  
+
     # Function to insert elements in linked list
     def push(self, data):
         newNode = Node(data)
         temp = self.head
-          
+
         newNode.next = self.head
-  
+
         # If linked list is not None then insert at last
         if self.head is not None:
-            while(temp.next != self.head):
-                temp = temp.next 
+            while (temp.next != self.head):
+                temp = temp.next
             temp.next = newNode
-  
+
         else:
-            newNode.next = newNode # For the first node
-  
+            newNode.next = newNode  # For the first node
+
         self.head = newNode
-  
+
     # Function to print given linked list
     def print_List(self):
         temp = self.head
         if self.head is not None:
-            while(True):
+            while (True):
                 print(temp.data)
                 temp = temp.next
                 if (temp == self.head):
-                    break 
-  
-  
-    # Function to calculate sum of a Linked list
+                    break
+
+                    # Function to calculate sum of a Linked list
+
     def sum_ll(self, head):
-        sum = 0
+        sum_ = 0
         temp = self.head
         if self.head is not None:
-            while(True):
-                sum += temp.data
-                temp=temp.next
+            while True:
+                sum_ += temp.data
+                temp = temp.next
 
-        return sum
+        return sum_
 
-  
+
 # Initialize lists as empty by creating Linkedlist objects
-head = LinkedList() 
-  
+head = LinkedList()
+
 # Pushing elements into LinkedList
-head.push(20)
-head.push(30)
-head.push(40)
-head.push(50)
+n = int(input("Enter the no. of elements you want to insert: "))
+for i in range(n):
+    number = int(input(f"Enter Element {i + 1}: "))
+    head.push(number)
 
 print("Entered Linked List: ")
 head.print_List()
 
-sum = head.sum_ll(head)
-print(f"\nSum of Linked List: {sum}")
-
+sum_ = head.sum_ll(head)
+print(f"\nSum of Linked List: {sum_}")
 
 """
 OUTPUT: 
@@ -80,7 +85,6 @@ Entered Circular Linked List:
 
 Sum of Linked List: 140
 
-
-
 Time Complexity: O(n)
+Space Complexity: O(n)
 """

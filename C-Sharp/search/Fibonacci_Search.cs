dd897a15cf4ce9efcec74b5ca9bce26898e4470b
Mihir0106 <65212857+Mihir0106@users.noreply.github.com>
2021-04-14 01:39:15 +0530

    Create Fibonacci_Search.cs

---
+++ C-Sharp/search/Fibonacci_Search.cs
@@ -0,0 +1,94 @@
+// C# program for Fibonacci Search 
+// Time Complexity - O(log(n))
+// Space Complexity - O(1)
+using System;
+
+class Fibonacci 
+{
+
+    public static int min(int x, int y)
+    {
+        return (x <= y) ? x : y;
+    }
+
+    /* Returns index of x if present, else returns -1 */
+    public static int FibMonaccianSearch(int[] arr, int x,
+                                         int n)
+    {
+        /* Initialize fibonacci numbers */
+        int fibMMm2 = 0; // (m-2)'th Fibonacci No.
+        int fibMMm1 = 1; // (m-1)'th Fibonacci No.
+        int fibM = fibMMm2 + fibMMm1; // m'th Fibonacci
+
+        while (fibM < n)
+        {
+            fibMMm2 = fibMMm1;
+            fibMMm1 = fibM;
+            fibM = fibMMm2 + fibMMm1;
+        }
+
+        int offset = -1;
+
+        while (fibM > 1)
+        {
+            int i = min(offset + fibMMm2, n - 1);
+
+            if (arr[i] < x)
+            {
+                fibM = fibMMm1;
+                fibMMm1 = fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+                offset = i;
+            }
+
+            else if (arr[i] > x)
+            {
+                fibM = fibMMm2;
+                fibMMm1 = fibMMm1 - fibMMm2;
+                fibMMm2 = fibM - fibMMm1;
+            }
+            
+            else
+                return i;
+        }
+
+        if (fibMMm1 == 1 && arr[n - 1] == x)
+            return n - 1;
+
+        return -1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter size of array that you like to create");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Enter values in array");
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter value that you like to find in array");
+        int x = int.Parse(Console.ReadLine());
+
+        int ind = FibMonaccianSearch(arr, x, n);
+        if (ind >= 0)
+            Console.WriteLine("Found at index: " + ind);
+        else
+            Console.WriteLine(x + " isn't present in the array");
+    }
+}
+
+/*
+Sample Input 
+Enter size of array that you like to create
+5
+Enter values in array
+45 56 76 87 89
+Enter value that you like to find in array
+87
+
+Sample Output 
+Found at Index: 3
+*/

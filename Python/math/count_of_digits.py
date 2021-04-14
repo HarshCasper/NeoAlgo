d2457f5ccb8f534a04a0872dbc7addfbe6acf1b1
TUSHAR KHANDELWAL <56471793+tk315@users.noreply.github.com>
2021-04-14 09:11:37 +0530

    Update count_of_digits.py

--- Python/math/count_of_digits.py
+++ Python/math/count_of_digits.py
@@ -3,42 +3,38 @@ Python program to get count of digits.
 Count of digits refers to the sum of the frequency of all distinct digits in a number.
 """
 
-#Function to count digits in the number
+# Function to count digits in the number
 
 def count_of_digits(n):
     if(n==0):
       return 1
-
-    #Initializing cnt variable to zero
+    # Initializing cnt variable to zero
 
     cnt = 0
     while True: 
         if(n==0):
           break
-
-        #Increasing the cnt
+        # Increasing the cnt
 
         cnt+=1 
-
-        #Removing the least significant digit
+        # Removing the least significant digit
 
         n = n//10
     return cnt
 
-#Taking Input
+if __name__ == '__main__':
+    #Taking Input
 
-n = int(input("Enter the number :"))
+    n = int(input("Enter the number :"))
+    #Printing Output
 
-#Printing Output
-
-print("The count of digits in the given number is: {} ".format(count_of_digits(n)))
+    print("The count of digits in the given number is: {} ".format(count_of_digits(n)))
 
 """
 Time Complexity - O(n), n is the count of digits in the number
 Space Complexity - O(1)
 
 SAMPLE INPUT AND OUTPUT
-
 SAMPLE I
 
 INPUT
@@ -48,3 +44,4 @@ OUTPUT
 The count of digits in the given number is: 4
 
 """
+

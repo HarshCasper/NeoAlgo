a89e31793c1b465471420ad9abb6819c69a85252
Megha gupta <65459236+Meghag5@users.noreply.github.com>
2021-04-14 14:24:15 +0530

    Update Minimum_number_of_deletion _to_make_palindrome.cpp

--- C-Plus-Plus/dp/Minimum_number_of_deletion _to_make_palindrome.cpp
+++ C-Plus-Plus/dp/Minimum_number_of_deletion _to_make_palindrome.cpp
@@ -6,14 +6,14 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int t[1001][1001];
+int dp[1001][1001];
 
-int longestSubsequence(string X,string Y,int m,int n){
+int minDeletion(string X,string Y,int m,int n){
     // if length of any string is 0 then we cannot find common string
     for(int i=0;i<m+1;i++){
         for(int j=0;j<n+1;j++){
             if(i==0 || j==0)
-            t[i][j] = 0;
+            dp[i][j] = 0;
         }
     }
 
@@ -21,27 +21,26 @@ int longestSubsequence(string X,string Y,int m,int n){
         for(int j=1;j<n+1;j++){
             //if letters of both string are equal then we will decrease row and column both by 1
             if(X[i-1] == Y[j-1]){
-                t[i][j] = t[i-1][j-1] + 1;
+                dp[i][j] = dp[i-1][j-1] + 1;
             }
             //if letters of both string are not equal then first we will decrease row by 1 and then column by 1 and find maximum value b/w two
             else{
-                t[i][j] = max(t[i-1][j], t[i][j-1]);
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
     }
     
-    return m - t[m][n];
+    return m - dp[m][n];
 }
 
 int main() {
-    string X;
-    cin>>X;
-    string Y = X;
-    reverse(Y.begin(),Y.end());
-    int m = X.length();
-    int n = Y.length();
-    //memset(t,-1,sizeof(t));
-    cout<<longestSubsequence(X,Y,m,n);
+    string s1;
+    cin>>s1;
+    string s2 = s1;
+    reverse(s2.begin(),s2.end());
+    int m = s1.length();
+    int n = s2.length();
+    cout<<minDeletion(s1,s2,m,n);
     return 0;
 }
 /*

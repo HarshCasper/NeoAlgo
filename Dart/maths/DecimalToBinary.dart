1a825565b4df306f5fa40774031476674fcf2140
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-04-14 15:53:47 +0530

    code file for decimal to binary added

---
+++ Dart/maths/DecimalToBinary.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+// Function to return binary conversion of number
+int toBinary(int num) {
+  int binary = 0;
+  int index = 1;
+
+  // Iterate till all divisions complete
+  while (num > 0) {
+    binary = binary + ( num % 2 * index);
+    num ~/= 2;
+    index *= 10;
+  }
+  return binary;
+}
+
+// Main Function with driver code
+void main() {
+  print("Enter a number :");
+  int num = int.parse(stdin.readLineSync()!);
+
+  // Call binary conversion function
+  int binary = toBinary(num);
+  print("Binary conversion of $num is $binary");  
+}
+
+/**
+Space Complexity O(1)
+Time Complexity  O(n)
+
+Sample input/output:
+Enter a number :
+13
+Binary conversion of 13 is 1101
+
+Enter a number :
+25
+Binary conversion of 25 is 11001
+*/
+

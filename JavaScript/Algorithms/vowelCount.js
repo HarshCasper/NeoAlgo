0a356e7c4737c47f98d220b9d9a213a427dfff58
SAY-droid427 <sayanimallick0218@gmail.com>
2021-04-14 19:43:47 +0530

    Updated the function

--- JavaScript/Algorithms/vowelCount.js
+++ JavaScript/Algorithms/vowelCount.js
@@ -1,6 +1,7 @@
 //Program to count the number of vowels in a sentence
-const vowels=["a","e","i","o","u","A","E","I","O","U"]
+
 function vowelCount(str){
+    const vowels=["a","e","i","o","u","A","E","I","O","U"]
     let count=0
     for(let character of str){
         if(vowels.includes(character)){
@@ -12,6 +13,7 @@ function vowelCount(str){
 const str=prompt("Enter string:")
 const count=vowelCount(str)
 console.log(count)
+
 /*
 Time Complexity: O(n)
 Example:

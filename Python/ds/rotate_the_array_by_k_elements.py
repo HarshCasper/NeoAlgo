e02ca2d881986765ef66dc5d1937fa6e4c7c0c0d
arpit456jain <111arpit1@gmail.com>
2021-04-16 12:26:56 +0530

    added the code for rotating the array

---
+++ Python/ds/rotate_the_array_by_k_elements.py
@@ -0,0 +1,53 @@
+import math
+
+'''
+it will rotate the array by k elements 
+input format
+1
+5 2
+1 2 3 4 5
+
+ans : 3 4 5 1 2
+'''
+
+#function to totate the array A
+def rotateArr(A, k, N):
+    # let if array is [1,2,3,4,5,6,7] and k = 2
+    #first k elements [1,2]
+    lst = A[0:k]
+
+    #shifting all elements to left by k steps
+    for i in range(0, N - k):
+        A[i] = A[i + k]
+
+    #adding first k elemets to end in rerverse order
+    A[N - k:] = lst[:]
+    return A
+
+
+
+
+def main():
+    #test cases
+
+    T = int(input())
+    while (T > 0):
+        nd = [int(x) for x in input().strip().split()]
+        N = nd[0]
+        k = nd[1]
+        A = [int(x) for x in input().strip().split()]
+
+        rotateArr(A, k, N)
+
+        # printing the result
+        for i in A:
+            print(i, end=" ")
+
+        print()
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends

b9a95291974a24c1a522a7b124b71ca7dea30daa
Abhijeet Sinha <abhijeetsinha.smith@gmail.com>
2021-04-17 00:13:55 +0530

    Updated matrix subtraction

--- C/math/matrix_symmetric.c
+++ C/math/matrix_symmetric.c
@@ -5,26 +5,19 @@ The code gives output as it checks whether the matrix is symmetric or not.
 */
 #include<stdio.h>
 #include<stdlib.h>
-
-int main(){
-	int row,column,i,j,flag=0;
-	int **matrix;
-	printf("Enter number of rows of matrix:");
-    scanf("%d",&row);
-    printf("Enter number of coloumns of matrix:");
-    scanf("%d",&column);
-    //checking whether the given matrix is square matrix or not
-    if(row == column){
-        matrix = (int **)malloc(row * sizeof(int *));
-        for(i = 0; i < row; i++)
-            matrix[i] = (int *)malloc(column * sizeof(int));
+//Function to check whether the given matrix is Symmetric or not
+void symmetric(int row,int column){
+    	int **matrix,flag = 0;
+       matrix = (int **)malloc(row * sizeof(int *));
+        for(int i = 0; i < row; i++)
+       matrix[i] = (int *)malloc(column * sizeof(int));
         //enter the value of matrices
         printf("Enter matrix:\n");
-        for(i = 0; i < row; i++)
-            for(j = 0; j < column; j++)
+        for(int i = 0; i < row; i++)
+            for(int j = 0; j < column; j++)
               scanf("%d",&matrix[i][j]);
-        for (i = 0; i < row; i++){
-            for (j = 0; j < column; j++){
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < column; j++){
       		     if (matrix[i][j] != matrix[j][i]){
       		         flag = 1;
                      break;
@@ -37,6 +30,17 @@ int main(){
             printf("The matrix isn't symmetric.\n");
         else
             printf("The matrix is symmetric.\n");
+    }
+
+int main(){
+	int row,column;
+	printf("Enter number of rows of matrix:");
+    scanf("%d",&row);
+    printf("Enter number of coloumns of matrix:");
+    scanf("%d",&column);
+    //checking whether the given matrix is square matrix or not
+    if(row == column){
+        symmetric(row,column);
     }
     else
         printf("The matrix is not square symmetric.\n");

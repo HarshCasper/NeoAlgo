794c2ff2022013afc7dce4ffa21c7143dd13bf5c
Avantika Singh <77248593+Avantika782@users.noreply.github.com>
2021-04-17 22:36:31 +0530

    Update Minimum_number_of_swaps_for_bracket_balancing.cpp
    Added to end of the code.

--- C-Plus-Plus/other/Minimum_number_of_swaps_for_bracket_balancing.cpp
+++ C-Plus-Plus/other/Minimum_number_of_swaps_for_bracket_balancing.cpp
@@ -1,35 +1,3 @@
-/*
-
-We are given a string of 2N characters
-consisting of N ‘[‘ brackets and N ‘]’
-brackets. A string is considered balanced
-if it can be represented in the for S2[S1] 
-where S1 and S2 are balanced strings.
-We can make an unbalanced string balanced 
-by swapping adjacent characters. 
-Calculate the minimum number of swaps 
-necessary to make a string balanced.
-
-Test Case:
-Input  : []][][
-Output : 2
-First swap: Position 3 and 4
-[][]][
-Second swap: Position 5 and 6
-[][][]
-
-Input  : [[][]]
-Output : 0
-The string is already balanced.
-
-Time Complexity = O(N) 
-Extra Space = O(N)
-
-
-
-
-*/
-
 // C++ program to count swaps required to balance string
 #include <iostream>
 #include <vector>
@@ -88,3 +56,37 @@ int main()
     cout << swapCount(s) << "\n";
     return 0;
 }
+
+
+
+/*
+
+We are given a string of 2N characters
+consisting of N ‘[‘ brackets and N ‘]’
+brackets. A string is considered balanced
+if it can be represented in the for S2[S1] 
+where S1 and S2 are balanced strings.
+We can make an unbalanced string balanced 
+by swapping adjacent characters. 
+Calculate the minimum number of swaps 
+necessary to make a string balanced.
+
+Test Case:
+Input  : []][][
+Output : 2
+First swap: Position 3 and 4
+[][]][
+Second swap: Position 5 and 6
+[][][]
+
+Input  : [[][]]
+Output : 0
+The string is already balanced.
+
+Time Complexity = O(N) 
+Extra Space = O(N)
+
+
+
+
+*/

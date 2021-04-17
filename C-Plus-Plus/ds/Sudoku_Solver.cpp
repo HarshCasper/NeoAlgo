c378b1b6375b182f76358b97c2f9a2274558008a
debashish <debashish1900@gmail.com>
2021-04-17 11:28:50 +0530

    Updated Sudoku solver

--- C-Plus-Plus/ds/Sudoku_Solver.cpp
+++ C-Plus-Plus/ds/Sudoku_Solver.cpp
@@ -7,83 +7,93 @@
 // Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
 // The '.' character indicates empty cells.
 
-
-int a=0;
-
-
-void solve(vector<vector<char>> &grid, int x, int y)
-{
-    int N=grid.size();
-    if(x<0 || y<0 || x>=N || y>=N) return;           
-    
-    vector<int> dict(N+1,1);
-    char tt=grid[x][y];
-    for(int i=0; i<N; i++) dict[grid[i][y]-'0']=0;    
-    for(int i=0; i<N; i++) dict[grid[x][i]-'0']=0;    
-    
-    for(int i=0;i<3;i++)                              
-        for(int j=0;j<3;j++) 
-            dict[grid[x/3*3+i][y/3*3+j]-'0']=0;
-    dict[tt-'0']=1;
-     
-    if(x==N-1 && y==N-1)                              
-    { 
-        int xx=0; for(int i=1; i<=N; i++) 
-            if(dict[i]==1) xx=i;
-        if(xx!=0) grid[x][y]='0'+xx, f=1;
-        return;
-    }
-    
-    if(grid[x][y]!='0')                              
-    {
-        if(dict[grid[x][y]-'0']==1) 
-            solve(grid,x+(y==(N-1)),(y+1)%N);
-        return;        
-    }
-    
-    for(int i=1; i<=N; i++) if(dict[i]==1)            
-    {
-        if(f==1) return; grid[x][y]='0'+i;
-        solve(grid,x+(y==(N-1)),(y+1)%N);
-        if(f==1) return; grid[x][y]='0';
-    }
-}
+// The following code can be used to solve a sudoku which is done using recursion and backtracking.
 
 
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) 
+    bool isSafe(vector<vector<char>> grid, int row,int col, char num)      //we check if block is safe or not
     {
-        a=0;
-        int N=board.size();
-        for(int i=0; i<N; i++)
-            for(int j=0; j<N; j++) 
-                if(board[i][j]=='.') board[i][j]='0';
-        solve(board,0,0);
+    for (int x = 0; x <= 8; x++){                                           //if no same value is present in the same row
+        if (grid[row][x] == num)
+            return false;
+    }
+    for (int x = 0; x <= 8; x++){                                           //if no same value is present in the same column
+        if (grid[x][col] == num)
+            return false;
+    }
+    int startRow = row - row % 3,startCol = col - col % 3;
+   
+    for (int i = 0; i < 3; i++){                                            //if no same value is present in the same 3*3 box
+        for (int j = 0; j < 3; j++){
+            if (grid[i+startRow][j+startCol] == num)
+                return false;
+        }
+    }
+ 
+    return true;
+}
+    bool recur(int i,int j,vector<vector<char>>& board){
+        if(j==9){                                                           //if j excedes the boundry means we need to move to new row
+          i++;  j=0;
+        }
+        if(i==9)
+            return true;
+        if(board[i][j]=='.'){                                               //we enter here if block is empty
+            for(int k=1;k<=9;k++){                                          //check for all values from [1,9], to see which suites better
+                if(isSafe(board,i,j,k+'0')){
+                    board[i][j]=char(k+'0');
+                    if(recur(i,j+1,board))                                  //if ones true,return same for all previous recursion calls
+                        return true;
+                    board[i][j]='.';
+                }
+            }
+        }
+        else{                                                               //if block is already filled we just move to other bolck
+                if(recur(i,j+1,board))
+                    return true;
+        }
+        return false;                                                       //if no value suites the position we return false
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        recur(0,0,board);
     }
 };
 
 
-Input: board = 
-            [["5","3",".",".","7",".",".",".","."],
-            ["6",".",".","1","9","5",".",".","."],
-            [".","9","8",".",".",".",".","6","."],
-            ["8",".",".",".","6",".",".",".","3"],
-            ["4",".",".","8",".","3",".",".","1"],
-            ["7",".",".",".","2",".",".",".","6"],
-            [".","6",".",".",".",".","2","8","."],
-            [".",".",".","4","1","9",".",".","5"],
-            [".",".",".",".","8",".",".","7","9"]]
-Output: 
-            [["5","3","4","6","7","8","9","1","2"],
-            ["6","7","2","1","9","5","3","4","8"],
-            ["1","9","8","3","4","2","5","6","7"],
-            ["8","5","9","7","6","1","4","2","3"],
-            ["4","2","6","8","5","3","7","9","1"],
-            ["7","1","3","9","2","4","8","5","6"],
-            ["9","6","1","5","3","7","2","8","4"],
-            ["2","8","7","4","1","9","6","3","5"],
-            ["3","4","5","2","8","6","1","7","9"]]
+// Simple Input and Output
+// Input: board = 
+//             [["5","3",".",".","7",".",".",".","."],
+//             ["6",".",".","1","9","5",".",".","."],
+//             [".","9","8",".",".",".",".","6","."],
+//             ["8",".",".",".","6",".",".",".","3"],
+//             ["4",".",".","8",".","3",".",".","1"],
+//             ["7",".",".",".","2",".",".",".","6"],
+//             [".","6",".",".",".",".","2","8","."],
+//             [".",".",".","4","1","9",".",".","5"],
+//             [".",".",".",".","8",".",".","7","9"]]
+<p align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png"  />
+</p>
+
+// Output: 
+//             [["5","3","4","6","7","8","9","1","2"],
+//             ["6","7","2","1","9","5","3","4","8"],
+//             ["1","9","8","3","4","2","5","6","7"],
+//             ["8","5","9","7","6","1","4","2","3"],
+//             ["4","2","6","8","5","3","7","9","1"],
+//             ["7","1","3","9","2","4","8","5","6"],
+//             ["9","6","1","5","3","7","2","8","4"],
+//             ["2","8","7","4","1","9","6","3","5"],
+//             ["3","4","5","2","8","6","1","7","9"]]
+
+<p align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Sudoku-by-L2G-20050714_solution.svg/250px-Sudoku-by-L2G-20050714_solution.svg.png"  />
+</p>
+
+// Explanation: The input board is shown above and the only valid solution is shown
 
 
-Explanation: The input board is shown above and the only valid solution is shown
+// Here we have used the same logic as that of N-queens problem
+// Time taken will be O(9^(n*n))
+// Space complexity O(n*n)

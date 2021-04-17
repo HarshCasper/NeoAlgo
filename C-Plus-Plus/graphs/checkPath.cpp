aea4a3bef99b15ae7c397a1c8c17f340675ed214
ishwari20 <56400099+ishwari20@users.noreply.github.com>
2021-04-17 17:47:43 +0530

    Update checkPath.cpp

--- C-Plus-Plus/graphs/checkPath.cpp
+++ C-Plus-Plus/graphs/checkPath.cpp
@@ -33,6 +33,7 @@ bool path(int V, int ** mat, bool * visited, int v1, int v2) {
 int main() {
   int V, E;
   //take input of the vertices and edges of the graph
+  cout<<"Enter the number of vertices and edges of the graph"<<endl;
   cin >> V >> E;
   int ** mat = new int * [V];
   for (int i = 0; i < V; i++) {
@@ -41,6 +42,7 @@ int main() {
       mat[i][j] = 0;
     }
   }
+  cout<<"Enter the two vertices that have a edge between them"<<endl;
   for (int i = 0; i < E; i++) {
     int f, g;
     cin >> f >> g;
@@ -55,6 +57,7 @@ int main() {
   }
   int v1, v2;
   //take input of the vertices v1 and v2, to check if a path exists between them
+  cout<<"Enter the two vertices v1 and v2, to check if a path exists between them"<<endl;
   cin >> v1 >> v2;
 
   //call the fucntion
@@ -75,12 +78,16 @@ Space Complexity: O(V^2)
 where V is the number of vertices and E is the number of edges in the input graph
 
 Sample Input 1 :
+Enter the number of vertices and edges of the graph
 4 4
+Enter the two vertices that have a edge between them
 0 1
 0 3
 1 2
 2 3
+Enter the two vertices v1 and v2, to check if a path exists between them
 1 3
+
 Sample Output 1 :
 true
 */

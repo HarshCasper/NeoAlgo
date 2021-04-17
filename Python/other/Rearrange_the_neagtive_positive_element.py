80e57b50c486b1a5c2659cb0b519ac05ea7c1094
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-04-17 14:34:05 +0530

    modified:   Python/other/Rearrange_the_neagtive_positive_element.py

--- Python/other/Rearrange_the_neagtive_positive_element.py
+++ Python/other/Rearrange_the_neagtive_positive_element.py
@@ -4,15 +4,17 @@ fashion such that every positive number is followed by negative and vice-versa
 maintaining the order of appearance. 
 Number of positive and negative numbers need not be equal. 
 If there are more positive numbers they appear at the end of the array.
- If there are more negative numbers, they too appear in the end of the array.
+If there are more negative numbers, they too appear in the end of the array.
+
 Examples : 
+
 Input:  arr[] = {1, 2, 3, -4, -1, 4}
 Output: arr[] = {-4, 1, -1, 2, 3, 4}
+
 Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
 output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0} 
 """
-# NOTE - Order of element has to be maintained.
-# Space complexity - O(1)
+
 def rearrange_list_with_order(arr):
     posi=[]
     negi=[]
@@ -44,10 +46,9 @@ Time Complexity: O(n^2)
 Space Complexity : O(1)
 
 INPUT: 
-Enter the list 1: 1 5 10 20 40 80
-Enter the list 2: 6 7 20 80 100
-Enter the list 3: 3 4 15 20 30 70 80 120 
+Enter the list 1: 1 2 3 -4 -1 4
+
 
 OUTPUT:
-Common elements from all the three lists are  [20, 80]
+After rearrange with order [-4, 1, -1, 2, 3, 4]
 """

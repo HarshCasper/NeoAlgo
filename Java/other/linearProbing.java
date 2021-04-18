50d85245de69d0832f617d3349de976eb87dbb8d
Vivekanand Bhardwaj <1vivekanandbhardwaj@gmail.com>
2021-04-18 11:06:06 +0530

    added code for linear probing

--- Java/other/linearProbing.java
+++ Java/other/linearProbing.java
@@ -5,86 +5,63 @@
  3.Collision happens when our hash function give the same hash value for two or more input values in that case we can use
    linear probing to avoid collisions.
  
+ 	Steps to be followed:
+ 	1.we will create an empty hash table and initialize all of its value as -1 which represent empty cells.
+ 	2.Then we will iterate over the input array and compute the hash value of all the keys to be inserted,
+ 	 put it in appropriate cells if that cell is already filled we will linearly search for next free cell
+ 	 and then put the key.
+ 	3.We have taken hash function as arr[i]%hashSize
 */
 
 import java.util.*;
 public class linearProbing {
 	
-	    static int[] linearProbing(int hashSize, int arr[], int arraySize)
-	        {
-	    	 //Function to fill the array elements into a hash table 
-		    //using Linear Probing to handle collisions.
-	            int hash_table[] = new int[hashSize];
-	            
-	            //putting -1 to the table which means empty cell
-	            for(int i = 0; i < hashSize; i++)
-	                 hash_table[i] = -1;
-	            
-	            //iterating over the array. 
-	            for(int i=0;i<arraySize;i++)
-	            {
-	                //if the value of hash table at index (arr[i]%hashSize) is -1 
-	                //which means empty then, we insert arr[i] at that place.
-	                if(hash_table[arr[i]%hashSize]==-1)
-	                {
-	                    hash_table[arr[i]%hashSize]=arr[i];
-	                }
-	                //else we move linearly from the filled position 
-	                //to find an index with -1 in hash table.
-	                else
-	                {
-	                    int counter=0;
-	                    int k=(1+arr[i])%hashSize;
-	                    
-	                    //using a loop which runs until we find an index with -1
-	                    //in hash table which means empty.
-	                    while(counter<hashSize&&hash_table[k]!=-1)
-	                    {
-	                         k=(k+1)%hashSize;
-	                         counter++;
-	                    }
-	                    //if we find a position where arr[i] can be inserted 
-	                    //then we insert the element.
-	                    if(counter<hashSize)
-	                    hash_table[k]=arr[i];
-	                }
-	            }
-	            //returning the hash table.
-	            return hash_table;
-	        }
-
-
-	public static void main(String[] args) {
-
-		// scanner class to take input from the user
-		Scanner sc = new Scanner(System.in);
-		
-		// Taking an Hash table size as input
-		int hashSize = sc.nextInt();
-				
-		// Taking an array size as input
-		int arraySize = sc.nextInt();
-		
-		// creating the array
-		int array[]=new int[arraySize];
-		
-		// Taking input in the array
-		for(int i=0;i<arraySize;i++) {
-			array[i]=sc.nextInt();
-		}
-		
-		// creating the hash table from the returned array form the function linearProbing
-		int hashTable[]= linearProbing(hashSize,array,arraySize);
-		
-		
-		for(int i=0;i<hashSize;i++) {
-			//printing the values
-			System.out.print(hashTable[i]+" ");
-		}
-		
-	}
+static int[] linearProbing(int hashSize, int arr[], int arraySize)
+    {
+    int hash_table[] = new int[hashSize];
+    
+    for(int i = 0; i < hashSize; i++)
+         hash_table[i] = -1;
+    
+    for(int i=0;i<arraySize;i++)
+    {
+        if(hash_table[arr[i]%hashSize]==-1)
+        {
+            hash_table[arr[i]%hashSize]=arr[i];
+        }
+        else
+        {
+            int counter=0;
+            int k=(1+arr[i])%hashSize;
+            while(counter<hashSize&&hash_table[k]!=-1)
+            {
+                 k=(k+1)%hashSize;
+                 counter++;
+            }
+            if(counter<hashSize)
+            hash_table[k]=arr[i];
+        }
+    }
+    return hash_table;
 }
 
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	int hashSize = sc.nextInt();
+	int arraySize = sc.nextInt();
+	int array[]=new int[arraySize];
+	
+	for(int i=0;i<arraySize;i++) {
+		array[i]=sc.nextInt();
+	}	
+	int hashTable[]= linearProbing(hashSize,array,arraySize);	
+	
+	for(int i=0;i<hashSize;i++) {
+		System.out.print(hashTable[i]+" ");
+		}		
+	}
+}
 /*
 Sample input 1:
 8 4

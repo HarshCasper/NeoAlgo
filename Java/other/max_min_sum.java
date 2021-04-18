70634f3d2b665fa1e2746220ab0942df8a034667
Aditi Sneh <56735634+aditisneh@users.noreply.github.com>
2021-04-18 19:46:21 +0530

    Update max_min_sum.java

--- Java/other/max_min_sum.java
+++ Java/other/max_min_sum.java
@@ -1,4 +1,4 @@
-/*Given five positive integers, find the minimum and maximum values 
+/*Given five positive long integers, find the minimum and maximum values 
 that can be calculated by summing exactly four of the five integers. 
 Then print the respective minimum and maximum values as a single line of two space-separated long integers.*/
 import java.io.*;

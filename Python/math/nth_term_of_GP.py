a84cac5b5874f851dec9034a5f083ff9e96b198c
arpit456jain <111arpit1@gmail.com>
2021-04-19 06:35:34 +0530

    requested changes done

--- Python/math/nth_term_of_GP.py
+++ Python/math/nth_term_of_GP.py
@@ -1,8 +1,11 @@
+'''
+This is a simple code in python to find the Nth term of a GP , 
+let say 1 3 9 27 is GP its first term (a) is 1 and the Common Ratio (r)
+is 3 and if n=3 it means 3rd term which is 9.
+Approach : Enter the values of A (first term of GP) and R (Common Ratio of GP)
+it will calculate the nth term.
+'''
 import math
-'''
-This is a simple code in python to find the Nth term of a GP , let say 1 3 9 27 is GP its first term (a) is 1 and the Common Ratio (r) is 3 and if n=3 it means 3rd term which is 9.
-Approach : Enter the values of A (first term of GP) and R (Common Ratio of GP) it will calculate the nth term.
-'''
 def termOfGP(A, B, N):
     r = B / A
     ans = A * ((math.pow(r, N - 1)))
@@ -11,7 +14,7 @@ def termOfGP(A, B, N):
 
 
 def main():
-    print("Enter the value of first term a and the Common Common Ratio r of a GP")
+    print("Enter the value of the first term 'a' and the Common Ratio 'r' of a GP")
     AR = [int(x) for x in input().strip().split()]
     A = AR[0]
     R = AR[1]
@@ -27,7 +30,7 @@ if __name__ == "__main__":
 '''
 Sample Input/Output:
 
-Enter the value of first term a and the Common Ratio r of a GP
+Enter the value of the first term 'a' and the Common Ratio 'r' of a GP
 1 3
 Enter the value of N to find the value of its Nth term
 3

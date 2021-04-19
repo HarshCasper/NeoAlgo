23bc77c1f7a36020177605754e62d3b3de84579e
Shivangi-ch <shivangi.ch.936@gmail.com>
2021-04-20 00:56:07 +0530

    fixed

--- C-Plus-Plus/ds/Number_of_good_pairs.cpp
+++ C-Plus-Plus/ds/Number_of_good_pairs.cpp
@@ -1,36 +1,39 @@
 /*
+ Introduction:
  Given an array of integers nums. A pair (i,j) is called good if nums[i] == nums[j] and i < j.
  Return the number of good pairs.
-
- Approach - Use a Hash Table to calculate the frequency of each element in the given array.
+ Approach:
+ Use a Hash Table to calculate the frequency of each element in the given array.
  count the number of pairs using the formula y(y-1)/2; where y is the frequency for the element.
 */
-
-
-// Solution
-
-    int numIdenticalPairs(vector<int>& nums) {
-        int n = nums.size();
-        // creating an unordered map
-        unordered_map<int ,int> umap;
-        // storing frequency of each element
-        for(int i = 0;i<n;i++)
-        {
-            umap[nums[i]]++; 
-        }
-        int ans = 0;
-        for(auto i:umap)
-        {
-            int y = i.second;
-            //count the number of pairs and add to answer.
-            ans = ans + (y * (y-1)) / 2; 
-        }
-        // returning final answer
-        return ans; 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int nums[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
     }
+    unordered_map<int ,int> u_map;
+    for(int i = 0; i < n; i++)
+    {
+        u_map[nums[i]]++;
+    }
+    int ans = 0;
+    for(auto i:u_map)
+    {
+        int y = i.second;
+        ans = ans+(y*(y-1)/2);
+    }
+    cout << ans << "\n";
+}
 /*
-
-Input: nums = [1,2,3,1,1,3]
+Input:
+6
+1 2 3 1 1 3
 Output: 4
 Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 

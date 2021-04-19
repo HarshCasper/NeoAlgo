6a734fa95a204b4df54f254a448bf50f351bbe78
Nidhi <nidhipr2074@gmail.com>
2021-04-19 17:00:28 +0530

    Improved code according to request

--- C-Plus-Plus/other/Rotate_by_90.cpp
+++ C-Plus-Plus/other/Rotate_by_90.cpp
@@ -8,50 +8,40 @@ without using any extra space.
 */
 
 #include <bits/stdc++.h>
+#define order 4 
 
 using namespace std;
 
-//for solving this problem we first find the transpose of the
-//given matrix and then reverse the columns without any extra space
+//for solving this problem we rotate the array in form of outer
+//squares making a boundary i.e first row, last column
+//last row and first column as 1 square and remaing inside elements considered similarly 
 
-//function to swap elements of columns for 
-//finding anti clockwise rotation of the given matrix by 90 degrees 
-void reverseCol(int matrix[][], int row, int column)
+//function to find the rotated matrix inplace
+void rotateBy90Deg(int matrix[][order])
 {
-    for(int pass = 0; pass < column; pass++)
+    //we consider the squares one by one
+    for(int squarCount = 0; squarCount < order / 2; squarCount++)
     {
-        for(int upperptr = 0, lowerptr = column - 1;
-             upperptr < lowerptr; upperptr++, lowerptr--)
+        //we consider the elements in a sets of 4 elements one by one
+        for(int i = squarCount; i < order - squarCount - 1; i++)
         {
-            //swap
-            int temp = matrix[upperptr][pass];
-            matrix[upperptr][pass] = matrix[lowerptr][pass];
-            matrix[lowerptr][pass] = temp;
+            //swapping for rotation
+            int tempmat = matrix[squarCount][i];
+            //rotating
+            matrix[squarCount][i] = matrix[i][order - squarCount - 1];
+            matrix[i][order - squarCount - 1] = matrix[order - squarCount - 1][order - i - 1];
+            matrix[order - squarCount - 1][order - i - 1] = matrix[order - i - 1][squarCount];
+            matrix[order - i - 1][squarCount] = tempmat;
         }
     }
 }
 
-//function to find the transpose of matrix
-void transposeOfMat(int matrix[][], int row, int column)
+///function for printing the matrix 
+void printMatrix(int matrix[][order])
 {
-    for(int pass = 0; pass < row; pass++)
+    for(int pass1 = 0; pass1 < order; pass1++)
     {
-        for(int upperptr = pass; upperptr < column; upperptr++)
-        {
-            //swap
-            int temp = matrix[pass][upperptr];
-            matrix[pass][upperptr] = matrix[upperptr][pass];
-            matrix[upperptr][pass] = temp;
-        }
-    }
-}
-
-///function for printing the new matrix which we obtain
-void printMatrix(int matrix[][], int row, int column)
-{
-    for(int pass1 = 0; pass1 < row; pass1++)
-    {
-        for(int pass2 = 0; pass2 < column; pass2++)
+        for(int pass2 = 0; pass2 < order; pass2++)
         {
             //printing
             cout << matrix[pass1][pass2] << " ";
@@ -60,55 +50,39 @@ void printMatrix(int matrix[][], int row, int column)
     }
 }
 
-//function to finally rotate the matrix by 90 degrees
-void rotateBy90Deg(int matrix[][], int row, int column)
-{
-    //to rotate by 90 degrees we first take transpose
-    //and then reverse the matrix obtained after taking transpose
-    transposeOfMat(matrix, row, column);
-    reverseCol(matrix, row, column);
-}
-
 //driver code
 int main()
 {
     //taking input
-    int row, column;
-    cout << "Enter the number of rows: ";
-    cin >> row;
-    cout << "Enter the number of columns: "
-    cin >> column;
-    int matrix[row][column];
+    int matrix[order][order];
     cout << "Enter the elements of matrix: ";
-    for( int i = 0; i < row; i++)
+    for( int i = 0; i < order; i++)
     {
-        for( int j = 0; j < column; j++)
+        for( int j = 0; j < order; j++)
         {
-            cin >> martix[i][j];
+            cin >> matrix[i][j];
         }
     }
     //for output
-    cout << "The rotated matrix is: "
-    rotateBy90Deg(matrix, row, column);
-    printMatrix(matrix, row, column);
+    cout << "The rotated matrix by 90 degrees is: ";
+    rotateBy90Deg(matrix);
+    printMatrix(matrix);
     return 0;
 }
 
 /*
 EXAMPLE--
 Input:
-Enter the number of rows: 4
-Enter the number of columns: 4
 Enter the elements of matrix: {[1, 2, 3, 4],
                                 [5, 6, 7, 8],
                                 [9, 10, 11, 12],
                                 [13, 24, 15, 16]}
 Output:
-The rotated matrix is: 4 8 12 16
-                       3 7 11 16
-                       2 6 10 14
-                       1 5 9 13
+The rotated matrix by 90 degrees is: 4 8 12 16
+                                     3 7 11 16
+                                     2 6 10 14
+                                     1 5 9 13
 
-TIME COMPLEXITY --> O(ROW * COLUMN)
+TIME COMPLEXITY --> O(n ^ 2); where n is order of the matrix
 SPACE COMPLEXITY --> 0(1)
 */

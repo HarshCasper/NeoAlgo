04acb289d5c5ca72784f795394cc2f6b0adc2376
gokulkannan7 <49653118+gokulkannan7@users.noreply.github.com>
2021-04-19 17:18:59 +0530

    updated roti_prata.cpp

--- C-Plus-Plus/cp/roti_prata.cpp
+++ C-Plus-Plus/cp/roti_prata.cpp
@@ -13,13 +13,19 @@ as he does not have enough time for the 4th prata).
 The person wants to know the minimum time to get the order done
 */
 
-// The idea is to use Binary search in an optimised way where we can apply the same
-// concept on an answer space of 0 to 10e6, and in all the itertions we divide the
-// search space into half by checking if the no of parats are made by that particular
-// time whose value is given the mid in all the iteartions.
+/*
+The question states that there are L stalls and each stall has a cook which have a
+particular rank (Chef with rank 2 can cook 1st parata in 2 mins, second parata in 4 mins..)
+We have to obtain P no of paratas, so we have to find out the minimum time in which the order is ready.
+*/
 
-// Time  O(logn)
+/* The idea is to use Binary search in an optimised way where we can apply the same
+concept on an answer space of 0-low to 10e6-high, and in all the itertions we divide the
+search space into half by checking if within mid = (low_high)/2 amount of time the no of parats are made.
+*/
 
+
+// This function is to check whether p number of paratas are made within the mid amuount of time
 bool check(int a[], int mid, int p,int n){
 	int par=0;
 	int time=0;
@@ -31,39 +37,50 @@ bool check(int a[], int mid, int p,int n){
         	par++;
         	time += (j*a[i]);
         	j++;
-        }
+           }
 	}
 	return (par>=p);
+}
+
+int binarysearch(int a[], int p, int n){
+       int low =0;
+       int high =10000000;
+       int ans = -1;
+       while(low<=high){
+	  int mid = high + (low-high)/2;
+	  if(check(a,mid,p,n)){
+		 ans = mid;
+		 high = mid-1;
+	    }
+	  else low = mid+1;
+      }
+      return ans;
 }
 
 int main(){
 	int t;
 	cin>>t;
 	while(t--){
-		int p,n;
-		cin>>p>>n;
-		int a[n];
-		for(int i=0;i<n;i++){
-            cin>>a[i];
-		}
-
-		int low =0;
-		int high =10000000;
-        int ans = -1;
-		while(low<=high){
-			int mid = high + (low-high)/2;
-			if(check(a,mid,p,n)){
-				ans = mid;
-				high = mid-1;
-			}
-			else low = mid+1;
-		}
-       cout<<ans<<endl;
-	}
-	return 0;
+	   int p,n;
+	   cin>>p>>n;
+           int a[n];
+           for(int i=0;i<n;i++){
+                cin>>a[i];
+	  }
+        cout<<binarysearch(a,p,n)<<endl; 
+     }
+   return 0;
 }
 
+/* 
+Time complexity - O(log	n)
+Space Complexity - O(1)
+*/
+
+
 /*
+SAMPLE INPUT AND OUTPUT
+
 INPUT
 3
 10 4

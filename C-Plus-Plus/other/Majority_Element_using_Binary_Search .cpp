9a4f697a813a7dae7ad097156541ab5830931e50
Abhimanyu Yadav <68424960+iamabhii1@users.noreply.github.com>
2021-04-19 20:03:41 +0530

    Update Majority_Element_using_Binary_Search .cpp

--- C-Plus-Plus/other/Majority_Element_using_Binary_Search .cpp
+++ C-Plus-Plus/other/Majority_Element_using_Binary_Search .cpp
@@ -1,47 +1,43 @@
+
+/*  Ques: Majority_Element_using_Binary_Search
+
+
+Given an array A of N elements. Find the majority element in the array.
+A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+    
+    
+Approach: 
+	 1:First sort the array 
+	 2:keep the mid element of array as the element which we have to find let say (majele)
+	 3:solve the array using modified  binary search and keep track of count of majele
+	 4:find the first occurence of majele and last occurence 
+	 5:now use this formula to find the answer ( ans=last-first+1 )
+	 6:If the count of majele is greater than tha n/2 of the size of element than return the majele as the majority element ans this the answer.
+	 7:If not than return -1 "as no majority element"  
+			  
+Algorithm: 
+	Create a variable to store the max_count, count = 0
+	Traverse through the array from start to end.
+	For every element in the array run another loop to find the count of similar elements in the given array.
+	If the count is greater than the max count update the max count and store the index in another variable.
+	If the maximum count is greater than the half the size of the array, print the element. Else print there is no majority element.
+	
+Time complexity :O(nlogn)
+Space Complexity:O(1)
+*/
+
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-    // Function to find majority element in the array
-    // arr is the array 
-    //start is the starting index of array
-    //end is the ending index of the array
-	/*  solvef function will give us the starting index of majority element
 
-	*/ 
-	/* 
-	Approach: The basic solution is to find the majority element form the array of size n.
-	          1:First sort the array 
-	          2:keep the mid element of array as the element which we have to find 
-			  3:solve the array using binary search and keep track of count of element
-			  4:If the count of element is greater than tha n/2 of the size of element than return the 
-			  element as the majority 
-			  5:If not than return -1 "as no majority element"  
-			  
-			My approach: 1:sort the array 
-			            2:keep the mid element as the findele 
-			            3:now to use modified search to find the first occurence of findele
-			            4:Same as find the last occurence of findele
-			            5:ans=last-first+1 use this formula and 
-			            6:check if the ans is greater than n/2 size of element
-						7:if yes then return the mid element of the sorted array
-						8:else return -1 "No Majority Element found"  
-			   
-		      
-		      
-	Algorithm: 
-			Create a variable to store the max count, count = 0
-			Traverse through the array from start to end.
-			For every element in the array run another loop to find the count of similar elements in the given array.
-			If the count is greater than the max count update the max count and store the index in another variable.
-			If the maximum count is greater than the half the size of the array, print the element. Else print there is no majority element.
-	
-   */
+ /*  solvef function will give us the  first occurence index of majority element (majele)
+    */
     int solvef(int arr[],int start,int end ,int s)
     {
         int fres=-1;
         while(start<=end)
-        {
-            
+	{
             int mid=start+(end-start)/2;
             if(s==arr[mid])
             {
@@ -54,18 +50,14 @@ using namespace std;
             }
             else
             {
-                start=mid+1;
-                
-            }
-            
+                start=mid+1;       
+	    }
         }
         return fres;
-        
     }
     
-    /*  solvel function will give us the  endling  index of majority element
-
-	*/ 
+    /*  solvel function will give us the  last occurence index of majority element (majele)
+    */
     int solvel(int arr[],int start,int end, int s)
     {
         int lres=-1;
@@ -91,6 +83,7 @@ using namespace std;
         return lres;
         
     }
+// this is function which will call the solvef,solvel function and check for the count greater than n/2 if or not?
     int majorityElement(int a[], int size)
     {
         if(size==1)
@@ -126,7 +119,7 @@ using namespace std;
         {
             //enter number of element
             int n;
-			cin>>n;
+	    cin>>n;
             //initializing the array
             int arr[n];
             for(int i=0;i<n;i++)
@@ -138,25 +131,25 @@ using namespace std;
         return 0;
     }
     
-    /*  
-    Input:
+/*____________________________________________________________________________________________________________________________*/
+ 
+/*  
+ Input-Output cases
+
+test case : t= 2
+Input (1):
      N = 3 
      A[] = {1,2,3} 
      
-    Output: -1
-    Explanation:
-				Since, each element in 
-				{1,2,3} appears only once so there 
-				is no majority element.
-		
-		
-	Input:
-		N = 5 
-		A[] = {1,4,4,4,2} 
-		Output: 4
-		Explanation:
-					Since, 4 is present more
-					than N/2 times, so it is 
-					the majority element.	
-				
-	*/
+Output (1): -1
+Explanation 1:
+Since, each element in {1,2,3} appears only once so there is no majority element.
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+Input (2):
+      N = 5 
+      A[] = {1,4,4,4,2} 
+Output (2): 4
+Explanation (2):
+Since, 4 is present more than N/2 times, so it is the majority element.	
+*/

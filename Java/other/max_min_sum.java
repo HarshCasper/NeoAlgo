407f3982a63b19a7ed64168175ef87e49bb00de7
Aditi Sneh <56735634+aditisneh@users.noreply.github.com>
2021-04-20 18:58:01 +0530

    Update max_min_sum.java

--- Java/other/max_min_sum.java
+++ Java/other/max_min_sum.java
@@ -1,49 +1,58 @@
 /*Given five positive long integers, find the minimum and maximum values 
 that can be calculated by summing exactly four of the five integers. 
 Then print the respective minimum and maximum values as a single line of two space-separated long integers.*/
+//impoting java packages
 import java.io.*;
-
-import java.util.*;//impoting java packages
-
+import java.util.*;
 public class MaxMinSum{
   
     public static void main(String[] args){
-      
         Scanner in = new Scanner(System.in);
-      
-        long arr[] = new long[5];
-      
+         long arr[] = new long[5];
+         System.out.println("Enter fiver numbers in the array - ");
         for(int i=0;i<5;i++){
           
-            arr[i] = in.nextLong();//taking input from user  --- time complexity of O(n)
-          
+          //taking input from user  --- time complexity of O(n)
+            arr[i] = in.nextLong();   
+            
         }
-      
-        Arrays.sort(arr);long min=0;long max=0;//sorting the array using Arrays.sort function
+      //function call with passing the array as argumment
+        MaxMinSumOfElements(arr);
+    }
+  
+    public static void MaxMinSumOfElements(long arr[]){
+        
+        //sorting the array using Arrays.sort function
+        Arrays.sort(arr);long min=0;long max=0;
       
         for(int i=0;i<4;i++){
-          
-            min+=arr[i];//calculating minimum sum by summing the first 4 digits--- time complxity of O(n)
+          //calculating minimum sum by summing the first 4 digits of the sortedd array --- time complxity of O(n)
+            min+=arr[i];
           
         }
         for(int i=1;i<5;i++){
-          
-            max+=arr[i];//calculating maximum sum by summing up the last 4 digits --- time complxity of O(n)
+          //calculating maximum sum by summing up the last 4 digits of the sorted array  --- time complxity of O(n)
+            max+=arr[i];
           
         }
-        System.out.println(min+" "+max);//printing the output.
+        //printing the output.
+        System.out.println("The minimum sum of elements excluding exactly one element is - "+min);
+        System.out.println("The maximum sum of elements excluding exactly one element is - "+max);
     }
 }
 /*
 Sample Input
 
+Enter five elements in the array - 
 4 3 9 8 2
 
 Sample Output
 
-17 24
+The minimum sum of elements excluding exactly one element is - 17 
+The maximum sum of elements excluding exactly one element is - 24
 
 Time Complexity - 
 
 O(n) + O(n) + O(n) = O(n)
 
+*/

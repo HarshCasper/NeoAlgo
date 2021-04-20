4febc277d58c21ef39a4b747497f9c5ed405701d
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-04-20 19:31:39 +0530

    add Longest Consecutive subsequence

--- Python/README.md
+++ Python/README.md
@@ -274,5 +274,6 @@ _add list here_
 - [Dutch National Flag Algorithm Implementation in Python](other/SortColors-DutchNationalFlag.py)
 - [Ploting Diffrent Graphs in Python](other/Plot_Graph.py)
 - [Remove Minimum Number of Parenthesis for Balanced String](other/check_valid_parenthesis_string.py)
+- [Longest Consecutive Subsequence](other/Longest_Consecutive_Subsequence.py)
 
 ## [Coding Template for Contribution](Code_Template.md)
---
+++ Python/other/Longest_Consecutive_Subsequence.py
@@ -0,0 +1,61 @@
+"""
+Given an array of positive integers. Find the length of the longest sub-sequence such that 
+elements in the subsequence are consecutive integers,
+the consecutive numbers can be in any order.
+
+Example:
+Input: {2,6,1,9,4,5,3}
+Output: 6
+
+Explanation:
+The consecutive numbers here
+are 1, 2, 3, 4, 5, 6. These 6 
+numbers form the longest consecutive
+subsquence.
+
+Example 2:
+Input: {1,9,3,10,4,20,2}
+Output: 4
+
+Explanation:
+1, 2, 3, 4 is the longest
+consecutive subsequence.
+
+"""
+
+def LongestConsecutiveSubsequence(arr):
+    #let take dict save the the element 
+    dict={}
+    
+    for l in arr:
+        if l not in dict.keys():
+            dict[l]=1
+
+    sub=[0]*len(arr)
+    #check  present or not then increase the count with 1 
+    for i in range(0,len(arr)):
+
+        key=arr[i]
+
+        while key in dict.keys():
+            key=key+1
+
+        sub[i]=key-arr[i]
+    #result
+    return sorted(sub,reverse=True)[0]
+
+if __name__ == '__main__':
+    arr = list(map(int,input("Enter the number: ").split()))
+    print("The longest consective subsequence is ",LongestConsecutiveSubsequence(arr))
+
+'''
+Time complexity : O(n^n) 
+Space complexity : O(n^n) 
+
+INPUT:-
+Enter the number: 36 41 56 35 44 33 34 92 43 32 42 
+
+OUTPUT:-
+The Longest Consecutive Subsequence is  5
+
+'''

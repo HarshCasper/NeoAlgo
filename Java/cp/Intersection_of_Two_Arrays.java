9d90b77506c2edcc19c5128f9e4eb224bd3bf741
Amanduttbhagat <bhagataman980@gmail.com>
2021-04-20 07:54:16 -0500

    updated code

--- Java/cp/Intersection_of_Two_Arrays.java
+++ Java/cp/Intersection_of_Two_Arrays.java
@@ -1,15 +1,15 @@
 /*
-  - Intersection :
-       We have to find intersection of two arrays :
-       The intersection of two arrays is a list of numbers which are present in both the arrays
-     Example :
-       input : Enter the number of elements you want in the first array : 4
-	           Enter the elements in the 1st array : 1 4 3 2
-	 		   Enter the number of elements you want in the second array : 3
-	 		   Enter the elements in the 2nd array : 1 2 3
-      output : The intersection of two arrays is : 1 2 3
-               Intersection of both arrays is [1 2 3] as these elements are present in both the arrays
- */
+  Intersection :
+     We have to find intersection of two arrays :
+     The intersection of two arrays is a list of numbers which are present in both the arrays
+  For example :
+     We have to input 2 arrays of integer, let first array be 1 4 3 2 and the second array be 1 3 2
+     So, intersection means the numbers which are common in both the arrays
+     here, 1 2 3 is common in both the arrays hence, it is the intersection of both the arrays
+
+     If the number is repeating in the array, for example one array is 1 2 2 2 and second array is
+     2 3 2 4 2 then the intersection would be 2 2 2 not just 2
+*/
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -21,48 +21,45 @@ public class Intersection_of_Two_Arrays {
 
 		int x = 0, y = 0;
 
-		//sorting 1st Array
 		Arrays.sort(array1);
-		//sorting 2nd Array
 		Arrays.sort(array2);
 
-		//for loop
-		for(  ; x < array1.length && y < array2.length ;  ) {
+		for(  ; x < array1.length && y < array2.length ;  )
+		{
 
-			//if array1[x] == array2[y] is true then print array2[y] and increment y and x
-			if(array1[x] == array2[y]) {
+			if(array1[x] == array2[y])
+			{
 				System.out.print(array2[y]+" ");
 				y = y+1;
 				x = x+1;
-			}else if(array1[x] < array2[y]) {
+			}
+			else if(array1[x] < array2[y])
+			{
 				x = x+1;
-			}else if(array1[x] > array2[y]) {
+			}
+			else if(array1[x] > array2[y])
+			{
 				y = y+1;
 			}
 		}
 	}
 
-	//main method
+	//Driver Code
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		//first array
-		//Input the number of elements you want in the first array
 		System.out.print("Enter the number of elements you want in the first array : ");
 		int first = sc.nextInt();
 
 		//Creating array
 		int[] array1 = new int[first];
 
-		//Putting elements in the array
 		System.out.print("Enter the elements in the 1st array : ");
 		for(int i = 0 ; i < first ; i++) {
 			array1[i] = sc.nextInt();
 		}
 
-		//second array
-		//Input the number of elements you want in the second array
 		System.out.print("Enter the number of elements you want in the second array : ");
 		int second = sc.nextInt();
 
@@ -76,26 +73,26 @@ public class Intersection_of_Two_Arrays {
 		}
 
 		//Creating object
-		Intersection_of_Two_Arrays obj =new Intersection_of_Two_Arrays();
+		Intersection_of_Two_Arrays obj = new Intersection_of_Two_Arrays();
 
 		System.out.print("The intersection of two arrays is : ");
+
 		//calling intersection method
 		obj.intersection(array1, array2);
 	}
 }
 /*
-- Test Cases :
-       input : Enter the number of elements you want in the first array : 4
-	           Enter the elements in the 1st array : 1 4 3 2
-	 		   Enter the number of elements you want in the second array : 3
-	 		   Enter the elements in the 2nd array : 1 2 3
-      output : The intersection of two arrays is : 1 2 3
 
-- Complexity Analysis :
- * Time Complexity : O(n)
-         Where, n = array1.length + array2.length
-  	     We are using only one for loop, so it iterates maximum 'n' times while traversing for loop
- * Space Complexity : O(n)
-	  	 Auxiliary space : O(1)  +  input space : O(n)
- */
- 
+Input:
+Enter the number of elements you want in the first array : 4
+Enter the elements in the 1st array : 1 2 3 4
+Enter the number of elements you want in the second array : 3
+Enter the elements in the 2nd array : 1 2 3
+
+Output:
+The intersection of two arrays is : 1 2 3
+
+Time Complexity : O(n)
+Space Complexity : O(n)
+
+*/

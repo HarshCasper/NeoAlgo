a83a34b71c157e454c519a722b648e967315c8a7
Ankit Aggarwal <ankitaggarwal1274@gmail.com>
2021-04-20 15:53:30 +0530

    Revert "Update find_duplicate_elements_from_an_array.cpp (#5629)" (#6276)
    This reverts commit 7e965239cc300f77ca9871208e106fdcf08efbb5.

--- C-Plus-Plus/other/find_duplicate_elements_from_an_array.cpp
+++ C-Plus-Plus/other/find_duplicate_elements_from_an_array.cpp
@@ -2,21 +2,34 @@
 Given an array of N elements, each element's occurrence in the array can be more than once. Find all elements whose occurrence is more than 1.
 Formally find all duplicate elements of the array. This can be done with the help of map and set data structure.
 */
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int>removeDuplicates(int*a,int size){
-	vector<int>output;
-	unordered_map<int,bool>seen;
-	for(int i=0;i<size;i++){
-		if(seen.count(a[i])>0){
-			 continue;
-		}
-		seen[a[i]]=true;
-		output.push_back(a[i]);
-	}
-	return output;
+void dupicate_elements(int ar[] ,int N)
+{
+    /* we will insert the array elements into the map. if current element already in the map,
+    then definitely that element's occurrence is more than 1. 
+    Hence it is duplicate and will insert in the set*/
+    map < int, int > unique_elements;
+    set < int > duplicates;
+    for(int i = 0; i < N; i++)
+    {
+        if(!unique_elements[ar[i]])
+            unique_elements[ar[i]]++;
+       else
+       {
+           // this is a duplicate
+           duplicates.insert(ar[i]);
+       }
+    }
+    cout << "Duplicate elements are : ";
+    for(auto i : duplicates)
+        cout << i << ' ';
+    cout << endl;
+    return;
 }
+
 int main()
 {
     cout << "Enter the size of the array : ";
@@ -28,28 +41,17 @@ int main()
     {
         cin >> ar[i];
     }
-    vector<int>output=removeDuplicates(ar,N);
-	for(int i=0;i<output.size();i++){
-		cout<<output[i]<<endl;
-	}
+    dupicate_elements(ar , N);
 }
 
 /*
-Standard Input:
+Standard Input and Output
 
-Enter the size of the array : 11
-Enter array elements :
-1 2 3 3 2 1 4 6 3 5 5
+Enter the size of the array : 12
+Enter array elements
+1 5 2 1 4 3 1 7 2 8 9 5
+Duplicate elements are : 1 2 5
 
-Output:
-1
-2
-3
-4
-6
-5
-
-------------------------------------------------------------------------
 Time Complexity : O(logN)
 Space Complexity : O(N)
 

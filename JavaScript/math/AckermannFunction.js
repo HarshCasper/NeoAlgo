2cf960c761b0088bb80194698f8861e27cac83cd
satcasm <satyamsingh200151@gmail.com>
2021-04-22 12:24:28 +0530

    added missing semicolon

--- JavaScript/math/AckermannFunction.js
+++ JavaScript/math/AckermannFunction.js
@@ -5,10 +5,10 @@
 
 const ackermann = (m, n) => {
 	if (m === 0) 
-	  return n+1
+	  return n+1;
 	if (n === 0)
 	  return ackermann((m - 1), 1);
-	return ackermann((m-1), ackermann(m, (n-1)))
+	return ackermann((m-1), ackermann(m, (n-1)));
 }
 
 console.log(`The result is : ${ackermann(2, 4)}`);

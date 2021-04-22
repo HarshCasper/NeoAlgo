5835e438918b441bf3224afcc0550aa8de7aeca9
Tauros-Silver <63157213+Tauros-Silver@users.noreply.github.com>
2021-04-22 13:26:57 +0530

    Updated GnomeSort.c
    Removed comments alongside code.

--- C/sort/GnomeSort.c
+++ C/sort/GnomeSort.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /*
 *  Gnome Sort or Stupid Sort is a sorting algorithm that arranges all the elements in their correct positions
 *  by means of a series of swaps in a single loop.
@@ -15,57 +12,53 @@
 *			Move to next number
 */
 
-void gnome_sort(int* arr, int size)
+#include <stdio.h>
+#include <stdlib.h>
+/* Takes integer array and size as input and sorts the array as per afore-mentioned method */
+void gnome_sort(int* array, int size)
 {
 	int index = 0;
-	while (index < size)
+	while (index < size) 
 	{
-		if (index == 0 || arr[index] >= arr[index - 1])		// If no previous position exists or if elements are sorted,
-			++index;										//   we move to next position.
-		else                                                // Else,
-		{
-			int temp = arr[index];							//    we swap elements
-			arr[index] = arr[index - 1];
-			arr[index - 1] = temp;
-			--index;										//    and move to previous position
+		if (index == 0 || array[index] >= array[index - 1])	
+			++index;
+		else {
+			int temp = array[index];
+			array[index] = array[index - 1];
+			array[index - 1] = temp;
+			--index;
 		}
 	}
 }
-
+/* In the main function, inputs from user are read, gnome_sort() is invoked and output is printed */ 
 int main()
 {
-	int size, i;
+	int size, index;
 	printf("How many numbers are to be sorted? ");
 	scanf("%d", &size);
-	int* arr = (int*)malloc(sizeof(int) * size);		//Creating an integer array (arr) of size, size
-
+	int* array = (int*)malloc(sizeof(int) * size);	
 	printf("Enter the numbers to be sorted: \n");
-	for (i = 0; i < size; ++i)
-	{
-		scanf("%d", &arr[i]);
+	for (index = 0; index < size; ++index) {
+		scanf("%d", &array[index]);
 	}
-
-	gnome_sort(arr, size);
-
+	
+	gnome_sort(array, size);
+	
 	printf("Numbers after sorting: \n");
-	for (i = 0; i < size; ++i)
-	{
-		printf("%d ", arr[i]);
-	}
+	for (index = 0; index < size; ++index) {
+		printf("%d ", array[index]);
+	}	
 	printf("\n\n");
-
 	return 0;
 }
-
 /*
-   [Sample I/O]
+                  [Sample I/O]
 	How many numbers are to be sorted? 6
 	Enter the numbers to be sorted:
 	91 87 75 1 3 108
 	Numbers after sorting:
 	1 3 75 87 91 108
-
-   [Complexities]
+                [Complexities]
 	Time Complexity  : O(n^2)
 	Space Complexity : O(1)
 */

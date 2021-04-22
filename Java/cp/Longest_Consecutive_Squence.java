217616453287d9ca0ab5ac8ed2d61932119cb419
apoorva0107 <67058038+apoorva0107@users.noreply.github.com>
2021-04-22 12:37:19 +0530

    Update Longest_Consecutive_Squence.java

--- Java/cp/Longest_Consecutive_Squence.java
+++ Java/cp/Longest_Consecutive_Squence.java
@@ -1,4 +1,7 @@
-/*Problem description: Return the length of the longest consecutive elements sequence in an unsorted array. */
+/*
+Problem Description: For a given unsorted array, your task is to return the length of longest sub-sequence such that elements in the subsequence are consecutive integers.
+For example: For an array [3, 6, 10, 2, 4] the longest sequence of consecutive elements is 2, 3, 4 so the length will be 3.
+*/
 
 import java.io.*;
 import java.util.*;
@@ -20,11 +23,13 @@ class Longest_Consecutive_Sequence {
         for(int i=0 ; i<nums.length-1 ; i++){
 
             //Check for duplicate elements
+            //Skip duplicates
             if(nums[i] == nums[i+1]){
                 continue;
             }
 
-            //Check if previous elements is one less than the current element, if true increase count by one
+            //Check if previous elements is one less than the current element,
+            //if true increase count by one
             else if(nums[i+1] - nums[i] == 1){
                 count++;
                 //update the maximum
@@ -38,7 +43,8 @@ class Longest_Consecutive_Sequence {
         //Return the maximum length
         return max;
     }
-
+    
+    //driver method
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.pritln("Enter number of elements: ");

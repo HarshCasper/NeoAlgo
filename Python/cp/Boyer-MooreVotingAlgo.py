5fbcd751d5a2f93c8150edca802ddc2a00ce5b98
Utkarsh <utkarshus2000@gmail.com>
2021-04-22 14:19:19 +0530

    Boyer-MooreVotingAlgo in Python

---
+++ Python/cp/Boyer-MooreVotingAlgo.py
@@ -0,0 +1,54 @@
+'''
+Boyer-Moore Voting Algorithm (The Majority Element)
+
+Objective: Given an array A of N elements, Find the majority element in the array.
+           A majority element in an array of N elements is an element that occurs 
+           N/2 times in an array.
+'''
+
+# Python Code:
+
+def majorityElement(A, n):
+ 
+    #here 'm' stores the majority element (if present)
+    mx = -1
+ 
+    # initialize `c` with 0
+    c = 0
+ 
+    #do for each element `A[j]` in the list of elements
+    for i in range(n):
+ 
+        #if counter `c` becomes 0
+        if c == 0:
+ 
+            #set the current candidate to `A[i]`
+            mx = A[i]
+ 
+            #reset the counter to 1
+            c = 1
+ 
+        #or increment the counter if `A[j]` is a current candidate
+        elif mx == A[i]:
+            c = c + 1
+ 
+        #or decrement the counter if `A[j]` is a current candidate
+        else:
+            c = c - 1
+ 
+    print (mx)
+
+# Driver code
+if __name__ == "__main__":
+    A = [2, 6, 4, 6, 2, 5, 6]
+    n = len(A)
+ 
+    # Function calling
+    majorityElement(A, n)
+
+'''
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Output: 6 ('6' occurs 3 times in list A so it is the majority element)
+'''

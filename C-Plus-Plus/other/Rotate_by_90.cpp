480f581cce4076b0388e37831c8756296a795d08
Nidhi <nidhipr2074@gmail.com>
2021-04-22 17:23:06 +0530

    Added changes

--- C-Plus-Plus/other/Rotate_by_90.cpp
+++ C-Plus-Plus/other/Rotate_by_90.cpp
@@ -76,13 +76,13 @@ int main()
 EXAMPLE--
 Input:
 Enter the order of matrix: 4
-Enter the elements of matrix: {[1, 2, 3, 4],
-                               [5, 6, 7, 8],
-                               [9, 10, 11, 12],
-                               [13, 24, 15, 16]}
+Enter the elements of matrix: 1 2 3 4
+                              5 6 7 8
+                              9 10 11 12
+                              13 24 15 16
 Output:
 The rotated matrix by 90 degrees is : 4 8 12 16
-                                      3 7 11 16
+                                      3 7 11 15
                                       2 6 10 14
                                       1 5 9 13
 

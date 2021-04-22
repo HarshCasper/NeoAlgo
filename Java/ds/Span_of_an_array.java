a8249913a56852ebbcfce299e6351b3e1e246235
Nikita Anil Gupta <61223163+gnikita432@users.noreply.github.com>
2021-04-22 16:45:50 +0530

    Made changes in span of an array program

--- Java/ds/Span_of_an_array.java
+++ Java/ds/Span_of_an_array.java
@@ -1,44 +1,44 @@
-// Given a number n, Given an array of n elements, find the span of array.
-// span of an array is defined as difference of maximum value and minimum value of an array.
+/* Given a number n, and an array of n elements, The program is to find the span of array.
+ Span of an array is defined as difference of maximum value of the array and minimum value of an array.*/
 
 import java.io.*;
 import java.util.*;
 
 public class span {
     public static void main(String[] args) {
-       //taking the input for array
+       /*taking the input for array*/
         Scanner scan = new Scanner(System.in);
         System.out.println("enter the size of array");
         int number = scan.nextInt();
-
         int[] arrelement = new int[number];
         System.out.println("enter the elements");
         for (int i = 0; i < arrelement.length; i++) {
             arrelement[i] = scan.nextInt();
          }
-         //Finding the maximum element from the array
+         /*Finding the maximum element from the array*/
         int max = arrelement[0];
         for (int j = 0; j < arrelement.length; j++) {
                 if (arrelement[j] > max) {
                 max = arrelement[j];
             }
         }
-        //Finding the minimum element from the array
+        /*Finding the minimum element from the array*/
         int min = arrelement[0];
         for (int k = 0; k < arrelement.length; k++) {
             if (arrelement[k] < min) {
                 min = arrelement[k];
             }
         }
-        //Taking the difference of maximum and minmum number
-        int span = max - min;
+        /*Taking the difference of maximum and minmum number*/
+        int span = max - min; 
         System.out.println("span of the array is"+ span);
     }
 }
 
 /*
-Standard Input and Output 
+Standard Input/Output 
 
+Input:
 enter the size of an array
 6
 enter the elements
@@ -48,9 +48,10 @@ enter the elements
 4
 11
 9
+
+Output:
 span of the array is 36
 
 Time complexity:o(n)
 space complexity:o(n)
 */
-

20551b8967dcbf546e51a8da5689c1facda2c7de
Vishruta Patil <72292532+Vishruta-Patil@users.noreply.github.com>
2021-04-23 23:04:28 +0530

    Update heapify.cpp

--- C-Plus-Plus/ds/heapify.cpp
+++ C-Plus-Plus/ds/heapify.cpp
@@ -4,15 +4,12 @@ i.e. Leaf elements are not processed. This makes the process faster.
 */
 #include <iostream>
 using namespace std;
-
 void swap(int A[], int i, int j)
 {
-
     int temp = A[i];
     A[i] = A[j];
     A[j] = temp;
 }
-
 int Delete(int A[], int n)
 {
     int x = A[0]; // Max element
@@ -21,13 +18,11 @@ int Delete(int A[], int n)
     int j = 2 * i + 1;
     while (j < n - 1)
     {
-
         // Compare left and right children
         if (A[j] < A[j + 1])
         {
             j = j + 1;
         }
-
         // Compare parent and largest child
         if (A[i] < A[j])
         {
@@ -42,14 +37,12 @@ int Delete(int A[], int n)
     }
     return x;
 }
-
 void Heapify(int A[], int n)
 {
     // # of leaf elements: (n+1)/2, index of last leaf element's parent = (n/2)-1
     for (int i = (n / 2) - 1; i >= 0; i--)
     {
         int j = 2 * i + 1; // Left child for current i
-
         while (j < n - 1)
         {
             // Compare left and right children of current i
@@ -57,9 +50,7 @@ void Heapify(int A[], int n)
             {
                 j = j + 1;
             }
-
             // Compare parent and largest child
-
             if (A[i] < A[j])
             {
                 swap(A, i, j);
@@ -73,7 +64,6 @@ void Heapify(int A[], int n)
         }
     }
 }
-
 template <class T>
 void Print(T &vec, int n, string s)
 {
@@ -88,30 +78,26 @@ void Print(T &vec, int n, string s)
     }
     cout << "]" << endl;
 }
-
 int main()
 {
-
     int A[] = {5, 10, 30, 20, 35, 40, 15};
     Print(A, sizeof(A) / sizeof(A[0]), "A");
     Heapify(A, sizeof(A) / sizeof(A[0]));
     Print(A, sizeof(A) / sizeof(A[0]), "Heapified A");
     cout << endl;
-
     int B[] = {5, 10, 30, 20};
     Print(B, sizeof(B) / sizeof(B[0]), "B");
     Heapify(B, sizeof(B) / sizeof(B[0]));
     Print(B, sizeof(B) / sizeof(B[0]), "Heapified B");
-
     return 0;
 }
 
 /*
+Sample I/O:
 A: [5, 10, 30, 20, 35, 40, 15]
 Heapified A: [40, 35, 30, 20, 10, 5, 15]
 B: [5, 10, 30, 20]
 Heapified B: [30, 10, 5, 20]
 Time Complexity: O(n)
 Space Complexity: O(n)
- 
  */

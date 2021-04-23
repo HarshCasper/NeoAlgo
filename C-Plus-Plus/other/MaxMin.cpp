ffa5b7417d8dab5071867f7c88f39460d552854f
Salma Zakaria <Salma2018170755@cis.asu.edu.eg>
2021-04-24 00:50:15 +0200

    Changes are done

--- C-Plus-Plus/other/MaxMin.cpp
+++ C-Plus-Plus/other/MaxMin.cpp
@@ -11,13 +11,14 @@ int main() {
     int n, k;
     cin >> n >> k;
     long A[n];
-
-    for (int i = 0; i < n; i++) {
+    cout << "Enter each elemnent in the array:" << endl;
+    for (int i = 0; i < n; i++) {    
         cin >> A[i];
-
     }
+    //Sort all elements in the array
     sort(A, A + n);
     long min = A[n - 1];
+    //Find Min Element according to the equation
     for (int i = 0; i <= n - k; i++)
     {
         if (A[k + i - 1] - A[i] < min)
@@ -25,6 +26,7 @@ int main() {
     }
     cout << min << endl;
     /*
+    * Sample Input/Output:
     * Input: 
     *   7
         3
@@ -37,6 +39,9 @@ int main() {
         30
     * Output:
         20
+
+    * Time Complexity: O(N(Log(N))
+    * Space Complexity: O(N)
     */
     return 0;
 }

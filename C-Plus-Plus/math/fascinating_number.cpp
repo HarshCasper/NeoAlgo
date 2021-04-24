451e63abaa8a2d1901d8e83b16829bdc5df619e8
Himanshi2997 <67272318+Himanshi2997@users.noreply.github.com>
2021-04-25 04:31:59 +0530

    created fascinating_number file

---
+++ C-Plus-Plus/math/fascinating_number.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,c=0;
+    string s,s1,s2;
+    s1=n*2;
+    s2=n*3;
+    s=s+s1+s2;
+    for(i=0;s[i]1=NULL;i++)
+    {
+        c=0;
+        if()
+
+    }
+
+}

9ce397087121a1f9fcca654ea490fbe219a70af8
maanpatel2492 <55906210+maanpatel2492@users.noreply.github.com>
2021-04-25 11:06:50 +0530

    Update bits_tobe_flipped.cpp

--- C-Plus-Plus/other/bits_tobe_flipped.cpp
+++ C-Plus-Plus/other/bits_tobe_flipped.cpp
@@ -19,17 +19,14 @@ int main()
 }
 
 /*
+Test Case 1:
 Input : a = 5, b = 3
 Output : 2
-Binary representation of a : 101
-Binary representation of b : 011
-We will flip 2 bits of a to make it b.
 
+Test Case 2:
 Input : a = 7, b = 10
 Output : 3
-Binary representation of a : 00000111
-Binary representation of b : 00001010
-We will flip 3 bits of a to make it b.
+
 */
 
 /*

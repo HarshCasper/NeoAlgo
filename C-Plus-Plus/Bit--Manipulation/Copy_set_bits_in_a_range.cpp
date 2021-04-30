6397ac5fb7536f8b5e582ae31d3550e1b078d157
scortier <onlytoaditya@gmail.com>
2021-04-30 22:48:08 +0530

    Changes in copy set bits in a range code

--- C-Plus-Plus/Bit--Manipulation/Copy_set_bits_in_a_range.cpp
+++ C-Plus-Plus/Bit--Manipulation/Copy_set_bits_in_a_range.cpp
@@ -1,38 +1,4 @@
-/*
-<<<<<<<<<<<---APPROACH-->>>>>>>>>>>>>>
-Main concern is to make mask to copy same number between l to r.
-*/
-#include<bits/stdc++.h>
-using namespace std;
-
-int copy_bits(int x, int y, int l, int r)
-{
-	int gap = r - l + 1;
-	/*
-	let gap=4; 00000001
-	1<<gap == 00010000 // shift 1 by 4 times
-	mask1=(1<<gap)-1 == 00001111	most imp
-
-	YOU CAN ALSO PERFORM ALL OPERATION WITH ONE VARIABLE AS MASK
-	*/
-	int mask1 = (1 << gap) - 1; //most imp
-	int m2 = (mask1 << (l - 1));
-	int mask = y & m2;
-	x = x | mask;
-	return x;
-}
-
-int32_t main()
-{   
-	int x = 10, y = 13, l = 2, r = 3;
-    cin>>x>>y>>l>>r;
-	/*
-	 987654321	//right to left l and r are assigned (it can also start from 0 but make sure while writing ans.)
-	y=101010101
-	*/
-	cout <<" Modified x is : "<< copy_bits(x, y, l, r);
-	return 0;
-}
+//Copy set bits in a range
 
 /*
 Given two numbers x and y, and a range [l, r]
@@ -43,3 +9,22 @@ Sample Input : x = 8, y = 7, l = 1, r = 2
 sample Output : x = 11
 
 */
+#include<bits/stdc++.h>
+using namespace std;
+int copy_bits(int x, int y, int l, int r)
+{
+	int gap = r - l + 1;
+	int mask1 = (1 << gap) - 1; //most imp
+	int m2 = (mask1 << (l - 1));
+	int mask = y & m2;
+	x = x | mask;
+	return x;
+}
+int32_t main()
+{   
+	int x ,y,l,r;
+    cin>>x>>y>>l>>r;
+	cout <<" Modified x is : "<< copy_bits(x, y, l, r);
+	return 0;
+}
+

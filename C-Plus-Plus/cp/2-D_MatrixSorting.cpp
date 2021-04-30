06d5b03579881665a8978d14a9490b2a436f5655
Ansh mishra <anshmishra9415@gmail.com>
2021-04-30 12:57:22 +0530

    refactor

--- C-Plus-Plus/cp/2-D_MatrixSorting.cpp
+++ C-Plus-Plus/cp/2-D_MatrixSorting.cpp
@@ -1,6 +1,5 @@
 
 /**
- * @author GrayFlask (Gaurav Kumar)
  * Given a n x n matrix. The problem is to sort the given matrix in strict order. 
  * Here strict order means that matrix is sorted in a way such that all elements
  * in a row are sorted in increasing order and for row ‘i’,
@@ -13,12 +12,15 @@
  *
  * Example:
  * Input:
+ * Enter the size of an array : 
  * 3
+ * Enter the elements in an array : 
  * 5 4 7
  * 1 3 8
  * 2 9 6
  *
  * Output:
+ * Sorted 2-D Matrix is:
  * 1 2 3
  * 4 5 6
  * 7 8 9
@@ -28,7 +30,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main() 
+{
     int n;
     cout << "Enter the size of an array : " <<endl;
     cin >> n;
@@ -39,22 +42,32 @@ int main() {
             cin >> arr[i][j];
         }
     }
-    int oneD_arr[n*n + 2]; // Array to store all the elements in 1_D for sorting
+    // Array to store all the elements in 1_D for sorting
+    int oneD_arr[n*n + 2];
     int pos = 0;
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             oneD_arr[pos++] = arr[i][j];
         }
     }
-    sort(oneD_arr, oneD_arr+pos); // Using inbuilt sort
+    // Using inbuilt sort
+    sort(oneD_arr, oneD_arr+pos);
     pos = 0;
     cout<<"\n Sorted 2-D Matrix is:"<<"\n";
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            arr[i][j] = oneD_arr[pos++]; // changing value of intial array with sorted values
-            cout<<arr[i][j]<<" "; // displays output
+            // changing value of intial array with sorted values
+            arr[i][j] = oneD_arr[pos++];
+            // displays output 
+            cout<<arr[i][j]<<" "; 
         }
         cout<<"\n";
     }
     return (0);
 }
+
+/*
+Time complexity : O(n)
+Space complexity : O(n)
+*/
+

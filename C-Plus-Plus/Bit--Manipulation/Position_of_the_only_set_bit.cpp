0b0a6f3403b75a725dc3f9cf730b907346d23f7d
RaveenaBhasin <raveenabhasin15@gmail.com>
2021-05-05 16:25:55 +0530

    Changes done

--- C-Plus-Plus/Bit--Manipulation/Position_of_the_only_set_bit.cpp
+++ C-Plus-Plus/Bit--Manipulation/Position_of_the_only_set_bit.cpp
@@ -1,19 +1,4 @@
 //Find the position of the only set bit
-//Time Complexity : O(log(N))
-//Space Complexity : O(1)
-
-/*Sample Input
-Example 1:
-4
-Example 2:
-15
-
-Sample Output
-Example 1:
-3      //4 is represented as "100" in Binary. As we see there's only one set bit and it's in Position 3 
-Example 2:
--1     //15 is represented as "1111" in Binary. As we see there's four set bits and thus the Output -1
-*/
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -41,3 +26,19 @@ int main(){
     cout<<findPosition(n)<<endl;
     return 0;
 }
+
+//Time Complexity : O(log(N))
+//Space Complexity : O(1)
+
+/*Sample Input
+Example 1:
+4
+Example 2:
+15
+
+Sample Output
+Example 1:
+3      //4 is represented as "100" in Binary. As we see there's only one set bit and it's in Position 3 
+Example 2:
+-1     //15 is represented as "1111" in Binary. As we see there's four set bits and thus the Output -1
+*/

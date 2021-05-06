dfa932e3589f4ec932704f33c10d1d773551ab01
Gayatri Munde <gayatri.munde19@vit.edu>
2021-05-06 14:12:06 +0530

    Changed the code as requested

--- Python/math/Next_Palindrome.py
+++ Python/math/Next_Palindrome.py
@@ -1,25 +1,7 @@
 '''
-Python Program to get the nth palindrome of a given number.
+Python Program to get the next nth palindrome of a given number.
 An integer is a palindrome if the reverse of that number is
 equal to the original number.
-
-Example of Palindromes are 11,44,101,121
-Example of nth palindrome: 2nd palindrome after 11 is 33
-
-Sample Case:
-Example 1:
-Enter a number: 12021
-Enter nth position: 10
-13031
-
-Example 2:
-Enter a number: 141
-Enter nth position: 5
-191
-
-Time Complexity: O(10^k)
-Space Complexity: O(1)
-where k = no of digits in given number
 '''
 
 
@@ -43,3 +25,19 @@ if __name__ == '__main__':
     print(getNthPalindrome(n, num))
 
 
+'''
+Sample Case:
+Example 1:
+Enter a number: 12021
+Enter nth position: 10
+13031
+
+Example 2:
+Enter a number: 141
+Enter nth position: 5
+191
+
+Time Complexity: O(10^k)
+Space Complexity: O(1)
+where k = no of digits in given number
+'''

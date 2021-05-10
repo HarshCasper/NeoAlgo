cfaf3ca9926eb7be8543f889e4f1499e47aa9cea
Nirpendra Chaudhary <nirpendra09@gmail.com>
2021-05-10 06:41:40 +0000

    Longest Repeating Subsequence

---
+++ Java/Algorithms/Longest_Repeating_Subsequence.java
@@ -0,0 +1,23 @@
+class Longest_Repeating_Subsequence {
+    static int longestRepeatingSubSeq(String str) {
+        int n = str.length();
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str.charAt(i - 1) == str.charAt(j - 1) && i != j)
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[n][n];
+    }
+
+    public static void main(String[] args) {
+        String str = "aabb";
+        System.out.println("The length is: " + longestRepeatingSubSeq(str));
+    }
+
+}

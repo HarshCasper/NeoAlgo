5355854802bf1b2d768373793c2a12d33b8bf53f
aparna <aparnasakshi912@gmail.com>
2021-05-12 00:32:36 +0530

    Enhanced code readability

--- Python/math/fib_logn.py
+++ Python/math/fib_logn.py
@@ -2,7 +2,7 @@
 
 # Matrix multiplication of two 2x2 matrices
 def matmult(Mat1, Mat2):
-    Mat3=[[0 for i in range(2)] for j in range(2)]
+    Mat3 = [[0 for i in range(2)] for j in range(2)]
     Mat3[0][0] = Mat1[0][0] * Mat2[0][0] + Mat1[0][1] * Mat2[1][0]
     Mat3[0][1] = Mat1[0][0] * Mat2[0][1] + Mat1[0][1] * Mat2[1][1]
     Mat3[1][0] = Mat1[1][0] * Mat2[0][0] + Mat1[1][1] * Mat2[1][0]
@@ -16,13 +16,14 @@ def fib(k):
          [1, 0]]
     if k <= 1:
         return A
+    
+    if k % 2 == 1:
+        return matmult(A, fib(k-1))
     else:
-        if k % 2 == 1:
-            return matmult(A, fib(k-1))
-        else:
-            fib_half = fib(k//2)
-            return matmult(fib_half, fib_half)
+        fib_half = fib(k//2)
+        return matmult(fib_half, fib_half)
 
 
-for k in range(10):
-    print(fib(k)[0][0])
+for ii in range(10):
+    print(fib(ii)[0][0])
+    

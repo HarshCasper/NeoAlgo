b2f3060715792ddbd893687b738ae1af4bf2582f
RaveenaBhasin <raveenabhasin15@gmail.com>
2021-05-13 10:35:51 +0530

    Update Position_of_the_only_set_bit.cpp

--- C-Plus-Plus/Bit--Manipulation/Position_of_the_only_set_bit.cpp
+++ C-Plus-Plus/Bit--Manipulation/Position_of_the_only_set_bit.cpp
@@ -1,8 +1,9 @@
-//Find the position of the only set bit
+/*Find the position of the only set bit
+The main principle of this logic is one by one right shift the set bit of given number n until n becomes 0.
+Count how many times we shifted to make n zero. So final count is position of set bit.*/
 
 #include<bits/stdc++.h>
 using namespace std;
-
 int findPosition(int n){
     int count=0;
     int position=0;
@@ -26,16 +27,14 @@ int main(){
     cout<<findPosition(n)<<endl;
     return 0;
 }
+/*Time Complexity : O(log(N))
+Space Complexity : O(1)
 
-//Time Complexity : O(log(N))
-//Space Complexity : O(1)
-
-/*Sample Input
+Sample Input
 Example 1:
 4
 Example 2:
 15
-
 Sample Output
 Example 1:
 3      //4 is represented as "100" in Binary. As we see there's only one set bit and it's in Position 3 

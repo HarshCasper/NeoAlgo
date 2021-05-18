2e9ed1954fd87c39c1921d53ecadfb248b7650c6
soumik2012 <66599363+soumik2012@users.noreply.github.com>
2021-05-18 11:36:20 +0530

    add comment in top of program

--- Python/backtracking/Maze-Generation-Visualiser.py
+++ Python/backtracking/Maze-Generation-Visualiser.py
@@ -1,9 +1,17 @@
+'''
+Maze Generation Visualizer using Backtracking Algorithm
+
+Python scripts for generating random solvable mazes using the depth-first search and recursive backtracking algorithms.
+The code also implements a recursive backtracking pathfinding algorithm for solving the generated mazes. 
+
+'''
+
 import pygame
 import random
 pygame.init()
 
 
-"""Initialise screen and define Cell dimensions"""
+#Initialise screen and define Cell dimensions
 width = 15
 height = 15
 screen = pygame.display.set_mode((735,735))
@@ -11,7 +19,7 @@ pygame.display.set_caption('Maze')
 
 
 class Cell():
-    """Cell class that defines each walkable Cell on the grid"""
+    #Cell class that defines each walkable Cell on the grid
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
@@ -20,7 +28,7 @@ class Cell():
 
 
     def getChildren(self, grid: list) -> list:
-        """Check if the Cell has any surrounding unvisited Cells that are walkable"""
+        #Check if the Cell has any surrounding unvisited Cells that are walkable
         a = [(1, 0), (-1,0), (0, 1), (0, -1)] # Surrounding squares
         children = []
 
@@ -38,7 +46,7 @@ class Cell():
 
 
     def show(self, width: int, height: int, c: int, c2: int):
-        """Draw a Cells existing walls"""
+        #Draw a Cells existing walls
         x = self.x
         y = self.y
 
@@ -53,14 +61,14 @@ class Cell():
     
 
     def mark(self, width: int, height: int):
-        """Mark the current cell"""
+        #Mark the current cell
         x = self.x*width
         y = self.y*height
         pygame.draw.rect(screen, (134, 46, 222), (x*2+width,y*2+height, width, height))
 
 
 def removeWalls(current: Cell, choice: Cell):
-    """Removeing the wall between two Cells"""
+    #Removeing the wall between two Cells
     if choice.x > current.x:     
         current.walls[1] = False
         choice.walls[0] = False
@@ -76,19 +84,19 @@ def removeWalls(current: Cell, choice: Cell):
 
 
 def cornerFill():
-    """Fill in the 'corners' of the grid (due to the walls being square)"""
+    #Fill in the 'corners' of the grid (due to the walls being square)
     for x in range(len(grid)+1):
         for y in range(len(grid)+1):
             pygame.draw.rect(screen, (20,20,20), (x*width*2, y*height*2, width, height))
 
 
-"""Define the grid, set the current Cell and initialise the stack"""
+#Define the grid, set the current Cell and initialise the stack#
 grid = [[Cell(x, y) for x in range(24)] for y in range(24)]
 current = grid[0][0]
 stack = []
 
 screen.fill((210, 210 ,210))
-"""Main loop"""
+#Main loop#
 exit = False
 while not exit:
     for x in range(len(grid)):
@@ -100,8 +108,8 @@ while not exit:
     current.visited = True
     current.mark(width, height) # Highlight the current Cell
 
-    """If the current has neighours then choose a random one, mark it as visited, """
-    """remove the walls between it and the current and set it as the new current"""
+    #If the current has neighours then choose a random one, mark it as visited, 
+    #remove the walls between it and the current and set it as the new current
     children = current.getChildren(grid) 
     if children:
         choice = random.choice(children)

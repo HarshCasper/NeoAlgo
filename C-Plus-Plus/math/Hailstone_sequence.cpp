482367d718f8603794be14919d18490034958b52
Drishika Chauhan <76254057+drishika2002@users.noreply.github.com>
2021-05-30 01:54:25 +0530

    Update Hailstone_sequence.cpp

--- C-Plus-Plus/math/Hailstone_sequence.cpp
+++ C-Plus-Plus/math/Hailstone_sequence.cpp
@@ -1,5 +1,12 @@
 // CPP program to find Hailstone Sequence of a given number N up to 1
 
+/* 
+The Hailstone sequence of numbers can be generated from a starting positive integer, N by:
+ If N = 1, then the sequence ends.
+ If N is even, then the next N of the sequence = N / 2
+ If N is odd, then the next N of the sequence = (3 * N) + 1 
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -46,3 +53,17 @@ int main()
     cout <<"\n\nNumber of Steps involved: "<< ans << endl;
     return 0;
 }
+
+/*
+OUTPUT 1:
+Enter a number: 7
+Hailstone Sequence upto 1:
+7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+Number of Steps involved: 17
+    
+OUTPUT 2:
+Enter a number: 6
+Hailstone Sequence upto 1:
+6 3 10 5 16 8 4 2 1
+Number of Steps involved: 9
+*/

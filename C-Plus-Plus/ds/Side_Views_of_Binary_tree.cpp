4dc802b3676a4c30bd2667d69de741182ca70752
Smriti <=>
2021-05-30 15:25:39 +0530

    added the time and space complexity and made the other changes

--- C-Plus-Plus/ds/Side_Views_of_Binary_tree.cpp
+++ C-Plus-Plus/ds/Side_Views_of_Binary_tree.cpp
@@ -1,21 +1,5 @@
 /* side views of any binary tree
 we can view the tree from two sides , i.e left and right.
- for example given a binary tree as considered in the solution
-       
-	   
-	     1
-     /   \
-    2    3
-   / \  / \
-  4  5 6  7
-   \     /
-   8    9
-        \1
-        10
-        
-        here the root node is 1
-        the right side view of the tree would print 1 3 7 9 10
-        the left side view of the tree would print  1 2 4 8 10
 */
 // including the header files
 #include <bits/stdc++.h>
@@ -51,15 +35,15 @@ we can view the tree from two sides , i.e left and right.
    {
    return;
     }
-inorder(root->left);
-cout<<root->val<<endl;
-inorder(root->right);
+   inorder(root->left);
+   cout<<root->val<<endl;
+   inorder(root->right);
    }
 
  /* we make a fuction "RightView" to print the right side of the tree.
     to print the right view we need to print all the rightmost elements in the level order traversal.
     therefore we approach the problem by defining a queue to store the elements for our each level and checking to print the rightmost node of the level.
-   */
+ */
 
  void RightView(Node* root)
  {
@@ -99,10 +83,10 @@ inorder(root->right);
 	 }
  }
 
-  /* we make a fuction "LeftView" to print the left side of the tree.
+ /* we make a fuction "LeftView" to print the left side of the tree.
     to print the left view we need to print all the leftmost elements in the level order traversal.
     therefore we approach the problem by defining a queue to store the elements for our each level and checking to print the leftmost node of the level.
-   */
+ */
 void LeftView(Node* root)
 {
 	if(root==NULL)
@@ -157,7 +141,33 @@ void LeftView(Node* root)
   cout<<"Left Side View of the Tree:"<<endl;
   LeftView(root);
   cout<<endl;
+  cout<<"\n";
    return 0;
 }
+
+/*
+ Time Complexity:O(n)
+ 
+ Space Compleity:O(1)
+ 
+*/
+/*for example given a binary tree as considered in the solution
+       
+	   
+	     1
+     /   \
+    2    3
+   / \  / \
+  4  5 6  7
+   \     /
+   8    9
+        \1
+        10
+        
+        here the root node is 1
+        Output:
+        the right side view of the tree would print : 1 3 7 9 10
+        
+        the left side view of the tree would print : 1 2 4 8 10*/
    
    

0ba0bc24d234c9888803306c56dd08699371c6f0
ishwari20 <56400099+ishwari20@users.noreply.github.com>
2021-05-30 23:58:33 +0530

    Update BraillesDilemma.cpp

--- C-Plus-Plus/dp/BraillesDilemma.cpp
+++ C-Plus-Plus/dp/BraillesDilemma.cpp
@@ -8,7 +8,7 @@ Find the minimum number of touches Harry has to make so that he finds that all s
 #include<bits/stdc++.h>
 #include <vector>
 using namespace std;
-int count_touches(int mask, vector<string>& arr, int n, int N, int pos = 0)
+int count_touches(int mask, vector<string>& arr, int n, int N, vector<vector<int>>& dp, int pos = 0)
 {
 	// When mask contains only 1 bit it means every string is recongnised.
 	if (mask && (mask & (mask - 1)) == 0)
@@ -20,6 +20,12 @@ int count_touches(int mask, vector<string>& arr, int n, int N, int pos = 0)
 	if (pos == n || mask == 0)
 	{
 		return 100000000;
+	}
+
+	// Adding memoization condition .
+	if (dp[mask][pos] != -1)
+	{
+		return dp[mask][pos];
 	}
 
 	int mask1 = 0;
@@ -44,18 +50,21 @@ int count_touches(int mask, vector<string>& arr, int n, int N, int pos = 0)
 		}
 	}
 
-	int a = cnt + count_touches(mask1, arr, n, N, pos + 1) + count_touches(mask2, arr, n, N, pos + 1);
-	int b = count_touches(mask, arr, n, N, pos + 1);
+	int a = cnt + count_touches(mask1, arr, n, N, dp, pos + 1) + count_touches(mask2, arr, n, N, dp, pos + 1);
+	int b = count_touches(mask, arr, n, N, dp, pos + 1);
 
-	return min(a, b);
+	return dp[mask][pos] = min(a, b);
 
 }
 int minimumTouchesRequired(int N, vector<string> arr) {
 
 	int n = arr[0].size();
 
-	return count_touches((1 << N) - 1, arr, n, N);
+	vector<vector<int>>dp((1 << N), vector<int>(n, -1));
+
+	return count_touches((1 << N) - 1, arr, n, N, dp);
 }
+
 
 int main(){
     int N;
@@ -84,9 +93,9 @@ int main(){
     Sample Output:
     The minimum number of touches required are: 5
 
-    Time Complexity:O((2^n) *N)
-    Space Complexity:O(1)
-    
+    Time Complexity:O((2^N) *n)
+    Space Complexity:O((2^N) *n)
+
     N is the number of strings and n is the length of the string.
 
 */

f5234411e6b9aa0113b1bc1de50ad7877562b218
Shreya0229 <62853109+Shreya0229@users.noreply.github.com>
2021-05-31 18:25:22 +0530

    Update BFS_RottenOranges.java

--- Java/cp/BFS_RottenOranges.java
+++ Java/cp/BFS_RottenOranges.java
@@ -1,19 +1,13 @@
 /*
-  Problem:
-    We have to determine what is the minimum time required to rot all oranges.
-    We can go in four directions i.e. up, down, left and right.
-    The problem can also be found on geeksforgeeks fro reference.
+  Rotten Oranges:
+    We have to determine what is the minimum time required to rot all oranges. So basically we will use breadth first search using queue.
+    We can go in four directions i.e. up, down, left and right. We traverse each node and if it is fresh or no oranges then we don't go to the
+    breadth first search else we go and traverse through the matrix. 
+    
+    Hence we get the time by updating the level variable each time we put
+    something in our queue that is the next iteration and hence we follow the same procedure starting from each rotten orange.
+    The problem can also be found on geeksforgeeks for reference.
 
-    Input: 
-        3 3 
-        0 1 2
-        0 1 2  
-        2 1 1
-    Output: 1
-
-  Explanation:
-    We will use bfs using a queue to find the time to rot all the oranges.
-    Oranges at positions (0,2), (1,2), (2,0) will rot oranges at (0,1), (1,1), (2,2) and (2,1) in unit time.
 */
 
 import java.util.*;
@@ -24,10 +18,12 @@ class BFS_RottenOranges
     public static void main(String[] args) throws IOException
     {
         Scanner sc= new Scanner(System.in);
-            
+            System.out.println("Enter the dimention of the matrix: ");
             int n = sc.nextInt();
             int m = sc.nextInt();
             int[][] grid = new int[n][m];
+      
+            System.out.println("Enter values of the matrix which consists of 1 for fresh, 0 for no oranges, 2 for rotten oranges: ");
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < m; j++){
                     grid[i][j] = sc.nextInt();
@@ -35,6 +31,7 @@ class BFS_RottenOranges
             }
             BFS_RottenOranges1 obj = new BFS_RottenOranges1();
             int ans = obj.orangesRotting(grid);
+            System.out.println("The time taken to rot all oranges is: ");
             System.out.println(ans);
 
         sc.close();
@@ -65,7 +62,8 @@ class BFS_RottenOranges1
                     queue.add(new Node(i, j));
             }
         }
-        
+        // Adding the node in the queue only when it is a rotten orange that is its value is 2 in the matrix.
+      
         int level=0;
         while(queue.isEmpty()==false){
             int size= queue.size();
@@ -100,6 +98,9 @@ class BFS_RottenOranges1
             if(flag==1)
             level++;
         }
+      
+      // Checking if all the oranges are rotten or not through traversing the matrix.
+      
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==1)
@@ -118,6 +119,21 @@ class BFS_RottenOranges1
 }
 
 /* 
+
+    Input: 
+        3 3 
+        0 1 2
+        0 1 2  
+        2 1 1
+    Output: 1
+
+    Input/Output Explanation:
+    We will use bfs using a queue to find the time to rot all the oranges.
+    Oranges at positions (0,2), (1,2), (2,0) will rot oranges at (0,1), (1,1), (2,2) and (2,1) in unit time and hence within
+    1 sec all the oranges present will be rotten.
+    
+    
+    
     Time complexity- O(n*m)
     As we are traversing the array only once.
     Space complexity- O(n*m)

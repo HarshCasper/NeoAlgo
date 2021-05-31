32c7adc9c12f296fd5a94d5e28dcf36f032579ff
Pritom Dutta <duttapritom777@gmail.com>
2021-05-31 19:48:29 +0530

    Add function

--- C-Plus-Plus/ds/Find_first_and_last_positions_of_an_element_in_a_sorted_array.cpp
+++ C-Plus-Plus/ds/Find_first_and_last_positions_of_an_element_in_a_sorted_array.cpp
@@ -2,31 +2,65 @@
 
 ALGORITHM: 
 * Take n = array size and element by user input
-* Take first = -1 and last = -1 
+* Take first = store first positions of an element and last = store last positions of an element
 * For finding first positions of an element
-   1.for i=0 to i<n
+   * First_positions:
+      int i,first=-1
+      for i=0 to i<n
         if a[i]==element
            first = i
            break
+      return first
    ** here first stored 1st index of an element
 * For finding last positions of an element
-   1.for i=n-1 to i>=0
+   * Last_positions:
+      int i,last=-1
+      for i=n-1 to i>=0
         if a[i]==element
            last = i
            break
+      return last
    ** here last stored last index of an element
 * print first and last.
 
 */
 
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+//Finding first positions of an element
+int First_positions(int a[],int n,int element)
 {
-    int n,element,i,first=-1,last=-1;
+    int i,first=-1;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==element)
+        {
+           first = i;
+           break;
+        }
+    }
+    return first;
+}
+
+//Finding last positions of an element
+int Last_positions(int a[],int n,int element)
+{
+    int i,last=-1;
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]==element)
+        {
+           last = i;
+           break;
+        }
+    }
+    return last;
+}
+
+ int main()
+{
+    int n,element,i,first,last;
     cout<<"array size : ";
     cin>>n;
     cout<<"element : ";
@@ -37,28 +71,15 @@ int main()
         cin>>a[i]; 
     }
     //Finding first positions of an element
-    for(i=0;i<n;i++)
-    {
-        if(a[i]==element)
-        {
-           first = i;
-           break;
-        }
-    }
+    first = First_positions(a,n,element);
     //Finding last positions of an element
-    for(i=n-1;i>=0;i--)
-    {
-        if(a[i]==element)
-        {
-           last = i;
-           break;
-        }
-    }
+    last = Last_positions(a,n,element);
 
-    cout<<"first positions of an element is : "<<first<<endl;
-    cout<<"last positions of an element is : "<<last<<endl;
+    cout<<"first position of an element is : "<<first<<endl;
+    cout<<"last position of an element is : "<<last<<endl;
 
 }
+
 
 /*
 
@@ -68,12 +89,10 @@ element : 4
 1 2 4 4 4 4 4 4 50 90
 
 OUTPUT: 
-first positions of an element is : 2
-last positions of an element is : 7
+first position of an element is : 2
+last position of an element is : 7
 
 Time Complexity: O(n)
-Space Complexity: O(1)
+Space Complexity: O(n)
+
 */
-
-
-

c89bfa66c62c345a3d33758b8c55347e5264658b
Deepti <64003317+deepti23-web@users.noreply.github.com>
2021-05-31 19:08:29 +0530

    Update BSTtoMinHeap.cpp

--- C-Plus-Plus/ds/BSTtoMinHeap.cpp
+++ C-Plus-Plus/ds/BSTtoMinHeap.cpp
@@ -1,14 +1,14 @@
-// Program to convert the given BST into a Min Heap.
-// Conditions are that all the values in the left subtree of a node should be less than all the values in the right subtree of the node.
+/* Program to convert the given BST into a Min Heap.
+ Conditions are that all the values in the left subtree of a node should be less than all the values in the right subtree of the node.*/
 #include <bits/stdc++.h>
 using namespace std;
-// Data structure to store a binary tree node
+/* Data structure to store a binary tree node*/
 struct Node
 {
     int key;
     Node *left, *right;
 };
-// Function to create a new binary tree node having a given key
+/* Function to create a new binary tree node having a given key*/
 Node* newNode(int key)
 {
     Node* node = new Node;
@@ -16,27 +16,27 @@ Node* newNode(int key)
     node->left = node->right = nullptr;
     return node;
 }
-// Recursive function to insert a key into a BST
+/* Recursive function to insert a key into a BST*/
 Node* insert(Node* root, int key)
 {
-    // if the root is null, create a new node and return it
+    /* if the root is null, create a new node and return it*/
     if (root == nullptr) {
         return newNode(key);
     }
-    // if the given key is less than the root node, recur for the left subtree
+    /* if the given key is less than the root node, recur for the left subtree*/
     if (key < root->key) {
         root->left = insert(root->left, key);
     }
-    // if the given key is more than the root node, recur for the right subtree
+    /* if the given key is more than the root node, recur for the right subtree*/
     else {
         root->right = insert(root->right, key);
     }
     return root;
 }
-// Helper function to perform level order traversal on a binary tree
+/* Helper function to perform level order traversal on a binary tree*/
 void printLevelOrderTraversal(Node* root)
 {
-    // base case: empty tree
+    /* base case: empty tree*/
     if (root == nullptr) {
         return;
     }
@@ -60,8 +60,8 @@ void printLevelOrderTraversal(Node* root)
         cout << endl;
     }
 } 
-// Function to perform inorder traversal on a given binary tree and
-// enqueue all nodes (in encountered order)
+/* Function to perform inorder traversal on a given binary tree and
+   enqueue all nodes (in encountered order)*/
 void inorder(Node* root, queue<int> &keys)
 {
     if (root == NULL) {
@@ -71,31 +71,31 @@ void inorder(Node* root, queue<int> &keys)
     keys.push(root->key);
     inorder(root->right, keys);
 }
-// Function to perform preorder traversal on a given binary tree.
-// Assign each encountered node with the next key from the queue
+/* Function to perform preorder traversal on a given binary tree.
+   Assign each encountered node with the next key from the queue*/
 void preorder(Node* root, queue<int> &keys)
 {
-    // base case: empty tree
+    /* base case: empty tree*/
     if (root == nullptr) {
         return;
     }
-    // replace the root's key value with the next key from the queue
+    /* replace the root's key value with the next key from the queue*/
     root->key = keys.front();
     keys.pop();
-    // process left subtree
+    /* process left subtree*/
     preorder(root->left, keys);
-    // process right subtree
+    /* process right subtree*/
     preorder(root->right, keys);
 }
-// Function to convert a BST into a min-heap
+/* Function to convert a BST into a min-heap*/
 void convert(Node* root)
 {
-    // maintain a queue to store inorder traversal on the tree
+    /* maintain a queue to store inorder traversal on the tree*/
     queue<int> keys;
-    // fill the queue in an inorder fashion
+    /* fill the queue in an inorder fashion*/
     inorder(root, keys);
-    // traverse tree in preorder fashion, and for each encountered node,
-    // dequeue a key and assign it to the node
+    /* traverse tree in preorder fashion, and for each encountered node,
+       dequeue a key and assign it to the node*/
     preorder(root, keys);
 }
 int main()
@@ -119,7 +119,18 @@ int main()
     printLevelOrderTraversal(root);
     return 0;
 }
-// Sample Input:
-// 7
-// 4 2 6 1 3 5 7
-// The time complexity of the above program is O(n) and the space complexity is also O(n).
+/*
+
+Sample Input/Output:
+
+Input:7
+      4 2 6 1 3 5 7
+      
+Output:1 
+       2 5 
+       3 4 6 7
+       
+Time complexity:O(n)
+Space complexity:O(n)
+
+*/

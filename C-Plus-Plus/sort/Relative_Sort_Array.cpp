41280fe191cabcfc7d1f8f4ee70106a4ce63fda1
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2021-05-31 18:58:55 +0530

    Update Relative_Sort_Array.cpp

--- C-Plus-Plus/sort/Relative_Sort_Array.cpp
+++ C-Plus-Plus/sort/Relative_Sort_Array.cpp
@@ -1,14 +1,12 @@
-//Relative Sort Array (Sorting an array according to the other) 
+/*Relative Sort Array (Sorting an array according to the other) 
 
-// Given two integer arrays A1[ ] and A2[ ] of size N and M respectively. Sort the first array A1[ ] such that all the relative 
-// positions of the elements in the first array are the same as the elements in the second array A2[ ].
-
-// Note: If elements are repeated in the second array, consider their first occurance only.
-
+Given two integer arrays A1[ ] and A2[ ] of size N and M respectively. Sort the first array A1[ ] such that all the relative 
+positions of the elements in the first array are the same as the elements in the second array A2[ ].
+Note: If elements are repeated in the second array, consider their first occurance only.
+*/
 
 
 //Initial template for C++
-
 #include <bits/stdc++.h> 
 using namespace std;
 
@@ -18,7 +16,6 @@ class Solution{
     vector<int> sortA1ByA2(vector<int> A1, int N, vector<int> A2, int M) 
     {
         map<int,int>mp;
-        
         //storing all the elements of first array in map.
         for(int i=0;i<N;i++)
         {
@@ -34,8 +31,7 @@ class Solution{
                 A1[j++]=A2[i];
                 mp[A2[i]]--;
             }
-        }
-        
+        } 
         //iterating over the map which helps in storing
         //elements in increasing order.
         for(auto it:mp)
@@ -55,57 +51,46 @@ class Solution{
         return A1;
     } 
 };
-
-
-int main(int argc, char *argv[]) 
+int main() 
 { 
-	
-	int t;
-	cin >> t;
-	while(t--){
 	    int n, m;
-	    cin >> n >> m;                 // 11 4
+	    cout<<"Enter size of both array: \n";
+	    cin >> n >> m;                
 	    vector<int> a1(n);
 	    vector<int> a2(m);
+	    cout<<"Enter elements of array A1: \n";
 	    for(int i = 0;i<n;i++){
-	        cin >> a1[i];             // 2 1 2 5 7 1 9 3 6 8 8
+	        cin >> a1[i];            
 	    }
+	    cout<<"Enter elements of array A2: \n";
 	    for(int i = 0;i<m;i++){
-	        cin >> a2[i];            // 2 1 8 3
+	        cin >> a2[i];            
 	    }
 	    Solution ob;
 	    a1 = ob.sortA1ByA2(a1, n, a2, m); 
+	    cout<<"Relative sorted array: \n";
 	    for (int i = 0; i < n; i++) 
 		    cout<<a1[i]<<" ";
 		
 	    cout << endl;
-	}
-	return 0; 
-} 
+}
+
+/*
+Sample Input/Output:
+
+Input: Enter size of both array:
+       11 4
+       Enter elements of array A1:
+       2 1 2 5 7 1 9 3 6 8 8
+       Enter elements of array A2:
+       2 1 8 3
+
+Output: Relative sorted array:
+	2 2 1 1 8 8 3 5 6 7 9 
+
+Time complexity:  O(m+n)
+Space complexity: O(n)    (Assuming n>m)
+
+*/
 
 
-
-// Input:
-
-// 11 4
-// 2 1 2 5 7 1 9 3 6 8 8
-// 2 1 8 3
-
-// output:
-
-// 2 2 1 1 8 8 3 5 6 7 9 
-
-// Input:
-
-// 11 4
-// 2 1 2 5 7 1 9 3 6 8 8
-// 99 22 444 56
-// Output: 
-
-// 1 1 2 2 3 5 6 7 8 8 9
-
-//Time Complexity : 
-// O(m+n)
-
-//Space Complexity:
-//O(n)    if n>m

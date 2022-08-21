c1ed65e0862ea87e831e45ce00e4686c367ad8df
Hieu Vu <star.hieu.99@gmail.com>
2022-08-21 12:13:14 +0700

    Add inorder, preorder, postorder traversal

--- Python/ds/Tree_Traversals_In_Pre_Post_Order.py
+++ Python/ds/Tree_Traversals_In_Pre_Post_Order.py
@@ -35,6 +35,50 @@ class Tree:
             path += str(node.val) + ' '
 
         return path
+    
+    def inorder_traversal_iterative(self, node, path):
+        if node:
+            stack = []
+            curr = node
+            while (curr or len(stack) > 0):
+                while curr != None:
+                  stack.append(curr)
+                  curr = curr.left
+                curr = stack.pop()
+                path += str(curr.val) + ' '
+                curr = curr.right
+        return path
+    
+    def preorder_traversal_iterative(self, node, path):
+        if node:
+          stack = []
+          stack.append(node)
+          while len(stack) > 0:
+            curr = stack.pop()
+            path += str(curr.val) + ' '
+            if curr.right != None:
+              stack.append(curr.right)
+            if curr.left != None:
+              stack.append(curr.left)
+        return path
+    
+    def postorder_traversal_iterative(self, node, path):
+      stack = []
+      curr = node
+      while (True):
+        while (curr != None):
+          stack.append(curr)
+          stack.append(curr)
+          curr = curr.left
+        if (len(stack) == 0):
+          return path
+        curr = stack.pop()
+        if (len(stack) > 0 and stack[-1] == curr):
+          curr = curr.right
+        else:
+          path += str(curr.val) + ' '
+          curr = None
+      return path
 
 # Creating a tree of the following structure:-
 
@@ -55,16 +99,29 @@ tree.root.right.left = TreeNode(6)
 tree.root.right.right = TreeNode(7)
 
 # In-Order Traversal :- left -> root -> right
-
+print("In-Order Traversal :- left -> root -> right")
+print("Recursive:")
 print(tree.inorder_traversal(tree.root, ''))
+print("Iterative:")
+print(tree.inorder_traversal_iterative(tree.root, ''))
 # Expected Output:- 4 2 5 1 6 3 7
 
-# Pre-Order Traversal :- root -> left -> right
+print("\n")
 
+# Pre-Order Traversal :- root -> left -> right
+print("Pre-Order Traversal :- root -> left -> right")
+print("Recursive:")
 print(tree.preorder_traversal(tree.root, ''))
+print("Iterative:")
+print(tree.preorder_traversal_iterative(tree.root, ''))
 # Expected Output:- 1 2 4 5 3 6 7
 
-# Post-Order Traversal :- left -> right -> root
+print("\n")
 
-print(tree.postorder_traversal(tree.root, ''))
+# Post-Order Traversal :- left -> right -> root
+print("Post-Order Traversal :- left -> right -> root")
+print("Recursive:")
+print(tree.postorder_traversal_iterative(tree.root, ''))
+print("Iterative:")
+print(tree.postorder_traversal_iterative(tree.root, ''))
 # Expected Output:- 4 5 2 6 7 3 1

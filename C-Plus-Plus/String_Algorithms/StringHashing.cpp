88434abeee8b2cda0d0e9844850b4b557a6876b5
Prasanna Kumar <prasannakroy2000@gmail.com>
2022-10-04 21:24:38 +0530

    String Hashing implementation

---
+++ C-Plus-Plus/String_Algorithms/StringHashing.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+using namespace std;
+using namespace std::chrono;
+
+#define __AcHiLlEs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define __AnAkLuSmOs freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define int long long
+#define span(a) a.begin(), a.end()
+#define sum(n) ((int)n * (int)(n + 1)) / 2
+#define sumr(l, r) sum(r) - sum(l)
+
+int p = 256; // For worst case scenerio considering all ascii values, could be anything depending upon the requirement
+int mod = (int)1e9 + 7; // Any large prime number that can avoid overflow
+
+int computeHash(string &s) { // Function to compute hash of string
+	int hs(0);
+	for (char &i : s) 
+		hs = (((hs * p) % mod) + i) % mod; // Computing Hash
+	return hs;
+}
+
+void solve() {
+	string s;
+	getline(cin, s);
+	cout << computeHash(s) << "\n";
+}
+
+signed main() {
+    // auto start = high_resolution_clock::now();
+    #ifndef ONLINE_JUDGE 
+        __AnAkLuSmOs    
+    #endif
+
+    __AcHiLlEs
+
+    int t(1);
+    // cin >> t;
+    for (int i = 1; i <= t; /*cout << "Case " << i << ": ",*/ solve(), i++);
+    // auto stop = high_resolution_clock::now();
+    // double duration = duration_cast<microseconds>(stop - start).count();
+    // cout << fixed << setprecision(4) << duration / 1000000 << "\n";  
+    return 0;
+}

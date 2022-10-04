8581acf53f20b60d828fa8e7f7abc4d786ec2a20
Prasanna Kumar <prasannakroy2000@gmail.com>
2022-10-04 21:34:05 +0530

    Update Rabin-KarpAlgorithmofStringMatching.cpp

--- C-Plus-Plus/String_Algorithms/Rabin-KarpAlgorithmofStringMatching.cpp
+++ C-Plus-Plus/String_Algorithms/Rabin-KarpAlgorithmofStringMatching.cpp
@@ -5,28 +5,36 @@ using namespace __gnu_pbds;
 using namespace std;
 using namespace std::chrono;
 
-#define __AcHiLlEs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define __AnAkLuSmOs freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
 #define int long long
 #define span(a) a.begin(), a.end()
 #define sum(n) ((int)n * (int)(n + 1)) / 2
 #define sumr(l, r) sum(r) - sum(l)
 
+// declaring primes for the hashing
 int p1 = 27, p2 = 33;
+// modular element for hashing
 int mod = (int)1e9 + 7;
 
+// converting character to integer
 int calcVal(char a) {
-	int x = 0;
-    if (a >= 'a' and a <= 'z') x = a - 'a' + 1;
-    else if (a >= 'A' and a <= 'Z') x = a - 'A' + 27;
-    else x = a - '0' + 53;
+    int x = 0;
+    if (a >= 'a' and a <= 'z') 
+	    x = a - 'a' + 1;
+    else if (a >= 'A' and a <= 'Z') 
+	    x = a - 'A' + 27;
+    else 
+	    x = a - '0' + 53;
     return x;
 }
 
+// precompute the hash for the string
 auto computeHash(string &s) {
 	array<int, 2> hs{0, 0};
 	for (auto &i: s) {
 		int x = calcVal(i);
+		// pick the previous hash, multiply it with the prime and add the new character value
+		// standard way to implement a polynomial of form (x * (a + x * (b + x * (c + ....  + x * z) which eventually converts into the polynomial determining
+		// the hash
 		hs[0] = (((hs[0] * p1) % mod) + x) % mod;
 		hs[1] = (((hs[1] * p2) % mod) + x) % mod;
 	}
@@ -46,22 +54,19 @@ void solve() {
 	pw1[0] = 1;
 	pw2[0] = 1;
 	for (int i = 1; i < n; i++) pw1[i] = (pw1[i - 1] * p1) % mod, pw2[i] = (pw2[i - 1] * p2) % mod; 
+	// precomputing hash for the pattern
 	auto res = computeHash(pat);
 	array<int, 2> hs{0, 0};
-	auto hs2 = hs;
+	// calculating hash for the first substring equal to the length of pattern for original string
 	for (int i = 0; i < m; i++) {
 		int x = calcVal(s[i]);
 		hs[0] = (((hs[0] * p1) % mod) + x) % mod;
 		hs[1] = (((hs[1] * p2) % mod) + x) % mod;
 	}
-
-	for (int i = 1; i <= m; i++) {
-		int x = calcVal(s[i]);
-		hs2[0] = (((hs2[0] * p1) % mod) + x) % mod;
-		hs2[1] = (((hs2[1] * p2) % mod) + x) % mod;
-	}
 	vector<int> flag;
-	if (hs == res) flag.push_back(0);
+	if (hs == res) 
+		flag.push_back(0);
+	// match for every substring of length equal to pattern in a rolling window fashion (therefore rolling hash)
 	for (int i = m; i < n; i++) {
 		int x = calcVal(s[i]), y = calcVal(s[i - m]);
 		hs[0] = hs[0] - (y * pw1[m - 1]) % mod;
@@ -84,12 +89,6 @@ void solve() {
 
 signed main() {
     // auto start = high_resolution_clock::now();
-    #ifndef ONLINE_JUDGE 
-        __AnAkLuSmOs    
-    #endif
-
-    __AcHiLlEs
-
     int t(1);
     cin >> t;
     for (int i = 1; i <= t; /*cout << "Case " << i << ": ",*/ solve(), i++);
